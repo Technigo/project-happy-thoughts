38485e760c682ca1b3cc17d357a1fce05338120d
nasimmhn <nasim.mahzoun@gmail.com>
2019-12-02 17:20:34 +0100

    initial commit

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+
+
+
+
 
 export const App = () => {
+
+  const [thoughts, setThoughts] = useState([]);
+
+
+  useEffect(() => {
+    fetch("https://technigo-thoughts.herokuapp.com/", {
+      method: "POST",
+      body: JSON.stringify({ message: "my happy thought" })
+
+    })
+      .then(res => res.json())
+      .then(json => console.log(json));
+  }, []);
+
+
+
   return (
+
     <div>
-      Find me in src/app.js!
+      <h3>my happy thought</h3>
     </div>
   )
+
+
 }
