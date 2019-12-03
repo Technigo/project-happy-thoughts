b40f928f80cdc26e09bde5d1b6aaed45ce171e87
Linda <isell.linda@gmail.com>
2019-12-03 16:14:56 +0100

    first try

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+// import { Heart } from "./Heart";
+import './app.css'
+
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState([])
+  const [selectedThoughts, setSelectedThoughts] = useState([])
+
+  useEffect(() => {
+    fetch("https://technigo-thoughts.herokuapp.com/")
+      .then(res => res.json())
+      .then(json => setThoughts(json))
+  }, [])
+
   return (
-    <div>
-      Find me in src/app.js!
+    <div className="app">
+      {thoughts.map(thought => (
+        <div className="inside-cards">
+          <p key={thought.id}>{thought.message}</p>
+          <ul>
+            <li><button>❤️</button></li>
+            <li>x {thought.hearts}</li>
+            <li>{thought.createdAt}</li>
+          </ul>
+        </div>
+      ))
+      }
     </div>
+
+
+
+
+
+    // <div>
+    //   <ul>
+    //     {thoughts.map(thought => (
+    //       <div className="message-box">
+    //         <li key={thought.message}>
+    //           <button onClick={() => setSelectedThoughts(thought)}>
+    //             {thought.message}
+    //           </button>
+    //         </li>
+    //       </div>
+    //     ))}
+    //   </ul>
+    //   {selectedThoughts && <Detail name={selectedThoughts.message} hearts={selectedThoughts.hearts} />}
+    // </div>
   )
+
 }
+
---
+++ code/src/Heart.js
@@ -0,0 +1,20 @@
+// import React, { useState, useEffect } from 'react';
+
+// export const Heart = props => {
+//   const [details, setDetails] = useState()
+
+//   useEffect(() => {
+//     fetch(props.hearts)
+//       .then(res => res.json())
+//       .then(json => {
+//         setDetails(json)
+//       })
+//   }, [props])
+
+//   return (
+//     <div>
+//       <h1>{props.hearts }</h1>
+//       <h2>{props.message} </h2>
+//     </div>
+//   )
+// }
