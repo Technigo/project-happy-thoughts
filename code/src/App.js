393aa369800c16a7c2d1ab4822cb5f2bd6acaa46
AnnaTerelius <tereliusanna@hotmail.com>
2019-12-03 15:46:28 +0100

    list of thoughts

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState([])
+  const [submitted, setSubmitted] = useState(false);
+
+  useEffect(() => {
+    fetch('https://technigo-thoughts.herokuapp.com/')
+    .then (res => res.json())
+    .then (json => setThoughts(json));
+
+    
+
+      
+
+  }, []);
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <form className="background">
+      <div className="inputField">
+      <p>What's making you happy right now?</p>
+      <input /><br />
+      <button className="button" type="submit" onClick={() => setSubmitted(true)}>Send happy thought</button>
+      </div>
+      <ul>
+        {thoughts.map(thought => (
+      <li className="li" key={thought.message}>{thought.message}<br></br>x{thought.hearts}</li>
+        ))}
+       
+      </ul>
+    </form>
   )
 }
