b83a5734db21f1fd29bf9fee9480bbed07b69c3b
Petra B <petrabrinck@icloud.com>
2019-12-03 16:04:41 +0100

    done fetch and working on adding things to th form

--- code/src/App.js
+++ code/src/App.js
@@ -1,25 +1,41 @@
 import React, { useState, useEffect } from 'react';
-import ReactDOM from "react-dom";
-
+import './App.css'
 
 
 export const App = () => {
   const [thouhgts, setThoughts] = useState([])
-
+  const [submitted, setSubmitted] = useState(false)
+  const [text, setText] = useState([])
 
   useEffect(() => {
     fetch('https://technigo-thoughts.herokuapp.com')
       .then(res => res.json())
-      .then(json => console.log(json))
+      .then(json => setThoughts(json))
+    // .then(json => console.log(json))
 
-  })
+  }, [])
 
 
   return (
     <div>
-      <ul>
-        <li>Name</li>
-      </ul>
+
+      <form onSubmit={event => event.preventDefault()}>
+        <div>
+          <h4>Whats making you happy right now?</h4>
+        </div>
+        <input className="formField"
+          type="text"
+          onChange={event => setText(event.target.value)}
+          value={text}
+        />
+
+        <ul>
+          {thouhgts.map(thought => (
+            <li key={thought.message} className="messages">{thought.message}</li>
+          ))}
+
+        </ul>
+      </form>
     </div>
   )
 }
