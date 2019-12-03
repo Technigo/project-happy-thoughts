c6c0119f940c6eddafd71279add1cf2b8a5595f7
Clara Gyökeres <claragyokeres@Claras-MacBook-Air.local>
2019-12-03 17:47:33 +0100

    updated with sections

--- code/src/App.js
+++ code/src/App.js
@@ -11,25 +11,38 @@ export const App = () => {
   }, []);
 
   const [thoughts, setThoughts] = useState([]);
+  const [userText, setUserText] = useState('');
   const handleFormSubmit = (event) => {
     event.preventDefault()
+    fetch('https://technigo-thoughts.herokuapp.com/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ message: userText })
+    })
+      .then((res) => res.json())
+      .then((userText) => {
+        setThoughts((previousThoughts) => [userText, ...previousThoughts])
+      })
   }
+
 
 
   return (
     <div>
 
       <div className="thoughtBox">
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <p> What's making you happy right now?</p>
-          <input type="text" />
-          <button type="submit">Send your happy thought!</button>
+          <input type="text" onChange={event => setUserText(event.target.value)} value={userText} required />
+          <button type="submit">❤️ Send your happy thought!❤️ </button>
         </form>
       </div>
 
 
       {thoughts.map(thought => (
-        <p key={thought._id}>{thought.message} Likes: {thought.hearts} Posted: {thought.createdAt}</p>
+        <div className="postedThoughts">
+          <p key={thought._id}><h3>{thought.message}</h3> ❤️ x {thought.hearts} {thought.createdAt}</p>
+        </div>
       )
       )}
     </div>
