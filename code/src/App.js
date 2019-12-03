ec716764d03737e22aa810a0578a4fd0906c4267
Clara Gyökeres <claragyokeres@Claras-MacBook-Air.local>
2019-12-03 16:30:24 +0100

    First draft

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
 
 export const App = () => {
+  useEffect(() => {
+    fetch("https://technigo-thoughts.herokuapp.com/")
+      .then(res => res.json())
+      .then(json => {
+        setThoughts(json)
+      });
+  }, []);
+
+  const [thoughts, setThoughts] = useState([]);
+  const handleFormSubmit = (event) => {
+    event.preventDefault()
+  }
+
+
   return (
     <div>
-      Find me in src/app.js!
+
+      <div className="thoughtBox">
+        <form>
+          <p> What's making you happy right now?</p>
+          <input type="text" />
+          <button type="submit">Send your happy thought!</button>
+        </form>
+      </div>
+
+
+      {thoughts.map(thought => (
+        <p key={thought._id}>{thought.message} Likes: {thought.hearts} Posted: {thought.createdAt}</p>
+      )
+      )}
     </div>
   )
-}
+};
