03624f7cfa64394ca5ac4cd76900f3a81e6c2a66
Jenny Jacobsson <enni@oans.net>
2019-12-04 15:21:34 +0100

    first commit

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState([]);
+  const [newThought, setNewThought] = useState("");
+
+  useEffect(() => {
+    fetch("https://technigo-thoughts.herokuapp.com/")
+      .then(res => res.json())
+      .then(json => setThoughts(json));
+  }, []);
+
+  const handleFormSubmit = event => {
+    event.preventDefault();
+
+    // Send the POST request with the input from your form (instead
+    // of 'Hello world' like this example does):
+    fetch("https://technigo-thoughts.herokuapp.com/", {
+      method: "POST",
+      body: JSON.stringify({ newThought })
+    })
+      .then(res => res.json())
+      .then(newThought => {
+        // Now you have `newThought` which is the response from the
+        // API as documented at the top of this readme. You can use
+        // it to update the `thoughts` array:
+        setThoughts(previousThoughts => [newThought, previousThoughts]);
+      });
+  };
+
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+    <>
+      <form>
+        <section className="post-wrapper">
+          <div className="new-post">
+            <h2>What's making you happy right now?</h2>
+            <label>
+              <input
+                type="text"
+                onChange={event => setNewThought(event.target.value)}
+                value={newThought}
+              />
+            </label>
+            <button type="submit" onClick={event => handleFormSubmit()}>
+              <span role="img" aria-label="heart">
+                ❤️ Send Happy Thought ❤️
+              </span>
+            </button>
+          </div>
+        </section>
+      </form>
+
+      {thoughts.map(thought => (
+        <div className="card" key={thought._id}>
+          <p>{thought.message}</p>
+          {thought.createdAt}
+        </div>
+      ))}
+    </>
+  );
+};
