087b49a0693e78c1a496f0dd78ca31f846774ec9
Petra B <petrabrinck@icloud.com>
2019-12-04 12:53:21 +0100

    like btn in place in the what's your-box, css on all btns

--- code/src/App.js
+++ code/src/App.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react';
 import './App.css'
 
 
+
 export const App = () => {
   const [thouhgts, setThoughts] = useState([])
-  const [messages, setMessages] = useState()
-  const [submitted, setSubmitted] = useState(false)
+  const [messages, setMessages] = useState('')
+  // const [submitted, setSubmitted] = useState(false)
   const [text, setText] = useState([])
+  const [heart, setHeart] = useState()
+
 
   useEffect(() => {
     fetch('https://technigo-thoughts.herokuapp.com')
@@ -14,8 +17,30 @@ export const App = () => {
       .then(json => setThoughts(json))
     // .then(json => console.log(json))
 
-  }, [])
 
+  })
+
+  const handleFormSubmit = () => {
+    // ssubmit and update thoughts
+    // fetch to submit thought
+  }
+
+  const handleHeartClick = () => {
+  }
+
+
+  // const Form = () => {
+  //   const [message, setMessage] = useState('')
+
+  //   const submitHandler = () => {
+  //     fetch('https://technigo-thoughts.herokuapp.com/', {
+  //       method: 'POST',
+  //       body: JSON.stringify({ message }),
+  //       headers: { 'Content-Type': 'application/json' }
+  //     }).catch(() => {
+  //       alert('Something gone wrong, but stay positive and try again :)')
+  //     })
+  //   }
 
   return (
     <div>
@@ -24,24 +49,37 @@ export const App = () => {
         <div className="headForm">
           <p>What's making you happy right now?</p>
 
-          <input className="form-text"
-            type="text"
-            onChange={event => setText(event.target.value)}
-            value={text}
+          <textarea className="form-text">
 
-          />
+            {/* minLenght: 2;
+          kMaxLength: 140; */}
+          </textarea>
+
+
+          {/* onChange={event => setText(event.target.value)}
+          value={text}>
+
+        /> */}
+
+          <button onClick={handleFormSubmit}>❤️ Send happy thought ❤️</button>
         </div>
       </form>
 
 
-      <form>
+      <section>
         <ul>
           {thouhgts.map(thought => (
-            <li key={thought.message} className="messages">{thought.message}</li>
+            <li key={thought.message} className="messages">
+              <div> {thought.message} </div>
+
+              <div className="likes"><button onClick={handleHeartClick}>❤️</button>{thought.hearts}</div>
+
+            </li>
           ))}
 
         </ul>
-      </form>
-    </div>
+      </section>
+    </div >
+
   )
 }
