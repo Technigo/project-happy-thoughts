77c6f2b6773e1b26f478454a8f68129b213a2c43
Michel Elmoznino Laufer <mitchel_85@hotmail.com>
2019-12-04 12:14:00 +0100

    Fetched recent thoughts from API, created form and did CSS-styling

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,89 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Timestamp from 'react-timeago'
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState([])
+  const [newThought, setNewThought] = useState("")
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    fetch("https://technigo-thoughts.herokuapp.com/")
+      .then(res => res.json())
+      .then(json => {
+        setThoughts(json)
+      })
+  }, [])
+
+  const handleFormSubmit = (event) => {
+    // event.preventDefault()
+
+    if(newThought.length>=5 && newThought.length<=140) {
+    fetch("https://technigo-thoughts.herokuapp.com/", {
+      method: 'POST',
+      body: JSON.stringify({message:'My happy thought'})
+    })
+      .then((res) => res.json())
+      .then((newThought) => {
+        setThoughts((thoughts) => [newThought, ...thoughts])
+      })
+    } else {
+      setError(true)
+    }
+  }
+
+  // const handleThoughtLike = (event) => {
+  //   event.preventDefault()
+
+  //   fetch("https://technigo-thoughts.herokuapp.com/{_id}/like", {
+  //     method: 'POST'
+  //   })
+  //     .then((res) => res.json())
+  //     .then(() => {
+  //     })
+  //   } 
+
+
   return (
-    <div>
-      Find me in src/app.js!
+    <div className="main-container">
+      <h1>😃 Happy Thoughts 😃</h1>
+    
+    <div className="question-card">
+      <form onSubmit={event => event.preventDefault()}>
+      {!error &&(<p className="question">What is making you happy right now?</p>)}
+      {error &&(<p className="required">Please write between 5 and 140 characters</p>)}
+
+      <label>
+      <input 
+        type="text"
+        value={newThought}
+        onChange={event => setNewThought(event.target.value)}
+      />
+      </label>
+      <button 
+        className="send-form"
+        type="submit"
+        onClick={() => handleFormSubmit()}
+        >
+        ❤️ Send Happy Thought ❤️
+      </button>
+
+      </form>
+    </div>
+
+      <ul>
+        {thoughts.map(thought => (
+          <div className="cards">
+            <li key={thought.id} className="message">{thought.message}</li>
+            <div className="cards-bottom">
+            <li className="hearts">
+              <button className="heart-button" type="submit">❤️</button> x {thought.hearts}</li>
+            <li className="time"><Timestamp date={thought.createdAt}/></li>
+            </div>
+          </div>
+        ))}
+      </ul>
+
     </div>
   )
 }
+
