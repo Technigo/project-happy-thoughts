39354ffb6347484e6951f09b0bc1addf2ccf87d6
Petra B <petrabrinck@icloud.com>
2019-12-05 15:32:55 +0100

    like btn not working btn not working yet but got help fron J with css, min was al lot bigger

--- code/src/components/HappyForm.js
+++ code/src/components/HappyForm.js
@@ -1,30 +1,32 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
+import "./happyForm.css"
 
-// export const HappyForm = (onFormSubmit) => {
 export const HappyForm = props => {
     const [message, setMessage] = useState("")
 
     const handleSubmit = event => {
         event.preventDefault()
-        props.onFormSubmit(message)
+        props.onFormSubmit(message) // this onFormSubmit comes as a props from App.js
     }
 
 
-
     return (
-        <form>
-            <h2>Post a happy thought!</h2>
-            <p>{message}</p>
+        <form className='happy-form'>
+            <h3>Post a happy thought!</h3>
             <textarea
                 rows='3'
-                on onChange={event => setMessage(event.target.value)}>
-            </textarea>
-            <button type='submit'
-                onClick={handleSubmit}
-                disabled={message.length < 5 && message.length > 140 ? true : false}>
-                ❤️ Send a happy thought ❤️
-            </button>
+                onChange={event => setMessage(event.target.value)}
+            ></textarea>
+            <div className='form-footer'>
+                <button
+                    type='submit'
+                    onClick={handleSubmit}
+                    disabled={message.length < 6 || message.length > 140 ? true : false}
+                >
+                    ❤️Send a happy thought❤️
+        </button>
+                <p>{message.length} / 140</p>
+            </div>
         </form>
     )
 }
-
--- code/src/components/HappyThought.js
+++ code/src/components/HappyThought.js
@@ -1,19 +1,21 @@
 import React from "react"
-import Moment from "moment"
+import moment from "moment"
 import "./happyThought.css"
 
-// export const HappyThought = ({ message, hearts, createdAt }) => {
 export const HappyThought = props => {
 
     const { message, hearts, createdAt } = props.thought
     return (
-        <article>
+        <article className='happy-thought'>
             <h3>{message}</h3>
-            <span>{hearts}</span>
-            {/* <p>time stam</p> */}
-            <p>{Moment(createdAt).fromNow()}</p>
-            {/* <p>{Moment().start0f().fromNow()}</p> */}
-            <p>{createdAt}</p>
+            <p>
+                <button role='img' aria-label='Heart'>                    {"❤️ "}
+                </button>
+                {hearts}
+            </p>
+            <p>{moment(createdAt).fromNow()}</p>
         </article>
     )
 }
+
+
