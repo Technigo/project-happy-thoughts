910ba334413fbe704057d06706401a4b885cbe72
Agnes Ketesdi <agnes@Agness-MacBook-Air.local>
2019-12-06 21:31:05 +0100

    added like buttons with heart emoji

--- code/src/Components/HappyThought.js
+++ code/src/Components/HappyThought.js
@@ -3,12 +3,30 @@ import "./happyThought.css";
 import moment from "moment";
 
 export const HappyThought = props => {
-  const { message, heart, createdAt } = props.thought;
+  const { message, hearts, createdAt, _id } = props.thought;
+
+  const handleClick = () => {
+    fetch(`https://technigo-thoughts.herokuapp.com/${_id}/like`, {
+      method: "POST",
+      body: "",
+      headers: { "Content-Type": "application/json" }
+    }).then(() => props.onLiked(_id));
+  };
 
   return (
     <article className="happy-thought">
-      <h1>{message}</h1>
-      <span>{heart} count</span>
+      <h3>{message}</h3>
+      <p>
+        <button
+          onClick={handleClick}
+          style={{ background: hearts > 0 ? "#ffadad" : "#f3f1f1  " }}
+        >
+          <span role="img" aria-label="Heart">
+            {"❤️"}
+          </span>
+        </button>
+        x {hearts}
+      </p>
       <p>{moment(createdAt).fromNow()}</p>
     </article>
   );
