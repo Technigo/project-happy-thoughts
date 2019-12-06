d23a852340457790b2a381d0d92508c6b7399f9f
Matilda Arvidsson <matilda.j.arvidsson@gmail.com>
2019-12-06 17:30:48 +0100

    Added styling for when message is not valid

--- code/src/Hearts.js
+++ code/src/Hearts.js
@@ -15,8 +15,6 @@ export const Hearts = (props) => {
   }
 
 
-
-
   return (
     <div className="hearts">
       <button className="like-heart"
--- code/src/NewThought.js
+++ code/src/NewThought.js
@@ -13,25 +13,27 @@ export const NewThought = ({ onFormSubmit }) => {
 
 
   return (
-    <div className="new-thought">
-      <form>
-        <label>
-          What's making you happy right now?
+    <form className="new-thought">
+      <label>
+        What's making you happy right now?
           <br></br>
-          <textarea
-            // autoFocus
-            onChange={(event) => setNewThought(event.target.value)}
-            value={newThought}
-          />
-        </label>
+        <textarea
+          autoFocus
+          onChange={(event) => setNewThought(event.target.value)}
+          value={newThought}
+        />
+      </label>
+      <div className="button-and-length">
         <button
           type="submit"
           className="send-button"
-          onClick={handleNewThought}>
+          onClick={handleNewThought}
+          disabled={newThought.length < 5 || newThought.length >= 140 ? true : false}>
           <span role="img" aria-label="heart-emoji">❤️</span>Send Happy Thought<span role="img" aria-label="heart-emoji">❤️</span>
         </button>
-      </form>
-    </div >
+        <p className={newThought.length < 5 || newThought.length >= 140 ? "red" : "black"}>{newThought.length}/140</p>
+      </div>
+    </form>
   )
 
 }
