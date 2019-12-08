05833816d6c09db1b1b08be7e403cd31d4c205df
ucha3i <jchabik@gmail.com>
2019-12-08 14:56:37 +0100

    timestamp resolved

--- code/src/components/HappyThought.js
+++ code/src/components/HappyThought.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "./happyThought.css"
-/* import moment from "moment"; */
+import moment from "moment";
 
 export const HappyThought = props => {
   const { message, hearts, createdAt, _id } = props.thought
@@ -14,21 +14,22 @@ export const HappyThought = props => {
   }
 
   return (
-    <article className='happy-thought'>
-      <h4>{message}</h4>
-      <p>
-        <button
-          onClick={handleClick}
-          style={{ background: hearts > 0 ? "#ffadad" : "#f3f1f1  " }}
-        >
-          <span role='img' aria-label='Heart'>
-            {"❤️"}
-          </span>
-        </button>
-        x {hearts}
-      </p>
-      {/* <span>{moment(createdAt).fromNow()}</span> */}
-      {/* <p>{moment(createdAt).fromNow()}</p> */}
+    <article className="happy-thought">
+      <h3>{message}</h3>
+      <div className="happy-footer">
+        <div className="heart-container">
+          <button
+            onClick={handleClick}
+            style={{ background: hearts > 0 ? "#ffadad" : "#f3f1f1  " }}
+          >
+            <span role="img" aria-label="Heart">
+              {"❤️"}
+            </span>
+          </button>
+          x {hearts}
+        </div>
+        <span>{moment(createdAt).fromNow()}</span>
+      </div>
     </article>
-  )
-}
+  );
+};
