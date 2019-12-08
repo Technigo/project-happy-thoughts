39ed3e058eb382113d7431bf4b809f7d36c05b26
Linda <isell.linda@gmail.com>
2019-12-08 15:20:06 +0100

    fixed bug in showing and storing likes

--- code/src/components/Heart.js
+++ code/src/components/Heart.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./heart.css";
 
 export const Heart = (props) => {
+  const [clicks, setClicks] = useState((props.hearts))
 
   if (!localStorage[props.id]) {
     localStorage.setItem(props.id, 0)
@@ -15,9 +16,14 @@ export const Heart = (props) => {
     }).then(() => {
       props.onThoughtLiked(props.id)
       localStorage[props.id] = Number(localStorage[props.id]) + 1
+      setClicks(clicks + 1)
+
     })
   }
-
+  // Det viktigaste är att du i din komponent för heart-button skapar en ny variabel - 
+  //jag kallade den för clicks och att du i useState ger den värdet som hearts har. 
+  //Sedan att du i handleClick efter att du fetchat tar +1, dvs. setClicks(clicks + 1) 
+  //och slutligen om du vill se ökningen renderas direkt i DOM-en att du tar x {clicks} istället för heart eftersom clicks är +1 än vad hearts är
   return (
     <li>
       <button className="like-button"
@@ -28,7 +34,7 @@ export const Heart = (props) => {
           ❤️
         </span>
       </button>
-      x{props.hearts} (you liked x{localStorage[props.id]})
+      x{clicks} (you liked x{localStorage[props.id]})
     </li >
   )
 }
