8b68d588bed7a3729ddb5c757f1d7c0d85d0c2e3
Camilla <camillalofroth@hotmail.com>
2019-12-09 20:04:08 +0100

    finished project

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { HappyThought } from './components/HappyThought'
+import { HappyForm } from 'components/HappyForm'
+
+const url = "https://technigo-thoughts.herokuapp.com/"
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState([])
+  const [postedMessage, setPostedMessage] = useState("")
+
+useEffect(()=> {
+  fetch(url)
+    .then(res => res.json())
+    .then(json => setThoughts(json))
+}, [postedMessage]);
+
+const handleFormSubmit = message => {
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify({ message }),
+    headers: { "Content-Type": "application/json" }
+  })
+    .then(() => setPostedMessage(message))
+    .catch(err => console.log("error",err))
+}
+
+const onLiked = (thoughtId) => {
+  console.log("logging in the app.js", thoughtId)
+  const updatedThoughts = thoughts.map(thought => {
+    if (thought._id === thoughtId) {
+      thought.hearts += 1
+    }
+    return thought
+  })
+  setThoughts(updatedThoughts)
+} //This function is to make the app know that we clicked on like in the happyThoughts-component.
+
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <main>
+      <HappyForm onFormSubmit={handleFormSubmit} />
+      {thoughts.map(thought => (
+        <HappyThought key={thought._id} 
+        thought={thought}//komponenten finns inte än
+        onLiked={onLiked}/> 
+      ))}
+    </main>    
   )
 }
+
---
+++ code/src/components/HappyThought.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './happyThought.css'
+import moment from 'moment'
+
+const url = `https://technigo-thoughts.herokuapp.com/`
+
+export const HappyThought = (props) => {
+  console.log('props', props)
+  const { message, hearts, createdAt, _id} = props.thought
+  const handleClick = () => {
+    console.log("clicking!", _id)
+    fetch(`https://technigo-thoughts.herokuapp.com/${_id}/like`, {
+      method: "POST",
+      body: "",
+      headers: {"Content-Type": "application/json"}
+    }).then(() => props.onLiked(_id))
+  }
+
+  return (
+    <article className='happy-thought'>
+      <h3>{message}</h3>
+      <p>
+      <button onClick={handleClick}>
+      <span role='img' aria-label='Heart'>
+        {"❤️ "}
+      </span>
+      </button>
+      x {hearts}
+      </p>
+      <p>{moment(createdAt).fromNow()}</p>
+    </article>
+  )
+}
