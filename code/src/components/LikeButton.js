eab0de8afee7e7c1b17c234324e254f6f0a9a7c5
nyblad <nyblad@hotmail.com>
2020-02-12 17:28:04 +0100

    Bug fix: an extra / in the fetch url

--- code/src/components/LikeButton.js
+++ code/src/components/LikeButton.js
@@ -7,8 +7,8 @@ export const LikeButton = (props) => {
   const handleOnClick = () => {
 
     // POST like to API and then add like to that matching id
-    //onThoughtLiked is passed via ListThoughts from the function onThoughtLikes in Thought
-    fetch(`https://nyblad-happy-thoughts.herokuapp.com//${props.id}/like`, {
+    // onThoughtLiked is passed via ListThoughts from the function onThoughtLikes in Thought
+    fetch(`https://nyblad-happy-thoughts.herokuapp.com/${props.id}/like`, {
       method: "POST", body: "", headers: { "Content-Type": "application/json" }
     }).then(() => {
       props.onThoughtLiked(props.id)
