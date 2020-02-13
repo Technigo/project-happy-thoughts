474c90e885a5f6d31daab001daebfb730aba8cd3
AnnaTerelius <tereliusanna@hotmail.com>
2020-02-13 17:20:15 +0100

    removed body and headers

--- code/src/App.js
+++ code/src/App.js
@@ -33,8 +33,7 @@ export const App = () => {
     console.log('test')
     fetch(`https://annatereliushappythoughtsapi.herokuapp.com/${_id}/like`, {
       method: 'POST',
-      body: '{}',
-      headers: { 'Content-Type': 'application/json' }
+     
     })
     .then((res) => res.json())
     .then((json) => console.log(json))
