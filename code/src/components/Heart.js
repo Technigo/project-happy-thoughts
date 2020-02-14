3d194daf710717c93533f24feffc7c3f95ce19e1
Linda <isell.linda@gmail.com>
2020-02-14 14:16:48 +0100

    Fix URL in heart.js

--- code/src/components/Heart.js
+++ code/src/components/Heart.js
@@ -9,7 +9,7 @@ export const Heart = (props) => {
   }
 
   const handleLikeClick = () => {
-    fetch(`http://localhost:8080/${props.id}/like`, {
+    fetch(`https://happy-thoughts-linda.herokuapp.com/${props.id}/like`, {
       method: "POST",
       body: "",
       headers: { "Content-Type": "application/json" }
