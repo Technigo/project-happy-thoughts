18ff0e66599afede256b7ba74bc13e8dad6401b1
Petra B <petrabrinck@icloud.com>
2020-02-15 15:56:06 +0100

    added {} again in body

--- code/src/components/HappyThought.js
+++ code/src/components/HappyThought.js
@@ -9,7 +9,7 @@ export const HappyThought = props => {
         // fetch(`https://technigo-thoughts.herokuapp.com/${_id}/like`, {
         fetch(`https://pb-happy-thoughts.herokuapp.com/${_id}/like`, {
             method: "POST",
-            body: "",
+            body: "{}",
             headers: { "Content-Type": "application/json" }
         }).then(() => props.onLiked(_id))
     }
