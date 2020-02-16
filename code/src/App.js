49af1804b3dd1c902683ca224faf54e4e2c0e048
Johanna Luks Wærn <johanna@misswaern.se>
2020-02-16 16:55:16 +0100

    update "" to ''

--- code/src/App.js
+++ code/src/App.js
@@ -1,56 +1,56 @@
-import React, { useState, useEffect } from "react"
-import { Thought } from "./Thought.js"
-import { Form } from "./Form.js"
-import { ReactComponent as Heart } from "./heart3.svg"
-import "./index.css"
+import React, { useState, useEffect } from 'react'
+import { Thought } from './Thought.js'
+import { Form } from './Form.js'
+import { ReactComponent as Heart } from './heart3.svg'
+import './index.css'
 
 export const App = () => {
   const [APIThoughts, setAPIThoughts] = useState([])
 
-  const postThoughtToAPI = message => {
-    fetch("https://technigo-thoughts.herokuapp.com/", {
-      method: "POST",
+  const postThoughtToAPI = (message) => {
+    fetch('https://technigo-thoughts.herokuapp.com/', {
+      method: 'POST',
       body: JSON.stringify({ message: message }),
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
       }
     })
-      .then(res => res.json())
-      .then(newThought => {
-        setAPIThoughts(previousThoughts => [newThought, ...previousThoughts])
+      .then((res) => res.json())
+      .then((newThought) => {
+        setAPIThoughts((previousThoughts) => [newThought, ...previousThoughts])
       })
   }
 
-  const postLikeToAPI = id => {
+  const postLikeToAPI = (id) => {
     fetch(`https://technigo-thoughts.herokuapp.com/${id}/like`, {
-      method: "POST"
+      method: 'POST'
     })
   }
 
   useEffect(() => {
-    fetch("https://technigo-thoughts.herokuapp.com")
-      .then(res => res.json())
-      .then(json => setAPIThoughts(json))
+    fetch('https://technigo-thoughts.herokuapp.com')
+      .then((res) => res.json())
+      .then((json) => setAPIThoughts(json))
   }, [])
 
   return (
     <div className='container'>
       <header>
-        {" "}
+        {' '}
         Happy Thoughts <Heart className='heart' />
       </header>
       <div>
         <Form postThoughtToAPI={postThoughtToAPI} />
       </div>
       <div>
-        {APIThoughts.map(oneAPIThought => {
+        {APIThoughts.map((oneAPIThought) => {
           return (
             <Thought
               key={oneAPIThought._id}
               thoughtInsideComponent={oneAPIThought}
               postLikeToAPI={() => {
                 postLikeToAPI(oneAPIThought._id)
-                setAPIThoughts(previousThoughts => {
+                setAPIThoughts((previousThoughts) => {
                   oneAPIThought.hearts = oneAPIThought.hearts + 1
                   return [...previousThoughts]
                 })
