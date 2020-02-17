710eedcf10949c84a43c79ea09053905277704c3
Clara Gyökeres <you@example.com>
2020-02-17 15:18:07 +0100

    new version

--- code/src/App.js
+++ code/src/App.js
@@ -4,7 +4,7 @@ import moment from 'moment'
 
 export const App = () => {
   useEffect(() => {
-    fetch("https://technigo-thoughts.herokuapp.com/")
+    fetch("https://happyapietc.herokuapp.com/")
       .then(res => res.json())
       .then(json => {
         setThoughts(json)
@@ -16,7 +16,7 @@ export const App = () => {
 
   const handleFormSubmit = (event) => {
     event.preventDefault()
-    fetch('https://technigo-thoughts.herokuapp.com/', {
+    fetch('https://happyapietc.herokuapp.com/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ message: userText })
@@ -28,12 +28,12 @@ export const App = () => {
   }
 
   const likeThought = (thought) => {
-    fetch(`https://technigo-thoughts.herokuapp.com/${thought._id}/like`, { method: 'POST' })
+    fetch(`https://happyapietc.herokuapp.com/${thought._id}/like`, { method: 'POST' })
       .then((res) => res.json())
       .then((responsejson) => {
         const updatedThoughts = thoughts.map((heartthought) => {
           if (heartthought._id === thought._id) {
-            heartthought.hearts += 1
+            heartthought.heart += 1
           }
           return heartthought
         })
@@ -59,7 +59,7 @@ export const App = () => {
           <section className="cardbottom">
             <section className="like-section">
 
-              <p> <button onClick={() => likeThought(thought)}><span role="img" aria-label="heart">❤️ </span></button> x {thought.hearts} {moment(thought.createdAt).fromNow()} </p>
+              <p> <button onClick={() => likeThought(thought)}><span role="img" aria-label="heart">❤️ </span></button> x {thought.heart} {moment(thought.createdAt).fromNow()} </p>
             </section>
           </section>
 
