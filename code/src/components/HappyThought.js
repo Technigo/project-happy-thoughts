118b1e4869292df866cc17f5ae9206ad7e42ec9b
JoacimNilsen <joacimnilsen1986@gmail.com>
2020-02-19 15:10:56 +0100

    Fixed page issue

--- code/src/components/HappyThought.js
+++ code/src/components/HappyThought.js
@@ -13,7 +13,7 @@ export const HappyThought = props => {
 	}
 
 	const handleClick = () => {
-		fetch(`https://technigo-thoughts.herokuapp.com/${_id}/like`, {
+		fetch(`https://technigo-thoughts.herokuapp.com/${_id}/likes`, {
 			method: 'POST',
 			body: '',
 			headers: { 'Content-Type': 'application/json' }
