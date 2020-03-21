2f199c0326e4c539932b3438113213b0740480f8
Mathias Thorström <thisen@gmail.com>
2020-03-21 13:34:57 +0100

    created like function

--- code/src/components/ToughtCard.js
+++ code/src/components/ToughtCard.js
@@ -3,15 +3,25 @@ import '../components/toughtcard.css'
 
 
 export const ToughtCard = (props) => {
+  const { message, hearts, CreatedAt, id } = props
 
-  const url = `https://technigo-thoughts.herokuapp.com/${props.id}/like`
-  
+  const url = `https://technigo-thoughts.herokuapp.com/${id}/like`
+
+  const handleLike = (event) => {
+    // Send the POST request with the input from your form (instead
+    // of 'Hello world' like this example does):
+    fetch(url, { 
+      method: 'POST', 
+    })
+  }
+
+  console.log(url)
   return (
     <article className="toughtCard">
-      <p>{props.message}</p>
+      <p>{message}</p>
       <section className="toughtCardDetails">
-        <p>{props.hearts}</p>
-        <p>{props.CreatedAt}</p>
+        <p onClick={() => handleLike()}>{hearts}</p>
+        <p>{CreatedAt}</p>
         {/* <p>Id: {props.id}</p> */}
       </section>
     </article>
