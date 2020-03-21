43b4abcc6d5535024579b75fe3c9eec1bbbe5f6c
Josephine <josephine@Josephines-Air.lan>
2020-03-21 15:11:24 +0100

    prints out message, likes and time

--- code/src/App.js
+++ code/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Thought } from './Thought'
 
 export const App = () => {
   const [thoughts, setThoughts] = useState([])
@@ -8,12 +9,12 @@ export const App = () => {
       .then(json => setThoughts(json))
   }, [])
   return (
+
     <div>
-      Happy Thoughts
-      <ul>
-        {thoughts.map(json =>
-          <li key={json._id}>{json.message}</li>)}
-      </ul>
+      {thoughts.map(json =>
+        <div key={json._id}>{json.message} {json.hearts} {json.createdAt}</div>
+      )}
     </div>
+
   )
 }
