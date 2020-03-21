34c5b57cc66863416ffda1ba5f035a9e74414d43
Josephine <josephine@Josephines-Air.lan>
2020-03-21 12:31:16 +0100

    made happy thoughts render in the DOM

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState([])
+  useEffect(() => {
+    fetch('https://technigo-thoughts.herokuapp.com/')
+      .then(res => res.json())
+      .then(json => setThoughts(json))
+  }, [])
   return (
     <div>
-      Find me in src/app.js!
+      Happy Thoughts
+      <ul>
+        {thoughts.map(json =>
+          <li key={json._id}>{json.message}</li>)}
+      </ul>
     </div>
   )
 }
