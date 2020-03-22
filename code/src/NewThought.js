14495d1908be94204e1234fead3a9a83ef908a7a
Frida Eriksson <Frida.Eriksson@silf.se>
2020-03-22 14:19:04 +0100

    added styling for NewThought and ThoughtForm

--- code/src/NewThought.js
+++ code/src/NewThought.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import './newthought.css'
 
 export const NewThought = ({ onChange }) => {
   return (
     <label>
-      <h2>What's making you happy right now?</h2>
-      <textarea
+      <h2 className="thought-question">What's making you happy right now?</h2>
+      <textarea className="text-field"
         minLength="5"
         maxLength="140"
-        rows="2"
+        rows="3"
         onChange={onChange} />
     </label>
   )
--- code/src/ThoughtForm.js
+++ code/src/ThoughtForm.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { NewThought } from './NewThought'
+import './thoughtform.css'
 
-export const ThoughtForm = ({ onSubmit, value, onChange }) => {
+export const ThoughtForm = ({ onSubmit, onChange }) => {
   return (
     <div>
       <form className="thought-form" onSubmit={onSubmit}>
-        <NewThought
-          onChange={onChange} />
+        <div className="new-thought-container">
+          <NewThought
+            onChange={onChange} />
+        </div>
         <button className="thougt-button" type="submit">
           <span className="heart-emoji" role="img" aria-label="red heart">❤️</span> Send Happy Thought <span className="heart-emoji" role="img" aria-label="red heart">❤️</span>
         </button>
