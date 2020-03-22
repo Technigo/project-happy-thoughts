dce7388094b43b81bd8b21b1f74735c294b3a795
L-Lisa <lisaojeland@gmail.com>
2020-03-22 09:48:50 +0100

    first fetch

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from "react"
+
+ 
 
 export const App = () => {
+const [thoughts, setThoughts] = useState([])
+
+
+  useEffect(() => {
+    fetch("https://technigo-thoughts.herokuapp.com/")
+    .then(res => res.json())
+    .then(json=> 
+    setThoughts(json)
+    )
+  },[]);
+
   return (
-    <div>
-      Find me in src/app.js!
+    <div className="HappyThoughtsCard">
+      <ul>
+         {thoughts.map(thought => (
+              <li> 
+               {thought.message}
+              </li>
+         ))};
+         </ul>
     </div>
   )
 }
