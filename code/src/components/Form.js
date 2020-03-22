ea4522495f4bf595bccebfae9c87fde74fa6725a
Emelie Loheden <emelie_loheden@hotmail.com>
2020-03-22 10:18:44 +0100

    css form

--- code/src/components/Form.js
+++ code/src/components/Form.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import './form.css'
 
 
 export const Form = props => {
@@ -12,21 +13,20 @@ export const Form = props => {
 
   return (
     <form>
-
+    <h1>What making you happy right now?</h1>
       <textarea value={message}
-        onChange={event => setMessage(event.target.value)}>
+        onChange={event => setMessage(event.target.value)}
+        className="input-text">
       </textarea>
       <div>
-        <p>{message.length}/140</p>
+        <p>{message.length}/90</p>
       </div>
       <div>
 
-        <button onClick={handleSubmit}
+        <button onClick={handleSubmit} className="send-love-button"
           type="submit" tabIndex="0" 
-          disabled={message.length <= 5 || message.length >= 140 ? true : false}>
-          <span >♥︎</span>
-          POST
-          <span >♥︎</span>
+          disabled={message.length <= 2 || message.length >= 90 ? true : false}>
+          <span>❤</span>Send Happy Thought<span>❤</span>
         </button>
         
       </div>
