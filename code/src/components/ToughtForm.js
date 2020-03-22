29a744a6c8c457337af12293a89c3909e18226f7
Mathias Thorström <thisen@gmail.com>
2020-03-22 15:48:04 +0100

    finished styling of input form

--- code/src/components/ToughtForm.js
+++ code/src/components/ToughtForm.js
@@ -2,6 +2,11 @@ import React from 'react'
 import '../components/toughtform.css'
 
 export const ToughtForm = () => {
+// En state att spara user input data i. Sedan uppdatera och skicka när man klickar på knappen.
+
+  const handleLike = () => {
+  }
+
   return (
     <article className="toughtForm">
       <p className="toughtText">Whats making you happy right now?</p>
@@ -14,8 +19,10 @@ export const ToughtForm = () => {
           placeholder="Write something nice here!"
           rows="3" 
           cols="63"
+          maxLength="140"
           />
-        </lable>        
+        </lable>
+        <span className="sendToughtButton" onClick={() => handleLike()}>❤️Send Happy Thought❤️</span>
       </form>
 
     </article>
