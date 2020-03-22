7b7073a4216fddcebbc52059de314368a6255dfc
crushoftheyear <kajsalovisaholmstrom@gmail.com>
2020-03-22 12:05:50 +0100

    Added styles to post card

--- code/src/components/PostThought.js
+++ code/src/components/PostThought.js
@@ -6,7 +6,7 @@ export const PostThought = () => {
     <section className="post-thought">
       <label htmlFor="">What's making you happy right now?</label>
       <textarea id=""></textarea>
-      <button>Send happy thought</button>
+      <button className="post-btn">❤️Send happy thought ❤️</button>
     </section>
   )
 }
