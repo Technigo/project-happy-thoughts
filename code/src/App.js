2ee731ae99390ef99bb7411e03b370b21f8c6ba7
Johanna Åstrand <johanna@johannas-mbp.mynet>
2020-03-22 17:07:43 +0100

    project started and first fetch done listing all the previous posted thoughts

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState([])
+
+  useEffect(() => {
+    fetch('https://technigo-thoughts.herokuapp.com/')
+      .then(res => res.json())
+      .then(json => setThoughts(json))
+  }, [])
+
   return (
     <div>
-      Find me in src/app.js!
+      {thoughts.map(thought => (
+        <p key={thought._id}>{thought.message}</p>
+      ))}
+
     </div>
   )
 }
