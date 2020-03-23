4860ac1bcda97b34ad1b96bece38140b0dcf94b3
annaelsamaria <hej@annagustafsson.se>
2020-03-23 21:18:56 +0100

    added the post-component and now its finally working

--- code/src/HappyThoughts.js
+++ code/src/HappyThoughts.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Emoji } from "./Emoji"
 import './HappyThoughts.css'
-import moment from 'moment';
+import moment from 'moment'
 
 
 export const HappyThoughts = () => {
@@ -10,31 +10,28 @@ export const HappyThoughts = () => {
 
   useEffect(() => {
     fetch(THOUGHTS_URL)
-      .then((res) => {
-        return res.json()
-      })
-      .then(json => {
-        setThoughts(json.reverse())
-      })
+      .then(res => res.json())
+      .then(json => setThoughts(json))
   }, [])
+
 
   return (
     <div>
-      {
-        thoughts.map(thought => (
-          <div className="thoughts-container">
-            <h2 className="message" key={thought._id}>
-              {thought.message}</h2>
-            <p>
-              <Emoji symbol="❤️" />
-              x {thought.hearts}</p>
-            <p>
-              <span className="message-time">
-                {moment(thought.date).fromNow()}
-              </span>
-            </p>
-          </div>
-        ))
+      {thoughts.map(thought => (
+        <article className="thoughts-container">
+          <h2 className="message" key={thought._id}>
+            {thought.message}</h2>
+          <p>
+            <Emoji symbol="❤️" />
+            x {thought.hearts}
+          </p>
+          <p>
+            <span className="message-time">
+              {moment(thought.createdAt).fromNow()}
+            </span>
+          </p>
+        </article>
+      ))
       }
     </div>
   )
--- code/src/PostThoughts.js
+++ code/src/PostThoughts.js
@@ -1,20 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Emoji } from "./Emoji"
 import './PostThoughts.css'
 
 export const PostThoughts = () => {
+  const THOUGHTS_URL = "https://technigo-thoughts.herokuapp.com/"
+  const [message, setMessage] = useState("")
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    fetch(THOUGHTS_URL,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ message: message })
+      }
+    ).then(() => {
+      window.location.reload()
+    })
+  }
 
   return (
-    <form className="post-thoughts">
+    <form onSubmit={handleSubmit} className="post-thoughts">
       <h2>What's making you happy right now?</h2>
       <input
         type="text"
+        className="form-text"
+        onChange={event => setMessage(event.target.value)}
         required
       />
-      <div>
-        <Emoji symbol="❤️" />
-        <button>Send happy-thought</button>
-        <Emoji symbol="❤️" />
+      <div className="post-button">
+        <button className="send-button" type="submit">
+          <Emoji symbol="❤️" /> Send Happy Thought! <Emoji symbol="❤️" />
+        </button>
       </div>
     </form>
   )
