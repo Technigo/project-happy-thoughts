9d244510b85eca5735870618325a4059076e88b2
camilla <camilla.gausland@gmail.com>
2020-03-25 11:05:11 +0100

    formatting

--- code/src/MessageInput.js
+++ code/src/MessageInput.js
@@ -28,27 +28,27 @@ export const MessageInput = (props) => {
 
   return (
     <form onSubmit={handleSubmit}>
-        <label className="text-area">
-          <p className="label-input">What's making you happy right now?</p> 
-          <textarea 
-            type="text"
-            minLength="6"
-            maxLength="140"
-            className="input-field" 
-            type="text"
-            id="myThought"
-            required
-            onChange={handleChange}
-            tabIndex='0'> 
-          </textarea>
-          <p className="char-count">{count}/140</p>
-        </label>
-          <input
-            type="submit"
-            value= '❤️    Add message    ❤️'
-            className="form-button"
-            tabIndex='0'
-            disabled={message.length < 6 || message.length > 140 ? true : false}>
-          </input>
+      <label className="text-area">
+        <p className="label-input">What's making you happy right now?</p> 
+        <textarea 
+          type="text"
+          minLength="6"
+          maxLength="140"
+          className="input-field" 
+          type="text"
+          id="myThought"
+          required
+          onChange={handleChange}
+          tabIndex='0'> 
+        </textarea>
+        <p className="char-count">{count}/140</p>
+      </label>
+      <input
+        type="submit"
+        value= '❤️    Add message    ❤️'
+        className="form-button"
+        tabIndex='0'
+        disabled={message.length < 6 || message.length > 140 ? true : false}>
+      </input>
     </form>
   )}
