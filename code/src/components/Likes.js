35f778a09238177025b34f1efd581962fbed8be1
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-03-27 17:45:43 +0100

    made the your like function work with the dom

--- code/src/components/Likes.js
+++ code/src/components/Likes.js
@@ -2,29 +2,31 @@ import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export const Likes = (props) => {
-
   const { hearts, id } = props
   const [likes, setLikes] = useState(hearts)
+  let [yourLikes, setYourLikes] = useState(0)
+  const [yourPreviousLikes, setYourPreviousLikes] = useState(Number(localStorage.getItem(id)) + 0)
 
-  if (!localStorage[id]) {
-    localStorage.setItem(id, 0)
-  }
+  localStorage.setItem(id, JSON.stringify(yourPreviousLikes))
 
   const handleClick = () => {
     fetch(`https://technigo-thoughts.herokuapp.com/${id}/like`, {
       method: "POST",
       body: "",
       headers: { "Content-Type": "application/json" }
-    }).then(() => {
-      setLikes(likes + 1)
-      localStorage[id] = Number(localStorage[id]) + 1
-    })
+    }).then((res) => res.json())
+      .then(() => { }, [])
+    setLikes((oldState) => oldState + 1)
+    setYourLikes((oldState) => oldState + 1)
+    setYourPreviousLikes((oldState) => oldState + 1)
+
+    localStorage.setItem(id, JSON.stringify(yourPreviousLikes))
   }
 
   return (
     <>
       <button onClick={handleClick}><FontAwesomeIcon icon="heart" /></button>
-      <p>{likes} likes (you liked this {localStorage[id]} times)</p>
+      <p>{likes} likes (you liked this {yourLikes} times)</p>
     </>
   )
 }
