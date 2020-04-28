58171e73541ba359362db2da306179562c43e660
Elin Aarenstrup <hej@elinaarenstrup.com>
2020-04-28 10:08:50 +0200

    added a header

--- code/src/App.js
+++ code/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { ThoughtsList } from "./components/ThoughtsList.js";
 import { ThoughtsForm } from "./components/ThoughtsForm.js";
 import "app.css";
+import { Header } from "./components/Header.js";
 
 export const App = () => {
   return (
     <section className="app-container">
+      <Header />
       <ThoughtsForm />
       <ThoughtsList />
     </section>
---
+++ code/src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <div className="header-div">
+      <h1>
+        [ HAPPY TH<span className="header-heart">{"❤️"}</span>UGHTS ]
+      </h1>
+    </div>
+  );
+};
