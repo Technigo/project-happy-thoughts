66e6f2a77aaa8e69b091b1d99460e4ed38e8320d
Frida Eriksson <Frida.Eriksson@silf.se>
2020-05-18 20:55:51 +0200

    started adding new components for commenting function, updated ThoughtsList and styling

---
+++ code/src/components/Comments.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react'
+
+export const Comments = () => {
+  return (
+    <div>Hello!</div>
+  )
+}
--- code/src/components/ThoughtsList.js
+++ code/src/components/ThoughtsList.js
@@ -2,23 +2,31 @@ import React from 'react'
 import moment from 'moment'
 import { HeartButton } from './HeartButton'
 import './thoughtslist.css'
+import { CommentForm } from './CommentForm'
+import { Comments } from './Comments'
 
 export const ThoughtsList = ({ thoughts, onHeartClicked }) => {
   return (
     <>
       {thoughts.map((thought) => (
         <article key={thought._id} className="thought-card">
-          <p className="happy-thought">{thought.message}</p>
-          <div className="heart-container">
-            <HeartButton
-              id={thought._id}
-              onHeartClicked={onHeartClicked}
-              hearts={thought.hearts} />
-            <p className="likes-counter">x {thought.hearts}</p>
+          <div className="thought-wrapper">
+            <p className="happy-thought">{thought.message}</p>
+            <div className="heart-container">
+              <HeartButton
+                id={thought._id}
+                onHeartClicked={onHeartClicked}
+                hearts={thought.hearts} />
+              <p className="likes-counter">x {thought.hearts}</p>
+            </div>
+            <div className="timestamp">
+              <p className="created">{thought.createdBy}</p>
+              <p className="created">{moment(thought.createdAt).fromNow()}</p>
+            </div>
           </div>
-          <div className="timestamp">
-            <p className="created">{thought.createdBy}</p>
-            <p className="created">{moment(thought.createdAt).fromNow()}</p>
+          <div className="comments-container">
+            <CommentForm />
+            <Comments />
           </div>
         </article>
       ))}
