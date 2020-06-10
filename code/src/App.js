bf921dafb38be66cf63c36f2d0a33ac38f7713d3
ebbabw <ebbabergholtz@gmail.com>
2020-06-10 20:38:35 +0200

    chnaged code

--- code/src/App.js
+++ code/src/App.js
@@ -12,41 +12,56 @@ export const App = () => {
 
   useEffect(() => {
     fetch(url)
-    .then(res => res.json())
-    .then(json => setThoughts(json))
-  },  [postedMessage])
+      .then(res => res.json())
+      .then(json => setThoughts(json))
+  }, [postedMessage])
 
-  const handleFromSubmit = message => {
-    setPostedMessage(message)
+  const handleFormSubmit = (message) => {
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({ message: message }),
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(() => setPostedMessage(message))
   }
-  
 
-const onLiked = (id) => {
-  const end = `${url}/${id}/like`
-
-
-fetch(end, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-})
-  .then((heartMessage) => {
-    const updatedThoughts = [...thoughts]
-    const index = thoughts.findIndex(thought => thought._id === id)
-    updatedThoughts.splice(index, 1, heartMessage)
-    setPostedMessage(setPostedMessage)
-
-  })
+  const onLiked = (likedThoughtId) => {
+    const updatedThoughts = thoughts.map((thought) => {
+      if (thought._id === likedThoughtId) {
+        thought.hearts += 1
+      }
+      return thought
+    })
+    setThoughts(updatedThoughts)
+  }
 
 
-}
+// const onLiked = (id) => {
+//   const end = `${url}/${id}/like`
+
+
+// fetch(end, {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+// })
+//   .then((heartMessage) => {
+//     const updatedThoughts = [...thoughts]
+//     const index = thoughts.findIndex(thought => thought._id === id)
+//     updatedThoughts.splice(index, 1, heartMessage)
+//     setPostedMessage(setPostedMessage)
+
+//   })
+
+
+// }
 
 return (
     <main>
 
 
-      <HappyForm handleFromSubmit={handleFromSubmit}/>
+      <HappyForm handleFormSubmit={handleFormSubmit}/>
 
       {thoughts.map(thought => (
 
