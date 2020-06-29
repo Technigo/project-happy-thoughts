75df819edec12b0c4e4c61a2c9cdd986aa91b65a
Emma Prené <emma.prene@gmail.com>
2020-06-29 20:54:04 +0200

    added google analytics maybe

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { NewThought } from './components/NewThought.js'
 import { Thoughts } from './components/Thoughts.js'
+import ReactGA from 'react-ga'
 import './app.css'
 
 export const App = () => {
+
+  ReactGA.initialize('UA-171137939-1')
 
   return (
     <div className='app-container'>
