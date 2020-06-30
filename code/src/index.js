6585864469f2c10acaaedd714ff5bc3382c60916
Jakob Lindblad <jakob.lindblad@servicewell.se>
2020-06-30 10:11:57 +0200

    ga

--- code/src/index.js
+++ code/src/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ReactGA from 'react-ga'
 import './index.css'
 import { App } from './App'
-
+import TagManager from 'react-gtm-module'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+ReactGA.initialize('UA-171137939-1')
+
+const tagManagerArgs = {
+    gtmId: 'GTM-MVWQQ72'
+}
+
+TagManager.initialize(tagManagerArgs)
