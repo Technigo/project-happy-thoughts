1f68eb7f92b14f45996e2f5c743793839810f7e1
Jakob Lindblad <jakob.lindblad@servicewell.se>
2020-07-01 08:52:54 +0200

    added gtm

--- code/src/App.js
+++ code/src/App.js
@@ -2,10 +2,16 @@ import React from 'react'
 import { NewThought } from './components/NewThought.js'
 import { Thoughts } from './components/Thoughts.js'
 import ReactGA from 'react-ga'
+import TagManager from 'react-gtm-module'
 import './app.css'
 
 export const App = () => {
+  const tagManagerArgs = {
+    gtmId: 'GTM-MVWQQ72'
+}
 
+
+TagManager.initialize(tagManagerArgs)
   ReactGA.initialize('UA-171137939-1')
 
   ReactGA.pageview('/homepage');
