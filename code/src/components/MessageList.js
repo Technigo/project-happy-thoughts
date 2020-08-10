42e2a3462a5bc6da07994a2682ed710974b8f9df
Lisas Sturegard <lisasturegard@hotmail.com>
2020-08-10 11:56:30 +0200

    Added Loader and a bit of code cleanup

--- code/src/components/MessageList.js
+++ code/src/components/MessageList.js
@@ -2,27 +2,31 @@ import React, { useState, useEffect } from 'react'
 import moment from 'moment'
 import './messageList.css'
 import { LikeHearts } from './LikeHearts'
+import { Loader } from './Loader'
 
 export const MessageList = () => {
   const MESSAGES_URL = 'https://happy-thoughts-week19.herokuapp.com/thoughts'
   const [messages, setMessages] = useState([])
+  const [isLoading, setIsloading] = useState(true)
 
   useEffect(() => {
     fetch(MESSAGES_URL)
-      .then((res) => {
+      .then(res => {
         return res.json()
       })
       .then(data => {
         setMessages(data)
+        setIsloading(false)
       })
   }, [])
 
   return (
-    <div className="messages-list">
+    <div className='messages-list'>
+      {isLoading && <Loader />}
       {messages.map(message => (
-        <section key={message._id} className="messages">
+        <section key={message._id} className='messages'>
           <h4> {message.message}</h4>
-          <div className="likes-and-when">
+          <div className='likes-and-when'>
             <LikeHearts message={message} />
             <h5>{moment(message.createdAt).fromNow()}</h5>
           </div>
