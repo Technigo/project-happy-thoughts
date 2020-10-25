1f0e04de77b8d059f9b0d4fbe3092f38f0179593
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-10-25 15:23:39 +0100

    Initial commit: getting messages showing in browser

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { ThoughtsCard } from 'components/ThoughtsCard';
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts')
+      .then(response => response.json())
+      .then(json => setThoughts(json))
+  });
+
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <>
+      {thoughts.map((thought) => (
+        <ThoughtsCard key={thought.id} message={thought.message} />
+      ))}
+    </>
   )
-}
+};
---
+++ code/src/components/ThoughtsCard.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const ThoughtsCard = ({key, message}) => {
+    return (
+        <p key={key}>{message}</p>
+    )
+};
