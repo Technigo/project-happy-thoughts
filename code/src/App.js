cfb9aaab7f013dd15e24da638a2192eb2a4e2797
Katsiez <katarinajovanovic.a@gmail.com>
2020-10-26 21:57:22 +0100

    preparing the ground to add the heart like button, working on app.js

--- code/src/App.js
+++ code/src/App.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { HappyForm } from "./components/HappyForm"
+import { HappyThoughts } from "./components/HappyThoughts"
+
+const url = "https://happy-thoughts-technigo.herokuapp.com/thoughts"
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState([])
+  const [postedMessage, setPostedMessage] = useState("")
   return (
     <div>
       <HappyForm />
---
+++ code/src/components/HappyThoughts.js
@@ -0,0 +1,32 @@
+import React from "react"
+import moment from "moment"
+// import "./happyThoughts.css"
+
+export const happyThoughts = props => {
+    const { message, hearts, createdAt, _id } = props.thought
+
+    const handleClick = () => {
+        fetch(`https://happy-thoughts-technigo.herokuapp.com/${_id}like`, {
+            method: "POST",
+            body: "",
+            headers: { "Content-Type": "application/json" }
+        }).then(() => props.onLiked(_id))
+    }
+
+    return (
+        <article className="happy-thought">
+            <h3>{message}</h3>
+            <p>
+                <button
+                    onClick={handleClick}
+                    style={{ background: hearts > 0 ? "#ffadd" : "#f3f1f1" }}>
+                    <span role="img" aria-label="heart">
+                        {"💖 "}
+                    </span>
+                </button>
+                x {hearts}
+            </p>
+            <p>{moment(createdAt).fromNow()}</p>
+        </article>
+    )
+}
