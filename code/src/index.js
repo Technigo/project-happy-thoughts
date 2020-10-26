902455f95a7a528b98c73caf7552d72dbe0d0aca
Katsiez <katarinajovanovic.a@gmail.com>
2020-10-26 20:16:42 +0100

    installed javascript time ago package to help display messages in respect to time

--- code/src/index.js
+++ code/src/index.js
@@ -2,5 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import { App } from './App'
+import JavascriptTimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
+
+JavascriptTimeAgo.locale(en)
+// Importing react time ago package to be able to display messages in order of posting 
 
 ReactDOM.render(<App />, document.getElementById('root'))
