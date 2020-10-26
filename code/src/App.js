f27fb9d8a48714aa7afb13a3e8e94154fa88d20b
caroline birgersson <caroline@birgerochblom.se>
2020-10-26 21:57:57 +0100

    added a form with label and input

--- code/src/App.js
+++ code/src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import "./index.css"
 import { ThoughtsCard } from "./components/js/ThoughtsCard"
+import { ThoughtsStatus } from "./components/js/ThoughtsStatus"
 
 
 export const App = () => {
 
   return (
     <div className="card">
+      <ThoughtsStatus />
       <ThoughtsCard />
     </div>
   )
