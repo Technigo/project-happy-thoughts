bccff8d0a793d52bd7c0513f75717692f927d576
nathalie <nathalie-nordstrom@hotmail.com>
2020-10-26 18:47:19 +0100

    started to fect

---
+++ code/src/components/Messages.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+
+
+
+export const Messages = () => {
+    const MESSAGES_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts';
+    const [messages, setMessages] = useState([]);
+  
+    useEffect(() => {
+
+    
+    fetch(MESSAGES_URL)
+    .then((res) => {
+        return res.json();
+    } )
+    .then((data) => {
+        console.log(data);
+        setMessages(data)
+    });
+},[]);
+    return <div>{messages.map((message) => message.text)}</div>;
+}; 
