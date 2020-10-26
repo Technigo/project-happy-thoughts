e02c7bf733871cd2367d3b4b29c2cbe66f392012
idsintehittapa <jblom15@gmail.com>
2020-10-26 20:58:52 +0100

    Added fetch for posting message

--- code/src/Components/HappyForm.js
+++ code/src/Components/HappyForm.js
@@ -1,28 +1,44 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
+import './HappyForm.css'
 
-const url = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
+export const HappyForm = () => {
+	const url = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
+	const [message, setMessage] = useState('')
 
-export const HappyForm= props => {
-  const [message, setMessage] = useState('')
-  
-  const handleFormSubmit = event => {
-    event.preventDefault()
-    fetch(url, {
-      method: 'POST',
-      body: JSON.stringify({message}), 
-    })
-    .then(() => {
-      setMessage('')
-      props.onFormSubmitt(message)
-    })
-    return (
-      <form>
-        <h3>Post a happy thought</h3>
-        <textarea
-        rows='3'
-        value='message'
-        onChange={event => setMessage(event.target.value)}>
-        </textarea>
-      </form>
-    )
-  }}
+	// a submit function witch POSTs the text input
+	const handleSubmit = event => {
+		event.preventDefault();
+
+		//send a POST request
+		fetch(url, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ message }),
+		})
+			.then(() => {
+				setMessage('')
+				window.location.reload();
+			})
+			.catch(error => console.log('error', error));
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+				<h1>What's making you happy right now?</h1>
+				<textarea
+        className="post-message"
+          rows="4"
+          value={message}
+					onChange={event => setMessage(event.target.value)}
+				/>
+			<div className="post-message-wrapper">
+				<button
+					type="submit"
+					disabled={message.length < 5 || message.length > 140 ? true : false}>
+            Send halp
+          </button>
+          <p>{message.length}/140 </p>
+			</div>
+		</form>
+	);
+};
