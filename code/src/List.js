b8067081b5157d1a0621fe29795e17cad6567dc3
Moa <63230455+mhammarback@users.noreply.github.com>
2020-10-26 17:34:40 +0100

    install and import momentjs, added message time to List

--- code/src/List.js
+++ code/src/List.js
@@ -1,24 +1,26 @@
 import React, { useState, useEffect } from "react";
+import moment from "moment";
 
 
 const List = () => { 
     const MESSAGES_URL ='https://happy-thoughts-technigo.herokuapp.com/thoughts';
     const [messages , setMessages] = useState ([]);
     useEffect(() => {
+        // Ask the server for the messages using a GET requests
+        fetch(MESSAGES_URL)
+            .then((res) => {
+                // Get the JSON of the response body
+                return res.json()
+            })
+            .then(data => {
+                // Set the state based on the response
+                setMessages(data.reverse());
 
-        fetch (MESSAGES_URL)
-        .then(res => res.json())
-        .then(json => setMessages(json))
-        }, [])
-        //.then ((data) => {
-      // console.log(data);
-      //  data.reverse();  
-        
-    //const filteredMessages = data.filter(message => message.message );
+                const filteredMessages = data.filter(message => message.message );
 
-    // setMessages(filteredMessages);
-   // });
-//}, []);  
+                setMessages(filteredMessages);
+            });
+    }, []);  
 
     return(
         <section className ="list">
@@ -27,7 +29,11 @@ const List = () => {
                     <>
                 <p className="previous-messages" key={message._id}>
                     {message.message}
+                    <span className ="message-time">
+                    {moment(message.created).fromNow()}
+                    </span>
                 </p>
+                
                 </>
                 );
             })};
