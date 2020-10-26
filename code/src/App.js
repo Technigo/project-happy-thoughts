8726d5a772688b7795e40bf6f76f3881c5be3f74
Karolin Albinsson <karolin.albinsson@gmail.com>
2020-10-26 16:29:39 +0100

    initial commit

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,100 @@
-import React from 'react'
-
+import { LikeCounter } from "LikeCounter";
+import { Loader } from "Loader";
+import React, { useState, useEffect } from "react";
+import { Thought } from "Thought";
+//import { Detail } from "./Detail";
+import { Form } from "./Form"
+/*TO DO
+  - Fixa datumformatet moment.js -> efter att föreläsningen är uppe
+  - CSS -> halvklart
+   klart - En counter som räknar hur många gånger man tryckt på like-knappen
+ klart  - En reset som resettar formuläret och count på validinput
+*/ 
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
+  const [thoughts, setThoughts] = useState([]);
+  const [newUserMessage, setNewUserMessage] = useState("");
+  const [messageOK, setMessageOK] = useState(true);
+  const [likeCount, increaseLikeCount] = useState(0);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    fetch("https://happy-thoughts-technigo.herokuapp.com/thoughts")
+    .then((res) => res.json())
+      //set the result from api-call in state variable.
+     .then((data) => {
+       setThoughts(data);
+       setLoading(false);
+      
+      });
+  }, []);
+
+  const handleLikes = () => {
+    console.log("In app, handle likes");
+    increaseLikeCount((likeCount) => likeCount+1);
+  }
+  const handleSubmit = (newMessage) =>{
+    setNewUserMessage(newMessage);
+    postThought(newMessage);
+  }
+
+  const postThought = (message) => {
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    const messageToPost = JSON.stringify({"message": message});
+    
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: messageToPost,
+      redirect: 'follow'
+    };
+    
+    fetch("https://happy-thoughts-technigo.herokuapp.com/thoughts", requestOptions)
+    .then((res) => 
+      res.json()
+    )
+    .then((newThought) => { 
+       console.log(newThought);
+       if(newThought.errors !== undefined)
+       {
+          setMessageOK(false);
+          console.log(newThought.message);
+
+       } else {
+        setMessageOK(true);
+       
+        setThoughts((previousThoughts) => [newThought, ...previousThoughts]) }
+        }
+    
+    )
+    
+}
+  
+return(
+<section className="app-container">
+{likeCount > 0 && <LikeCounter numberOfLikes={likeCount}/>}
+  <Form 
+      submitForm = {handleSubmit}
+  />
+
+  {!messageOK && <p>Couldn't save message, check the length (5-140) and try again.</p>}
+  {loading && <Loader />}
+  {!loading &&
+  <section className="thoughts-list">
+      {thoughts.map((thought) => (
+        <Thought 
+         key= {thought._id}
+          id = {thought._id}
+          message = {thought.message}
+          createdAt = {thought.createdAt}
+          hearts = {thought.hearts}
+          onLike = {handleLikes}
+        />
+        ))}
+  </section>}
+   
+</section>
+)
 }
---
+++ code/src/Thought.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+
+export const Thought = (props) => {
+
+  const[liked, setLiked] = useState(false);
+  const[numLikes, setNumLikes] = useState(props.hearts);
+
+
+
+  const handleLikeCount = (numLikes) =>{
+    setNumLikes(numLikes);
+    props.onLike();
+  }
+
+  const clickedLikeButton = (thoughtID) => {
+    console.log("in clicklikebutton, Liked thought:" + thoughtID);
+    //The user can only like a message once
+    setLiked(true);
+    //postLike(thoughtID);
+  }
+
+  const postLike = (thoughtID) =>{
+    
+    console.log("in post like, sending to api")
+    console.log(thoughtID);
+    var requestOptions = {
+      method: 'POST',
+      redirect: 'follow'
+    };
+    
+    fetch(`https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtID}/like`, requestOptions)
+    .then((res) => res.json())
+    //set the result from api-call in state variable.
+    .then((data) => {
+     console.log("Number of likes after like: ",data.hearts)
+     handleLikeCount(data.hearts)
+    });
+  }
+  useEffect(() => {
+    console.log("Thought Change!");
+    //here goes the api call, when state is changed to liked = true. Need to check if like === true
+    //otherwise the useeffect runs anyway. 
+    if(liked) postLike(props.id);
+    // eslint-disable-next-line
+  },[liked]); 
+
+  return(
+  <div className="thought-card">
+    <p className="message">{props.message}</p>
+    <div className="bottom-container">
+   
+    {!liked && <div>
+      <button className="like-button" onClick=
+      {() => clickedLikeButton(props.id)}
+        ><span role="img" aria-label="heart emoji">
+          ❤️</span>
+          </button>
+      <span className="number-of-initial-likes">{numLikes}</span>
+    </div>}
+    {liked && 
+    <div>
+      <button disabled className="disabled-button"
+        ><span role="img" aria-label="heart emoji">
+        ❤️</span></button>
+      <span className="number-of-likes">{numLikes}</span>
+    </div>}
+    <p className="date">
+    <p>{moment(props.createdAt).fromNow()}</p>
+    </p></div>
+  </div>
+  )
+}
