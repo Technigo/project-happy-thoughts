35922ac5abb1a5719b22eb125c3a0fc0fb87ee83
idsintehittapa <jblom15@gmail.com>
2020-10-27 16:36:28 +0100

    Added change of color

--- code/src/Components/LikedMessage.js
+++ code/src/Components/LikedMessage.js
@@ -3,30 +3,25 @@ import img from './media/pixel_heart.png'
 import './LikedMessage.css'
 
 export const LikedMessage = ({ hearts, id }) => {
-	const [likes, setLikes] = useState(hearts)
+  const [likes, setLikes] = useState(hearts)
 
   const handleLikes = () => {
     fetch(`https://happy-thoughts-technigo.herokuapp.com/thoughts/${id}/like`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: '',
-    })
-    setLikes(likes + 1)
-  }
+    }).then(() => {setLikes(likes + 1)
+    window.location.reload()} /// this needs to be fixed - look at the tips 
+    )}
   return (
-		<div className='heart-lies'>
+    <>
 			<button
-      className='like-buttons'
+      className={hearts > 0 ? 'liked' : 'not-liked'}
 				type='button'
         onClick={handleLikes}
-        style={{background: hearts > 0 ? '#ffadad' : '#f3f1f1'}}
         >
-					{/* <span className='liked-heartemoji' role='img' aria-label='Heart'>
-						{'❤️' }
-					</span> */}
         <img src={img} alt='Heart'></img>
       </button>
-      <p>x {likes}</p>
-		</div>
+      <p className='times-liked'>x {likes}</p>
+      </>
 	);
 };
