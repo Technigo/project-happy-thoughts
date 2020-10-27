2daeec480b84f47d57f652f9af5ed11de8e47b83
ClaireCaudwell <clairecaudwell@gmail.com>
2020-10-27 14:57:35 +0100

    Updated ThoughtsMessage.js: 1. Imported moment to convert the createdAt object property. 2. Imported the css file for this component. 3. Made the div an article as it will be more semantically correct. 4. Created a h3 for the message. 5. Created a button for the heart, a span for the heart icon to be placed in, a style comparison for the heart button for before and after it's clicked where the background colour will change depending which part of the conditional is true and created an area for the x and then used the hearts object property name from the array element to show how many times it had been liked. 6. Wrapper the time stamp, createdAt, in a p tag.

--- code/src/Components/ThoughtsMessage.js
+++ code/src/Components/ThoughtsMessage.js
@@ -1,12 +1,26 @@
 import React, { useState } from 'react'
+import moment from 'moment'
+
+import './ThoughtsMessage.css'
 
 export const ThoughtsMessage = ({messageDetails}) => {
 
    return (
-       <p>
-            {messageDetails.message}
-            <span>{messageDetails.hearts}</span>
-            <span>{messageDetails.createdAt}</span>
-       </p>
-   )
-}
+    <article className="message-container">
+        <h3>{messageDetails.message}</h3>
+        <p>
+            <button className="hearts-button"
+            onClick={handleClick}
+            style={{background: messageDetails.hearts > 0 ? "#fff" : "#000" }} >
+                <span role="img" aria-label="Heart">
+                    {"❤"}                
+                </span>
+            </button>
+            x {messageDetails.hearts}
+        </p>
+        <p className="message-time">
+            {moment(messageDetails.createdAt).fromNow()}
+        </p>
+    </article>
+   );
+};
