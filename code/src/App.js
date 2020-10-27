2766ab188ee10a709d79fa0747c82cee1b90ff2c
nathalie <nathalie-nordstrom@hotmail.com>
2020-10-27 14:52:23 +0100

    POST

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Messages } from './components/Messages';
-
+import { Thoughts } from './components/Thoughts';
 
 export const App = () => {
   return (
     <div>
+     
+     
+      <Thoughts />
       <Messages />
-    
     </div>
   )
 }
---
+++ code/src/components/Thoughts.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+export const Thoughts = () => {
+    const [thought, setThought] = useState("");
+    
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts',
+            {
+                method:"POST",
+                headers: {
+                    'Content-Type':'application/json'
+                },
+                body: JSON.stringify({message: thought})
+            }
+        ).then(() => {
+         window.location.reload();
+        })
+    };
+    return (
+        <div className="thought-container">
+            <div className="thoughts">
+        <form onSubmit={handleSubmit}>
+            <div className="rubrik">What´s making you happy right now?</div>
+            <input
+                type="text"
+                className="form-text"
+                onChange={event => setThought(event.target.value)}>
+                </input><br></br>
+                <input
+                type="submit"
+                className="form-button"
+                value="❤️ Send Happy Thought ❤️">
+                </input>
+        </form></div></div>
+    )
+}
