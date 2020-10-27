3aae53597b62545968b3f6bc747a961a18f29348
Karan Mann <62376840+karanmann@users.noreply.github.com>
2020-10-27 22:32:46 +0100

    Indentation and Linting error fixed, hearts and time moved to the same line

--- code/src/components/InputCard.js
+++ code/src/components/InputCard.js
@@ -3,34 +3,34 @@ import React, { useState } from 'react'
 const FETCH_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
 
 export const InputCard = () => {
-    const [newThought, setNewThought] = useState('')
+	const [newThought, setNewThought] = useState('')
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+	const handleSubmit = (event) => {
+		event.preventDefault();
 
-        fetch(FETCH_URL, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ message: newThought })
-        })
-            .then(() => {
-                window.location.reload();
-            });
-    };
+		fetch(FETCH_URL, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ message: newThought })
+		})
+			.then(() => {
+				window.location.reload();
+			});
+	};
 
-    return (
-        <form onSubmit={handleSubmit} className="input-card">
-            <p className="input-card-title">What's making you happy right now?</p>
-            <textarea
-                type="text"
-                maxLength="160"
-                onChange={(event) => setNewThought(event.target.value)} />
-            <p className="text-counter">{160 - newThought.length} x characters left</p>
-            <button className="input-card-button" type="submit">
-                <span role="img" aria-label="heart">&#128151; </span>
-                 Send Happy Thoughts
+	return (
+		<form onSubmit={handleSubmit} className="input-card">
+			<p className="input-card-title">What's making you happy right now?</p>
+			<textarea
+				type="text"
+				maxLength="160"
+				onChange={(event) => setNewThought(event.target.value)} />
+			<p className="text-counter">{160 - newThought.length} x characters left</p>
+			<button className="input-card-button" type="submit">
+				<span role="img" aria-label="heart">&#128151; </span>
+        Send Happy Thoughts
         <span role="img" aria-label="heart"> &#128151;</span>
-            </button>
-        </form>
-    )
+			</button>
+		</form>
+	)
 };
--- code/src/components/MessageList.js
+++ code/src/components/MessageList.js
@@ -2,34 +2,34 @@ import moment from 'moment';
 import React, { useState, useEffect } from 'react'
 
 export const MessageList = () => {
-    const FETCH_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
-    const [messages, setMessages] = useState([])
+  const FETCH_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
+  const [messages, setMessages] = useState([])
 
-    useEffect(() => {
-        fetch(FETCH_URL)
-            .then((response) => {
-                return response.json()
-            })
-            .then((data) => {
-                //console.log(data)
-                const filteredData = data.filter((getMessage) => getMessage.message)
-                setMessages(filteredData.slice(0, 20))
-            })
-    }, [])
+  useEffect(() => {
+    fetch(FETCH_URL)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        //console.log(data)
+        const filteredData = data.filter((getMessage) => getMessage.message)
+        setMessages(filteredData.slice(0, 20))
+      })
+  }, [])
 
-    return (
-        <div>
-            {messages.map((getMessages) => {
-                return (
-                    <div className="message-card" key={getMessages._id}>
-                        <p className="message-text">{getMessages.message}</p>
-                        <div>
-                            <p><span role="img" aria-label="Heart Icon">&#128151;  </span>x<span>  {getMessages.hearts}</span></p>
-                            <p className="message-time">{moment(getMessages.createdAt).fromNow()}</p>
-                        </div>
-                    </div>
-                )
-            })}
-        </div>
-    )
+  return (
+    <div>
+      {messages.map((getMessages) => {
+        return (
+          <div className="message-card" key={getMessages._id}>
+            <p className="message-text">{getMessages.message}</p>
+            <div className="likes-time-container">
+              <p className="likes"><span role="img" aria-label="Heart Icon">&#128151;  </span>x<span>  {getMessages.hearts}</span></p>
+              <p>{moment(getMessages.createdAt).fromNow()}</p>
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  );
 }
