2b27aa6f3f44d67723929f9368c9db742d5ae74d
AxelHammarback <axel.hammarback@gmail.com>
2020-10-27 22:59:44 +0100

    Added a message box. No idea if it's the right method, but we'll try to make it work.

--- code/src/components/MessageList.js
+++ code/src/components/MessageList.js
@@ -8,7 +8,8 @@ import ic_like_1 from '../assets/ic_like_1.svg';
 export const MessageList = () => {
   // Default method for fetch is GET.
   // 4. All code inside the component is executed again. 
-  const MESSAGES_URL = "https://wk11livesession.herokuapp.com/messages";
+  // const MESSAGES_URL = "https://wk11livesession.herokuapp.com/messages";
+  const MESSAGES_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
   const [messages, setMessages] = useState([]);
 
   // 5. useEffect is another hook. Helps us with life cycle. Allows us to execute code based on something that's been updated. Primary usage is that we only want this to execute the first time the component is rendered – not everytime the state updates. 
@@ -20,12 +21,12 @@ export const MessageList = () => {
         return res.json();
       })
       .then((data) => {
-        // 12. Reverses the array.
-        data.reverse()
+        // 12. Reverses the array. Not needed for the project.
+        // data.reverse()
         // data.sort((a,b) => a.created > b.created)
         // Filter if message contains some non-null value for "text". "IF THIS EXISTS".
-        const filteredMessages = data.filter((message) => message.text);
-        const limitedMessages = filteredMessages.slice(0,20);
+        const filteredMessages = data.filter((message) => message.message);
+        const limitedMessages = filteredMessages.slice(0, 20);
         // 
         // 3. Here is where we have access to the server response. ONLY HERE.
         // 2. Save this data variable, and then we want the component to re-render.
@@ -46,17 +47,17 @@ export const MessageList = () => {
     <>{messages.map((message) => {
       return (
         <div className="card" key={message._id}>
-          <p>{message.text}</p>
+          <p>{message.message}</p>
 
           <div className="card-bottom">
             <div className="card-bottom-likes">
               <button className="button-like">
                 <img src={ic_like_0} />
               </button>
-              <p className="p-subtle">0</p>
+              <p className="p-subtle">{message.hearts}</p>
             </div>
             <div className="card-bottom-time">
-              <p className="p-subtle">{moment(message.created).fromNow()}</p>
+              <p className="p-subtle">{moment(message.createdAt).fromNow()}</p>
             </div>
           </div>
 
