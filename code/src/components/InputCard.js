cfcc5897ad46cfd4b0b95f4fd376a8d15fd5a491
Karan Mann <62376840+karanmann@users.noreply.github.com>
2020-10-27 21:48:46 +0100

    Fixed indent issue and wrapping issue

--- code/src/components/InputCard.js
+++ code/src/components/InputCard.js
@@ -1,33 +1,36 @@
 import React, { useState } from 'react'
 
+const FETCH_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
+
 export const InputCard = () => {
     const [newThought, setNewThought] = useState('')
-    const FETCH_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
 
-    const handleSubmit = event => {
+    const handleSubmit = (event) => {
         event.preventDefault();
 
-
-        fetch(FETCH_URL,
-            {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ message: newThought })
-            })
+        fetch(FETCH_URL, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ message: newThought })
+        })
             .then(() => {
                 window.location.reload();
             });
     };
 
-
     return (
         <form onSubmit={handleSubmit} className="input-card">
             <p className="input-card-title">What's making you happy right now?</p>
-            <textarea type="text" maxLength="160" onChange={event => setNewThought(event.target.value)}></textarea>
-            <p className="text-counter">{160 - newThought.length} characters left</p>
-            <button className="input-card-button" type="submit">&#128151; Send Happy Thoughts &#128151;</button>
+            <textarea
+                type="text"
+                maxLength="160"
+                onChange={(event) => setNewThought(event.target.value)} />
+            <p className="text-counter">{160 - newThought.length} x characters left</p>
+            <button className="input-card-button" type="submit">
+                <span role="img" aria-label="heart">&#128151; </span>
+                 Send Happy Thoughts
+        <span role="img" aria-label="heart"> &#128151;</span>
+            </button>
         </form>
     )
 };
