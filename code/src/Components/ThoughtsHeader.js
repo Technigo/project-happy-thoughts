328362a4a2bcd8b8a23714891d84afd7d1e16a30
ClaireCaudwell <clairecaudwell@gmail.com>
2020-10-28 15:08:28 +0100

    Created the ThoughtsHeader.js and added a header and some content to it!

---
+++ code/src/Components/ThoughtsHeader.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const ThoughtsHeader = () => {
+
+    return (
+        <header className="message-container header">
+            <h2 className="header-text"> <span role="img" aria-label="heart emoji">❤️ ❤️</span> Happy Thoughts <span role="img" aria-label="heart emoji"> ❤️ ❤️</span></h2>
+        </header>
+    );    
+};
