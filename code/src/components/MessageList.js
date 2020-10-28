018943f6114bf72bafd20aaaaa02a0e1f56e4acd
Joel Harde <joelharde@Joels-MacBook-Pro.local>
2020-10-28 20:53:28 +0100

    started with basic structure

---
+++ code/src/components/MessageList.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import moment from "moment";
+
+export const MessageList = () => {
+  //   const MESSAGES_URL = "https://wk11livesession.herokuapp.com/messages";
+  //   const [messages, setMessages] = useState([]);
+  //   //Possibility to create function, extract it and import it to the useEffect//
+  //   useEffect(() => {
+  //     fetch(MESSAGES_URL)
+  //       .then((res) => {
+  //         return res.json();
+  //       })
+  //       .then((data) => {
+  //         console.log(data);
+  //         //Reverses the messages, newest on top
+  //         data.reverse();
+  //         //Another method to reverse messages
+
+  //         //data.sort((a, b) => a.created > b.created);
+
+  //         //Function to sort out the blank messages
+  //         const filteredMessages = data.filter((message) => message.text);
+  //         //Save the data to state
+  //         setMessages(filteredMessages.slice(0, 10));
+  //       });
+  //   }, []);
+
+  return (
+    <div>
+      {messages.map((message) => {
+        return (
+          <p className="message" key={message._id}>
+            {" "}
+            {message.text}
+            <span className="message-time">
+              {moment(message.created).fromNow()}
+            </span>
+          </p>
+        );
+      })}
+    </div>
+  );
+};
