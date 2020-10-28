a301e2ab5a691a52708f86e7e825d8678599a016
Sara Sundblom <sundblomsara@gmail.com>
2020-10-28 10:45:04 +0100

    still looks funny

--- code/src/MessageInput.js
+++ code/src/MessageInput.js
@@ -24,7 +24,7 @@ export const MessageInput = () => {
 
     return (
         <form onSubmit={handleSubmit}>
-            <h3> Share a happy thought <span className='heart' role='img' aria-label='heart'>♥</span> </h3>
+            <h3> Share a happy thought <span className='heart-one' role='img' aria-label='heart'>♥</span> </h3>
             <textarea className="text-input"
                 rows='5'
                 value={message}
