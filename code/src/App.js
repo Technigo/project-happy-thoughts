d6bb5f2825863cbda35c62ac146c95d86eabc3c9
ClaireCaudwell <clairecaudwell@gmail.com>
2020-10-28 16:47:50 +0100

    Updated App.js: 1. Updated apiUrl varaible name to apiUrlMessages to signify that this is the url used to both access the messages and post messages to. 2. Created a fetchMessages function which does the fetch to the API to collect the api data which is then stored in the messages state by way of the setMessages function. Also added a catch error to help catch any errors that may arise when the fetch happens. 3. The fetchMessages function iis written instead inside the useEffect function. Right now I'm still trying to understand why we need to do this. 4. Then created the postSingleMessage function. Inside of this function we're doing a post to the api, this is then triggered in the ThoughtsForm component handlSubmit. So when the user submits the form the message that's stored in the newMessages variable (see ThoughtsForm.js) will then be passed back to the App.js. 5. After this post request is done then this new message is sent to the fetchMessages and then the fetchMessages function is re-called, updating the array of fetched messages. Or at least that's how I understand it to work. 6. In the ThoughtsForm jsx tag I added the postSingleMessage into the onMessageChange prop to be used as a way for the post request to access the newMessage submitted in the onSubmit function in the ThoughtsForm.js.

--- code/src/App.js
+++ code/src/App.js
@@ -7,23 +7,49 @@ import { ThoughtsForm } from './Components/ThoughtsForm.js'
 
 export const App = () => {
 
-  const apiUrl = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
+  const apiUrlMessages = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
   const [messages, setMessages] = useState([]);
 
-/* Fetching data from the Happy thoughts API
-Passing the data, an array of 20 object elements, to the setMessages function which in turn passes it tp the messages state variable 
-to the messages */
-/*What is the useEffect used for? */
   useEffect(() => {
-    fetch(apiUrl)
+    fetchMessages();    
+  }, []);
+
+  /* Fetching data from the Happy thoughts API
+  Passing the data, an array of 20 object elements, to the setMessages function which in turn passes it to the messages state variable so the data is stored there */
+  const fetchMessages = () => {
+    fetch(apiUrlMessages)
       .then((apiData) => {
         return apiData.json()
       })
         .then((data) => {
           setMessages(data);
-        });
-  }, []);
+        })
+          .catch((error) => {
+            console.error(error);
+          })
+  };  
 
+  /* Created a function, postSingleMessage, which fetches the apiUrl and creates a post request. Then I believe that after this post is done to the api the fetchMessages function is updated with the new posted message */
+  const postSingleMessage = newMessage => {
+    fetch(apiUrlMessages, 
+      {
+          method: "POST",
+          headers: {
+              "content-Type": "application/json"
+          },
+          body: JSON.stringify({ message: newMessage })
+      })
+        .then(() => {
+          fetchMessages();
+        })
+          .then(() => {
+            window.location.reload();
+          })
+          .catch((error) => {
+            console.error(error);
+          })
+  };
+    
   const onLiked = messageId => {
     const updatedMessage = messages.map(thought => {
       if (thought._id === messageId) {
@@ -37,7 +63,8 @@ to the messages */
   return (
   <main>
     <ThoughtsHeader />
-    <ThoughtsForm apiURL={apiUrl} />
+    {/* Sending to the ThoughtsForm component the postSingleMessage function which as I understand it allows for the onChange that changes the neewMessages variable and the onSubmit function in the ThoughtsForm component*/}
+    <ThoughtsForm onMessageChange={postSingleMessage} />
     {/* Mapping through the array data that's in the messages state variable so each array element is returned via the messageDetails argument and prop to the ThoughtsMessage component */}
     {messages.map((messageObject) => (
       <ThoughtsMessage key={messageObject._id} messageDetails={messageObject} onLiked={onLiked}/>
