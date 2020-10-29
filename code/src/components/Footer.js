6ff41760bbba5ce44d333116c9bdeb4e64472721
MindstormingAB <s.elander@mindstorming.se>
2020-10-29 20:39:35 +0100

    Added footer

--- code/src/components/Footer.js
+++ code/src/components/Footer.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer>This is a footer</footer>
+    <footer>
+      <div>
+        <p className="signature">Technigo Bootcamp Fall 2020</p>
+        <p className="signature">&#169; 2020 Sandrine Elander</p>
+      </div>
+      <p className="attribution">
+        Icon made by <a href="https://www.flaticon.com/authors/dinosoftlabs" title="DinosoftLabs">DinosoftLabs</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+      </p>
+    </footer>
   )
 }
 
