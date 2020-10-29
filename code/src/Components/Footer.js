ff11aefdaa8400ce9dabf6ab00a02265c5dc51c8
Gabbi-89 <gabriellabolin@hotmail.com>
2020-10-29 15:25:41 +0100

    Tabindex in footer

--- code/src/Components/Footer.js
+++ code/src/Components/Footer.js
@@ -3,7 +3,7 @@ import 'styles/footer.css';
 
 export const Footer = () => {
   return (<footer className="footer">
-    <p>This is a project made by Gabriella Bolin 2020 @ Technigo Bootcamp</p>
+    <p tabIndex="0">This is a project made by Gabriella Bolin 2020 @ Technigo Bootcamp</p>
     </footer>
   );
 };
