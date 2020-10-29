efcdcc16711ce2e9473e36574719262bb2f8895f
Amanda Flodström <amandaflodstroem@gmail.com>
2020-10-29 11:37:09 +0100

    Fetch data from API and display in browser

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+
+const THOUGHTS_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState()
+
+  useEffect(() => {
+    fetch(THOUGHTS_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        const filteredData = data.filter((thought) => thought.text !== '')
+        filteredData.reverse();
+        setThoughts(filteredData)
+      })
+  }, [])
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <main>
+      {
+        thoughts && thoughts.map((thought) => (
+          <p className="thought">
+            {thought.message}
+            <span className="thought-time">
+              {moment(thought.createdAt).fromNow()}
+            </span>
+          </p>
+        ))
+      }
+    </main>
   )
 }
