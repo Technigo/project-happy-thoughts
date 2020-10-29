908120f4cfebecb9a784b09579d3ec128227bc58
ClaireCaudwell <clairecaudwell@gmail.com>
2020-10-29 14:56:35 +0100

    Updated ThoughtsForm.js & css: 1. Added some notes for my own reference. 2. Wrapped the button and p tag in a span so I can impliment the display flex and have them side by side. 3. Added the comparison for the button. So it'll be disabled if the user writes 6 or less or 140 04 more letters in the textarea. Otherwise the button will be un-disabled allowing them to submit the message. 4. Replaced the 5 with the newMessage.length, so when the user starts to write in the textarea the number will reflect how many letters htey've submitted.

--- code/src/Components/ThoughtsForm.js
+++ code/src/Components/ThoughtsForm.js
@@ -5,6 +5,11 @@ import './ThoughtsForm.css'
 export const ThoughtsForm = ({onMessageChange}) => {
     const [newMessages, setNewMessages] = useState("");
 
+    /*onHandleSubmit handling what happens when the form is submitted. 
+    Sending the inputted data to the postSingleMessage function is App.js via the onMessageChange prop. 
+    
+    Also clearing what the user has inputted in the textarea by setting hte newMessages back to an empty string by way of the setNewMessages function once the form is submitted. This is supported in the textarea value attribute where it's set to the newMessage state*/
+
     const handleSubmit = event => {
         event.preventDefault();
         onMessageChange(newMessages);
@@ -26,7 +31,13 @@ export const ThoughtsForm = ({onMessageChange}) => {
                 value={newMessages}
                 onChange={event => setNewMessages(event.target.value)}
                 />
-                <button className="form-button"><span role="img" aria-label="heart emoji">❤️</span> Send Happy Thought<span role="img" aria-label="heart emoji">❤️</span></button>
+                <span className="form-message-footer">
+                    <button className="form-button" disabled={newMessages.length < 6 || newMessages.length > 140 ? true : false}>
+                        {/* Above comparing if the message length is less than 6 or higher than 140 make the button disabled, otherwise undisable it */}
+                        <span role="img" aria-label="heart emoji">❤️</span> Send Happy Thought<span role="img" aria-label="heart emoji"> ❤️</span>
+                    </button>
+                    <p className="letter-counter">{newMessages.length} / 140</p>
+                </span>
             </form>
         </article>
     );
