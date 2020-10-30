e240cd54febf07b69670a109108fd371f7b17248
emeliesv <emeliesv@live.se>
2020-10-30 16:19:37 +0100

    last fixes

--- code/src/components/Thoughts.js
+++ code/src/components/Thoughts.js
@@ -25,11 +25,11 @@ export const Thoughts = ({ thoughts, likeMessage }) => {
                         <p className="thoughts__message">{thought.message}</p>
                     </div>
                     <div className="thoughts__info-container">
-                            <div className="thoughts__like-container">
-                                <Heart styled={'heart__like'} text={'Like'} onClick={() => likeMessage(thought._id)}/>
-                                {thought.hearts > 0 && <p className="thoughts__amount-of-likes">x{thought.hearts}
-                                </p>
-        }</div>
+                        <div className="thoughts__like-container">
+                            <Heart styled={'heart__like'} text={'Like'} onClick={() => likeMessage(thought._id)} />
+                            {thought.hearts > 0 && <p className="thoughts__amount-of-likes">x{thought.hearts}
+                            </p>
+                            }</div>
                         <p className="thoughts__time-posted"> {moment(thought.created).fromNow()}</p>
                     </div>
                 </div>
