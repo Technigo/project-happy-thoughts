cb6ee5114b52e7a40be135e672fcb026c8442e01
nathalie <nathalie-nordstrom@hotmail.com>
2020-10-30 16:29:32 +0100

    done

---
+++ code/src/components/HappyForm.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+
+const url = "https://happy-thoughts-technigo.herokuapp.com/thoughts"
+
+export const HappyForm = props => {
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        fetch(url, {
+                method: "POST",
+                body: JSON.stringify({ message }),
+                headers: { "Content-Type": "application/json" }
+            })
+            .then(() => {
+                setMessage('')
+                props.onFormSubmit(message)
+        })
+        .catch(err => console.log('error:', err))
+    };
+
+    return (
+
+      <form className='happy-form'>
+          <h3>Post a happy thought!</h3>
+          <textarea
+          className="form-text"
+          rows='3'
+          value={message}
+          onChange={event => setMessage(event.target.value)}>
+          </textarea>
+          <div className='form-footer'>
+              <button
+              className="form-button"
+              type='submit'
+              onClick={handleSubmit}
+              disabled={message.length < 6 || message.length > 140 ? true : false}>
+                  <span role='img' aria-label="heart emoji">❤️ Send a happy thought ❤️
+                            </span>
+              </button>
+              <div className="thought-length">
+              <p>{message.length} / 140</p></div>
+          </div>
+      </form>
+    )
+}
---
+++ code/src/components/HappyThought.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import moment from "moment";
+
+
+export const HappyThought = props => {
+    const { message, hearts, createdAt, _id } = props.thought
+
+    const handleClick = () => {
+        fetch(`https://happy-thoughts-technigo.herokuapp.com/thoughts/${_id}/like`, {
+            method: 'POST',
+            body: '',
+            headers: { 'Content-Type': 'application/json' }
+        }).then(() => props.onLiked(_id))
+    }
+
+    return (
+        <article className='happy-thought'>
+            <div className="message">{message}</div>
+            <div className="thought-footer">
+                <p>
+                    <div className="likes">
+                    <button
+                        onClick={handleClick}
+                        type="submit"
+                        className="like"
+                    >
+                        
+                            <span role='img' aria-label="heart emoji">❤️
+                            </span>
+                    </button>
+                    <div className="total-likes">
+                        x {props.thought.hearts}
+                    </div></div>
+                </p>
+                <div className="message-time">
+                <p>{moment(props.thought.createdAt).fromNow()}</p></div>
+            </div>
+        </article>
+    )
+}
