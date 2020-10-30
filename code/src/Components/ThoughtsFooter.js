833b3ad694ef7d1cb18f12ddaa0d5bacf507c67a
ClaireCaudwell <clairecaudwell@gmail.com>
2020-10-30 15:26:03 +0100

    Created a ThoughtsFooter.js and css and gave it some content and styling

---
+++ code/src/Components/ThoughtsFooter.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './ThoughtsFooter.css'
+
+export const ThoughtsFooter = () => {
+
+    return (
+        <footer className="message-container footer">
+            <span role="img" aria-label="heart emoji">💌</span>
+            <h3>Happy thoughts project</h3>
+            <p>Created by Claire Caudwell for the Technigo Bootcamp 2020</p>
+        </footer>
+    );    
+};
