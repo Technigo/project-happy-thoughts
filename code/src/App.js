fd6d54efb6793945eebb647f69ea3fbf3cc216e4
nathalie <nathalie-nordstrom@hotmail.com>
2020-10-30 12:02:52 +0100

    a

--- code/src/App.js
+++ code/src/App.js
@@ -3,10 +3,23 @@ import { Messages } from './components/Messages';
 import { Thoughts } from './components/Thoughts';
 
 export const App = () => {
+
+  const onLiked = thoughtId => {
+    console.log("Logging in the APP.js", thoughtId)
+  }
+
+  const updatedThoughts = thought.map(thought => {
+    if (thought._id === thoguhtId) {
+      thought.hearts += 1
+    }
+    return thought
+  })
+  setThoughts(updatedThoughts)
+
   return (
     <div> 
       <Thoughts />
-      <Messages />
+      <Messages key={thought._id} thought={thought} onLiked={onLiked} />
     </div>
   )
 }
--- code/src/components/Messages.js
+++ code/src/components/Messages.js
@@ -5,7 +5,7 @@ import moment from "moment";
 export const Messages = (props) => {
     const MESSAGES_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts';
     const [messages, setMessages] = useState([]);
-   
+   const [clickLike, setClickLike] = useState(0);
     useEffect(() => {
 
     
@@ -17,6 +17,20 @@ export const Messages = (props) => {
         setMessages(data)
     });
 },[]);
+
+
+
+const handleLiked = () => {
+    fetch(` https://happy-thoughts-technigo.herokuapp.com/thoughts/${_id}/like`,{
+        method: 'POST',
+        body:'',
+        headers: { 'Content-Type': 'application/json'}
+    }).then(() => onLiked(_id))
+
+ 
+}
+
+
     return <div className="form-container">{messages.map((message) => {
         return  <div className="message" key={message._id}>
         {message.message} 
@@ -25,13 +39,16 @@ export const Messages = (props) => {
         <span className="message-time">
         {moment(message.createdAt).fromNow()}
         </span>
-        
+        <div className="likes">
+        <p class="total-likes"> x {clickLike}</p>
         <button 
+        onClick={() => handleLiked()}
         className="like"
         type="submit"
-        ><span>❤️</span>
+        ><span role="img" aria-label="heart" >❤️</span>
+        
         </button>
-
+      </div>
         </div>
        </div>
         
