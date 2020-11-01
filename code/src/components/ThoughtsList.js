c6af6053c85de5f475f56af61c7749620eb4a1ef
emma-tech <emma.urman@gmail.com>
2020-11-01 19:35:50 +0100

    Updated path to css-files.

--- code/src/components/ThoughtsList.js
+++ code/src/components/ThoughtsList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import moment from 'moment';
 
-import '../Styles/thoughtslist.css';
+import './Styles/thoughtslist.css';
 
 export const ThoughtsList = ({thought, onLike}) => {
   
