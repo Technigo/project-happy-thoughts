d2e3dda110f47ae4b826e437c9d3b5642fa81a5c
emeliesv <emeliesv@live.se>
2020-11-11 08:11:30 +0100

    changed after second codereview

--- code/src/components/App.js
+++ code/src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { Thoughts } from './Thoughts'
 import { Input } from './Input'
@@ -11,53 +11,53 @@ import './app.css'
 export const App = () => {
   const [thoughts, setThoughts] = useState([]);
 
-    /* We need to use useEffect because we don't want this to rerender on 
-    each refresh, only when it's neccessary and [] is updated.
-    You need to use the empty array to only render on change, 
-    otherwise it will contiue to do the fetch in an infinite loop */
+  /* We need to use useEffect because we don't want this to rerender on 
+  each refresh, only when it's neccessary and [] is updated.
+  You need to use the empty array to only render on change, 
+  otherwise it will contiue to do the fetch in an infinite loop */
   useEffect(() => {
     getMessages();
   }, [])
 
-   const getMessages = () => {
+  const getMessages = () => {
     fetch(THOUGHTS__URL)
-    .then((res) => {
+      .then((res) => {
         return res.json();
-    })
-    .then((data) => {
+      })
+      .then((data) => {
         setThoughts(data);
-    })
-  } 
+      })
+  }
 
   const saveNewMessage = (newMessage) => {
     fetch(THOUGHTS__URL, {
       method: 'POST',
-      headers: { 'content-Type': 'application/json'},
+      headers: { 'content-Type': 'application/json' },
       body: JSON.stringify({ message: newMessage })
     })
-    .then(getMessages)
+      .then(getMessages)
   }
 
-  const likeMessage = (id) => {  
+  const likeMessage = (id) => {
     const LIKE__URL = `https://happy-thoughts-technigo.herokuapp.com/thoughts/${id}/like`
     fetch(LIKE__URL, {
       method: 'POST',
-      headers: { 'content-Type': 'application/json'}
+      headers: { 'content-Type': 'application/json' }
     })
-    .then(getMessages)
+      .then(getMessages)
   }
-  
+
   return (
     <div className="app__grid">
-      <Header/>
+      <Header />
       <Input
-      onMessageChange={saveNewMessage}
+        onMessageChange={saveNewMessage}
       />
       <Thoughts
-      likeMessage={likeMessage}
-      thoughts={thoughts}
+        likeMessage={likeMessage}
+        thoughts={thoughts}
       />
-      <Footer/>
+      <Footer />
     </div>
   )
 }
--- code/src/components/Input.js
+++ code/src/components/Input.js
@@ -13,13 +13,13 @@ export const Input = ({ onMessageChange, inputType }) => {
     /* This function checks if the inputmessage contains less than 5 characters
     or more than 140 characters. If so an errormessage is shown. */
     const checkIfMessageLengthValid = (input) => {
-        if (input.length <5) {
+        if (input.length < 5) {
             setError('Enter a thought longer than 5 characters please!')
             return false
-        } else if (input.length >140) {
+        } else if (input.length > 140) {
             setError('Enter a thought shorter than 140 characters please')
             return false
-        } 
+        }
         setError('')
         return true
     }
@@ -31,19 +31,19 @@ export const Input = ({ onMessageChange, inputType }) => {
 
     return (
         <>
-        <form onSubmit={handleSubmit} className="input__container" id="input__form">
-        <h2>What's making you happy right now?</h2>
-        <label className="input__user-input">
-            <textarea 
-            rows='4'
-            value={newMessage}
-            onChange={event => setNewMessage(event.target.value)}></textarea>
-        </label>
-        <Error message={error}/>
-        <Heart
-        styled={'heart__btn'}
-        text={'Send thought!'}/>
-        </form>
+            <form onSubmit={handleSubmit} className="input__container" id="input__form">
+                <h2>What's making you happy right now?</h2>
+                <label className="input__user-input">
+                    <textarea
+                        rows='4'
+                        value={newMessage}
+                        onChange={event => setNewMessage(event.target.value)}></textarea>
+                </label>
+                <Error message={error} />
+                <Heart
+                    styled={'heart__btn'}
+                    text={'Send thought!'} />
+            </form>
         </>
     )
 }
