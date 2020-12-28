dccd470166d8baf11b8b3302a036d31751e5b3f8
Sara Heijkenskjöld <sara.heijkenskjold@gmail.com>
2020-12-28 14:10:12 +0100

    Added some styling

--- code/src/components/Sort.js
+++ code/src/components/Sort.js
@@ -4,15 +4,28 @@ import Button from './Button';
 
 const Sort = ({ onClick }) => {
   return (
-    <div>
+    <div className="Button__sort-container">
       <Button
+        className="Button__sort"
         button="button"
         onClick={onClick}
         text="Most likes"
         value="hearts"
       />
-      <Button button="button" onClick={onClick} text="Oldest" value="oldest" />
-      <Button button="button" onClick={onClick} text="Newest" value="newest" />
+      <Button
+        className="Button__sort"
+        button="button"
+        onClick={onClick}
+        text="Oldest"
+        value="oldest"
+      />
+      <Button
+        className="Button__sort"
+        button="button"
+        onClick={onClick}
+        text="Newest"
+        value="newest"
+      />
     </div>
   );
 };
