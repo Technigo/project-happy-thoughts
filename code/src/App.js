6d5d93e21f65276fd10cd6db157a191c671531c1
Gabbi-89 <gabriellabolin@hotmail.com>
2021-01-05 12:06:24 +0100

    New paths

--- code/src/App.js
+++ code/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import { ThoughtsList } from 'components/ThoughtsList';
-import { Postthoughts } from 'components/Postthoughts';
-import { Footer } from 'components/Footer';
+import { ThoughtsList } from './components/ThoughtsList';
+import { Postthoughts } from './components/Postthoughts';
+import { Footer } from './components/Footer';
 
 export const App = () => {
 
