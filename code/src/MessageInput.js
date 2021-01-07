461ac836e9aaca31c45ffc843f886c54a12e4986
Sara Sundblom <sundblomsara@gmail.com>
2021-01-07 17:30:37 +0100

    changed the character counter

--- code/src/MessageInput.js
+++ code/src/MessageInput.js
@@ -4,40 +4,42 @@ import './MessageInput.css'
 
 export const MessageInput = () => {
 
-    const [message, setMessage] = useState('')
+  const [message, setMessage] = useState('')
 
-    const handleSubmit = event => {
-        event.preventDefault()
+  const handleSubmit = event => {
+    event.preventDefault()
 
-        fetch(`${BASE_API}/thoughts`,
-            {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ message: message })
-            }
-        ).then(() => {
-            window.location.reload()
-        })
-    }
+    fetch(`${BASE_API}/thoughts`,
+      {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ message: message })
+      }
+    ).then(() => {
+      window.location.reload()
+    })
+  }
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <h3> Share a happy thought <span className='heart-one' role='img' aria-label='heart'>♥</span> </h3>
-            <textarea className="text-input"
-                rows='8'
-                value={message}
-                placeholder='Share something that made you smile today 🙂'
-                onChange={event => setMessage(event.target.value)}>
-            </textarea>
-            <p>{message.length} / 140 </p>
-            <input
-                type="submit"
-                className="button"
-                disabled= {message.length < 5 || message.length > 140}
-                value="Share">
-            </input>
-        </form>
-    )
+  return (
+    <div className='form-container'>
+      <form onSubmit={handleSubmit}>
+        <h3> Share a happy thought <span className='heart-one' role='img' aria-label='heart'>♥</span> </h3>
+        <textarea className="text-input"
+          rows='8'
+          value={message}
+          placeholder='Share something that made you smile today 🙂'
+          onChange={event => setMessage(event.target.value)}>
+        </textarea>
+        <p>{message.length} / 140 </p>
+        <input
+          type="submit"
+          className="button"
+          disabled={message.length < 5 || message.length > 140}
+          value="Share">
+        </input>
+      </form>
+    </div>
+  )
 }
