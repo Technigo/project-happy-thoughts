46e33f2c8c395ded185ba9f6da72b4b59a494aad
LillanEriksson <nailsbylillan@hotmail.com>
2021-01-08 11:36:49 +0100

    Changed the URL for fetching thoughts

--- code/src/urls.js
+++ code/src/urls.js
@@ -1,2 +1,4 @@
-export const THOUGHTS_URL =
-	'https://happy-thoughts-technigo.herokuapp.com/thoughts';
+// export const THOUGHTS_URL =
+// 	'https://happy-thoughts-technigo.herokuapp.com/thoughts';
+
+export const THOUGHTS_URL = 'https://my-happy-api.herokuapp.com/thoughts';
