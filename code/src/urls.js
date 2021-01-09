7dc7c9f6273fc7825982effd9ed4f15e273dd814
Rebkova <rebkova@gmail.com>
2021-01-09 18:32:53 +0100

    added thoughts at the end of URL and some ccs property

--- code/src/urls.js
+++ code/src/urls.js
@@ -2,4 +2,4 @@
 // export const THOUGHTS_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
 
 //my own backend
-export const THOUGHTS_URL = "https://rbk-project-happy-thoughts-api.herokuapp.com/"
+export const THOUGHTS_URL = "https://rbk-project-happy-thoughts-api.herokuapp.com/thoughts"
