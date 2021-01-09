81a590690e3e0d5d0be324e5c05de9e53ae1cf3e
Nadia Mazzoni <Nadia@Nadias-MBP.lan>
2021-01-09 16:12:12 +0100

    updated API

--- code/src/component/Urls.js
+++ code/src/component/Urls.js
@@ -1,3 +1,3 @@
-export const THOUGHTS_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
-export const likeUrlFor = (messageId) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${messageId}/like`;
+export const THOUGHTS_URL = "https://happy-thoughts-nm.herokuapp.com/thoughts";
+export const likeUrlFor = (messageId) => `https://happy-thoughts-nm.herokuapp.com/thoughts/${messageId}/like`;
 // had to create a function to be able to use the template url outside of it´s scope
