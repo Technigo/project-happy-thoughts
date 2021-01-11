edccc7d2a92be999d726375c8212a06b873e44a9
petrame <63057558+petrame@users.noreply.github.com>
2021-01-12 00:18:43 +0100

    change url

--- code/src/urls.js
+++ code/src/urls.js
@@ -1,3 +1,5 @@
 export const MESSAGE_URL =
-  // "https://happy-thoughts-technigo.herokuapp.com/thoughts/";
   "https://happythoughts-api.herokuapp.com/";
+
+  // Change to this URL to retrieve messages from the common database
+  // "https://happy-thoughts-technigo.herokuapp.com/thoughts/";
