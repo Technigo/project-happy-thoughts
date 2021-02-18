17f50278e3991a88e452eea2c873b0c260742435
Amanda Flodström <amandaflodstroem@gmail.com>
2021-02-18 20:30:35 +0100

    Added link to new backend

--- code/src/App.js
+++ code/src/App.js
@@ -3,7 +3,7 @@ import { HappyForm } from 'components/HappyForm'
 import { HappyThought } from 'components/HappyThought'
 import './App.css'
 
-export const THOUGHTS_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
+export const THOUGHTS_URL = 'https://happy-thoughts-amanda.herokuapp.com/thoughts'
 
 export const App = () => {
   return (
