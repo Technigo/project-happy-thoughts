cd862db5b35a8f2422a452ea07db2f5553a85380
JOHANNA REXIN <johanna.rexin@gmail.com>
2021-02-21 21:20:14 +0100

    Added onSubmit to form

--- code/src/components/MessageInput.js
+++ code/src/components/MessageInput.js
@@ -3,11 +3,29 @@ import React, { useState } from 'react'
 import '../styling/MessageInput.css'
 
 export const MessageInput = () => {
+  const THOUGHTS_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts"
+  const [thought, setThought] = useState("")
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    fetch(THOUGHTS_URL,
+      {
+        method: "post",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({message: thought})
+      }
+      )
+  }
+
   return(
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
-        className="form-text">
+        className="form-text"
+        onChange={event => setThought(event.target.value)}>
       </input>
       <input 
         type="submit"
