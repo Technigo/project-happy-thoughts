8abd3a8955eb925b73528763b0e6492bb60da7ae
Irina <jekabsone.irina@gmail.com>
2021-03-22 23:01:59 +0100

    Sending fetch post request, updating thoughtsList with newThought value

--- code/src/App.js
+++ code/src/App.js
@@ -5,7 +5,7 @@ import { FETCH_URL } from './reusable/urls'
 
 export const App = () => {
   // STATES
-  const [thoughts, setThoughts] = useState([])
+  const [thoughtsList, setThoughtsList] = useState([])
   const [isPending, setIsPending] = useState(true)
   const [newThought, setNewThought] = useState('')
 
@@ -15,15 +15,14 @@ export const App = () => {
     fetchThoughtsList()
   }, [])
 
-  console.log(thoughts)
-
   // FUNCTIONS
+
   // Fetching data from an API
   const fetchThoughtsList = () => {
     fetch(FETCH_URL)
     .then (response => response.json())
     .then(data => {
-      setThoughts(data);
+      setThoughtsList(data);
       //Changing the state of isPending to false when the fetching is completed 
       setIsPending(false);
     })
@@ -31,13 +30,33 @@ export const App = () => {
     .catch(err => console.error(err))
   }
 
+  //
   const onNewThoughtChange = event => {
     setNewThought(event.target.value)
   }
 
+  //When the form is submitted save the newThought message on the server by using fetch post request
+  const onFormSubmit = event => {
+    event.preventDefault()
+
+    const postRequest = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ message: newThought })
+    }
+
+    fetch(FETCH_URL, postRequest)
+      .then(response => response.json())
+      .then(receivedThought => setThoughtsList([...thoughtsList, receivedThought]))
+      .catch(error => console.error(error))
+  }
+
   return (
     <main>
-      <form>
+      {/* Form for sending new happy thought message */}
+      <form onSubmit={onFormSubmit}>
         <label htmlFor='newThought'>
           <h1>What's making you happy right now?</h1>
           <input 
@@ -50,19 +69,19 @@ export const App = () => {
           <button type='submit'>Send Happy Thought</button>
         </label>
       </form>
+
       {/* Conditional template that will show a loading message while the data is being fetched */}
       {isPending && <div className='loading-message'>Loading...</div>}
+
       {/* Iterating over the array of thoughts and returning JSX for each thought  */}
-      {thoughts.map(thought => {
+      {thoughtsList.map(thought => {
         return(
           <div key={thought._id}>
             <h2>{thought.message}</h2>
-            <p>-{moment(thought.createdAt).fromNow()}</p>
+            <p className='date'>-{moment(thought.createdAt).fromNow()}</p>
           </div>
         )
-        
       })}
-
     </main>
   )
 }
