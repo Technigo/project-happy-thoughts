2c7e182d99415fea3db71706d3c7e250f942f1a8
[Darcy Winterhill] <[darcy.winterhill@gmail.com]>
2021-03-22 20:51:38 +0100

    started to fetch data

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import moment from 'moment'
+
+import { GET_API, POST_API } from './reusables/urls'
 
 export const App = () => {
+
+  const [messageList, setMessageList] = useState([])
+  const [newMessage, setNewMessage] = useState('')
+
+  useEffect(() => {
+    fetchList()
+  }, []);
+
+  const fetchList = () => {
+      fetch(GET_API)
+        .then(res => res.json())
+        .then(thoughts => setMessageList(thoughts))
+        .catch(error => console.error(error))
+    }
+
+  const onNewMessageChange = (e) => {
+    setNewMessage(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ message: newMessage })
+    };
+
+    fetch(POST_API, options)
+      .then(res => res.json())
+      .then(newThoughts => setMessageList(newThoughts))
+      .catch(error => console.error(error))
+  }
+
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor='newMessage'>New message</label>
+            <input
+              id='newMessage'
+              type='text'
+              value={newMessage}
+              onChange={onNewMessageChange}
+            />
+            <button type='submit'>Post message</button>
+          
+        </form>
+
+
+        {messageList.map(thoughts => (
+          <div key={thoughts._id}>
+            <h4>{thoughts.message}</h4>
+            <p>{moment(thoughts.createdAt).fromNow()}</p>
+            </div>
+        ))}
+      </div>
+      </>
   )
 }
+
---
+++ code/src/reusables/urls.js
@@ -0,0 +1,6 @@
+
+
+export const GET_API = 'https://happy-thoughts-technigo.herokuapp.com/thoughts' 
+export const POST_API = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
+export const LIKE_API = 'https://happy-thoughts-technigo.herokuapp.com/thoughts/THOUGHT_ID/like'
+
