307a24f92edecaaf6c035a1a1b2396776bf06477
Ylva Landoff Lindberg <ylvalandofflindberg@Ylvas-MBP-2.lan>
2021-03-23 21:16:13 +0100

    made the fetch work

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+// import moment from 'moment';
+import { API_URL } from './reusable/urls';
 
 export const App = () => {
+  const [messageList, setMessageList] = useState([]);
+  // const [messageNew, setMessageNew] = useState('')
+
+  useEffect(() => {
+    fetchMessageList();
+  }, []);
+
+  const fetchMessageList = () => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(messages => setMessageList(messages))
+      .catch(err => console.error(err));
+  }
+  
+
+  
+
+  
+  // const onMessageNewChange = (event) =>{
+  //   setMessageNew(event.target.value);
+  // }
+
+  // const onFormSubmit = (event) => {
+  //   event.preventDefault();
+
+  //     const options = {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       },
+  //       body: JSON.stringify({ text: messageNew })
+  //     };
+
+  //     fetch(API_URL, options)
+  //       .then(res => res.json())
+  //       .then(receivedMessage => setMessageList([...messageList, recivedMessage]))
+  //       .catch(err => console.error(err));
+  // }
+
   return (
     <div>
-      Find me in src/app.js!
+     
+        {messageList.map(message => [
+          <div key={message._id}>
+
+         
+            <h3>{message.message}</h3>
+            
+          </div>
+        ])}
+      
     </div>
   )
 }
