2c405647198898be0205ad3a1425f359666b4cc1
sirihemsley <siri.hemsley@gmail.com>
2021-03-23 22:04:43 +0100

    POST and GET for messages work fine, but the POST for hearts is not working as it should yet.

--- code/src/App.js
+++ code/src/App.js
@@ -7,6 +7,8 @@ export const App = () => {
 
   const [messageList, setMessageList] = useState([])
   const [messageNew, setMessageNew] = useState('')
+  const [hearts, setHearts] = useState([])
+  const [error, setError] = useState('')
 
   useEffect(() => {
     fetchMessageList()
@@ -23,6 +25,44 @@ export const App = () => {
       setMessageNew(event.target.value)
   }
 
+  const onHeartClick = (event, id) => {
+    event.preventDefault()
+    fetch(`https://happy-thoughts-technigo.herokuapp.com/thoughts/${id}/like`, {
+        method: 'POST', 
+        headers: {
+          'Content-Type': 'application/json'
+        }})
+    .then(res => res.json())
+    .then(receivedHearts => setHearts([receivedHearts, ...hearts]))
+  }
+
+  // const isValidMessage = () => {
+  //   if (messageNew.length > 4 && messageNew.length < 140) {
+  //     return true
+  //   } else {
+  //     return false
+  //   }
+  // }
+
+  // const onFormSubmit = (event) => {
+  //   event.preventDefault()
+
+  //   if (messageNew.length > 4 && messageNew.length < 140) {
+  //   fetch(API_URL, {
+  //       method: 'POST', 
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       }, 
+  //       body: JSON.stringify({ message: messageNew })
+  //       })
+  //   .then(res => res.json())
+  //   .then(receivedMessage => setMessageList([receivedMessage, ...messageList]))
+  //   .catch(err => console.error(err))
+  //  } else {
+  //    alert("Please insert at least 4 and maximum 140 characters")
+  //  }
+  // }
+
   const onFormSubmit = (event) => {
     event.preventDefault()
 
@@ -34,8 +74,9 @@ export const App = () => {
         body: JSON.stringify({ message: messageNew })
         })
     .then(res => res.json())
-    .then(receivedMessage => setMessageList([...messageList, receivedMessage]))
-   }
+    .then(receivedMessage => setMessageList([receivedMessage, ...messageList]))
+    .catch(err => setError(err, 'Please make sure you write at least 4 characters and maximum 140'))
+  }
 
     return (
       <main className="main">
@@ -48,13 +89,20 @@ export const App = () => {
             value={messageNew}
             onChange={onMessageNewChange}
           />
-          <button className="submit-button" type="submit">❤️ Send Happy Thought ❤️</button>
+          <p>{error}</p>
+          <button className="submit-button" type="submit"><span>&#10084;&#65039;</span> Send Happy Thought <span>&#10084;&#65039;</span></button>
         </form>
 
         {messageList.map(message => (
           <div className="message-container" key={message._id}>
-              <h4>{message.message}</h4>
+              <h4 className="message">{message.message}</h4>
+              <div className="time-hearts-container">
+              <div className="likes-container">
+                <button onClick={(event) => onHeartClick(event, message._id)} className="heart"><span>&#10084;&#65039;</span></button>
+                <p>x {message.hearts}</p>
+              </div>
               <p className="time">- {moment(message.createdAt).fromNow()}</p>
+              </div>
           </div>
         ))}
 
