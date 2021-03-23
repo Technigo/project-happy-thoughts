ccffe6692f8f6a2f108df7ce88e4ba589668d7df
Eleonor Frankenberg <elle@Eleonors-MacBook-Air.local>
2021-03-23 21:23:08 +0100

    made a function to fetch the messages from the api. and made a input element to write messages in, and a function to post the message to the api

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import moment from 'moment'
+
+import { THOUGHTS_URL } from './reusable/urls'
 
 export const App = () => {
+
+  const [messageList, setMessageList] = useState([])
+  const [newMessage, setNewMessage] = useState('')
+  useEffect(() => {
+    fetchMessageList()
+  }, [])
+
+  const fetchMessageList = () => {
+    fetch(THOUGHTS_URL)
+      .then(res => res.json())
+      .then(message => setMessageList(message))
+      .catch(err => console.error(err))
+  }
+
+  const onNewmessageChange = (event) => {
+    setNewMessage(event.target.value)
+  }
+
+  const onFormSubmit = (event) => {
+    event.preventDefault()
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify({ message: newMessage }) 
+    }
+    fetch(THOUGHTS_URL, options)
+    .then(res => res.json())
+    .then(postMessage => setMessageList([...messageList, postMessage]))
+  }
+
+  
+
+
   return (
     <div>
-      Find me in src/app.js!
+      <form onSubmit={onFormSubmit}>
+        <label htmlFor='newMessage'>Write your happy shit here</label>
+        <input 
+        id='newMessage' 
+        type='text' 
+        value={newMessage}
+        onChange={onNewmessageChange}
+        />
+        <button type='submit'>Send Happy Thought</button>
+      </form>
+      
+      {messageList.map(message => (
+        <div key={message._id}>
+          <h4>{message.message}</h4>
+          <p>{moment(message.createdAt).fromNow()}</p>
+        </div>
+      )
+        )}
     </div>
   )
 }
---
+++ code/src/components/MessageList.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react'
+
+import { THOUGHTS_URL } from '../reusable/urls'
+
+
+export const MessageList = () => {
+  
+return (
+  <div>
+    hej
+  </div>
+)
+}
+
