9b5bd81cde344bae659a051e3bc690f0255ed87a
johlofg <jh.lofgren@gmail.com>
2021-03-23 18:03:34 +0100

    Think that I added a loader, just need to set a delay on the api so I can see it

--- code/src/App.js
+++ code/src/App.js
@@ -6,21 +6,24 @@ import { Fetch_API } from './reusable/urls'
 export const App = () => {
   const [thoughtsList, setThoughtsList] = useState([])
   const [thoughtsNew, setThoughtsNew] = useState('')
-  
+  const [loader, setLoader] = useState (true)
+  // const [heart, setHeart] = useState('') 
+
   useEffect(() => {
     fetchThoughtsList()
   }, [])
 
   const fetchThoughtsList = () => {
     fetch(Fetch_API)
-      .then(res => res.json())
-      .then(thoughts => setThoughtsList(thoughts))
+    .then(res => res.json())
+    .then(thoughts => setThoughtsList(thoughts))
+    .then(setLoader(false))
       .catch(err => console.error(err))          
   }
 
   const onThoughtsNew = (event) =>  {
     setThoughtsNew(event.target.value)
-    }
+    }  
 
   const onSubmitForm = (event) => {
     event.preventDefault() 
@@ -39,24 +42,41 @@ export const App = () => {
     .catch(err => console.error(err))
   }  
 
+  // const onHeartClick = () => {   
+  //   fetch(`https://happy-thoughts-technigo.herokuapp.com/thoughts/{}}/like`, {
+  //     method: 'POST',
+  //     headers: {
+  //     'Content-Type': 'application/json'
+  //   }
+  // })
+  //   .then(res => res.json())
+  //   .
+    
+  // }
+
   return (
     <div>
-      <form onSubmit={onSubmitForm}>
-        <label htmlFor="newThougt">Write a thought</label>
-        <input 
-          id="newThougt"
-          type="text"
-          value={thoughtsNew}
-          onChange={onThoughtsNew}
-          />
-          <button type="submit">Submit</button>
-      </form>
-      {thoughtsList.map(thoughts => (
-        <div key={thoughts._id}>
-          <h4>{thoughts.message}</h4>
-          <p>{moment(thoughts.created).fromNow()}</p>
-        </div>
-      ))}
+      {loader === true && <div class="lds-heart"><div></div></div>}   
+      
+        <form onSubmit={onSubmitForm}>
+          <label htmlFor="newThougt">Write a thought</label>
+          <input 
+            id="newThougt"
+            type="text"
+            value={thoughtsNew}
+            onChange={onThoughtsNew}
+            />
+            <button type="submit">Submit</button>
+        </form>
+
+        {thoughtsList.map(thoughts => (
+          <div key={thoughts._id}>
+            <h4>{thoughts.message}</h4>
+            <p>{moment(thoughts.created).fromNow()}</p>
+            <p></p>
+            <button>heart</button>
+          </div>
+        ))}      
     </div>
   )
 }
