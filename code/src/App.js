94d17b85fce7977d07c35982471e247aa2fb210f
Aepera <astridepersson@gmail.com>
2021-03-23 22:05:29 +0100

    Added form which sends new msg to backend and render on website

--- code/src/App.js
+++ code/src/App.js
@@ -5,6 +5,7 @@ import React, { useState, useEffect } from 'react'
 export const App = () => {
 
   const [messageList, setMessageList] = useState([])
+  const [newMessage, setNewMessage] = useState('')
 
   const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
 
@@ -21,9 +22,40 @@ export const App = () => {
       .catch(error => console.log(error))
   }
 
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({message: newMessage})
+  }
+
+  const onSubmitForm = (event) => {
+    event.preventDefault()
+    fetch(API_URL, options)
+    .then(response => response.json())
+    .then(receivedMessage => setMessageList([...messageList, receivedMessage]))
+    .catch(error => console.log(error))
+  }
+ 
+  const onNewMessage = (event) => {
+      setNewMessage(event.target.value)
+  }
 
   return (
     <div>
+
+      <form onSubmit={onSubmitForm}>
+        <label htmlFor='newMessage'>Your message:</label>
+        <input
+          id='newMessage'
+          type='text'
+          value={newMessage}
+          onChange={onNewMessage}
+        />
+        <button type='submit'>SEND</button>
+      </form>
+
       {messageList.map(message => (
        <div key={message._id} className='message'>  
          <h3>{message.message}</h3>
