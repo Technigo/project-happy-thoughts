b466521ddc4b405ef92b991cfab172ef3c2d5eba
Aepera <astridepersson@gmail.com>
2021-03-23 14:02:12 +0100

    Fetching messages from API and rendering on website

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+
 
 export const App = () => {
+
+  const [messageList, setMessageList] = useState([])
+
+  const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
+
+
+  useEffect(() => {
+    fetchMessageList()
+  }, [])
+
+
+  const fetchMessageList = () => { 
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(messages => setMessageList(messages))
+      .catch(error => console.log(error))
+  }
+
+
   return (
     <div>
-      Find me in src/app.js!
+      {messageList.map(message => (
+       <div key={message._id} className='message'>  
+         <h3>{message.message}</h3>
+         <p>Posted: {new Date(message.createdAt).toLocaleDateString()} at {new Date(message.createdAt).toLocaleTimeString()}  </p>
+       </div>
+      ))}
+     
     </div>
   )
+
+
 }
