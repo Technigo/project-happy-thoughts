0387e327083ba018d3a5ae79d0db075cb35f758a
mmolliss <malin.vannesjo@gmail.com>
2021-03-23 15:53:43 +0100

    fixed fetch with time stamp and a post form that doesn't post yet

--- code/src/App.js
+++ code/src/App.js
@@ -1,11 +1,14 @@
 import moment from 'moment';
 import React, { useState, useEffect } from 'react'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 // import{ Form } from './Components/Form';
 import { API_URL } from './reusable/urls';
+// const heart = <FontAwesomeIcon icon={faHeart} />
 
 export const App = () => {
   const [messageList, setMessageList] = useState([]);
+  const [newMessage, setNewMessage] = useState('')
 
   useEffect(() => {
     fetchMessages();
@@ -14,20 +17,44 @@ export const App = () => {
   const fetchMessages = () => {
     fetch(API_URL)
       .then(res => res.json())
-      .then(messages => setMessageList(messages))
+      .then(messages => {
+        console.log(messages);
+        setMessageList(messages)
+      })
       .catch(err => console.error(err));
-      console.log(messages);
   }
+
+  const onNewMessageChange = (event) => {
+    setNewMessage(event.target.value);
+  }
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+  };
 
   return (
     <div>
       {/* <Form /> */}
-      {messageList.map(message => (
-        <div key={message._id}>
-          <h4>{message.text}</h4>
-          <p>{moment(message.created).fromNow()}</p>
-        </div> 
-      ))}
-    </div>
+        <form className="new-message-form" htmlFor="newMessage" onSubmit={onFormSubmit}>
+          <input
+            id="newMessage"
+            type="text"
+            value={newMessage}
+            onChange={onNewMessageChange}
+            className="new-message-input"
+          />
+          <button className="submit-btn" type="submit">Post message!</button>
+        </form>
+        {messageList.map(message => (
+          <div className="thought-div" key={message._id}>
+            <h4>{message.message}</h4>
+            <p>
+              {/* <span>{heart}</span> */}
+              {message.hearts}
+            </p>
+            <p>{moment(message.createdAt).fromNow()}</p>
+          </div> 
+        ))}
+      </div>
   )
 }
