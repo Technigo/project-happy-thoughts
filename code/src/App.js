b8033191a867baf02b813710921c52fab19b2cb2
mmolliss <malin.vannesjo@gmail.com>
2021-03-23 12:37:20 +0100

    basic fetch

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import moment from 'moment';
+import React, { useState, useEffect } from 'react'
+
+// import{ Form } from './Components/Form';
+import { API_URL } from './reusable/urls';
 
 export const App = () => {
+  const [messageList, setMessageList] = useState([]);
+
+  useEffect(() => {
+    fetchMessages();
+  }, []);
+
+  const fetchMessages = () => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(messages => setMessageList(messages))
+      .catch(err => console.error(err));
+      console.log(messages);
+  }
+
   return (
     <div>
-      Find me in src/app.js!
+      {/* <Form /> */}
+      {messageList.map(message => (
+        <div key={message._id}>
+          <h4>{message.text}</h4>
+          <p>{moment(message.created).fromNow()}</p>
+        </div> 
+      ))}
     </div>
   )
 }
