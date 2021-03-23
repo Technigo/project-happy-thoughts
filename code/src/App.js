983ad44b1159931160e8dd96a4f0893f9f0e60f4
Erika Andersson Porath <erika.porath@hotmail.com>
2021-03-23 10:53:15 +0100

    Code working for fetching messages and heart count

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { API_URL } from "./reusable/urls"
 
 export const App = () => {
+  const [messageList, setMessageList] = useState([])
+
+  useEffect(() => {
+    fetchMessageList()
+  }, [])
+
+  const fetchMessageList = () => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(messages => setMessageList(messages))
+      .catch(err => console.error(err))
+  }
+
   return (
     <div>
-      Find me in src/app.js!
+      {messageList.map(message => (
+        <div key={message._id}>
+          <h4>{message.message}</h4>
+          <p>x {message.hearts}</p>
+        </div>
+      ))}
     </div>
   )
 }
