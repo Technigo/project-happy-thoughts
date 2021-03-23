fc9e3918d1fbd37a510874dec51e346cb97c0e45
Your Name <isabellamarkskog@live.se>
2021-03-23 22:24:42 +0100

    Added code for a message form that is able to POST to the API and code to fetch the message list from the API

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';  
 
-export const App = () => {
+import HeartButton from './HeartButton';
+
+import API_URL from './reusable/urls';
+
+const App = () => {
+  const [messageList, setMessageList] = useState([]);
+  const [newMessage, setNewMessage] = useState(''); 
+
+  useEffect(() => {
+    fetchMessageList();
+  }, []);  
+
+  const fetchMessageList = () => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(messages => setMessageList(messages))   // set this in a separate component
+      .catch(err => console.error(err));      
+  };
+
+  const handleNewMessage = (event) => {
+    setNewMessage(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const options = {   
+        method: 'POST', 
+        headers: {              //specific information for the backend application
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ message: newMessage })     //we are just sending an object with text
+    };
+
+    fetch(API_URL, options)      
+      .then(res => res.json())
+      .then(receivedMessage => setMessageList([...messageList, receivedMessage]))
+      .catch(err => console.error(err));
+  };
+
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="newMessage">What's making you happy right now?</label>
+        <input
+          id="newMessage"
+          type="text"
+          value={newMessage}
+          onChange={handleNewMessage}
+        />
+        <button type="submit">Send happy thought</button>
+      </form>
+
+      {messageList.map(message => (
+       <div key={message._id}>
+         <h4>{message.message}</h4>
+         <p>- {moment(message.createdAt).fromNow()}</p>
+       </div> 
+      ))}      
+    </>
   )
-}
+};
+
+export default App;
---
+++ code/src/HeartButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HeartButton = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default HeartButton;
