4599be0c79898eb7f28c4cb5025c5e3731ecf07f
“yennie <yennie.leek@hotmail.se>
2021-03-24 18:29:32 +0100

    added basic structure for posting hearts and all code still in same file. App.js

--- code/src/App.js
+++ code/src/App.js
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from 'react'
 import moment from 'moment'
 
 import { API_URL } from './reusable/urls'
+import { HEART_URL } from './reusable/urls'
 
 export const App = () => {
   const [happyList, setHappyList] = useState([])   //state properties
   const [newMessage, setNewMessage] = useState ('')
   
-useEffect(()=> {
-  fetchHappyThoughtList()
-}, []) //only for when the component is mounted. add empty string.Sending request to fetch funct
+  useEffect(()=> {
+    fetchHappyThoughtList()
+  }, []) //only for when the component is mounted. add empty string.Sending request to fetch funct
 
   const fetchHappyThoughtList = () => {
     fetch (API_URL)
@@ -35,31 +36,55 @@ useEffect(()=> {
 
     fetch(API_URL, options)
     .then(res => res.json())
-    .then(postMessage => setHappyList([...happyList, postMessage]))  
+    .then(receivedMessage => setHappyList([...happyList, receivedMessage]))  
   }
 
-  
+  const onHeartsIncrease = (id) => {
 
-  return (
-    <div>
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
 
-      <form onSubmit={onFormSubmit}>
-        <label htmlFor='newMessage'>What makes you happy right now!</label>
-        <input id='newMessage'
-        type='text'
-        value={newMessage}
-        onChange={onNewMessageChange}
-        />
-        <button type='submit'>Send happy thoughts!</button>
+    fetch(HEART_URL(id), options)
+      .then(res => res.json())
+      .then (receivedMessage => {
+        const updatedHappyList = happyList.map(message => {
+          if (message._id === receivedMessage._id) {
+            message.hearts += 1
+         } 
+         return message 
+       })
+       setHappyList(updatedHappyList)
+
+      })
+    .catch(err => console.log(err))
+
+  }
+
+    return (
+      <div>
+        <form onSubmit={onFormSubmit}>
+          <label htmlFor='newMessage'>What makes you happy right now!</label>
+          <input id='newMessage'
+            type='text'
+            value={newMessage}
+            onChange={onNewMessageChange}
+          />
+          <button type='submit'>Send happy thoughts!</button>
         </form>
-
-      {happyList.map(message =>(
-      <div key={message._id}>
-        <h4>{message.message}</h4>
-        <p>-{moment(message.createdAt).fromNow()}</p>
-      
+        {happyList.map(message =>(
+          <div key={message._id}>
+            <h4>{message.message}</h4>
+            <button onClick={ () => onHeartsIncrease(message._id)}>
+              {message.hearts}
+              ❤️
+            </button>
+            <p>-{moment(message.createdAt).fromNow()}</p>
+          </div>
+        ))}
       </div>
-      ))}
-    </div>
-  )
+    )
 }
---
+++ code/src/components/MessageForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MessageForm = () => {
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default MessageForm
