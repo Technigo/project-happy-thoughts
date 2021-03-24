308eae2d3d9a3734e32de549c33ebd48ee8b02c3
Tobias Hagberg <tbs.hagberg@gmail.com>
2021-03-24 14:14:00 +0100

    got message list

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
+import { API_URL }  from './reuseable/api_endpoints.js'
+
 export const App = () => {
+  const fetchMessageList = () => {
+    fetch (API_URL)
+      .then(res => res.json())
+      .then(messages => console.log(messages))
+      .catch(err => console.error(err))
+  }
+ 
+  fetchMessageList()
+  
   return (
-    <div>
+    <main>
       Find me in src/app.js!
-    </div>
+    </main>
   )
 }
---
+++ code/src/reuseable/api_endpoints.js
@@ -0,0 +1,2 @@
+export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts';
+/*   https://happy-thoughts-technigo.herokuapp.com/thoughts/THOUGHT_ID/like   */
