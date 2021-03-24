381b45f6905705e1858167b77c8695db61f73e50
katliebig <liebig.kat@gmail.com>
2021-03-24 09:33:18 +0100

    feat: added loader for initial page load

--- code/src/App.js
+++ code/src/App.js
@@ -3,10 +3,12 @@ import React, { useEffect, useState } from 'react'
 import API_URL from "./utils/urls"
 import Thought from "./components/Thought"
 import ThoughtForm from "./components/ThoughtForm"
+import Loader from "./components/Loader"
 
 export const App = () => {
   const [thoughts, setThoughts] = useState([])
   const [newThought, setNewThought] = useState("")
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     fetchThoughts()
@@ -17,6 +19,8 @@ export const App = () => {
       .then(res => res.json())
       .then(data => setThoughts(data))
       .catch(err => (console.log(err)))
+
+    setLoading(false)
   }
 
   return (
@@ -27,12 +31,15 @@ export const App = () => {
         newThought={newThought}
         setNewThought={setNewThought}
       />
-      {thoughts.map(thought => (
-        <Thought
-          key={thought._id}
-          thought={thought}
-        />
-      ))}
+      {loading && <Loader />}
+      {loading === false &&
+        thoughts.map(thought => (
+          <Thought
+            key={thought._id}
+            thought={thought}
+          />
+        ))
+      }
     </>
   )
 }
