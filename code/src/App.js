9b639497be45f5d3a1479798ad3cca334c805572
Tobias Hagberg <tbs.hagberg@gmail.com>
2021-03-24 15:13:46 +0100

    imported moment and successfully transformed time stamp on massages/thoughts

--- code/src/App.js
+++ code/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
+import Moment from 'moment'
 
 import { API_URL }  from './reuseable/api_endpoints.js'
+import moment from 'moment'
 
 export const App = () => {
   const [thoughtList, setThoughtList] = useState([])   
@@ -23,6 +25,7 @@ console.log(thoughtList)
       {thoughtList.map(thought => (
         <div key={thought._id}>
           <h4>{thought.message}</h4>
+          <p>{moment(thought.createdAt).fromNow()}</p>
         </div>
       ))}
     </main>
