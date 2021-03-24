c6bb570363d0dc12ed67eec8b9c2e2b5f200836f
Andrea Larsson <andrea.e.larsson@hotmail.se>
2021-03-24 09:16:28 +0100

    I have fetch from API and can show the thoughtslist through map

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { API_URL } from './reusable/Urls'
 
 export const App = () => {
+  const [thoughtsList, setThoughtsList] = useState([])
+
+  useEffect(() => {
+    fetchThoughtsList()
+  }, [])
+
+  const fetchThoughtsList = () => {
+    fetch (API_URL)
+      .then(res => res.json())
+      .then(thoughts => setThoughtsList(thoughts))
+      .catch(err => console.error(err))
+  }
+  fetchThoughtsList()
   return (
     <div>
-      Find me in src/app.js!
+      {thoughtsList.map(thought =>(
+        <div key={thought._id}>
+          <h4>{thought.message}</h4>
+        </div>
+      ))}
     </div>
   )
 }
