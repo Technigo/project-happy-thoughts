c3cab5344da957f8b51897e9c304a6becbb95f07
Rasmus Persson <rrasmuspersson@gmail.com>
2021-03-24 13:07:07 +0100

    added some styling and spacing.

--- code/src/component/Post.js
+++ code/src/component/Post.js
@@ -9,39 +9,33 @@ import { IconButton } from "@material-ui/core";
 
 const Post = ({ message, likes, timeStamp, postID, handlePostLike }) => {
   return (
-    <Paper square elevation={10} className="postCard">
-      <Grid container spacing={1}>
-        <Grid item xs={12}>
-          <Typography
-            className="break-line"
-            variant="subtitle1"
-            color="initial"
-          >
-            {message}
-          </Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Grid container justify="flex-start">
-            {
+    <Grid container justify="center">
+      <Paper square elevation={10} className="postCard">
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <Typography className="break-line" variant="h6" color="initial">
+              {message}
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Grid container justify="flex-start">
               <IconButton
                 aria-label="like"
                 onClick={() => handlePostLike(postID)}
               >
                 <FavoriteIcon className="heartIcon" />
               </IconButton>
-            }
-            x {likes}
+              <p> &nbsp; x {likes}</p>
+            </Grid>
+          </Grid>
+          <Grid item xs={6}>
+            <Grid container justify="flex-end">
+              <p>{moment(timeStamp).fromNow()}</p>
+            </Grid>
           </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <Grid container justify="flex-end">
-            <Typography variant="body2" color="initial">
-              {moment(timeStamp).fromNow()}
-            </Typography>
-          </Grid>
-        </Grid>
-      </Grid>
-    </Paper>
+      </Paper>
+    </Grid>
   );
 };
 
--- code/src/component/PostForm.js
+++ code/src/component/PostForm.js
@@ -4,58 +4,46 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import ButtonGroup from "@material-ui/core/ButtonGroup";
-import RefreshIcon from "@material-ui/icons/Refresh";
-import SendIcon from "@material-ui/icons/Send";
+import FavoriteIcon from "@material-ui/icons/Favorite";
 
-const PostForm = ({
-  postFormValue,
-  setPostFormValue,
-  postRequest,
-  refreshPosts,
-}) => {
+const PostForm = ({ postFormValue, setPostFormValue, postRequest }) => {
   const postFormValueChange = (event) => {
     setPostFormValue(event.target.value);
   };
 
   return (
-    <Paper square elevation={10} className="postCard">
-      <Grid container spacing={1}>
-        <Grid item xs={12}>
-          <Grid container>
-            <TextField
-              variant="outlined"
-              id="input"
-              label="What's on your mind?"
-              value={postFormValue}
-              onChange={postFormValueChange}
-            />
+    <Grid container justify="center">
+      <Paper square elevation={10} className="postCard">
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <Grid container justify="center">
+              <TextField
+                variant="outlined"
+                id="input"
+                label="What's on your mind?"
+                value={postFormValue}
+                onChange={postFormValueChange}
+              />
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Grid container>
+              <Grid container justify="center">
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={postRequest}
+                >
+                  <FavoriteIcon />
+                  &nbsp; Send happy thought &nbsp;
+                  <FavoriteIcon />
+                </Button>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <Grid container>
-            <ButtonGroup variant="text" color="default" aria-label="">
-              <Button
-                startIcon={<SendIcon />}
-                variant="contained"
-                color="primary"
-                onClick={postRequest}
-              >
-                Send
-              </Button>
-              <Button
-                startIcon={<RefreshIcon />}
-                variant="contained"
-                color="primary"
-                onClick={refreshPosts}
-              >
-                Refresh
-              </Button>
-            </ButtonGroup>
-          </Grid>
-        </Grid>
-      </Grid>
-    </Paper>
+      </Paper>
+    </Grid>
   );
 };
 
