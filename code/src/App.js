161f23b4ba03fbc9cad80b73aa9adc30fc89e1a4
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-03-25 15:54:24 -0600

    Added state properties, useEffect, fetch function and return of jsx

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
+import { API_URL } from './reusable/url'
+ 
 export const App = () => {
+
+  const [thoughtsList, setThoughtsList] = useState([]);
+
+  useEffect(() => {
+    fetchMessageList();
+
+  },[])
+
+
+    const fetchMessageList = () => {
+      fetch(API_URL)
+      .then(res => res.json())
+      .then(thoughts => setThoughtsList(thoughts))
+      .catch(err => console.error(err)) 
+
+    }
+
   return (
     <div>
-      Find me in src/app.js!
+      {thoughtsList.map(thought => (
+        <div key={thought._id}>
+          <h4>{thought.text}</h4>
+        </div>  
+
+      ))}
     </div>
   )
 }
