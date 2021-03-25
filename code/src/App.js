dbbd32cda712c3373092c1b8fa243f2dedbdf61d
Marielle Ohlsson <ohlssonmarielle@gmail.com>
2021-03-25 18:06:01 +0100

    created fetchfunctio and useEffect function and now the messages show in the browser

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import API_URL from './reusable/urls';
+
 
 export const App = () => {
+
+  const [thoughtsList, setThoughtsList] = useState([]);
+
+  useEffect(() => {
+    fetchThoughtList()
+  }, []);
+
+  const fetchThoughtList = () => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(thoughts => setThoughtsList(thoughts))
+      .catch(err => console.error(err));
+  }
+
+
   return (
+
     <div>
-      Find me in src/app.js!
+      {thoughtsList.map(thought => (
+        <div
+          key={thought._id}>
+          <h4>{thought.message}</h4>
+        </div>
+      ))}
     </div>
   )
 }
