4af014009601fe6323a0dd6da6719156f60b5f61
Tina <tina.juren@gmail.com>
2021-03-25 11:36:13 +0100

    installed external dependency moment.js

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
+import moment from 'moment'
 
 import { API_URL_THOUGHTS } from './reusable/urls'
 
 export const App = () => {
-  const [thoughtList, setThoughtList] = useState([]) // state properties
+  const [thoughtList, setThoughtList] = useState([]) // state properties initialized as empty array
   
   useEffect(() => {    // useEffect hook
     fetchThoughtList()
@@ -12,17 +13,18 @@ export const App = () => {
   const fetchThoughtList = () => {  // fetch request for resent thoughts
     fetch(API_URL_THOUGHTS)
       .then(res => res.json()) // response unpacked
-      .then(thoughts => setThoughtList(thoughts)) // data
+      .then(thoughts => setThoughtList(thoughts)) // data recieved and applied
       .catch(err => console.error(err)) 
   }
 
   fetchThoughtList()
 
   return (
-    <div>
+    <div>  
       {thoughtList.map(thought => (
         <div key={thought._id}>
           <h4>{thought.message}</h4>
+          <p>{moment(moment.createdAt).fromNow()}</p>
         </div>
       ))}
     </div>
