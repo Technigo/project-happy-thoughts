a2ce424bc1d837d138b8219436e0c366b5369bf2
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-03-25 17:48:25 -0600

    Adding POST request and getting confirmation from server

--- code/src/App.js
+++ code/src/App.js
@@ -5,49 +5,74 @@ import { API_URL } from './reusable/url'
  
 export const App = () => {
 
+  //State variables 
+
   const [messageList, setMessageList] = useState([])
   const [newMessage, setNewMessage] = useState(' ')
+
+  
+  const onNewMessageChange = (event) => {
+    setNewMessage(event.target.value);
+  }
+
+  // Hook that will call the function after component is mounted
 
   useEffect(() => {
     fetchMessageList();
 
   },[])
 
-    const fetchMessageList = () => {
-      fetch(API_URL)
-      .then(res => res.json())
-      .then(message => setMessageList(message))
-      .catch(err => console.error(err)) 
-    }
+  //Function to fetch message list from outer API
 
-    const onNewMessageChange = (event) => {
-      setNewMessage(event.target.value)
-    }
+  const fetchMessageList = () => {
+    fetch(API_URL)
+    .then(res => res.json())
+    .then(message => setMessageList(message))
+    .catch(err => console.error(err)) 
+  }
 
-    const onFormSubmit = (event) => {
-      event.preventDefault()
-      console.log('Form submitted!', newMessage)
-    }
+    
+ //Function that will prevent the form to refresh and that will send the POST request
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ message: newMessage })
+      };
+  
+      fetch(API_URL, options)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.error(err));
+  }
+   
 
     return (
-    <div>
-       <form onSubmit={onFormSubmit}>
-        <label htmlFor="newMessage">Write new message!</label>
-        <input
-          id="newMessage"
-          type="text"
-          value={newMessage}
-          onChange={onNewMessageChange}
-        />
-        <button type="onSubmit">Send message!</button>
-      </form> 
-      
-      {messageList.map(message => (
-        <div key={message._id}>
-          <h4>{message.message}</h4>
-          <p className ="date-created">-{new Date(message.createdAt).toDateString()}</p> 
-        </div>  
-      ))}
-    </div>
+      <div>
+        <form onSubmit={onFormSubmit}>
+          <label htmlFor="newMessage">Write new message!</label>
+          <input
+            id="newMessage"
+            type="text"
+            value={newMessage}
+            onChange={onNewMessageChange}
+          />
+          <button type="onSubmit">Send message!</button>
+        </form> 
+        
+        {/* Iterating over the array of messages */}
+
+        {messageList.map(message => (
+          <div key={message._id}>
+            <h4>{message.message}</h4>
+            <p className ="date-created">-{new Date(message.createdAt).toDateString()}</p> 
+          </div>  
+        ))}
+      </div>
   )
 }
