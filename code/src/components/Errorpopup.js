e1e2342f76725f7fa2ecbc53866a2626a331444a
Frivridna <54737644+Frivridna@users.noreply.github.com>
2021-03-25 12:29:33 +0100

    change to see if deployment on Netlify works now

--- code/src/components/Errorpopup.js
+++ code/src/components/Errorpopup.js
@@ -4,7 +4,7 @@ const Popup = ({ setError }) => {
   return (
     <span className="error-popup-container">
       <p className="error-popup">
-        Oops, your message needs to be more than 4 characters. Please give it one more try! 
+        Oops, your message needs to be more than 4 characters. Please give it one more
         <button className="error-button" onClick={() => setError('hide')}>
           OK!
         </button>
