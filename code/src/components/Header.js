deb852194622954e5c8c4e192e228d411b2613b5
Sara Carlstein <carlstein.sara@gmail.com>
2021-03-25 13:36:31 +0100

    changes

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -3,7 +3,13 @@ import './Styles/Header.css'
 
 const Header = () => {
     return (
-        <div className="header-wrapper">Happy thoughts!</div>
+        <>
+            <div className="header-wrapper"><h1 className="heading">Happy thoughts!</h1>
+            <div className="wrapper-heart">
+                <div className="pulsing-heart"></div>
+            </div>
+            </div>
+        </>
     )
 }
 
