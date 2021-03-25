da228d991e1c579bcf9bcd7aad224be8f7ee6c02
Mir Albertioni <fethullah.dincer@gmail.com>
2021-03-25 18:51:39 +0100

    going to start with red requirements

---
+++ code/src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <>
+            <header className="hero">
+                <img className="hero-image"  src="./assets/hero.jpg" alt="Hero Image" />  
+            </header>
+            <img className="hero-title" src="./assets/logo.png" alt="logotype" />
+            
+        </>
+    )
+}
+
+export default Hero
