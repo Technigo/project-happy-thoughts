dc6150224b67341eb56a036ba65d021aabfdcd9e
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-03-25 17:08:29 -0600

    Added form, states for the new message and  button w/o functionality

--- code/src/App.js
+++ code/src/App.js
@@ -1,32 +1,47 @@
 import React, { useState, useEffect } from 'react'
+/* import moment from 'moment'  */
 
 import { API_URL } from './reusable/url'
  
 export const App = () => {
 
-  const [thoughtsList, setThoughtsList] = useState([]);
+  const [messageList, setMessageList] = useState([])
+  const [newMessage, setNewMessage] = useState(' ')
 
   useEffect(() => {
     fetchMessageList();
 
   },[])
 
-
     const fetchMessageList = () => {
       fetch(API_URL)
       .then(res => res.json())
-      .then(thoughts => setThoughtsList(thoughts))
+      .then(message => setMessageList(message))
       .catch(err => console.error(err)) 
-
     }
 
-  return (
-    <div>
-      {thoughtsList.map(thought => (
-        <div key={thought._id}>
-          <h4>{thought.text}</h4>
-        </div>  
+    const onNewMessageChange = (event) => {
+      setNewMessage(event.target.value)
+    }
 
+    return (
+    <div>
+       <form>
+        <label htmlFor="newMessage">Write new message!</label>
+        <input
+          id="newMessage"
+          type="text"
+          value={newMessage}
+          onChange={onNewMessageChange}
+        />
+        <button>Send message!</button>
+      </form> 
+      
+      {messageList.map(message => (
+        <div key={message._id}>
+          <h4>{message.message}</h4>
+          <p className ="date-created">-{new Date(message.createdAt).toDateString()}</p> 
+        </div>  
       ))}
     </div>
   )
