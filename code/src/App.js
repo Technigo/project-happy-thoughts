9000bc62f59e7d222dfb961e671d608a7e9e9dcf
Tina <tina.juren@gmail.com>
2021-03-25 10:47:56 +0100

    added state properties, useEffect hook and made a fetch request

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { API_URL_THOUGHTS } from './reusable/urls'
 
 export const App = () => {
+  const [thoughtList, setThoughtList] = useState([]) // state properties
+  
+  useEffect(() => {    // useEffect hook
+    fetchThoughtList()
+  }, [])  
+  
+  const fetchThoughtList = () => {  // fetch request for resent thoughts
+    fetch(API_URL_THOUGHTS)
+      .then(res => res.json()) // response unpacked
+      .then(thoughts => setThoughtList(thoughts)) // data
+      .catch(err => console.error(err)) 
+  }
+
+  fetchThoughtList()
+
   return (
     <div>
-      Find me in src/app.js!
+      {thoughtList.map(thought => (
+        <div key={thought._id}>
+          <h4>{thought.message}</h4>
+        </div>
+      ))}
     </div>
   )
 }
---
+++ code/src/reusable/urls.js
@@ -0,0 +1,1 @@
+export const API_URL_THOUGHTS = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
