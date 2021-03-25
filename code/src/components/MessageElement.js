610ca69966ae9ac4adeee21e23ae58abd3662721
Theresehag <tezzinthecity@gmail.com>
2021-03-25 22:47:57 +0100

    change heart counter, make buttons round

--- code/src/components/MessageElement.js
+++ code/src/components/MessageElement.js
@@ -5,11 +5,13 @@ const MessageElement = ({ message, onLikesIncrease }) => {
     return (
         <div className="happy-card">
               <h4>{message.message}</h4>
-              <button onClick={() => onLikesIncrease(message._id)}>
-                {message.hearts}
-                ❤
+              <div className="button-wrapper">
+              <button className="heart-button" onClick={() => onLikesIncrease(message._id)}>
+              <span className={message.hearts > 0 ? 'heart-button-active' : 'heart-button'} role='img' aria-label='heart icon'>❤</span>
               </button>
-              <p className="date">- {moment(message.created).fromNow()}</p>
+              <p className="heart-count">x {message.hearts}</p>
+              </div>
+              <p className="date">- {moment(message.createdAt).fromNow()}</p>
             </div>
     )
 }
