c8b7a85e182d09a66423bf3eeafe0ef9568d1c43
Eleonor Frankenberg <elle@Eleonors-MacBook-Air.local>
2021-03-26 22:19:20 +0100

    made word-wrap on massage-container

--- code/src/components/MessageElement.js
+++ code/src/components/MessageElement.js
@@ -5,11 +5,13 @@ const MessageElement = ( {message, onLikesIncrease} ) => {
   return (
     <div className="chat-containers message-container">
       <h4>{message.message}</h4>
-      <button className="send-like-button"onClick={() => onLikesIncrease(message._id)}>
-        <span className="like-heart" role="img" aria-label="heart">❤️</span>
-      </button>
-        <p> x {message.hearts}</p>
-        <p className="date">{moment(message.createdAt).fromNow()}</p>
+      
+        <button className="send-like-button"onClick={() => onLikesIncrease(message._id)}>
+          <span className="like-heart" role="img" aria-label="heart">❤️</span>
+        </button>
+          <p> x {message.hearts}</p>
+          <p className="date">{moment(message.createdAt).fromNow()}</p>
+      
     </div>
 
   )
