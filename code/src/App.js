c85c930d1521bcca99c66805b6de6c34773c8a58
NoemiSalome <noemifrischknecht@macbook-pro.home>
2021-03-26 10:33:32 +0100

    basic function is set up

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import moment from 'moment'
+
+import { API_URL, LIKES_URL } from './reusable/urls'
 
 export const App = () => {
+  const [thoughtList, setThoughtList] = useState ([])
+  const [newThought, setNewThought] = useState('')
+
+  useEffect(() => {
+    fetchThoughtList()
+  }, [])
+
+  const fetchThoughtList = () => {
+    fetch (API_URL)
+      .then(res => res.json())
+      .then(thoughts => setThoughtList(thoughts))
+      .catch(error => console.error(error))
+  }
+
+  // happy thoughts //
+  //here is something going wrong
+  const onNewThoughtChange = (e) => {
+    setNewThought(e.target.value);
+  }
+  const onSubmitThought = (e) => {
+    e.preventDefault()
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ text: newThought })
+    };
+
+    fetch(API_URL, options)
+      .then(res => res.json())
+      .then(receivedThought => setThoughtList([receivedThought, ...thoughtList]))
+      .catch(err => console.error(err))
+  }
+
+// likes //
+  const onLikesIncrease = (id) => {
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      };
+
+      fetch(LIKES_URL(id), options)
+        .then(res => res.json())
+        .then(receivedThought => {
+          const updatedThoughtList = thoughtList.map(thought => {
+              return thought
+          })
+          setThoughtList(updatedThoughtList)
+        })
+        .catch(err => console.error(err))
+  }
+
+  console.log(thoughtList)
+
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <>
+      <form onSubmit={onSubmitThought}>
+        <label htmlFor="newThought"></label>
+        <input
+          id="newThought"
+          type="text"
+          value={newThought}
+          onChange={onNewThoughtChange}
+          placeholder="type here"
+        /> 
+        <button type="submit">send</button> 
+      </form>
+      {thoughtList.map(thought => (
+        <div key={thought._id}>
+          <h4>{thought.message}</h4>
+            <button onClick={() => onLikesIncrease(thought._id)}>
+              {thought.hearts}
+              ❤️
+            </button>
+          <p className="time">{moment(thought.createdAt).fromNow()}</p>
+        </div>
+      ))}
+    </>
   )
 }
