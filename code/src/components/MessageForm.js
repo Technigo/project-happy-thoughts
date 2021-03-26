55de3630c7d71f95c96b93ef423d7f965777bf73
Ulve17 <ulyssesmiklosveghelyi@gmail.com>
2021-03-26 15:22:35 +0100

    styling, textarea and letter counter added

--- code/src/components/MessageForm.js
+++ code/src/components/MessageForm.js
@@ -3,14 +3,19 @@ import React from 'react';
 const MessageForm = ({ messsageNew, onMessageNewChange, onFormSubmit }) => {
     return (
         <form className='form-container' onSubmit={onFormSubmit}>
-            <label htmlFor='newMessage'>Share a Happy thought!</label>
-            <input 
+            <label className='label-header' htmlFor='newMessage'>Share a Happy thought!</label>
+            <textarea className='text-area-input' 
                 id='newMessage'
                 type='text'
                 value={messsageNew}
-                onChange={onMessageNewChange}
-            />
-            <button className='submit-button' type='submit'>
+                onChange={onMessageNewChange}>
+            </textarea>
+            <p>{messsageNew.length} / 140</p>
+            <button 
+                className='submit-button' 
+                type='submit'
+                disabled={messsageNew.length < 3 || messsageNew.length > 140 ? true : false}
+            >
                 <span role='img' aria-label='Heart'> {'❤️ '}</span>
                 Send a happy thought
                 <span role='img' aria-label='Heart'> {' ❤️'}</span>
