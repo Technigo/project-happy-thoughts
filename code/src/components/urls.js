a9d094be167b3ba7dafc0e03dcc125fdffa5d540
vladjnbykov <67795638+vladjnbykov@users.noreply.github.com>
2021-03-26 21:14:53 +0100

    rebuilding after Netlify server failure

--- code/src/components/urls.js
+++ code/src/components/urls.js
@@ -1,2 +1,4 @@
+/* eslint-disable linebreak-style */
+
 export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
 export const API_LIKES_URL = (messageID) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${messageID}/like`
