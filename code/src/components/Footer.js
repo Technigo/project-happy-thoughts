c1e3cbf544176871d567d34f1a2590249bbbf14b
Sofarz <72409381+Sofarz@users.noreply.github.com>
2021-03-27 20:49:24 +0100

    removed image in footer

--- code/src/components/Footer.js
+++ code/src/components/Footer.js
@@ -4,10 +4,6 @@ const Footer = () => {
     return (
         <div className='footer'>
             <p>Sofia Arzt Wallén 2021</p>
-            <img className='heart-img' 
-                 src={process.env.PUBLIC_URL + './icons/favourite.png'}
-                 alt='heart image' 
-            />
         </div>
     )
 }
