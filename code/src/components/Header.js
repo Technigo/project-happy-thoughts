fe457bbc3c3f3affe4c3d93ec69c25486d2253ac
Sofarz <72409381+Sofarz@users.noreply.github.com>
2021-03-27 20:44:41 +0100

    temporary removal of image due to problems deploying.

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -3,15 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <div className='header'>
-            <img className='heart-img-header' 
-                    src={process.env.PUBLIC_URL + './icons/favourite.png'}
-                    alt='heart image' 
-            />
             <h1>Happy thoughts</h1>
-            <img className='heart-img-header' 
-                    src={process.env.PUBLIC_URL + './icons/favourite.png'}
-                    alt='heart image' 
-            />
         </div>
     )
 }
