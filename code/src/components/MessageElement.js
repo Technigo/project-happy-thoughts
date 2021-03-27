c0255098aa64fe47baae22dcb80858283c7366cc
Eleonor Frankenberg <elle@Eleonors-MacBook-Air.local>
2021-03-27 15:56:42 +0100

    made a caracter counter for the text aria that goes red when there is 10 caracters left to write.

--- code/src/components/MessageElement.js
+++ code/src/components/MessageElement.js
@@ -3,14 +3,14 @@ import moment from 'moment'
 
 const MessageElement = ( {message, onLikesIncrease} ) => {
   return (
-    <div className="chat-containers message-container">
+    <div className='chat-containers message-container'>
       <h4>{message.message}</h4>
-      <div className="likes-container">
-        <button className="send-like-button"onClick={() => onLikesIncrease(message._id)}>
-          <span className="like-heart" role="img" aria-label="heart">❤️</span>
+      <div className='likes-container'>
+        <button className='send-like-button' onClick={() => onLikesIncrease(message._id)}>
+          <span className='like-heart' role='img' aria-label='heart'>❤️</span>
         </button>
-          <p className="number-of-likes"> x {message.hearts}</p>
-          <p className="date">{moment(message.createdAt).fromNow()}</p>
+          <p className='number-of-likes'> x {message.hearts}</p>
+          <p className='date'>{moment(message.createdAt).fromNow()}</p>
       </div>
     </div>
 
--- code/src/components/MessageForm.js
+++ code/src/components/MessageForm.js
@@ -4,20 +4,27 @@ import React from 'react'
  const MessageForm = ({ newMessage, onNewmessageChange, onFormSubmit}) => {
   return (
     
-    <form className="chat-containers form-container" onSubmit={onFormSubmit}>
+    <form className='chat-containers form-container' onSubmit={onFormSubmit}>
         <label htmlFor='newMessage'>What´s making you happy right now?</label>
         <textarea
-          className="message-input-aria"
+          className='message-input-aria'
+          maxLength='140'
           id='newMessage' 
           name='newMessage' 
           value={newMessage}
           onChange={onNewmessageChange}
           >
-        </textarea> 
-        <button className="send-message-button"type='submit'>
-          <span className="send-message-heart" role="img" aria-label="heart">❤️</span>
+        </textarea>
+        <p 
+        className='text-input-counter'
+        style={{color: newMessage.length > 130 ? 'red' : 'black' }}
+        >
+          {140 - newMessage.length} x caracters left
+        </p> 
+        <button className='send-message-button' type='submit'>
+          <span className='send-message-heart' role='img' aria-label='heart'>❤️</span>
           <span>Send Happy Thought</span>
-          <span className="send-message-heart"role="img" aria-label="heart">❤️</span>
+          <span className='send-message-heart' role='img' aria-label='heart'>❤️</span>
         </button>
     </form>
     
