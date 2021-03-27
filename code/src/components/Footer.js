43973ae18790ab248c324ddf9a2c0d8cae56de23
Sofarz <72409381+Sofarz@users.noreply.github.com>
2021-03-27 20:52:06 +0100

    trying to re-add image

--- code/src/components/Footer.js
+++ code/src/components/Footer.js
@@ -4,6 +4,11 @@ const Footer = () => {
     return (
         <div className='footer'>
             <p>Sofia Arzt Wallén 2021</p>
+            <img className='heart-img-header' 
+                    src={process.env.PUBLIC_URL + './icons/favourite.png'}
+                    alt='heart' 
+            /> 
+
         </div>
     )
 }
