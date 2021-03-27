6e1cfc22b4d6d0627973a05c62e8fdf93f9aa2f9
Elaine Bergstrom <elainebergstrom26@gmail.com>
2021-03-27 19:19:11 +0100

    message fetch function added, useState added for messageList

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+/* local dependencies */
+import { API_URL } from './reusable/urls';
 
 export const App = () => {
+
+  useEffect(() => {
+    fetchMessageList();
+  }, []);
+
+  const fetchMessageList = () => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(messages => console.log(messages))
+      .catch(err => console.error(err));
+  }
+
+
   return (
     <div>
-      Find me in src/app.js!
+      
     </div>
   )
 }
