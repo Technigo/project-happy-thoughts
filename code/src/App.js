260dce067c15e463e639a23f97ce0ecc6f73f63c
liaback <felicia.liaback@gmail.com>
2021-03-28 15:22:51 +0200

    fetch messagelist, return messages and when created

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import moment from 'moment'
+
+import { API_URL } from "./reusable/urls";
 
 export const App = () => {
+  const [messageList, setMessageList] = useState([]);
+
+  useEffect(() => {
+    fetchMessageList();
+  }, []);
+
+  const fetchMessageList = () => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((messages) => setMessageList(messages))
+      .catch((err) => console.error(err));
+  };
+
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+  <div>
+    {messageList.map(message => (
+      <div key= {message._id}> 
+        <h4>{message.message}</h4>
+        <p>{moment(message.createdAt).fromNow()}</p>
+      </div>
+    ))}
+  </div>
   )
 }
---
+++ code/src/reusable/urls.js
@@ -0,0 +1,5 @@
+// fetch recent thoughts https://happy-thoughts-technigo.herokuapp.com/thoughts
+// create a thought https://happy-thoughts-technigo.herokuapp.com/thoughts
+// like a thought https://happy-thoughts-technigo.herokuapp.com/thoughts/THOUGHT_ID/like
+
+export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
