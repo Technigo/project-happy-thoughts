7717b41ed29a47d56c56249926a93d489e50811c
Anna Honkonen <ak.honkonen@gmail.com>
2021-03-28 20:35:39 +0300

    wrote readme

--- code/src/App.js
+++ code/src/App.js
@@ -19,6 +19,7 @@ export const App = () => {
       .then((res) => res.json())
       .then((messages) => setMessageList(messages))
       .catch((err) => console.error(err))
+      setMessageNew('')
   }
 
   const handleMessageNewChange = (event) => {
