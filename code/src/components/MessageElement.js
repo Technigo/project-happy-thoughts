3a191e3f9ce116314d1ff15de7af158263ed6215
M4L5 <malin.gu2017@gmail.com>
2021-03-28 22:45:00 +0200

    can't deploy my project to netlify

--- code/src/components/MessageElement.js
+++ code/src/components/MessageElement.js
@@ -8,7 +8,7 @@ const MessageElement = ({ input, onLikesIncrease }) => {
         <h4>{input.message}</h4>
         <button onClick={() => onLikesIncrease(input._id)}>
           {input.hearts}
-          <span>❤️</span>
+          ❤️
         </button>
         <p className="date">- {moment(input.createdAt).fromNow()}</p>
       </div>
