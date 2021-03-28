7063cb09c0749eed79248e2637c271468ada3c92
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-03-28 15:50:17 -0600

    Styled mobile view

--- code/src/components/MessageElement.js
+++ code/src/components/MessageElement.js
@@ -4,16 +4,18 @@ import moment from 'moment'
 
 const MessageElement = ({ message, onLikesIncrease } ) => {
   return (
-      <div className= 'printed-message-container'>
-        <p className= "printed-message">{message.message}</p>
-          <button 
-          className="likes-button"
-          onClick= {() => onLikesIncrease(message._id)}>
-            {message.hearts}
-             ♥️
-          </button> 
-          <p className ="date-created">-{moment(message.createdAt).fromNow()}</p> 
-      </div>
+    <div className= 'printed-message-container'>
+      <p className= "printed-message">{message.message}</p>
+        <div className="heart-counting-date-wrapper">
+          <p className ="date-created">{moment(message.createdAt).fromNow()}</p> 
+            <div className="button-counting-wrapper">
+              <button className="heart-button" onClick= {() => onLikesIncrease(message._id)}>
+                <span role="img" aria-label="heart">♥️</span>
+              </button> 
+              <p className="counting-likes"> x {message.hearts}</p>
+            </div>
+        </div>
+    </div>
     )
 }
 
--- code/src/components/MessageForm.js
+++ code/src/components/MessageForm.js
@@ -9,17 +9,18 @@ const MessageForm = ({ newMessage, onFormSubmit, onNewMessageChange }) => {
         <label htmlFor="newMessage"
           className="form-title">What's making you happy right now?</label>
         <textarea
+          className="input-box"
+          placeholder="Write your happy thought here..."
           id="newMessage"
           type="text"
           value={newMessage}
           onChange={onNewMessageChange}
-          className="input-box"
+          maxLength="140"
         />
-        <button 
-          type="onSubmit"
-          className="send-message-btn"
-        ><span className="heart" role="img" aria-label="heart-symbol">❤️</span> 
-        Send Happy Thought<span className="heart" role="img" aria-label="heart-symbol">❤️</span>
+        <button type="onSubmit"className="send-message-btn">
+          <span className="heart-emoji" role="img" aria-label="heart-symbol">❤️</span> 
+            Send Happy Thought
+          <span className="heart-emoji" role="img" aria-label="heart-symbol">❤️</span>
         </button>
       </form>    
     </>
