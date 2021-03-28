c95ab166434cd909f943da15b2d799ece311211a
Elaine Bergstrom <elainebergstrom26@gmail.com>
2021-03-29 00:01:01 +0200

    more styling, trying to fix margins

--- code/src/components/MessageElement.js
+++ code/src/components/MessageElement.js
@@ -8,16 +8,18 @@ const MessageElement = ({ messagePost, onLikesIncrease }) => {
     return (
         <>
             <div className="recieved-message-container" key={messagePost._id}>
-                <h4>{messagePost.message}</h4>
+                <h4 className="message-input">{messagePost.message}</h4>
                 <div className="like-button-and-date-container">
                     <button className="like-button" onClick={() => onLikesIncrease(messagePost._id)}>
                         <div className="heart-container">
                             <Emoji symbol="❤️" />
                         </div>
+                        <div aria_label="number of likes" className="like-count">
                         x
-                        {messagePost.hearts}                                                               
+                        {messagePost.hearts}
+                        </div>                                                               
                     </button>
-                    <p className="date">{moment(messagePost.created).fromNow()}</p>
+                    <p aria_label="time when posted" className="date">{moment(messagePost.created).fromNow()}</p>
                 </div>
             </div>
         </>
