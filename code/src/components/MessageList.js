05ab76d2c20d2916e4bcd2e80cf4913e1fc4c9cd
Lars Söderman <lars.soderman@gmail.com>
2021-03-28 21:29:44 +0200

    deploy bug

--- code/src/components/MessageList.js
+++ code/src/components/MessageList.js
@@ -10,8 +10,7 @@ const MessageList = ({thoughtList, onLikesIncrease}) => {
           <h4>{thought.message}</h4>
           <div className="message-info">
             <button 
-            className="heart-btn"
-              className={"heart-btn " + (thought.hearts > 0 ? 'pink' : null)}
+              className={"heart-btn " + (thought.hearts > 0 ? 'pink' : 'null')}
               onClick={() => onLikesIncrease(thought._id)}>
                 <span role="img" aria-label="heart">❤️</span>
             </button>
