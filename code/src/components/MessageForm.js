09954896d7741f929eaef25a88e5f39907a1333a
Theresehag <tezzinthecity@gmail.com>
2021-03-28 23:29:16 +0200

    change a few things in css

--- code/src/components/MessageForm.js
+++ code/src/components/MessageForm.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React from 'react';
+
 
 const MessageForm = ({ messageNew, onMessageNewChange, onFormSubmit }) => {
     return (
         <form className ="happy-form" onSubmit={onFormSubmit}>
-        <label className="message-field" htmlFor="newMessage">Share your most happy thoughts!</label>
+        <label className="message-field" htmlFor="messageNew">Share your most happy thoughts!</label>
         <input className="message-field" 
-        id="newMessage"
+        id="messageNew"
         type="text"
+        placeholder="Write happy thoughts here :)"
         value={messageNew}
-        onChange={onMessageNewChange}
-
-        />
-        <button className="submit-button" type="submit">❤ Send happy thoughts ❤</button>
+        onChange={onMessageNewChange}    
+      />
+      
+        <button 
+        className="submit-button" 
+        type="submit"
+        >
+          ❤ Send happy thoughts ❤
+        </button>
+        
       </form>
     );
 
