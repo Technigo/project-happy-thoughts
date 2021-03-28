a2a0aae7cc69a35d768d62458e51f621259d912f
liaback <felicia.liaback@gmail.com>
2021-03-28 17:35:58 +0200

    add submit button and add new message to api

--- code/src/App.js
+++ code/src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
-import moment from 'moment'
+import moment from "moment";
 
 import { API_URL } from "./reusable/urls";
 
 export const App = () => {
   const [messageList, setMessageList] = useState([]);
+  const [messageNew, setMessageNew] = useState([]);
 
   useEffect(() => {
     fetchMessageList();
@@ -17,14 +18,47 @@ export const App = () => {
       .catch((err) => console.error(err));
   };
 
+  const onMessageNewChange = (event) => {
+    setMessageNew(event.target.value);
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: messageNew }),
+    };
+
+    fetch(API_URL, options)
+      .then((res) => res.json())
+      .then((recivedMessage) =>
+        setMessageList([...messageList, recivedMessage])
+      )
+      .catch((err) => console.error(err));
+  };
+
   return (
-  <div>
-    {messageList.map(message => (
-      <div key= {message._id}> 
-        <h4>{message.message}</h4>
-        <p>{moment(message.createdAt).fromNow()}</p>
-      </div>
-    ))}
-  </div>
-  )
-}
+    <div>
+      <form onSubmit={onFormSubmit}>
+        <label htmlFor="newMessage">Write a message</label>
+        <input
+          id="newMessage"
+          type="text"
+          value={messageNew}
+          onChange={onMessageNewChange}
+        />
+        <button type="submit">send</button>
+      </form>
+      {messageList.map((message) => (
+        <div key={message._id}>
+          <h4>{message.message}</h4>
+          <p>{moment(message.createdAt).fromNow()}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
