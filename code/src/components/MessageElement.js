009b50c7af43ea3db3e4da5495c8d8f84c070c36
Sari-Anne Peltola <sarianne.peltola@gmail.com>
2021-03-28 20:35:59 +0200

    Styling

--- code/src/components/MessageElement.js
+++ code/src/components/MessageElement.js
@@ -3,15 +3,19 @@ import moment from 'moment'
 
 const MessageElement = ( {onLikesIncrease, message}) => {
   return (
-    <div>
-      <h4>{message.message}</h4>
-      <button 
-        onClick={() => onLikesIncrease(message._id)} 
-        className='likes-button'>
-        <span role='img' aria-label='heart-emoji'>❤️</span>
-      </button>
-      <span className='amount-likes'>x {message.hearts}</span>
-      <p className='date'>{moment(message.createdAt).fromNow()}</p>
+    <div class='message-container'>
+      <p className='message'>{message.message}</p>
+      <div className='flex-wrapper'>
+        <div>
+          <button 
+            onClick={() => onLikesIncrease(message._id)} 
+            className='likes-button'>
+            <span role='img' aria-label='heart-emoji'>❤️</span>
+          </button>
+          <span className='amount-likes'>  x   {message.hearts}</span>
+        </div>
+        <p className='date'>{moment(message.createdAt).fromNow()}</p>
+      </div>
     </div>
   )
 }
--- code/src/components/MessageForm.js
+++ code/src/components/MessageForm.js
@@ -2,19 +2,18 @@ import React from 'react'
 
 const MessageForm = ({ onFormSubmit, messageNew, onMessageNewChange }) => {
   return (
-    <form onSubmit={onFormSubmit}>
-      <label htmlFor='messageForm'>What's making you happy right now?</label>
+    <form onSubmit={onFormSubmit} className='message-container form'>
+      <label htmlFor='messageForm' className='form-label'>What's making you happy right now?</label>
       <textarea
         id='messageForm'
-        className='message-form'
+        className='textarea'
         value={messageNew}
         onChange={onMessageNewChange}
         minLength='5'
         maxLength='140'
         tabIndex='0'
-        rows='3'
       />
-      <button type='submit'><span role='img' aria-label='heart-emoji'>❤️Send Happy Thought❤️</span></button>
+      <button type='submit' className='submit-button'><span role='img' aria-label='heart-emoji'>❤️ Send Happy Thought ❤️</span></button>
     </form>
   )
 }
