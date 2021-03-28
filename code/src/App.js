a4df87dcc6bb674ef8d4e1078ee5b02ae5ab6a73
Sari-Anne Peltola <sarianne.peltola@gmail.com>
2021-03-28 22:13:23 +0200

    Fixed error

--- code/src/App.js
+++ code/src/App.js
@@ -66,6 +66,11 @@ export const App = () => {
 
   return (
     <div>
+      <h1>
+        <i class="far fa-grin-hearts"></i>
+        Happy Thoughts
+        <i class="far fa-envelope"></i>
+      </h1>
       <MessageForm 
         messageNew={messageNew} 
         onMessageNewChange={handleMessageNewChange} 
