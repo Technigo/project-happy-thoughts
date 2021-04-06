c3c4f47d7498b6590695731d9c17924be6e20f79
Pauan8 <pauline.j.andersson@vgregion.se>
2021-04-06 03:49:30 +0200

    clearInterval in a better position, fixed with likes (removed the possibility to unlike, as it doesn't work with the post URL for likes

--- code/src/reusable/urls.js
+++ code/src/reusable/urls.js
@@ -1,2 +1,2 @@
 export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts';
-export const LIKES_URL = (messageID) => {return `//happy-thoughts-technigo.herokuapp.com/thoughts/${messageID}/like`}
+export const LIKES_URL = (messageID) => (`//happy-thoughts-technigo.herokuapp.com/thoughts/${messageID}/like`)
