ff19e8f6845abee2222363bb29c7798702a3c2f2
Maria Sparre <you@example.com>
2021-04-16 00:18:15 +0200

    background img

--- code/src/components/MessageForm.js
+++ code/src/components/MessageForm.js
@@ -6,7 +6,8 @@ const MessageForm = ({newMessage, onNewMessageChange, onFormSubmit}) => {
         <div className="send-message-card">What makes you happy right now?
           <div class="textarea-container">
             <label htmlFor="newMessage"></label>
-            <textarea aria-label="write your happy thoughts here"
+            <textarea 
+              aria-label="write your happy thoughts here"
               className="input-field"
               placeholder="Your happy thoughts here"
               id="newMessage"
@@ -28,7 +29,7 @@ const MessageForm = ({newMessage, onNewMessageChange, onFormSubmit}) => {
               aria-label="upload happy thoughts here" 
               role="img"
             >
-             &nbsp; &#10084;&#65039; &nbsp; Upload happiness&nbsp; &#10084;&#65039; &nbsp;   {/* is there a cleaner way of typing spaces?*/}
+             &nbsp; &#10084;&#65039; &nbsp; Upload happiness&nbsp; &#10084;&#65039; &nbsp;  
             </span>
           </button> 
         </div>
