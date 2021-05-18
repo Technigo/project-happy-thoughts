a52a3c04a39e0dd4eb8f953707eb74eadd5c712d
Ann-Sofi Jönsson <annsofi.jonsson@hotmail.se>
2021-05-18 21:53:52 +0200

    changed URL to my own backend

--- code/src/reusable/urls.js
+++ code/src/reusable/urls.js
@@ -1,2 +1,5 @@
-export const API_URL =" https://happy-thoughts-technigo.herokuapp.com/thoughts";
-export const API_LIKES = (id) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${id}/like`;
+// export const API_URL =" https://happy-thoughts-technigo.herokuapp.com/thoughts";
+// export const API_LIKES = (id) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${id}/like`;
+
+export const API_URL ="https://annsofi-books-api.herokuapp.com/thoughts";
+export const API_LIKES = (id) => `https://annsofi-books-api.herokuapp.com/thoughts/${id}/like`;
