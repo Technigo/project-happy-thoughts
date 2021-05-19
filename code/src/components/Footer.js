88c2c23a573c72c51b7c28cfd7767d1d5fa96175
Sara Carlstein <carlstein.sara@gmail.com>
2021-05-19 16:00:42 +0200

    spelling tweaks

--- code/src/components/Footer.js
+++ code/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <>
             <div className="footer-wrapper"> 
                 <img className="logo"src="./assets/logo.svg" alt="technigo logo" />
-                <p className="footer-paragraphs">&copy; developed 2020 by Sara Carlstein </p> 
+                <p className="footer-paragraphs">&copy; developed 2021 by Sara Carlstein </p> 
                 <p className="footer-paragraphs">Week 11: Happy thoughts project ❤️ | Technigo bootcamp</p>
                 <p className="footer-paragraphs"><a href="https://www.linkedin.com/in/sara-carlstein-532b2737/"target="_blank" rel="noopener noreferrer">Linkedin</a>
                 |<a href="https://github.com/Sartish" target="_blank" rel="noopener noreferrer" >GitHub</a></p>
