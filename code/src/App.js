4696407be06c052b173cbed1ce53c417eeef180f
Ylva Landoff Lindberg <ylva.lan.lin@gmail.com>
2021-05-19 20:13:23 +0200

    Background

--- code/src/App.js
+++ code/src/App.js
@@ -1,75 +1,74 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
 import { API_URL, API_URL_LIKES } from './reusable/urls';
 
-import { MessageList } from './components/MessageList'
-import { MessageForm } from './components/MessageForm'
+import { MessageList } from './components/MessageList';
+import { MessageForm } from './components/MessageForm';
 
 export const App = () => {
-  const [messageList, setMessageList] = useState([]);
-  const [messageNew, setMessageNew] = useState('')
+	const [messageList, setMessageList] = useState([]);
+	const [messageNew, setMessageNew] = useState('');
 
-  useEffect(() => {
-    fetchMessageList();
-  }, []);
+	useEffect(() => {
+		fetchMessageList();
+	}, []);
 
-  const fetchMessageList = () => {
-    fetch(API_URL)
-      .then(res => res.json())
-      .then(messages => setMessageList(messages))
-      .catch(err => console.error(err));
-      
-  }
+	const fetchMessageList = () => {
+		fetch(API_URL)
+			.then((res) => res.json())
+			.then((messages) => setMessageList(messages))
+			.catch((err) => console.error(err));
+	};
 
-  const onMessageNewChange = (event) =>{
-    setMessageNew(event.target.value);
-  }
+	const onMessageNewChange = (event) => {
+		setMessageNew(event.target.value);
+	};
 
-  const onFormSubmit = (event) => {
-    event.preventDefault();
-  
-      const options = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ message: messageNew })
-      };
+	const onFormSubmit = (event) => {
+		event.preventDefault();
 
-      fetch(API_URL, options)
-        .then(res => res.json())
-        .then(() => fetchMessageList())
-        .then(() => setMessageNew(''))
-        .catch(err => console.error(err));
-    }
+		const options = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ message: messageNew }),
+		};
 
-  const onLikesIncrease = (id) => {
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }
+		fetch(API_URL, options)
+			.then((res) => res.json())
+			.then(() => fetchMessageList())
+			.then(() => setMessageNew(''))
+			.catch((err) => console.error(err));
+	};
 
-    fetch(API_URL_LIKES(id), options)
-      .then(res => res.json())
-      .then(() => fetchMessageList())
-      .catch(err => console.erroe(err));
-  }
+	const onLikesIncrease = (id) => {
+		const options = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		};
 
-  return (
-    <div className="main-forms-wrapper">
-      <div className="submit-message-container">
-        <MessageForm 
-          messageNew={messageNew} 
-          onMessageNewChange={onMessageNewChange} 
-          onFormSubmit={onFormSubmit} 
-          />
-      </div>
-      <MessageList 
-        messageList={messageList} 
-        onLikesIncrease={onLikesIncrease}
-      />
-    </div>
-  )
-}
+		fetch(API_URL_LIKES(id), options)
+			.then((res) => res.json())
+			.then(() => fetchMessageList())
+			.catch((err) => console.erroe(err));
+	};
+
+	return (
+		<div className='main-forms-wrapper'>
+			<div className='submit-message-container'>
+				<MessageForm
+					messageNew={messageNew}
+					onMessageNewChange={onMessageNewChange}
+					onFormSubmit={onFormSubmit}
+				/>
+			</div>
+			<MessageList
+				messageList={messageList}
+				onLikesIncrease={onLikesIncrease}
+			/>
+		</div>
+	);
+};
