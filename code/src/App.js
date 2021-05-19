35a623a88b0ffa0a2acf4827a964e7603b81399f
Nazanin Adampira <Nazi.pira88@gmail.com>
2021-05-19 18:31:07 +0200

    fix API result

--- code/src/App.js
+++ code/src/App.js
@@ -1,50 +1,45 @@
 import React, { useState, useEffect } from "react";
-import { PostHappyThought } from "./components/PostHappyThought"
-import { ThoughtList } from "./components/ThoughtList"
-
+import { PostHappyThought } from "./components/PostHappyThought";
+import { ThoughtList } from "./components/ThoughtList";
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState([]);
 
-  const [thoughts, setThoughts] = useState([])
-
-  const [postedMessage, setPostedMessage] = useState("")
+  const [postedMessage, setPostedMessage] = useState("");
 
   useEffect(() => {
     fetch("https://happy-thoughts.herokuapp.com/thoughts", {
       method: "GET",
     })
-      .then(res => res.json())
-      .then(json => { setThoughts(json) })
-      .catch(error => console.error("Error:", error))
-  }, [postedMessage])
+      .then((res) => res.json())
+      .then((json) => {
+        setThoughts(json.results);
+      })
+      .catch((error) => console.error("Error:", error));
+  }, [postedMessage]);
 
+  const onFormSubmit = (message) => {
+    setPostedMessage(message);
+  };
 
-  const onFormSubmit = message => {
-    setPostedMessage(message)
-  }
-
-  const onLiked = thoughtId => {
-    console.log("Logging in the APP.js", thoughtId)
+  const onLiked = (thoughtId) => {
+    console.log("Logging in the APP.js", thoughtId);
     // just to check that the func is being called and has the id
-
-    const updatedThoughts = thoughts.map(thought => {
+    const updatedThoughts = thoughts.map((thought) => {
       if (thought._id === thoughtId) {
-        thought.hearts += 1
+        thought.hearts += 1;
       }
-      return thought
-    })
-    setThoughts(updatedThoughts)
-  }
+      return thought;
+    });
+    setThoughts(updatedThoughts);
+  };
 
   return (
-
     <div className="container">
       <main>
         <PostHappyThought onFormSubmit={onFormSubmit} />
-
-        {thoughts.map(thought => (
+        {thoughts.map((thought) => (
           <ThoughtList key={thought._id} thought={thought} onLiked={onLiked} />
-
         ))}
       </main>
 
@@ -52,6 +47,5 @@ export const App = () => {
         <p> NAZANIN </p>
       </footer>
     </div>
-
-  )
-}
+  );
+};
--- code/src/components/ThoughtList.js
+++ code/src/components/ThoughtList.js
@@ -1,43 +1,38 @@
-import React from "react"
-import "./happythoughts.css"
+import React from "react";
+import "./happythoughts.css";
 
-
-
-export const ThoughtList = props => {
-
-
-  const { _id, message, hearts, createdAt } = props.thought
+export const ThoughtList = (props) => {
+  const { _id, message, hearts } = props.thought;
 
   const handleHeartClick = () => {
     fetch(`https://happy-thoughts-technigo.herokuapp.com/thoughts${_id}/like`, {
-      method: 'POST'
+      method: "POST",
     })
       .then(() => props.onLiked(_id))
-      .catch(error => console.error("Error:", error))
-  }
+      .catch((error) => console.error("Error:", error));
+  };
 
   return (
-
-
     <article className="thought-card">
-
-      <div className="message">
-        {message}
-      </div>
-
+      <div className="message">{message}</div>
       <div className="card-bottom">
-
         <div>
-          <button className="heart-btn"
+          <button
+            className="heart-btn"
             onClick={handleHeartClick}
-            style={hearts > 0 ? { backgroundColor: "pink" } : { backgroundColor: "#e7e7e7" }}>
-            <span role="img" aria-label="Like post">{"❤️"}</span>
+            style={
+              hearts > 0
+                ? { backgroundColor: "pink" }
+                : { backgroundColor: "#e7e7e7" }
+            }
+          >
+            <span role="img" aria-label="Like post">
+              {"❤️"}
+            </span>
           </button>
           <span className="highlight-grey"> x {hearts} </span>
         </div>
-
       </div>
-
     </article>
-  )
-}
+  );
+};
