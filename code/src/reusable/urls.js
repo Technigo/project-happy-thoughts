02e901dc97d1645cfb880296aa5c057aa36008b3
Pauan8 <pauline.j.andersson@vgregion.se>
2021-05-19 16:44:04 +0200

    updated links

--- code/src/reusable/urls.js
+++ code/src/reusable/urls.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts';
-export const LIKES_URL = (messageID) => {return `//happy-thoughts-technigo.herokuapp.com/thoughts/${messageID}/like`}
+export const API_URL = 'https://happy-thoughts-pauline.herokuapp.com/thoughts';
+export const LIKES_URL = (messageID) => {return `https://happy-thoughts-pauline.herokuapp.com/thoughts/${messageID}/like`}
