a712b84b259361882b88091f486eb6e14faf77f8
Bellsibub <isabella.billgren@gmail.com>
2021-05-19 12:27:25 +0200

    env dependent url

--- code/src/helpers/reusables.js
+++ code/src/helpers/reusables.js
@@ -1,6 +1,7 @@
-export const URL = 'https://happy-thinking.herokuapp.com/thoughts';
+const mainURL = process.env.NODE_ENV === 'development' ? 'http://localhost:1337' : 'https://happy-thinking.herokuapp.com';
+export const URL = `${mainURL}/thoughts`;
 
-export const URL_LIKE = (id) => `https://happy-thinking.herokuapp.com/thoughts/${id}/like`;
+export const URL_LIKE = (id) => `${mainURL}/thoughts/${id}/like`;
 
 export const options = (newThought) => {
   const { message, category } = newThought;
