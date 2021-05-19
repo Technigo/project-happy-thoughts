dd144d4e40588b5e4953798658beac86a989449c
Sofarz <sofia.wallen@gmail.com>
2021-05-19 20:48:52 +0200

    change url

--- code/src/reusable/urls.js
+++ code/src/reusable/urls.js
@@ -1,2 +1,5 @@
-export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
-export const LIKE_URL = (_id) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${_id}/like`
+// export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
+// export const LIKE_URL = (_id) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${_id}/like`
+
+export const API_URL = 'https://sofias-happy-thought-api.herokuapp.com/thoughts'
+export const LIKE_URL = (_id) => `https://sofias-happy-thought-api.herokuapp.com/thoughts/${_id}/like`
