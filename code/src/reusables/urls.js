ab1f4d83272ca9288993b72ccc9356d23b8e150b
sirihemsley <siri.hemsley@gmail.com>
2021-05-20 12:41:48 +0200

    Updated API URL to be my own API

--- code/src/reusables/urls.js
+++ code/src/reusables/urls.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts"
-export const HEART_URL = (id) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${id}/like`
+export const API_URL = "https://happy-thoughts-siri.herokuapp.com/thoughts"
+export const HEART_URL = (id) => `https://happy-thoughts-siri.herokuapp.com/thoughts/${id}/like`
