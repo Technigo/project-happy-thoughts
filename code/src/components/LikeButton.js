a32dba2288123d0bb2c346da47e2832dbfecda10
johlofg <jh.lofgren@gmail.com>
2021-05-20 10:06:23 +0200

    removed lottie again, dident work

--- code/src/components/LikeButton.js
+++ code/src/components/LikeButton.js
@@ -1,21 +1,10 @@
 import React from 'react'
 import lottie from 'lottie-web'
 
-export const LikeButton = (props) => {  
-
-  useEffect(() => {
-    lottie.likeAnimation({
-      container: lottieContainer.current,
-      render: 'svg',
-      loop: false,
-      autoplay: onClick,
-      // eslint-disable-next-line global-require
-      animationData: require('../assets/heartbtn.json')
-    })
-  }, [])
+export const LikeButton = (props) => {    
 
   return (    
-      <button className={`like-button ${props.thoughts.hearts === 0 ? "not-liked" : "liked"}`} onClick= {() => props.OnLikesIncrease(props.thoughts._id), ref={lottieContainer}}>
+      <button className={`like-button ${props.thoughts.hearts === 0 ? "not-liked" : "liked"}`} onClick= {() => props.OnLikesIncrease(props.thoughts._id)}>
         <span className="heart-on-button-icon" role="img" aria-label="Heart">💗</span>
       </button>    
   )
