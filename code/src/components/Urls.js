53e11ae052520bd10703e33099e6acab2c00aba2
Aepera <astridepersson@gmail.com>
2021-05-20 16:25:16 +0200

    changed API to my own in week 18

--- code/src/components/Urls.js
+++ code/src/components/Urls.js
@@ -1,3 +1,5 @@
-export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
+//export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
+export const API_URL = 'https://happythoughts-api-mongodb.herokuapp.com/thoughts'
 
-export const APIHEARTS_URL = (THOUGHT_ID) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${THOUGHT_ID}/like`
+//export const APIHEARTS_URL = (THOUGHT_ID) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${THOUGHT_ID}/like`
+export const APIHEARTS_URL = (THOUGHT_ID) => `https://happythoughts-api-mongodb.herokuapp.com/thoughts/${THOUGHT_ID}/hearts`
