847e1529321a3c9fc66c6873740533532a1e0fb5
Anna Honkonen <ak.honkonen@gmail.com>
2021-05-20 10:12:58 +0300

    fixed a typo

--- code/src/reusable/url.js
+++ code/src/reusable/url.js
@@ -1,2 +1,2 @@
 export const URL = 'https://honkoan-happy-thoughts-api.herokuapp.com/thoughts'
-export const LIKES_URL = (messageID) => `https://honkoan-happy-thoughts-api.herokuapp.com/thoughts/${messageID}/like`
+export const LIKES_URL = (messageID) => `https://honkoan-happy-thoughts-api.herokuapp.com/thoughts/${messageID}/likes`
