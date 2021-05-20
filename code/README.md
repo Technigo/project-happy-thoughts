09be5a3c3f344a5578695ce23aeba5f4ca0d6d4e
johlofg <jh.lofgren@gmail.com>
2021-05-20 09:43:59 +0200

    added lottieanimation

--- code/README.md
+++ code/README.md
@@ -8,3 +8,8 @@ Its build in react with fetch from a external API, and with a seperate for the c
 This project was pretty straight forward with clear goals and an already made up design. 
 
 If I had more time I would add animation to newly posted thought and a charachter counter. 
+
+Update! 
+Added my own API so its no longer a joint. 
+
+https://project-happy-thoughts-johan.netlify.app/
--- code/src/components/LikeButton.js
+++ code/src/components/LikeButton.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import lottie from 'lottie-web'
 
 export const LikeButton = (props) => {  
+
+  useEffect(() => {
+    lottie.likeAnimation({
+      container: lottieContainer.current,
+      render: 'svg',
+      loop: false,
+      autoplay: onClick,
+      // eslint-disable-next-line global-require
+      animationData: require('../assets/heartbtn.json')
+    })
+  }, [])
+
   return (    
-      <button className={`like-button ${props.thoughts.hearts === 0 ? "not-liked" : "liked"}`} onClick= {() => props.OnLikesIncrease(props.thoughts._id)}>
+      <button className={`like-button ${props.thoughts.hearts === 0 ? "not-liked" : "liked"}` ref={lottieContainer}} onClick= {() => props.OnLikesIncrease(props.thoughts._id)}>
         <span className="heart-on-button-icon" role="img" aria-label="Heart">💗</span>
       </button>    
   )
