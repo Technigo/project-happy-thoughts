6eec2814af7c9e6bb77f15278166efa65fd9144f
newmarkdev <newmarkdev@gmail.com>
2021-05-20 13:22:18 +0200

    added new api urls

--- code/src/urls.js
+++ code/src/urls.js
@@ -1,2 +1,8 @@
-export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts"
-export const LIKES_URL = (messageID) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${messageID}/like`
+
+// Initial API 
+// export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts"
+// export const LIKES_URL = (messageID) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${messageID}/like`
+
+// Custom Built API for project week 19
+export const API_URL = "https://happy-thoughts-api-sn.herokuapp.com/thoughts"
+export const LIKES_URL = (messageID) => `https://happy-thoughts-api-sn.herokuapp.com/thoughts/${messageID}/likes`
