6756098320eaa412d0c46362990b8d2bbf313055
johlofg <jh.lofgren@gmail.com>
2021-05-20 09:49:53 +0200

    added lottieanimation

--- code/src/components/LikeButton.js
+++ code/src/components/LikeButton.js
@@ -15,7 +15,7 @@ export const LikeButton = (props) => {
   }, [])
 
   return (    
-      <button className={`like-button ${props.thoughts.hearts === 0 ? "not-liked" : "liked"}` ref={lottieContainer}} onClick= {() => props.OnLikesIncrease(props.thoughts._id)}>
+      <button className={`like-button ${props.thoughts.hearts === 0 ? "not-liked" : "liked"}`} onClick= {() => props.OnLikesIncrease(props.thoughts._id), ref={lottieContainer}}>
         <span className="heart-on-button-icon" role="img" aria-label="Heart">💗</span>
       </button>    
   )
