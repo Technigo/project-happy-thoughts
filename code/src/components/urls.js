7e10a0e792a6129d2498748ca57fe669a0aafb86
vladjnbykov <67795638+vladjnbykov@users.noreply.github.com>
2021-05-20 11:20:39 +0200

    cleaning

--- code/src/components/urls.js
+++ code/src/components/urls.js
@@ -3,9 +3,3 @@
 export const API_URL = 'https://vlad-happy-thoughts-api.herokuapp.com/thoughts'
 export const API_LIKES_URL = (messageID) => `https://vlad-happy-thoughts-api.herokuapp.com/thoughts/${messageID}/likes`
 
-
-// https://vlad-happy-thoughts-api.herokuapp.com/
-
-
-// export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
-// export const API_LIKES_URL = (messageID) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${messageID}/like`
