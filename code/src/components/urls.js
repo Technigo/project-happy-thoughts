ce574a588e0c95e1906e927a57ce51b64b9505b8
vladjnbykov <67795638+vladjnbykov@users.noreply.github.com>
2021-05-20 11:10:19 +0200

    changing urls

--- code/src/components/urls.js
+++ code/src/components/urls.js
@@ -1,4 +1,11 @@
 /* eslint-disable linebreak-style */
 
-export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
-export const API_LIKES_URL = (messageID) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${messageID}/like`
+export const API_URL = 'https://vlad-happy-thoughts-api.herokuapp.com/thoughts'
+export const API_LIKES_URL = (messageID) => `https://vlad-happy-thoughts-api.herokuapp.com/thoughts/${messageID}/likes`
+
+
+// https://vlad-happy-thoughts-api.herokuapp.com/
+
+
+// export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
+// export const API_LIKES_URL = (messageID) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${messageID}/like`
