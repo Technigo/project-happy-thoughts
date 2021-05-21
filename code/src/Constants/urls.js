71f6b01b789658374f0e1f5ad35640d2e84435cc
annalenksjo <75678284+annalenksjo@users.noreply.github.com>
2021-05-21 09:38:23 +0200

    added my own api

--- code/src/Constants/urls.js
+++ code/src/Constants/urls.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts' 
-export const POST_HEART_URL = (messageID) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${messageID}/like`
+export const API_URL = 'https://a-lenksjo-happy-thoughts-api.herokuapp.com/thoughts' 
+export const POST_HEART_URL = (messageID) =>  `https://a-lenksjo-happy-thoughts-api.herokuapp.com/thoughts/${messageID}/likes`
