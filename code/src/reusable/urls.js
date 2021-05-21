88991604723b9e27efd6fb3baccde8027b27743b
Laima Duhovnaja <laima@macbook-air.mynet>
2021-05-21 10:21:28 +0200

    changed url

--- code/src/reusable/urls.js
+++ code/src/reusable/urls.js
@@ -3,4 +3,4 @@ export const API_URL = "https://my-happy-thoughts-app.herokuapp.com/thoughts";
 
 // export const LIKES_URL = (id) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${id}/like`;
 export const LIKES_URL = (id) =>
-  `https://happy-thoughts-technigo.herokuapp.com/thoughts/${id}/likes`;
+  `https://my-happy-thoughts-app.herokuapp.com/thoughts/${id}/likes`;
