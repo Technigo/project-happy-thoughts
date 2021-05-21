165f44514382e064fd2b17afe8a86cd507702454
Sofarz <sofia.wallen@gmail.com>
2021-05-21 21:36:30 +0200

    troubleshooting

--- code/src/App.js
+++ code/src/App.js
@@ -18,7 +18,7 @@ export const App = () => {
 
   useEffect(() => {
     fetchMessageList()
-  }, [page, perPage])
+  }, [API_URL])
 
   const fetchMessageList = () => {
     fetch(API_URL(page, perPage))
