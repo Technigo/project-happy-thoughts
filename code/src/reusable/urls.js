be82c7a09845844faabdd555c34ac62ab76342d7
Erika Andersson Porath <erika.porath@hotmail.com>
2021-05-21 10:37:03 +0200

    updated urls

--- code/src/reusable/urls.js
+++ code/src/reusable/urls.js
@@ -1,2 +1,2 @@
-export const API_URL = (page) =>`http://localhost:8080/thoughts?page=${page}`
-export const LIKES_URL = (messageID) => `http://localhost:8080/thoughts/${messageID}/like`
+export const API_URL = (page) =>`https://happy-nobel.herokuapp.com/thoughts?page=${page}`
+export const LIKES_URL = (messageID) => `https://happy-nobel.herokuapp.com/thoughts/${messageID}/like`
