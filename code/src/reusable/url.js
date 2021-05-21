1a25e7ab5700b63c42895c798dea9117dc5efbac
Maria Sparre <you@example.com>
2021-05-21 12:13:25 +0200

    added my own backend api

--- code/src/reusable/url.js
+++ code/src/reusable/url.js
@@ -1,5 +1,5 @@
 
 // GET This will return the latest 20 thoughts from the API
-export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts"
+export const API_URL = "https://all-happy-thoughts.herokuapp.com/thoughts"
 
-export const API_URL_LIKES = (messageID) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${messageID}/like` //why do I need backticks and $, no ""?, --to pass argument to a string.
+export const API_URL_LIKES = (messageID) => `https://all-happy-thoughts.herokuapp.com/thoughts/${messageID}/likes` //why do I need backticks and $, no ""?, --to pass argument to a string.
