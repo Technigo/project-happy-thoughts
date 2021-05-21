113fa30ae06bcb3d47ce9702c60e605b76d9069f
Åsa Hildebrand <asahildebrand@yahoo.com>
2021-05-21 22:22:47 +0200

    Try change fetch

--- code/src/App.js
+++ code/src/App.js
@@ -36,8 +36,8 @@ export const App = () => {
     
   fetch(API_URL, options)
     .then(res => res.json())
-    .then(receivedMessage => setThoughtsList([receivedMessage, ...thoughtsList]));
-  
+    //.then(receivedMessage => setThoughtsList([receivedMessage, ...thoughtsList]));
+    .then(() => fetchMessageList())
     setNewMessage('')
   }
 
