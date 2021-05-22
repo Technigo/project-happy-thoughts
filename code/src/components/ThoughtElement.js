296ffe6a5e3c936efc88035868104b7573bcdd76
Marielle Ohlsson <ohlssonmarielle@gmail.com>
2021-05-22 18:58:23 +0200

    changed back the id

--- code/src/components/ThoughtElement.js
+++ code/src/components/ThoughtElement.js
@@ -7,7 +7,7 @@ const ThoughtElement = ({ thought, onLikesIncrease }) => {
         <div className="thoughts-container">
             <h4>{thought.message}</h4>
             <div className="likes-and-time-container">
-                <button className="heart-button" onClick={() => onLikesIncrease(thought.id)}>
+                <button className="heart-button" onClick={() => onLikesIncrease(thought._id)}>
                     <span className={thought.hearts === 0 ? "heart-grey" : "heart-pink"} role="img" aria-label="heart-icon">
                         ❤️
                    </span>
