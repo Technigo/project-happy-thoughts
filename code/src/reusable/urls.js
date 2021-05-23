636916cab6d46ea27dbf055e3a4fa7dd8e5aff67
liaback <felicia.liaback@gmail.com>
2021-05-23 11:11:35 +0200

    change URL

--- code/src/reusable/urls.js
+++ code/src/reusable/urls.js
@@ -1,4 +1,5 @@
 
-export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
-export const LIKES_URL = (THOUGHT_ID) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${THOUGHT_ID}/like`;
- 
+// export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
+
+export const API_URL = "https://happy-thoughts-fll.herokuapp.com/thoughts";
+export const LIKES_URL = (THOUGHT_ID) => `https://happy-thoughts-fll.herokuapp.com/thoughts/${THOUGHT_ID}/like`;
