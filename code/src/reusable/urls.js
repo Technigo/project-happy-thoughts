f832d2a97b9ccf3f93afaf6c32ceee41ea073f92
liaback <felicia.liaback@gmail.com>
2021-05-23 11:35:48 +0200

    change URL for likes

--- code/src/reusable/urls.js
+++ code/src/reusable/urls.js
@@ -2,4 +2,4 @@
 // export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
 
 export const API_URL = "https://happy-thoughts-fll.herokuapp.com/thoughts";
-export const LIKES_URL = (THOUGHT_ID) => `https://happy-thoughts-fll.herokuapp.com/thoughts/${THOUGHT_ID}/like`;
+export const LIKES_URL = (_id) => `https://happy-thoughts-fll.herokuapp.com/thoughts/${_id}/like`;
