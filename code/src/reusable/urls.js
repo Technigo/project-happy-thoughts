73c7c1cd58d0672e68f40086b81c04ccd004daf2
liaback <felicia.liaback@gmail.com>
2021-05-23 11:39:46 +0200

    fix URL

--- code/src/reusable/urls.js
+++ code/src/reusable/urls.js
@@ -2,4 +2,4 @@
 // export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
 
 export const API_URL = "https://happy-thoughts-fll.herokuapp.com/thoughts";
-export const LIKES_URL = (_id) => `https://happy-thoughts-fll.herokuapp.com/thoughts/${_id}/like`;
+export const LIKES_URL = (id) => `https://happy-thoughts-fll.herokuapp.com/thoughts/${id}/likes`;
