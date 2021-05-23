bad7ec161037c19f39555ea7e49d32080e7c5f01
Sari-Anne Peltola <sarianne.peltola@gmail.com>
2021-05-23 18:12:25 +0200

    changed the API address to my own

--- code/src/reusable/urls.js
+++ code/src/reusable/urls.js
@@ -1,2 +1,3 @@
-export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
-export const LIKES_URL = (messageID) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${messageID}/like`
+export const API_URL = 'https://project-happy-thoughts-sari.herokuapp.com/thoughts'
+
+export const LIKES_URL = (messageID) => `https://project-happy-thoughts-sari.herokuapp.com/thoughts/${messageID}/like`
