f187bc80e2dcb7e3fe68ccbb23cd24d7331b1aef
M4L5 <malin.gu2017@gmail.com>
2021-05-26 21:27:42 +0200

    saved changes regarding the url

--- code/src/reusable/urls.js
+++ code/src/reusable/urls.js
@@ -3,4 +3,4 @@
 // export const HEARTS_URL = (message_id) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${message_id}/like`;
 
 export const HAPPY_THOUGHTS_URL = 'https://goethe-happy-thoughts.herokuapp.com/thoughts';
-export const HEARTS_URL = (message_id) => `https://goethe-happy-thoughts.herokuapp.com/thoughts/${_id}/like`;
+export const HEARTS_URL = (_id) => `https://goethe-happy-thoughts.herokuapp.com/thoughts/${_id}/like`;
