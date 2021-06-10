e1bc347d8760f37be6e0099bbc286425387e969f
Elaine Bergstrom <elainebergstrom26@gmail.com>
2021-06-10 20:59:21 +0200

    update message input, clears after submit, added placeholder

--- code/src/components/MessageForm.js
+++ code/src/components/MessageForm.js
@@ -9,9 +9,11 @@ const MessageForm = ({ messageNew, onMessageNewChange, onFormSubmit }) => {
       <form className="new-message-form" onSubmit={onFormSubmit}>
         <label htmlFor="newMessage">What's making you happy right now?</label>
         <textarea
-          className="new-message"
+          className= {messageNew.length > 140 ? 'textarea-invalid' : 'textarea-valid'}
           id="newMessage"
           type="text"
+          rows="5"
+          placeholder="Share the happy!"
           value={messageNew}
           onChange={onMessageNewChange}
         ></textarea>
