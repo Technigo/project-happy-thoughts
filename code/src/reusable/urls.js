c6ccdcad928a41e07026db3974790ae7521a7dd4
Elaine Bergstrom <elainebergstrom26@gmail.com>
2021-06-10 18:53:18 +0200

    ... that darn missing 's' on likes

--- code/src/reusable/urls.js
+++ code/src/reusable/urls.js
@@ -1,2 +1,2 @@
 export const API_URL = 'https://happy-thoughts-only.herokuapp.com/'
-export const LIKES_URL = (messageID) => `https://happy-thoughts-only.herokuapp.com/thoughts/${messageID}/like`
+export const LIKES_URL = (messageID) => `https://happy-thoughts-only.herokuapp.com/thoughts/${messageID}/likes`
