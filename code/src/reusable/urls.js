7ef75957761689ff7a52d5942dd7fdf731b59b43
Elaine Bergstrom <elainebergstrom26@gmail.com>
2021-06-10 19:14:52 +0200

    update API_URL

--- code/src/reusable/urls.js
+++ code/src/reusable/urls.js
@@ -1,3 +1,3 @@
-export const API_URL = 'https://happy-thoughts-only.herokuapp.com/'
+export const API_URL = 'https://happy-thoughts-only.herokuapp.com/thoughts'
 export const LIKES_URL = (_id) => `https://happy-thoughts-only.herokuapp.com/thoughts/${_id}/likes`
 
