a05220a56ae2aff55b8e2bfa3bb6ed746d18b3d6
NoemiSalome <n.s.frischknecht@gmail.com>
2021-06-11 09:06:24 +0200

    corrected URL

--- code/src/reusable/urls.js
+++ code/src/reusable/urls.js
@@ -1,2 +1,2 @@
 export const API_URL = 'https://happy-thoughts-noemi.herokuapp.com/thoughts'; 
-export const LIKES_URL = (THOUGHTID) => `https://happy-thoughts-noemi.herokuapp.com/thoughts/${THOUGHTID}/like`
+export const LIKES_URL = (thoughtId) => `https://happy-thoughts-noemi.herokuapp.com/thoughts/${thoughtId}/like`
