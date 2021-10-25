a716b30a6d3a7a90fdf7610e5d9331e6fd1b028c
Lovisa Carling <lovisa.carling@gmail.com>
2021-10-25 19:06:05 +0200

    removed unused variable

--- code/src/App.js
+++ code/src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import moment from 'moment'
 import { API_URL } from 'urls'
-import { LIKE_URL } from 'urls'
 import { Form } from 'Form'
 
 export const App = ({ username }) => {
--- code/src/urls.js
+++ code/src/urls.js
@@ -1,3 +1,1 @@
 export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
-export const LIKE_URL =
-  'POST https://happy-thoughts-technigo.herokuapp.com/thoughts/thought.id/like'
