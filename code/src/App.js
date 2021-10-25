b4dae7eb7b3e5aeab9ae2177b5c4d1221a02ec97
mamite100 <mamite.andersson@student.kyh.se>
2021-10-25 20:52:01 +0200

    first commit fetched the API and made messages visible in the browser

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import moment from 'moment';
 
 export const App = () => {
+  const [thoughts, setThoughts]= useState ([]); 
+  const [newThought, setNewThought] = useState ('');
+  
+  
+  useEffect(()=> {
+    fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts' )
+      .then(res => res.json())
+      .then(data => setThoughts (data)); 
+
+  }, [] ); 
+
+const onFormSubmit = (event) => {
+  event.preventDefault();
+
+  const options = {
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/json', 
+      },
+     // converting to JSON is the same as wrapping the message
+      body: JSON.stringify({message: newThought}), 
+  };
+
+  fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts', options)
+  .then(res => res.json())
+  .then((data) => setThoughts([data, ...thoughts]));
+};
+
+
   return (
-    <div>
-      Find me in src/app.js!
+     <div>
+          <form onSubmit ={onFormSubmit} >
+<label htmlFor="newThought">Type your thought</label>
+<input 
+        id="newThought"
+        type= "text"
+        value={newThought}
+        onChange= {(event) => setNewThought(event.target.value)} 
+       />
+       <button type="submit">Send thought!</button>
+    
+          </form>
+
+         {thoughts.map(thought=>(
+
+          <div key={thought._id}>
+             <p>{thought.message}</p>
+            <button> &hearts;{thought.hearts}</button>
+            <p className="date">- Createt at: {moment (thought.createdA ).fromNow()}
+            </p>
+     </div>
+    ))}
+    
     </div>
-  )
-}
+  );
+};
