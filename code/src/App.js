0ac238f24a6980203fb506149e7075493d60c741
A1eksa <80787783+A1eksa@users.noreply.github.com>
2021-10-25 17:06:07 +0200

    likes

--- code/src/App.js
+++ code/src/App.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
+import moment from "moment";
 
 export const App = () => {
   const [thoughts, setThoughts] = useState([]);
+  const [newThought, setNetThought] = useState("");
   useEffect(() => {
     fetch("https://happy-thoughts-technigo.herokuapp.com/thoughts")
       .then((res) => res.json())
@@ -10,9 +12,15 @@ export const App = () => {
   console.log(thoughts);
   return (
     <div>
+      <form>
+        <label>Type your thought</label>
+        <input type="text" />
+      </form>
       {thoughts.map((thought) => (
-        <div>
+        <div key={thought._id}>
           <p>{thought.message}</p>
+          <button className="btn">&hearts;{thought.hearts}</button>
+          <p className="date">Created:{moment(thought.createdAt).fromNow()}</p>
         </div>
       ))}
     </div>
