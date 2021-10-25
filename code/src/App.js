e083d515118da73036b1f7717938c7bc7c3803fd
jakobxlindstrom <jake@lindstroem.se>
2021-10-25 21:58:07 +0200

    initial commit

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import API_URL from './utils/Commons'
+import moment from 'moment'
+import Form from 'components/Form'
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState([])
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => setThoughts(data))
+  })
+
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <>
+      <Form />
+      {thoughts.map((thought) => (
+        <div className="main-container" key={thought.id}>
+          <p>{thought.message} </p>
+          <button> &hearts; {thought.hearts} </button>
+          <p className="date">
+            -- Thought shared at: {moment(thought.createdAt).fromNow()} --{' '}
+          </p>
+        </div>
+      ))}
+      <div>Dev by Jakob </div>
+    </>
   )
 }
---
+++ code/src/components/Form.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import API_URL from 'utils/Commons'
+
+const Form = ({ thoughts, setThoughts }) => {
+  const [newThought, setNewThought] = useState('')
+
+  const onNewThoughtChange = (e) => {
+    setNewThought(e.target.value)
+  }
+
+  const onFormSubmit = (e) => {
+    e.preventDefault()
+
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ message: newThought }),
+    }
+    fetch(API_URL, options)
+      .then((response) => response.json)
+      .then((data) => setThoughts([data, ...thoughts]))
+  }
+
+  return (
+    <>
+      <div>
+        <form onSubmit={onFormSubmit}>
+          <label htmlfor="newThought"> Write a happy thought </label>
+          <input
+            type="textarea"
+            rows="3"
+            id="newThought"
+            value={newThought}
+            onChange={onNewThoughtChange}
+            placeholder="Write a happy thought"
+          />
+          <button
+            className="submit-button"
+            type="submit"
+            disabled={newThought.length < 6 || newThought.length > 140}
+          >
+            {' '}
+            Share thought!{' '}
+          </button>
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default Form
