f70dc22c201011e25229a0e5bf2b3f6ce4d7f90d
dandeloid <82587220+dandeloid@users.noreply.github.com>
2021-10-25 20:25:01 +0200

    base build+styling

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import moment from 'moment'
+
+import { API_URL } from './utils/urls'
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState([])
+  const [newThought, setNewThought] = useState('')
+  //const [heart, setHeart] = useState()
+
+  useEffect (() => {
+    fetch(API_URL)
+    .then((res) => res.json())
+    .then((data) => setThoughts(data))
+  }, [])
+
+  const onFormSubmit = (event) => {
+    event.preventDefault()
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ message: newThought }),
+    }
+
+    fetch(API_URL, options)
+      .then((res) => res.json())
+      .then((data) => setThoughts([data, ...thoughts]))
+  }
+
   return (
     <div>
-      Find me in src/app.js!
+      <div className="form-container">
+        <form onSubmit={onFormSubmit}>
+          <label htmlFor="newThought">What's making you happy right now?</label>
+          <textarea 
+            id="newThought" 
+            type="text"
+            maxLength="140"
+            value={newThought} 
+            onChange={(e) => setNewThought(e.target.value)}
+          />
+          <button className="send-button" type="submit">
+            <span className="heart" role="img" aria-label="heart">❤️  </span>  
+              Send Happy Thought  
+            <span className="heart" role="img" aria-label="heart">  ❤️</span>
+          </button>
+        </form>
+      </div>
+      
+      {thoughts.map((thought) => (
+        <div className="thought-container" key={thought._id}>
+          <p className="thought-message">{thought.message}</p>
+          <div className="thought-info">
+            <div className="thought-heart">
+              <button className="heart-button"><span className="heart" role="img" aria-label="heart">❤️</span></button>
+              <span> x {thought.hearts}</span>
+            </div>
+            <p className="date"> {moment(thought.createdAt).fromNow()} </p>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
---
+++ code/src/utils/urls.js
@@ -0,0 +1,2 @@
+export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
+export const API_URL_HEARTS = (thoughtID) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtID}/like`
