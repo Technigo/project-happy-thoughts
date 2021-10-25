3a905032a7974bd0c1211a34475e212e9f2ce027
A1eksa <80787783+A1eksa@users.noreply.github.com>
2021-10-25 16:01:14 +0200

    displayed thoughts

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState([]);
+  useEffect(() => {
+    fetch("https://happy-thoughts-technigo.herokuapp.com/thoughts")
+      .then((res) => res.json())
+      .then((data) => setThoughts(data));
+  }, []);
+  console.log(thoughts);
   return (
     <div>
-      Find me in src/app.js!
+      {thoughts.map((thought) => (
+        <div>
+          <p>{thought.message}</p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
