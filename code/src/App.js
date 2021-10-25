f8a3b56c457c62d890a4f8a01b6e5edde546e6da
Jessica Nygren <jessicanygren@C02D54BJMD6R.local>
2021-10-25 20:43:55 +0200

    coding along with Maks, styled the basics

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import moment from 'moment'
+
+import { API_URL } from './utils/urls'
 
 export const App = () => {
+const [thoughts, setThoughts] = useState([])
+const [newThought, setNewThought] = useState('')
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => setThoughts(data))
+  }, []) 
+
+  const onFormSubmit = (event) => {
+    event.preventDefault()
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ message: newThought })
+    }
+
+    fetch(API_URL, options)
+      .then(res => res.json())
+      .then(data => setThoughts([data, ...thoughts]))
+  }
+
   return (
     <div>
-      Find me in src/app.js!
+      <form className="form" onSubmit={onFormSubmit}>
+        <label htmlFor='newThought'className='input-label'>Type your thought</label>
+        <input 
+        className='text-input'
+          id='newThought'
+          type='text' 
+          value={newThought} 
+          onChange={(e) => setNewThought(e.target.value)}
+          />
+        <button type='submit' className='btn-send'>Send happy thought <span role='img' aria-label='heart emoji'>❤️</span></button>
+      </form>
+
+     {thoughts.map(thought => (
+       <div key={thought._id} className='thought-item'>
+         <p className='thought-message'>{thought.message}</p>
+         <div className='likesAndTime'>
+          <button className='btn'><span className='btn-heart' role='img' aria-label='heart emoji'>❤️</span> x {thought.hearts}</button>
+          <p className='time'>{moment(thought.createdAt).fromNow()}</p>
+         </div>
+       </div>
+      ))}
     </div>
-  )
+)
 }
