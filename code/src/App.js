323ca355475ee18869c0a31f5a3936cbad42269b
Linnea <linnea.iseb@gmail.com>
2021-10-25 14:50:13 +0200

    First commit. Added functionality, the app is getting data from the API and allows me to post data to it

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+import { API_URL } from './utils/urls';
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState([]);
+  const [newThought, setNewThoughts] = useState('');
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => setThoughts(data));
+  }, []);
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ message: newThought }),
+    };
+
+    fetch(API_URL, options)
+      .then((res) => res.json())
+      .then((data) => setThoughts([data, ...thoughts]));
+  };
+
   return (
     <div>
-      Find me in src/app.js!
+      <form className="newThought" onSubmit={onFormSubmit}>
+        <label>What is making you happy right now?</label>
+        <input
+          id="newThought"
+          type="text"
+          value={newThought}
+          onChange={(e) => setNewThoughts(e.target.value)}
+        />
+        <button type="submit">&hearts; Send happy thought! &hearts;</button>
+      </form>
+      {thoughts.map((thought) => (
+        <div className="posted-thoughts" key={thought._id}>
+          <p>{thought.message}</p>
+          <button> &hearts; {thought.hearts}</button>
+          <p className="dage">Created at: {moment(thought.createdAt).fromNow()}</p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
