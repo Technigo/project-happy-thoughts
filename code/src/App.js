a8d80c3f39d2d75e74c6cc992bfde364fdbc6f0c
loulunds <lousanne.lundstrom@gmail.com>
2021-10-26 13:38:48 +0200

    sending message to backend done

--- code/src/App.js
+++ code/src/App.js
@@ -1,5 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import moment from 'moment';
+
+import { API_URL } from './utils/urls';
 
 export const App = () => {
-  return <div>Find me in src/app.js!</div>;
+  const [thoughts, setThoughts] = useState([]); // empty array
+  const [newThought, setNewThought] = useState('');
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => setThoughts(data));
+  }, []);
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ message: newThought })
+    };
+    fetch(API_URL, options)
+      .then((res) => res.json())
+      .then((data) => setThoughts([data, ...thoughts]), setNewThought(''));
+  };
+
+  return (
+    <div>
+      <form onSubmit={onFormSubmit}>
+        <label htmlFor='newThought'>
+          Type your thought:
+          <input
+            id='newThought'
+            type='text'
+            value={newThought}
+            onChange={(e) => setNewThought(e.target.value)}
+          />
+        </label>
+        <button type='submit'>Send thought!</button>
+      </form>
+      {thoughts.map((thought) => (
+        // eslint-disable-next-line no-underscore-dangle
+        <div key={thought._id}>
+          <p>{thought.message}</p>
+          <button type='button'> &hearts;{thoughts.hearts}</button>
+          <p className='date'>
+            - Created at: {moment(thoughts.createdAt).fromNow()}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
 };
