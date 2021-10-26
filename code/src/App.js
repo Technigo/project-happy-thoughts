08aa93d6887228d40cf0e820b6d7405f4a39338b
Elin Elmvik Diczfalusy <elinelmvikdiczfalusy@Elins-iMac.local>
2021-10-26 15:43:38 +0200

    rewrite first lesson for better understanding, added first layer of styling

--- code/src/App.js
+++ code/src/App.js
@@ -2,58 +2,89 @@ import React, { useState, useEffect } from "react";
 import moment from "moment";
 import { API_URL } from "./utils/urls";
 
-export const App = () => {
-  const [thoughts, setThoughts] = useState([
-    { message: "Hello", createdAt: "2021/10/25" },
-  ]);
+function App() {
+  // * * * * * states * * * * *
   const [newThought, setNewThought] = useState("");
+  const [thoughts, setThoughts] = useState([]);
+  console.log("här kan jag skriva uaf");
+  console.log(newThought);
 
+  // * * * * * useEffect * * * * *
   useEffect(() => {
     fetch(API_URL)
       .then(res => res.json())
-      .then(data => setThoughts(data));
+      .then(data => {
+        console.log(data);
+        setThoughts(data);
+      });
   }, []);
 
-  console.log(thoughts);
-
-  const onFormSubmit = event => {
-    event.preventDefault();
+  // * * * * * function * * * * *
+  const onFormSubmit = e => {
+    e.preventDefault();
 
     const options = {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-type": "application/json" },
       body: JSON.stringify({ message: newThought }),
+      // we have to wrapp it to able to send it (other way around from getting it)
     };
 
     fetch(API_URL, options)
       .then(res => res.json())
-      .then(data => setThoughts([data, ...thoughts]));
+      .then(data => {
+        console.log("this is fetch2 with POST", data);
+        setThoughts([data, ...thoughts]);
+      });
+  };
+
+  const onSetThoughtChange = e => {
+    setNewThought(e.target.value);
   };
 
   return (
-    <div>
-      <form onSubmit={onFormSubmit}>
-        <label htmlFor="newThought">Type your thought</label>
-        <input
-          id="newThought"
-          type="text"
-          value={newThought}
-          onChange={e => setNewThought(e.target.value)}
-        />
-        <button type="submit">Send thought</button>
+    <>
+      <form onSubmit={onFormSubmit} className="question-card">
+        <div>
+          <label htmlFor="newThought" className="question">
+            What is making you happy right now?
+          </label>
+          <input
+            id="newThought"
+            type="text"
+            placeholder="Type your happy thougt here"
+            value={newThought}
+            onChange={onSetThoughtChange}
+          ></input>
+        </div>
+        <button className="submit-btn" type="submit">
+          <span className="icon-btn">&hearts;</span>Send Happy Thought
+          <span className="icon-btn">&hearts;</span>
+        </button>
       </form>
 
-      {thoughts.map(thought => (
-        <div key={thought._id}>
-          <p>{thought.message}</p>
-          <button> &hearts; {thought.hearts}</button>
-          <p className="date">
-            - Created at: {moment(thought.createdAt).fromNow()}
-          </p>
-        </div>
-      ))}
-    </div>
+      {thoughts.map(thought => {
+        return (
+          <>
+            <div className="answer-card">
+              <div className="answer">
+                <p>{thought.message}</p>
+                <button className="heart-btn">
+                  {" "}
+                  <span className="heart">&hearts;</span> x {thought.hearts}
+                </button>
+                <p className="date">
+                  -Created at: {moment(thought.createdAt).fromNow()}
+                </p>
+              </div>
+            </div>
+          </>
+        );
+      })}
+
+      <div> icon</div>
+    </>
   );
-};
+}
+
+export default App;
