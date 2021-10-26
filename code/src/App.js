b51822af2960c95a43bb68bb1563193b13f26662
Eddie Lundgren <Eddie.lundgren@hotmail.com>
2021-10-26 15:07:02 +0200

    added components to split up the code

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import moment from "moment";
+import { Form } from "Form";
+import { API_URL } from "./utils/urls";
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => setThoughts(data));
+  }, []);
+
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+    <section className="container">
+      <Form thoughts={thoughts} setThoughts={setThoughts} />
+
+      <div>
+        {thoughts.map(thought => (
+          <div key={thought._id}>
+            <p>{thought.message}</p>
+            <button> &hearts; {thought.hearts}</button>
+            <p className="date">
+              - Created: {moment(thought.createdAt).fromNow()}
+            </p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
