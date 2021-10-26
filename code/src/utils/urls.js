98eaef4deb8800c71a37d7491888734b3c9ae35b
Lisa Blomé <81302626+annaester@users.noreply.github.com>
2021-10-26 21:15:55 +0200

    input, get and post

---
+++ code/src/utils/urls.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
+
+//GET https://happy-thoughts-technigo.herokuapp.com/thoughts
+//POST https://happy-thoughts-technigo.herokuapp.com/thoughts
+//POST HEARTS https://happy-thoughts-technigo.herokuapp.com/thoughts/THOUGHT_ID/like
