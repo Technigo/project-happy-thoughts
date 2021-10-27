ec52f70357e0954d7c1e97f5a9705a86e07de448
Eddie Lundgren <Eddie.lundgren@hotmail.com>
2021-10-27 22:34:25 +0200

    some styling

--- code/src/Components/Form.js
+++ code/src/Components/Form.js
@@ -2,26 +2,25 @@ import React from "react";
 
 export const Form = ({ newThought, onNewThoughtChange, onFormSubmit }) => {
   return (
-    <section className="input-container">
-      <form onSubmit={onFormSubmit}>
-        <div className="input-text">
-          <label htmlFor="newThought">
-            <h1>Type your thought</h1>
-          </label>
-          <textarea
-            rows="3"
-            id="newThought"
-            type="text"
-            value={newThought}
-            onChange={onNewThoughtChange}
-            placeholder="Share your thoughts!"
-          />
+    <form onSubmit={onFormSubmit}>
+      <div className="input-container">
+        <label htmlFor="newThought">
+          <h1>Share something that makes you happy!</h1>
+        </label>
+        <textarea
+          rows="3"
+          id="newThought"
+          className="message-box"
+          type="text"
+          value={newThought}
+          onChange={onNewThoughtChange}
+          placeholder="Share your thoughts!"
+        />
 
-          <button className="submit-button" type="submit">
-            &#10084;&#65039;Send Happy Thought!&#10084;&#65039;
-          </button>
-        </div>
-      </form>
-    </section>
+        <button className="submit-button" type="submit">
+          &#10084;&#65039;Send Happy Thought!&#10084;&#65039;
+        </button>
+      </div>
+    </form>
   );
 };
