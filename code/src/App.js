43de040ee8c28693b5c349b0a9aabcbb271fc2f6
Jenny Quach <quach.jen@gmail.com>
2021-10-27 12:59:25 +0200

    initial commit

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
-    </div>
+  const [thoughts, setThoughts] = useState([]) //should be empty array because later it will be non empty array
+
+  useEffect (() => {
+    fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts')
+    .then((res) => res.json())
+    .then((data) => setThoughts(data))
+  }, []) //pass an empty array
+
+  console.log(thoughts)
+
+
+
+  return ( //component renders some js6. After js6 rendered first time then useEffect gets triggered with console log
+    <div>Find me in src/app.js!</div>
   )
 }
