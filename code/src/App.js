3822533ba72c09e968449f21685bf3cbffe557d3
pdetli <74938586+pdetli@users.noreply.github.com>
2021-10-27 23:06:05 +0200

    coded along with Maxy

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from "react"
+import moment from "moment"
+
+import { API_URL } from "utils/urls"
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState([])
+  const [newThought, setNewThought] = useState("")
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => setThoughts(data))
+  }, [])
+
+  const onFormSubmit = (event) => {
+    event.preventDefault()
+
+    // Send a POST request with a JSON body
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: newThought }),
+    }
+
+    fetch(API_URL, options)
+      .then((res) => res.json())
+      .then((data) => setThoughts([data, ...thoughts]))
+  }
+
   return (
     <div>
-      Find me in src/app.js!
+      {/* this is only for the button and the box to type the thoughts */}
+      <form onSubmit={onFormSubmit}>
+        <label htmlFor="newThought">Type your thought</label>
+        <input
+          id="newThought"
+          type="text"
+          value={newThought}
+          onChange={(e) => setNewThought(e.target.value)}
+        />
+        <button type="submit">Send thought!</button>
+      </form>
+
+      {thoughts.map((thought) => (
+        <div key={thought._id}>
+          <p>{thought.message}</p>
+          <button> &hearts; {thought.hearts}</button>
+          <p className="date">
+            - Created at: {moment(thought.createdAt).fromNow()}
+          </p>
+        </div>
+      ))}
     </div>
   )
 }
