15d86cd42abd3a4f6bd941f8894992166aeaa44a
Jenny Quach <quach.jen@gmail.com>
2021-10-28 22:25:22 +0200

    posting thoughts works

--- code/src/App.js
+++ code/src/App.js
@@ -1,19 +1,57 @@
 import React, { useState, useEffect } from 'react'
+import moment from 'moment'
+
+import { API_URL } from './utils/urls'
 
 export const App = () => {
-  const [thoughts, setThoughts] = useState([]) //should be empty array because later it will be non empty array
+  const [thoughts, setThoughts] = useState([{message: 'hello', createdAt: '2021/10/12', hearts: 1, _id: 'asdf'}]) //should be empty array because later it will be non empty array
+  const [newThought, setNewThought] = useState ('')
 
+//calling useEffect after the component gets mounted
   useEffect (() => {
-    fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts')
-    .then((res) => res.json())
-    .then((data) => setThoughts(data))
-  }, []) //pass an empty array
+    fetch(API_URL) // fetch request when component is mounted to get the data from the API
+      .then((res) => res.json())  //unpack json                  
+      .then((data) => setThoughts(data)) //save the data in the state
+  
+    }, []) //pass an empty array
 
-  console.log(thoughts)
+  const onFormSubmit = (event) => {
+    event.preventDefault() //because we dont want the submission of the form to refresh the page
+  
+    const options = {
+      method: 'POST',
+      headers: { //for the API to know what type of data we will send
+        'Content-Type': 'application/json', //type is json
+      },
+      body: JSON.stringify({ message: 'newThought' }) //"message" is the key of the object and value is the "my happy thought"
+    }
 
+    fetch(API_URL, options)
+      .then(res => res.json())
+      .then(newThought => setThoughts([newThought, ...thoughts]))
+  }
 
+  return ( //since state is updated, component renders some JS6. After JS6 rendered first time then useEffect gets triggered with console log
+    <div>
+      <form onSubmit={onFormSubmit}>
+        <label htmlFor="newThought">Type your thought</label> 
+        <input 
+          id="newThought"
+          type="text" 
+          value={newThought}
+          onChange={e => setNewThought(e.target.value)}/>
+          <button type="submit">Send thought!</button>
+      </form>
 
-  return ( //component renders some js6. After js6 rendered first time then useEffect gets triggered with console log
-    <div>Find me in src/app.js!</div>
+      {thoughts.map(thought => (
+        <div key={thought._id}>
+          <p>{thought.message}</p>
+          <button>&hearts; {thought.hearts}</button>
+          <p className="date">
+            Created date: {moment(thought.createdAt).fromNow()}
+          </p>
+        </div>
+      ))}
+    </div>
   )
 }
