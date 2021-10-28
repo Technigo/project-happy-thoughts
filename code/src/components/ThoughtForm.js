0b86c462c3656dc5027f3bb791597174b25ee0e3
MT-dotse <madelene.trang333@gmail.com>
2021-10-28 21:45:11 +0300

    added some styling to the forms and buttons

--- code/src/components/ThoughtForm.js
+++ code/src/components/ThoughtForm.js
@@ -2,17 +2,24 @@ import React from 'react';
 
 const ThoughtForm = ({ onFormSubmit, newThought, setNewThought }) => {
 	return (
-		<form onSubmit={onFormSubmit}>
-			<label htmlFor="newThought"> Type your thought</label>
-			<input
+		<form className="question-form" onSubmit={onFormSubmit}>
+			<label htmlFor="newThought"> What's making you happy right now?</label>
+			<textarea
 				id="newThought"
 				type="textarea"
+				maxLength="140"
 				value={newThought}
 				onChange={(e) => setNewThought(e.target.value)}
-			/>
+			>
+				{' '}
+			</textarea>
 
-			<button disabled={newThought.length < 5} type="submit">
-				Send thought!
+			<button
+				className="thought-btn"
+				disabled={newThought.length < 5}
+				type="submit"
+			>
+				Send Happy Thought
 			</button>
 		</form>
 	);
--- code/src/components/ThoughtItem.js
+++ code/src/components/ThoughtItem.js
@@ -3,13 +3,19 @@ import moment from 'moment'; //import outer packages
 
 const ThoughtItem = ({ thought, onLikesIncrease }) => {
 	return (
-		<div>
-			<p>{thought.message}</p>
-			<button onClick={() => onLikesIncrease(thought._id)}>
-				{' '}
-				&hearts;{thought.hearts}
-			</button>
-			<p className="date">-Created at:{moment(thought.createdAt).fromNow()}</p>
+		<div className="thought-container">
+			<div className="thought-message">
+				<p>{thought.message}</p>
+			</div>
+			<div className="heart-date-container">
+				<p class="heart-btn">
+					<button onClick={() => onLikesIncrease(thought._id)}>
+						{' '}
+						&hearts;{thought.hearts}
+					</button>
+				</p>
+				<p className="date">{moment(thought.createdAt).fromNow()}</p>
+			</div>
 		</div>
 	);
 };
