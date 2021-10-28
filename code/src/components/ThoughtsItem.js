ba83ca98b18eb7c4352609e5e425315902e8eedd
camekman <ekman.camilla@gmail.com>
2021-10-28 14:14:53 +0200

    styled onclick button and fixed margins etc

--- code/src/components/ThoughtsItem.js
+++ code/src/components/ThoughtsItem.js
@@ -7,15 +7,20 @@ const ThoughtsItem = ({ thought, onLikesIncrease }) => {
     <div className="thought-container">
       <p className="thoughts-text">{thought.message}</p>
       <div className="input-section">
-        <button
-          className="like-button"
-          onClick={() => onLikesIncrease(thought._id)}
-        >
-          <span className="heart-emoji" role="img" aria-label="Heart-emoji">
-            ❤️
-          </span>
-        </button>
-        <p className="likes"> x {thought.hearts}</p>
+        <div className="like-section">
+          <button
+            className="like-button"
+            onClick={() => onLikesIncrease(thought._id)}
+            style={{
+              backgroundColor: thought.hearts > 0 ? "rgb(245, 177, 169)" : "",
+            }}
+          >
+            <span className="heart-emoji" role="img" aria-label="Heart-emoji">
+              ❤️
+            </span>
+          </button>
+          <p className="likes"> x {thought.hearts}</p>
+        </div>
         <p className="date">{moment(thought.createdAt).fromNow()}</p>
       </div>
     </div>
