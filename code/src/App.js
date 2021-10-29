b1abf63290f7ced2bc6a31912c538f25a4d0fa64
Jsfrulle <Jessicamhermansson@gmail.com>
2021-10-29 13:55:15 +0200

    favicon + title

--- code/src/App.js
+++ code/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import AddDataToList from 'AddDataToList'
-
+import { Helmet } from 'react-helmet'
 
 export const App = () => {
 
@@ -9,7 +9,11 @@ return (
 
 <div className="container">
 
-
+<Helmet>
+                <meta charSet="utf-8" />
+                <title>Happy Thought</title>
+                
+            </Helmet>
 
 <AddDataToList />
 
