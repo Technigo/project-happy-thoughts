7a93965de0df9174eacef7ee378256e9ffd2360d
ruruahn <81482699+ruruahn@users.noreply.github.com>
2021-10-29 10:11:53 +0200

    some css changes

--- code/src/Header.js
+++ code/src/Header.js
@@ -4,8 +4,8 @@ import './Header.css'
 export const Header = () => {
     return (
         <div className="header-content">
-            <h1 className="header-text">HAPPINESS CABINET</h1>
-            <h2 className="header-text">Filled with happiness!</h2>
+            <h1 className="header-text">Project-Happy-Thought</h1>
+            <h2 className="header-text">Share the happy thoughts💕</h2>
         </div>
     )
 }
