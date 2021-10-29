f6d74e73f94c841af4279c459399e5bdd1e6a56f
Kara Howes <karahowes@Karas-MacBook-Air.local>
2021-10-29 17:05:03 +0200

    added pink background to ckicked button

--- code/src/components/ThoughtForm.js
+++ code/src/components/ThoughtForm.js
@@ -6,14 +6,14 @@ const ThoughtForm = ({onFormSubmit, setNewThought, newThought}) => {
 
       <form onSubmit={onFormSubmit} className="form-container">
         <label htmlFor="newThought" className="label-form">What's making you Happy right now?</label>
-        <input 
+        <textarea 
           className="thought-input"
           id="newThought"
           type="text" 
           value={newThought}
           onChange= {(e) => setNewThought(e.target.value)}
-          />
-          <button type="submit" className="submit-button">
+          ></textarea>
+          <button disabled = {newThought.length<5 || newThought.length>140}type="submit" className="submit-button">
             <div className="send-wrapper">
               <img src="/assets/red-heart.png" alt="red heart" className="heart"></img> 
               <span className="send-happy">Send Happy Thought</span>
--- code/src/components/ThoughtItem.js
+++ code/src/components/ThoughtItem.js
@@ -5,12 +5,18 @@ const ThoughtItem = ({thought, onLikesIncrease}) => {
 
     return (
         <div className="message-container">
-          <p className="thought-message">{thought.message}</p>
+          <div className="displayed-message">
+            <p className="thought-message">{thought.message}</p>
+          </div>
+  
           <div className="button-row">
             <div className="heart-button-wrapper">
               <button onClick = {() => onLikesIncrease(thought._id)}
-                className="heart-button">
-                  <img src="/assets/red-heart.png" alt="red heart" className="heart"></img> 
+                className="heart-button" style={{
+                  backgroundColor: thought.hearts > 0 ? "#fbabab" : "#f3f2f2",
+                }}>
+                  <img src="/assets/red-heart.png" alt="red heart" className="heart"
+                  ></img> 
                 </button>
               <p> x {thought.hearts}</p>
             </div>
