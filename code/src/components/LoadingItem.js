1444cc03fc7c9af734474fed2afcc74557cb1ebd
Kara Howes <karahowes@Karas-MacBook-Air.local>
2021-10-30 22:08:43 +0200

    added alt tag to image

--- code/src/components/LoadingItem.js
+++ code/src/components/LoadingItem.js
@@ -4,7 +4,7 @@ const LoadingItem = () => {
 
     return (
         <div className="loading-overlay">
-            <img src="/assets/star.png"></img>
+            <img src="/assets/star.png" alt ="star spinning icon"></img>
         </div>
     )
 }
