f30f8a207d94c7b9f458a6f0cf79181df6607b35
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2021-10-30 11:39:47 +0200

    add :focus-effect

--- code/src/components/CharCounter.js
+++ code/src/components/CharCounter.js
@@ -4,7 +4,7 @@ const CharCounter = (props) => {
   const { countChar } = props
   return (
     <p className={
-      countChar < 4 || countChar > 140 ? "red-text" : "black-text"
+      countChar < 5 || countChar > 140 ? "red-text" : "black-text"
     }>{countChar}/140</p>
   )
 }
