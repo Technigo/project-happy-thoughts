4b31e2c381c78dcdadfbd774548c6191b7b188c0
Rephili <72921631+Rephili@users.noreply.github.com>
2021-10-30 12:23:52 +0200

    started with the project

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import moment from "moment";
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://happy-thoughts-technigo.herokuapp.com/thoughts")
+      .then((res) => res.json())
+      .then((data) => setThoughts(data));
+  }, []);
+
+  console.log(thoughts);
+
   return (
     <div>
-      Find me in src/app.js!
+      {thoughts.map((thought) => (
+        <div key={thought._id}>
+          <p>{thought.message}</p>
+          <button> &hearts; {thought.hearts}</button>
+          <p>Created at: {moment(thought.createdAt).fromNow()}</p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
