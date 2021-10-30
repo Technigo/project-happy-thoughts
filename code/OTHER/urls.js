a43fc05c8aab413d7c3e1d501dc535f1b1588aa6
Maria Petersson <mariapetersson@Marias-MacBook.lan>
2021-10-30 12:43:42 +0200

    removed other folder

--- code/OTHER/urls.js
+++
@@ -1,3 +0,0 @@
-export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts';
-export const LIKES_URL = (thoughtId) =>
-	`https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`;
