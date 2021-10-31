4dc0442951050b05be78a16fde90e6efd9e539c2
molbimien <43146046+molbimien@users.noreply.github.com>
2021-10-31 18:11:16 +0100

    replaced input with textarea for adding new thoughts

--- code/src/components/ThoughtForm.js
+++ code/src/components/ThoughtForm.js
@@ -4,16 +4,17 @@ const ThoughtForm = ( {onFormSubmit, newThought, setNewThought }) => {
     return (
       <form className="happy-form" onSubmit={onFormSubmit}>
         <label htmlFor="newThought">What's making you happy right now?
-          <input
-            className="newThought"
-            id="newThought"
-            type="text"
-            placeholder="Type your thoughts here..."
-            autoComplete="off"
-            value={newThought}
-            onChange={(e) => setNewThought(e.target.value)}
-            />
-          </label>
+        <textarea
+          className="newThought"
+          id="newThought"
+          type="text"
+          placeholder="Type your thoughts here..."
+          autoComplete="off"
+          value={newThought}
+          onChange={(e) => setNewThought(e.target.value)}
+          >
+        </textarea>
+        </label>
         <button
             disabled={newThought.length < 5}
             className="send-btn"
