d98220caed6e23953d9aa37dd56c45b8d710c4f2
vrill <52007698+vrill@users.noreply.github.com>
2021-11-04 13:13:44 +0100

    Initial commit. Added ability to post msg, with time and amount of likes.

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import moment from 'moment'
+
+import { API_URL } from 'utils/urls'
 
 export const App = () => {
+
+  const [thoughts, setThoughts] = useState([ { message: 'Hello', createdAt: '2021-11-01', hearts: 0, _id: 'abc123' } ])
+  const [newThought, setNewThought] = useState('')
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(data => setThoughts(data))
+  }, [])
+
+  const onFormSubmit = (event) => {
+    event.preventDefault() //stops the submit from refreshing the page
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ message: newThought })
+    }
+
+    fetch(API_URL, options)
+      .then(response => response.json())
+      .then(data => setThoughts([data, ...thoughts]))
+  }
+
+
+
   return (
     <div>
-      Find me in src/app.js!
+
+      <form onSubmit={onFormSubmit}>
+        <label htmlFor="newThough">Type your thought</label>
+        <input
+          id="newThought"
+          type="text"
+          value={newThought}
+          onChange={(event) => setNewThought(event.target.value)}
+        />
+        <button type="submit">Send thought!</button>
+      </form>
+
+      {thoughts.map((thought) => (
+        <div key={thought._id}>
+          <p>{thought.message}</p>
+          <button>&hearts; {thought.hearts}</button>
+          <p className="date">Created at: {moment(thought.createdAt).fromNow()}</p>
+        </div>
+      ))}
+      
     </div>
   )
 }
