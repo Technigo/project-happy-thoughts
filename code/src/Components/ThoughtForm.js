a65b47bab1dbafb82b3507e942270c886c6e4861
A1eksa <80787783+A1eksa@users.noreply.github.com>
2021-12-15 15:39:29 +0100

    left fixed

--- code/src/Components/ThoughtForm.js
+++ code/src/Components/ThoughtForm.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import React from 'react';
 import CharCount from './CharCount';
-=======
-import React from "react";
-import CharCount from "./CharCount";
->>>>>>> b26ebeb9849581edd0bfb79510a39c73be4693c2
 
 const ThoughtForm = ({
   onFormSubmitt,
@@ -15,7 +10,6 @@ const ThoughtForm = ({
   thought,
 }) => {
   return (
-<<<<<<< HEAD
     <div className='thought-wrapper'>
       <form className='form' onSubmit={onFormSubmitt}>
         <p className='form-title'>What is making you happy right now?</p>
@@ -47,37 +41,6 @@ const ThoughtForm = ({
         </div>
       </form>
     </div>
-=======
-    <form className="form" onSubmit={onFormSubmitt}>
-      <p className="form-title">What is making you happy right now?</p>
-      <label
-        htmlFor="label-title"
-        disabled={newThought.length < 5 || newThought.length > 140}
-      ></label>
-      <textarea
-        placeholder="React is making me happy!"
-        className="textarea"
-        id="newThought"
-        type="text"
-        value={newThought}
-        onChange={(e) => setNewThought(e.target.value)}
-      />
-      <div className="counter-btn">
-        <button
-          onClick={handleIncrement}
-          type="submit"
-          id="myInput"
-          className="send-btn"
-        >
-          <span className="send-heart">❤</span>
-          Send happy thought
-          <span className="send-heart">❤</span>
-          {count}times
-        </button>
-        <CharCount charCount={newThought.length} />
-      </div>
-    </form>
->>>>>>> b26ebeb9849581edd0bfb79510a39c73be4693c2
   );
 };
 
--- code/src/Components/ThoughtItem.js
+++ code/src/Components/ThoughtItem.js
@@ -3,7 +3,6 @@ import moment from 'moment';
 
 const ThoughtItem = ({ thought, onLikesIncrease }) => {
   return (
-<<<<<<< HEAD
     <div className='thoughts-card'>
       <div className='test'>
         <p className='thought-p'>{thought.message}</p>
@@ -22,25 +21,8 @@ const ThoughtItem = ({ thought, onLikesIncrease }) => {
             <p>x{thought.hearts} </p>
           </div>
           <p className='date'>{moment(thought.createdAt).fromNow()}</p>
-=======
-    <div className="thoughts-card">
-      <p className="thought-p">{thought.message}</p>
-      <div className="thoughts-container">
-        <div className="heart-likes">
-          <button
-            style={{
-              backgroundColor: thought.hearts > 0 ? "lightpink" : "lightgrey",
-            }}
-            onClick={() => onLikesIncrease(thought._id)}
-            className="btn"
-          >
-            {" "}
-            <span>❤</span>
-          </button>
-          <p>x{thought.hearts} </p>
->>>>>>> b26ebeb9849581edd0bfb79510a39c73be4693c2
         </div>
-        <p className="date">{moment(thought.createdAt).fromNow()}</p>
+        <p className='date'>{moment(thought.createdAt).fromNow()}</p>
       </div>
     </div>
   );
