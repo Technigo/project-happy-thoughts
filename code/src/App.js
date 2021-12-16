0b942b76f63a4e3aa4bc8ca60d2fd2b67a49711d
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2021-12-16 14:19:30 +0100

    update url for posting likes and move state for countChar

--- code/src/App.js
+++ code/src/App.js
@@ -1,66 +1,71 @@
-import React, { useEffect, useState } from 'react'
-import NewThoughts from './components/NewThoughts'
-import Thoughts from './components/Thoughts'
-import {API_URL} from './utils/urls.js'
+import React, { useEffect, useState } from "react";
+import NewThoughts from "./components/NewThoughts";
+import Thoughts from "./components/Thoughts";
+import { API_URL } from "./utils/urls.js";
 
 export const App = () => {
-  const[thoughts, setThoughts] = useState([])
-  const[newThought, setNewThought] = useState('')
-  const[loading, setLoading] = useState(false)
+  const [thoughts, setThoughts] = useState([]);
+  const [newThought, setNewThought] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [countChar, setCountChar] = useState(0);
 
   useEffect(() => {
-    fetchThoughts()
-  }, [])
+    fetchThoughts();
+  }, []);
 
   const fetchThoughts = () => {
+    setLoading(true);
     fetch(API_URL)
-    .then((res) => res.json())
-    .then((data) => setThoughts(data))
-    .finally(() => setLoading(false))
-  }
+      .then((res) => res.json())
+      .then((data) => setThoughts(data))
+      .finally(() => setLoading(false));
+  };
 
   const onFormSubmit = (event) => {
-    event.preventDefault()
-    setLoading(true)
+    event.preventDefault();
+    setLoading(true);
 
     fetch(API_URL, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({message: newThought })
+      body: JSON.stringify({ message: newThought }),
     })
-    .then((res) => res.json())
-    .then(setNewThought(''))
-    .finally(() => fetchThoughts())
-  }
-  
+      .then((res) => res.json())
+      .then(setNewThought(""))
+      .then(setCountChar(0))
+      .finally(() => fetchThoughts());
+  };
+
   const onLikesIncreased = (thought_id) => {
     const options = {
-      method: 'POST'
-    }
-    setLoading(true)
+      method: "POST",
+    };
+    setLoading(true);
 
-    fetch(`https://happy-thoughts-technigo.herokuapp.com/thoughts/${thought_id}/like`, options)
-    .then(res => res.json())
-    .then(fetchThoughts())
-    .finally(() => setLoading(false))
-  }
+    fetch(`${API_URL}/${thought_id}/like`, options)
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .then(fetchThoughts())
+      .finally(() => setLoading(false));
+  };
 
   return (
     <main className="main-container">
-      <NewThoughts 
+      <NewThoughts
         onFormSubmit={onFormSubmit}
         newThought={newThought}
         setNewThought={setNewThought}
         loading={loading}
+        charCount={countChar}
+        setCountChar={setCountChar}
       />
 
-      <Thoughts 
+      <Thoughts
         thoughts={thoughts}
         onLikesIncreased={onLikesIncreased}
-      /> 
+      />
     </main>
-  )
-}
-
+  );
+};
--- code/src/components/NewThoughts.js
+++ code/src/components/NewThoughts.js
@@ -1,42 +1,55 @@
-import React, { useState } from 'react'
-import Loading from './Loading'
-import CharCounter from './CharCounter'
+import React, { useState } from "react";
+import Loading from "./Loading";
+import CharCounter from "./CharCounter";
 
 const NewThought = (props) => {
-  const {onFormSubmit, newThought, setNewThought, loading} = props
-
-  const [countChar, setCountChar] = useState(0)
+  const {
+    onFormSubmit,
+    newThought,
+    setNewThought,
+    loading,
+    countChar,
+    setCountChar,
+  } = props;
 
   return (
     <div className="container">
       <form className="inner-container-form" onSubmit={onFormSubmit}>
-        <label htmlFor="newThought">What's making you happy right now?</label>
-        <textarea 
+        <label htmlFor="newThought">
+          What's making you happy right now?
+        </label>
+        <textarea
           id="newThought"
           type="text"
           value={newThought}
           className="thought-input"
           onChange={(e) => {
-            setNewThought(e.target.value)
-            setCountChar(e.target.value.length)
-          }}/>
+            setNewThought(e.target.value);
+            setCountChar(e.target.value.length);
+          }}
+        />
         <div className="heart-time">
           <button
             type="submit"
-            disabled={newThought.length < 5 || newThought.length > 140}
-            className="thought-btn">
-              <span role="img" aria-label="heart-emoji">&#10084;&#65039;</span>
-              Send happy thought
-              <span role="img" aria-label="heart-emoji">&#10084;&#65039;</span>
+            disabled={
+              newThought.length < 5 || newThought.length > 140
+            }
+            className="thought-btn"
+          >
+            <span role="img" aria-label="heart-emoji">
+              &#10084;&#65039;
+            </span>
+            Send happy thought
+            <span role="img" aria-label="heart-emoji">
+              &#10084;&#65039;
+            </span>
           </button>
-          <CharCounter 
-            countChar={countChar}
-          />
+          <CharCounter countChar={countChar} />
         </div>
         {loading && <Loading />}
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default NewThought;
