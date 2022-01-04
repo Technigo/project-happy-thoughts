182dede9fa01dae6e10fb442c3210f0434c4ad44
Jessica Nygren <jessicanygren@C02D54BJMD6R.lan>
2022-01-04 22:01:49 +0100

    url change again

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,5 +1,5 @@
-export const API_URL = 'https://jes-happy-thoughts-api.herokuapp.com/happy-thoughts'
-export const LIKES_URL = (thoughtId) => `https://jes-happy-thoughts-api.herokuapp.com/happy-thoughts/${thoughtId}/likes`
+export const API_URL = 'https://jes-happy-thoughts-api.herokuapp.com/thoughts'
+export const LIKES_URL = (thoughtId) => `https://jes-happy-thoughts-api.herokuapp.com/thoughts/${thoughtId}/likes`
 
 
 // export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
