612b039afdba7ff2823aaf50a5d7668f50100b47
JohannaMJ <johanna.mannestal@gmail.com>
2022-01-04 11:31:49 +0100

    changing API URLs to my own database

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,3 +1,8 @@
-export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts';
+// export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts';
+// export const LIKES_URL = (thoughtId) =>
+//   `https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`;
+
+export const API_URL =
+  'https://johannamj-happy-thoughts.herokuapp.com/thoughts';
 export const LIKES_URL = (thoughtId) =>
-  `https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`;
+  `https://johannamj-happy-thoughts.herokuapp.com/thoughts/${thoughtId}/like`;
