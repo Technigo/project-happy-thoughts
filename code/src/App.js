8fe0af81af27d42cbdffa5816cf36e493b9da711
Mattsson717 <83518778+Mattsson717@users.noreply.github.com>
2022-01-05 15:35:52 +0100

    added data.response in API_URL fetch

--- code/src/App.js
+++ code/src/App.js
@@ -1,92 +1,86 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-import Header from "components/Header"
-import MessageForm from 'components/MessageForm'
-import MessageList from 'components/MessageList'
-import LoadingItem from './components/Loading'
+import Header from "components/Header";
+import MessageForm from "components/MessageForm";
+import MessageList from "components/MessageList";
+import LoadingItem from "./components/Loading";
 
-import { API_URL, LIKES_URL } from './utils/urls'
+import { API_URL, LIKES_URL } from "./utils/urls";
 
 export const App = () => {
-	const [thoughts, setThoughts] = useState([])
-	const [newThought, setNewThought] = useState('')
-  	const [loading, setLoading] = useState(false)
-	const [counter, setCounter] = useState (0)
+  const [thoughts, setThoughts] = useState([]);
+  const [newThought, setNewThought] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [counter, setCounter] = useState(0);
 
   useEffect(() => {
-	fetchThoughts();
-}, []);
+    fetchThoughts();
+  }, []);
 
-	const fetchThoughts = () => {
-		setLoading(true);
-		fetch(API_URL)
-			.then((res) => res.json())
-			.then((data) => setThoughts(data))
-			.finally(() => setLoading(false));	
-	}
-	
-	const handleNewThoughtChange = (event) => {
-		setNewThought(event.target.value)
-		setCounter(event.target.value.length)
-	}
+  const fetchThoughts = () => {
+    setLoading(true);
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => setThoughts(data))
+      .finally(() => setLoading(false));
+  };
 
-	const handleFormSubmit = (event) => {
-		event.preventDefault()
+  const handleNewThoughtChange = (event) => {
+    setNewThought(event.target.value);
+    setCounter(event.target.value.length);
+  };
 
-		const options = {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({ message: newThought }),
-		}
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
 
-		fetch(API_URL, options)
-			.then((res) => res.json())
-			.then((data) => setThoughts([data, ...thoughts]))
-			
-			setNewThought('')
-			setCounter(0)
-	}
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: newThought }),
+    };
 
-	const handleLikesIncrease = (thoughtId) => {
-		const options = {
-			method: 'POST',
-		};
+    fetch(API_URL, options)
+      .then((res) => res.json())
+      .then((data) => setThoughts([data.response, ...thoughts]));
 
-		fetch(LIKES_URL(thoughtId), options)
-			.then((res) => res.json())
-			.then((data) => {
-	
-				const updatedThoughts = thoughts.map((item) => {
-					if (item._id === data._id) {
-						item.hearts += 1;
-						return item;
-					} else {
-						return item;
-					}
-				});
-				
-				setThoughts(updatedThoughts);
+    setNewThought("");
+    setCounter(0);
+  };
 
-			});
-		}
+  const handleLikesIncrease = (thoughtId) => {
+    const options = {
+      method: "POST",
+    };
 
-	return (
+    fetch(LIKES_URL(thoughtId), options)
+      .then((res) => res.json())
+      .then((data) => {
+        const updatedThoughts = thoughts.map((item) => {
+          if (item._id === data._id) {
+            item.hearts += 1;
+            return item;
+          } else {
+            return item;
+          }
+        });
+
+        setThoughts(updatedThoughts);
+      });
+  };
+
+  return (
     <main>
-		{loading && <LoadingItem />}
-	<Header />	
-      <MessageForm 
-       newThought={newThought}
-       onNewThoughtChange={handleNewThoughtChange} 
-       onFormSubmit={handleFormSubmit}
-	   counter={counter}
+      {loading && <LoadingItem />}
+      <Header />
+      <MessageForm
+        newThought={newThought}
+        onNewThoughtChange={handleNewThoughtChange}
+        onFormSubmit={handleFormSubmit}
+        counter={counter}
       />
-      <MessageList 
-        thoughts={thoughts}
-		onLikesIncrease={handleLikesIncrease}
-      />
+      <MessageList thoughts={thoughts} onLikesIncrease={handleLikesIncrease} />
     </main>
-		
-	)
-}
+  );
+};
