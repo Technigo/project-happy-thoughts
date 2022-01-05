1a51443a08db0ee12f699734e75b844227b7ba3a
Ida Aspen <ida.aspen@outlook.com>
2022-01-05 15:15:16 +0100

    Refactor: change to new database

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,4 +1,8 @@
-export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts';
+export const API_URL =
+  'https://ida-aspen-happy-thoughts.herokuapp.com/thoughts';
 
 export const LIKES_URL = (thoughtId) =>
-  `https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`;
+  `https://ida-aspen-happy-thoughts.herokuapp.com/thoughts/${thoughtId}/like`;
+
+// https://happy-thoughts-technigo.herokuapp.com/thoughts
+// https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like
