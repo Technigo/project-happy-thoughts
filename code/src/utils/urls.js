3a842563a8d2f9196b8c7b77b2df15bae69cc967
A1eksa <80787783+A1eksa@users.noreply.github.com>
2022-01-05 22:53:41 +0100

    url

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://a1eksa-happythoughtapi.herokuapp.com/';
 export const LIKES_URL = (thoughtId) =>
-  `https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`;
+  `https://a1eksa-happythoughtapi.herokuapp.com/${thoughtId}/like`;
 
 //"https://happy-thoughts-technigo.herokuapp.com/thoughts"
