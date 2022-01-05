a275ebb001954e986589804afb0ecba729087c19
Mattsson717 <83518778+Mattsson717@users.noreply.github.com>
2022-01-05 14:57:10 +0100

    changed the api url

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://happy-thoughts-patrik.herokuapp.com/";
+export const API_URL = "https://happy-thoughts-patrik.herokuapp.com/thoughts";
 export const LIKES_URL = (thoughtId) =>
   `https://happy-thoughts-patrik.herokuapp.com/thoughts/${thoughtId}/hearts`;
