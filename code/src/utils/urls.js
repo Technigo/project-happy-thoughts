3ab8b5c3c14cb9c29b84a3c0063a84e71b280da4
Linnea <linnea.iseb@gmail.com>
2022-01-05 17:47:46 +0100

    Changed URLs in urls

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,4 +1,4 @@
 export const API_URL = 'https://alisebrink-happy-thoughts.herokuapp.com/';
 
 // URL for getting the post that you click on
-export const LIKES_URL = (thoughtId) => `https://alisebrink-happy-thoughts.herokuapp.com/${thoughtId}/like`;
+export const LIKES_URL = (thoughtId) => `https://alisebrink-happy-thoughts.herokuapp.com/thoughts/${thoughtId}/likes`;
