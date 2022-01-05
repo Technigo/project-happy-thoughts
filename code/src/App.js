0884ab1901976a37616595393f0d7d1d9f607b8e
Elin Elmvik Diczfalusy <elinelmvik@gmail.com>
2022-01-05 15:44:08 +0100

    added usestate hook in useEffect

--- code/src/App.js
+++ code/src/App.js
@@ -1,75 +1,75 @@
-import React, { useState, useEffect } from "react";
-import moment from "moment";
-import { API_URL, API_HEART } from "./utils/urls";
-import Form from "components/Form";
-import ThoughtsCard from "components/ThoughtsCard";
-import Loading from "components/Loading";
-import Footer from "components/Footer";
+import React, { useState, useEffect } from 'react'
+import moment from 'moment'
+import { API_URL, API_HEART } from './utils/urls'
+import Form from 'components/Form'
+import ThoughtsCard from 'components/ThoughtsCard'
+import Loading from 'components/Loading'
+import Footer from 'components/Footer'
 
 function App() {
   // * * * * * states * * * * *
-  const [newThought, setNewThought] = useState("");
-  const [thoughts, setThoughts] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [newThought, setNewThought] = useState('')
+  const [thoughts, setThoughts] = useState([])
+  const [loading, setLoading] = useState(true)
 
   // * * * * * useEffect * * * * *
   useEffect(() => {
     fetch(API_URL)
-      .then(res => res.json())
-      .then(data => setThoughts(data))
-      .finally(() => setLoading(false));
-  }, []);
+      .then((res) => res.json())
+      .then((data) => setThoughts(data))
+      .finally(() => setLoading(false))
+  }, [thoughts])
 
   // * * * * * function * * * * *
   // 1
-  const onFormSubmit = e => {
-    e.preventDefault();
+  const onFormSubmit = (e) => {
+    e.preventDefault()
 
     const options = {
-      method: "POST",
-      headers: { "Content-type": "application/json" },
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
       body: JSON.stringify({ message: newThought }),
       // we have to wrapp it to able to send it (other way around from getting it)
-    };
+    }
 
     fetch(API_URL, options)
-      .then(res => res.json())
-      .then(data => {
-        setThoughts([data, ...thoughts]);
-      });
+      .then((res) => res.json())
+      .then((data) => {
+        setThoughts([data, ...thoughts])
+      })
 
-    setNewThought("");
-  };
+    setNewThought('')
+  }
   // 2
 
-  const onSetThoughtChange = e => {
-    setNewThought(e.target.value);
-  };
+  const onSetThoughtChange = (e) => {
+    setNewThought(e.target.value)
+  }
 
   // 3
 
-  const heartCounter = thought_id => {
+  const heartCounter = (thought_id) => {
     const options = {
-      method: "POST",
+      method: 'POST',
       // if there is no body we dont need a header
-    };
+    }
 
     fetch(API_HEART(thought_id), options)
-      .then(res => res.json())
-      .then(data => {
-        const updatedThoughts = thoughts.map(item => {
+      .then((res) => res.json())
+      .then((data) => {
+        const updatedThoughts = thoughts.map((item) => {
           if (thought_id === item._id) {
-            item.hearts += 1;
+            item.hearts += 1
 
-            return item;
+            return item
           } else {
-            return item;
+            return item
           }
-        });
+        })
 
-        setThoughts(updatedThoughts);
-      });
-  };
+        setThoughts(updatedThoughts)
+      })
+  }
 
   return (
     <>
@@ -83,7 +83,7 @@ function App() {
       />
       {/* * * * * * * * * * * * CARDS * * * * * * * * * */}
 
-      {thoughts.map(thought => {
+      {thoughts.map((thought) => {
         return (
           <ThoughtsCard
             thought={thought}
@@ -91,11 +91,11 @@ function App() {
             moment={moment}
             key={thought._id}
           />
-        );
+        )
       })}
       <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
