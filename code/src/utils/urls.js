64ac26bf28ffa72af9ade9517eab88767539e49b
A1eksa <80787783+A1eksa@users.noreply.github.com>
2022-01-05 11:08:10 +0100

    adde my api

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,3 +1,5 @@
-export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
+export const API_URL = 'https://a1eksa-happythoughtapi.herokuapp.com/';
 export const LIKES_URL = (thoughtId) =>
   `https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`;
+
+//"https://happy-thoughts-technigo.herokuapp.com/thoughts"
