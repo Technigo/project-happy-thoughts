18e149091858575629a1ffc6d32e4679fd275dc4
Priscila Alfaro <priscila24n@hotmail.com>
2022-01-06 15:17:35 +0100

    change url api to new backend

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
-export const LIKE_URL = (id) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${id}/like`;
+export const API_URL = "https://project-happy-thoughts-backend.herokuapp.com/thoughts";
+export const LIKE_URL = (id) => `https://project-happy-thoughts-backend.herokuapp.com/thoughts/${id}/like`;
