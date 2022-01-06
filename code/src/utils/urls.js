d0319c4f98d294c9d699fee49feb572e9134761b
HedvigM <hedvig@mejstedt.se>
2022-01-06 11:52:37 +0100

    Update urls.js

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,3 +1,5 @@
-export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
+export const API_URL = 'https://hedvigs-happy-thoughts.herokuapp.com/thoughts';
 export const LIKES_URL = (thoughtId) =>
-  `https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`;
+  `https://hedvigs-happy-thoughts.herokuapp.com/thoughts/${thoughtId}/likes`;
+
+/* https://hedvigs-happy-thoughts.herokuapp.com/thoughts */
