3a586820e4bf27d2e0b9f3bb93e9c3e70dcad780
Helena Wiklund <helena.wiklund@live.se>
2022-01-06 12:24:53 +0100

    added my backend url

--- code/src/utilis/urls.js
+++ code/src/utilis/urls.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
+export const API_URL = "https://happy-thoughts-db.herokuapp.com/thoughts";
 export const LIKE_URL = (id) => `${API_URL}/${id}/like`;
