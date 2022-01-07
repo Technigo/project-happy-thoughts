092419c085d83669055466421d834af5dbd7f74a
A1eksa <80787783+A1eksa@users.noreply.github.com>
2022-01-07 15:09:48 +0100

    another test

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,9 +1,3 @@
-export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts';
+export const API_URL = 'https://a1eksa-happythoughtapi.herokuapp.com/thoughts';
 export const LIKES_URL = (thoughtId) =>
-  `https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`;
-
-//"https://happy-thoughts-technigo.herokuapp.com/thoughts"
-
-// export const API_URL = 'https://a1eksa-happythoughtapi.herokuapp.com/thoughts';
-// export const LIKES_URL = (thoughtId) =>
-//   `https://a1eksa-happythoughtapi.herokuapp.com/thoughts/${thoughtId}/like`;
+  `https://a1eksa-happythoughtapi.herokuapp.com/thoughts/${thoughtId}/like`;
