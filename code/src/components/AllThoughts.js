a47b9e807d509b04ac565d64410ac6732f6e85cd
Linnea <linnea.iseb@gmail.com>
2022-01-07 10:13:51 +0100

    Fixing

--- code/src/components/AllThoughts.js
+++ code/src/components/AllThoughts.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import moment from 'moment';
 
-const AllThoughts = ({ thought, onLikesIncrease, heart }) => {
+const AllThoughts = ({ message, onLikesIncrease, heart }) => {
   return (
-    <div className="posted-thoughts" key={thought._id}>
-      <p className="thought-message">{thought.message}</p>
+    <div className="posted-thoughts" key={message._id}>
+      <p className="thought-message">{message.message}</p>
       <div className="like-button">
-        <button onClick={() => onLikesIncrease(thought._id)}>
+        <button onClick={() => onLikesIncrease(message._id)}>
           <img src={heart} alt="Red heart emoji" />
         </button>
-        <span className="thoughts-likes"> x {thought.hearts}</span>
+        <span className="thoughts-likes"> x {message.hearts}</span>
       </div>
-      <p className="date">Created at: {moment(thought.createdAt).fromNow()}</p>
+      <p className="date">Created at: {moment(message.createdAt).fromNow()}</p>
     </div>
   );
 };
