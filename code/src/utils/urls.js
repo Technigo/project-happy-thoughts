206b6c43e6a263b1565f099a9118e51e61f9482a
e-delsol <80672920+e-delsol@users.noreply.github.com>
2022-01-07 17:24:25 +0100

    Changing some comments

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,10 +1,12 @@
 export const API_URL = "https://happy-tweets-api.herokuapp.com/thoughts";
 
-// new URL: https://happy-tweets-api.herokuapp.com/thoughts
-// old URL: https://happy-thoughts-technigo.herokuapp.com/thoughts
+// NEW URL: https://happy-tweets-api.herokuapp.com/thoughts
+
+// OLD URL: https://happy-thoughts-technigo.herokuapp.com/thoughts
 
 export const LIKES_URL = (thoughtId) =>
 	`https://happy-tweets-api.herokuapp.com/thoughts/${thoughtId}/like`;
 
-// new URL: https://happy-tweets-api.herokuapp.com/thoughts/${thoughtId}/like
-// old URL: https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like
+// NEW URL: https://happy-tweets-api.herokuapp.com/thoughts/${thoughtId}/like
+
+// OLD URL: https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like
