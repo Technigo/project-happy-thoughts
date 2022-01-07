98137df957f50888c182d76bac29173e4ef66dc4
e-delsol <80672920+e-delsol@users.noreply.github.com>
2022-01-07 17:08:26 +0100

    Updating the URL

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,2 +1,10 @@
-export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
-export const LIKES_URL = (thoughtId) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`;
+export const API_URL = "https://happy-tweets-api.herokuapp.com/thoughts";
+
+// new URL: https://happy-tweets-api.herokuapp.com/thoughts
+// old URL: https://happy-thoughts-technigo.herokuapp.com/thoughts
+
+export const LIKES_URL = (thoughtId) =>
+	`https://happy-tweets-api.herokuapp.com/thoughts/${thoughtId}/like`;
+
+// new URL: https://happy-tweets-api.herokuapp.com/thoughts/${thoughtId}/like
+// old URL: https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like
