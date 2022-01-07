19ce718eb11fe0576d585224e0e9bc29bf6a5fe0
Bruna Santos Araujo <brunasantosaraujo@Brunas-Air.lan>
2022-01-07 01:31:52 +0100

    fixed likes url

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,4 +1,4 @@
 export const API_URL = "https://myhappy-thoughts-api.herokuapp.com/thoughts";
 
 export const LIKES_URL = (thoughtId) =>
-  `https://myhappy-thoughts-api.herokuapp.com/thoughts${thoughtId}/like`;
+  `https://myhappy-thoughts-api.herokuapp.com/thoughts/${thoughtId}/like`;
