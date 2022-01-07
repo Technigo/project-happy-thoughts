839444c2b16ee2fe409689a24fef21602813cde6
Bruna Santos Araujo <brunasantosaraujo@Brunas-Air.lan>
2022-01-07 01:11:44 +0100

    changed thoughts url

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
+export const API_URL = "https://myhappy-thoughts-api.herokuapp.com/thoughts";
 
 export const LIKES_URL = (thoughtId) =>
   `https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`;
