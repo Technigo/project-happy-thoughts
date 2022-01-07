2045368478219d576193e3ec47f0fcd80eeae00e
A1eksa <80787783+A1eksa@users.noreply.github.com>
2022-01-07 15:07:17 +0100

    test frontend

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,5 +1,9 @@
-export const API_URL = 'https://a1eksa-happythoughtapi.herokuapp.com/';
+export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts';
 export const LIKES_URL = (thoughtId) =>
-  `https://a1eksa-happythoughtapi.herokuapp.com/${thoughtId}/like`;
+  `https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`;
 
 //"https://happy-thoughts-technigo.herokuapp.com/thoughts"
+
+// export const API_URL = 'https://a1eksa-happythoughtapi.herokuapp.com/thoughts';
+// export const LIKES_URL = (thoughtId) =>
+//   `https://a1eksa-happythoughtapi.herokuapp.com/thoughts/${thoughtId}/like`;
