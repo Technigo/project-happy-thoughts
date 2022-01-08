14861d8f3e0bab2b2f19e389c8c016ca831c6fb8
Amanda Tilly <amandatilly@Amandas-2.lan>
2022-01-08 16:43:50 +0100

    updated urls to my own api

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,3 +1,8 @@
-export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
+// export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
+// export const URL_LIKES = (thoughtId) =>
+//   `https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`;
+
+export const API_URL =
+  "https://amandatilly-happy-thoughts-api.herokuapp.com/thoughts";
 export const URL_LIKES = (thoughtId) =>
-  `https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`;
+  `https://amandatilly-happy-thoughts-api.herokuapp.com/thoughts/${thoughtId}/like`;
