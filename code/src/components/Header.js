1bf76cd31c8f08c7c304fe0c2a9056db6d7409c0
Fatima-GR <fatigr2692@gmail.com>
2022-01-11 22:52:42 +0100

    added role=img in header

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 const Header = () => {
-  return <h1 className="heading-one"> &#10084;&#65039;&#9829;&#9825; Happy thoughts &#9825;&#9829;&#10084;&#65039;
-  </h1>;
+  return (
+    <h1 className="heading-one">
+      {" "}
+      <span role="img">
+        &#10084;&#65039;&#9829;&#9825; Happy thoughts
+        &#9825;&#9829;&#10084;&#65039;
+      </span>
+    </h1>
+  );
 };
 
 export default Header;
