4bf2bfad9c5da430541c378bd53ed83cef912997
intehon <emelie.soderhielm@gmail.com>
2022-01-11 17:57:48 +0100

    change in url

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://intehon-happy-thoughts.herokuapp.com/thoughts'
+export const API_URL = 'https://intehon-happy-thoughts.herokuapp.com/thoughts/'
 export const API_LIKES = (thoughtId) => `https://intehon-happy-thoughts.herokuapp.com/thoughts/${thoughtId}/likes`
