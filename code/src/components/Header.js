aa289f8f6424d3b224a3eae803f2f240b81a2969
Fatima-GR <fatigr2692@gmail.com>
2022-01-11 22:55:49 +0100

    fixed errors

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <h1 className="heading-one">
       {" "}
-      <span role="img">
+      <span role="img" aria-label="Hearts">
         &#10084;&#65039;&#9829;&#9825; Happy thoughts
         &#9825;&#9829;&#10084;&#65039;
       </span>
