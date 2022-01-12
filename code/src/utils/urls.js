fc1b6d4434ba81ba00e2a0c37bdc942711eb26bf
AschwinSiaila <aschwin76@hotmail.com>
2022-01-12 23:17:49 +0100

    changing herokuapp

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,2 +1,5 @@
-export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
-export const LIKES_URL = (thoughtId) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`;
+// export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
+// export const LIKES_URL = (thoughtId) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`;
+
+export const API_URL = "https//happythoughts-api-aschwin.herokuapp.com/thoughts";
+export const LIKES_URL = (thoughtId) => `https://happythoughts-api-aschwin.herokuapp.com/thoughts/${thoughtId}/like`;
