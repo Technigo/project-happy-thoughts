cb27af739220ef61096ec37065feb2686354971b
Maria Thomasson <mariathomasson@hotmail.se>
2022-01-18 18:19:02 +0100

    Updated with my own API

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,3 +1,3 @@
-export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
+export const API_URL = 'https://maria-happy-thoughts.herokuapp.com/thoughts'
 export const API_LIKES_URL = (thoughtId) =>
-`https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`
+`https://maria-happy-thoughts.herokuapp.com/thoughts/${thoughtId}/like`
