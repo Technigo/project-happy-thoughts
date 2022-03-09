eae4742d6946c017ba74f34a8fb1fcb35c3ae12d
themisk84 <efthymios.karakasis@gmail.com>
2022-03-09 14:43:14 +0100

    fix: update loading component

--- code/src/components/Loading.js
+++ code/src/components/Loading.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Lottie from "react-lottie";
 
 const Loading = () => {
   return (
     <div className="loading-overlay">
+      Loading
       <div className="loading-container">
         <div className="loading" />
       </div>
