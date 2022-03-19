1bcc4e14e5e15a0edea8a60dacdf17dcc11c8681
Emma Högberg <emmahogberg88@gmail.com>
2022-03-19 19:55:37 +0100

    API fetch and form

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import moment from "moment";
 
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+	const [thoughts, setThoughts] = useState([]);
+	const [newThought, setNewThought] = useState("");
+
+	useEffect(() => {
+		fetch("https://happy-thoughts-technigo.herokuapp.com/thoughts")
+			.then((res) => res.json())
+			.then((data) => {
+				setThoughts(data);
+			}, thoughts);
+	});
+
+	return (
+		<main>
+			<form>
+				<label>Type your thought</label>
+				<textarea
+					name="newThought"
+					type="text"
+					rows="5"
+					columns="150"
+					value={newThought}
+					onChange={(e) => setNewThought(e.target.value)}
+				></textarea>
+				<button>&hearts; Send Happy Thought! &hearts;</button>
+			</form>
+
+			<div className="thoughts-container">
+				{thoughts.map((thought) => (
+					<div key={thought._id}>
+						<p>{thought.message}</p>
+						<div>
+							<button>&hearts;{thought.hearts}</button>
+							<p>created at: {moment(thought.createdAt).fromNow()}</p>
+						</div>
+					</div>
+				))}
+			</div>
+		</main>
+	);
+};
