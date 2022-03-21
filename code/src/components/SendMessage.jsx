ac559637707904a0750dfaba66e7397998419ff7
Michael Chang <michaelchang@MacBook-Pro.lan>
2022-03-21 21:42:01 +0100

    finished up some more conditional styling for 140 characters

--- code/src/components/SendMessage.jsx
+++ code/src/components/SendMessage.jsx
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 const SendMessage = ({messageSent, setMessageSent}) => {
     const [message, setMessage] = useState('')
     const [messageLength, setMessageLength] = useState(0)
+    const [error, setError] = useState(false)
 
     const SEND_API = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
 
@@ -41,22 +42,37 @@ const SendMessage = ({messageSent, setMessageSent}) => {
         setMessageLength(message.length)
         // console.log(message, messageLength)
         if (message.length > 140) {
-            alert('Attention! You have exceeded the character limit of 140!')
+            setError(true)
+            // alert('Attention! You have exceeded the character limit of 140!')
+        } else {
+            setError(false)
         }
+    }
+
+    let textErrorStyle = {}
+    let buttonErrorStyle = {}
+    let buttonMessage = '💖 Send Happy Thought 💖'
+
+    if (error === true) {
+        textErrorStyle = { fontWeight: 700, color: 'red' }
+        buttonErrorStyle = { backgroundColor: 'black', color: 'white', cursor: 'not-allowed' }
+        buttonMessage = 'Too long to send! 💀'
+    } else {
+        textErrorStyle = {}
     }
 
     return (
         <>
         <div className="thought-container form-container">
             <div className="top-bar">
-                <label htmlFor="sendThought" className="thought-text">What's making you happy right now?</label>
+                <label htmlFor="sendThought" className="thought-text bold">What's making you happy right now?</label>
                 <textarea className="send-thought-input" id="sendThought" onChange={event => wordCount(event.target.value)} />
             </div>
             <div className="bottom-bar">
-                <button className="send-thought-btn">
-                    <span role="img" aria-label="send happy thought button" onClick={sendMessage}>💖 Send Happy Thought 💖</span>
+                <button className="send-thought-btn bold" style={buttonErrorStyle}>
+                    <span role="img" aria-label="send happy thought button" onClick={sendMessage}>{buttonMessage}</span>
                 </button>
-                <p>{messageLength}/140</p>
+                <p style={textErrorStyle}>{messageLength}/140</p>
             </div>
         </div>
         </>
