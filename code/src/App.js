1b28133235fa8bdd0ff8effbb37db37665cc7fca
Isabell Wästfelt <isabell@iMac-som-tillhor-Isabell.local>
2022-03-21 09:53:38 +0100

    added components, url and first fetch

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+
+import ThoughtForm from "Components/ThoughtForm";
+import ThoughtItem from "Components/ThoughtItem";
+
+import { API_URL, URL_LIKES } from "utils/urls";
 
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+  const [thoughts, setThoughts] = useState([]);
+
+  useEffect(() => {
+    fetchThoughts();
+  }, []);
+
+  const fetchThoughts = () => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => setThoughts(data));
+  };
+};
