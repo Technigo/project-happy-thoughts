d91cd47d9bade156181ca75d2cc68f166b5d2b95
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2022-03-22 22:46:23 +0100

    Create time stamp component

--- happy-thoughts/src/components/ThoughtCards.jsx
+++ happy-thoughts/src/components/ThoughtCards.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+
+import TimeStamp from "./partials/TimeStamp";
 import ThoughtForm from "./ThoughtForm";
 
 const ThoughtCards = () => {
@@ -6,21 +8,29 @@ const ThoughtCards = () => {
 
   useEffect(() => {
     fetch("https://happy-thoughts-technigo.herokuapp.com/thoughts")
-    .then(res => res.json())
-    .then(thoughts => setThoughts(thoughts))
+      .then(res => res.json())
+      .then(thoughts => setThoughts(thoughts))
   }, []);
 
   return (
     <>
-    <ThoughtForm setThoughts={setThoughts} />
-    <div>
-      {thoughts.map((thought, index) => (
-        <div className="thought-cards cards">
-          <p key={index}>{thought.message}</p>
-          <button className="heart-button">❤️</button>
-        </div>
+      <ThoughtForm setThoughts={setThoughts} />
+      <div>
+      {thoughts.map(thought => (
+        <section className="thought-cards cards" key={thought._id}>
+          <p>{thought.message}</p>
+          <div className="details-wrapper">
+            <div>
+              <button className={(thought.hearts === 0 ? "heart-button" : "heart-button red-heart-button")}>
+                <span role="img" aria-label="heart icon">❤️</span>
+              </button>
+              <p className="likes">x {thought.hearts}</p>
+            </div>
+            <TimeStamp createdAt={thought.createdAt} />
+          </div>
+        </section>
       ))}
-    </div>
+      </div>
     </>
   );
 };
--- happy-thoughts/src/components/ThoughtForm.jsx
+++ happy-thoughts/src/components/ThoughtForm.jsx
@@ -1,35 +1,35 @@
 import React, { useState } from "react";
 
 const ThoughtForm = (props) => {
-  const [thought, setThought] = useState("");
+  const [newThought, setNewThought] = useState("");
 
   const handleFormSubmit = (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  fetch("https://happy-thoughts-technigo.herokuapp.com/thoughts", { 
-    method: "POST", 
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ message: thought })
-  })
-    .then((res) => res.json())
-    .then((newThought) => props.setThoughts((thoughts) => [newThought, ...thoughts]))
+    fetch("https://happy-thoughts-technigo.herokuapp.com/thoughts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: newThought })
+    })
+      .then((res) => res.json())
+      .then((newThought) => props.setThoughts((thoughts) => [newThought, ...thoughts]))
   }
 
   return (
     <form onSubmit={handleFormSubmit} className="form cards">
       <label htmlFor="thoughtInput">What's making you happy right now?</label>
-        <textarea
-          type="text" 
-          id="thoughtInput"
-          name="thought"
-          onChange={event => setThought(event.target.value)}
-        />
+      <textarea
+        type="text"
+        id="thoughtInput"
+        name="thought"
+        onChange={event => setNewThought(event.target.value)}
+      />
       <button type="submit">
-        <span className="icon">❤️ </span> 
-          Send Happy Thought 
-        <span className="icon"> ❤️</span>
+        <span role="img" aria-label="heart icon">❤️ </span>
+        Send Happy Thought
+        <span role="img" aria-label="heart icon"> ❤️</span>
       </button>
     </form>
   );
