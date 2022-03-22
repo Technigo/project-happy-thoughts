53538d17be3ec9a3e7ed99406ea14f2b7336e452
Mimmisen <mimmi.fordaluddin@hotmail.com>
2022-03-22 12:51:03 +0100

    started with the project, added the fetch, form,heart button and the thoughts

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+
+import { API_URL } from "./utils/urls";
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState([]);
+  const [newThought, SetNewThought] = useState("");
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => setThoughts(data));
+  }, []);
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: newThought }),
+    };
+
+    fetch(API_URL, options)
+      .then((res) => res.json())
+      .then((data) => setThoughts([data, ...thoughts]));
+  };
+
   return (
     <div>
-      Find me in src/app.js!
+      <form onSubmit={onFormSubmit}>
+        <label htmlFor="newThought">What's making you happy right now?</label>
+        <input
+          id="newThought"
+          type="text"
+          value={newThought}
+          onChange={(e) => SetNewThought(e.target.value)}
+        />
+        <button type="submit">Send happy thought</button>
+      </form>
+
+      {thoughts.map((thought) => (
+        <div key={thought._id}>
+          <p>{thought.message}</p>
+          <button>
+            <span role="emoji" aria-label="heart for likes">
+              ❤️
+            </span>{" "}
+            {thought.hearts}
+          </button>
+          {/* &hearts; also works  */}
+          <p className="date">
+            Created at: {moment(thought.createdAt).fromNow()}
+          </p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
