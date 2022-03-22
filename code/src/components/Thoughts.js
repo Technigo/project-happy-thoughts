46d060efe8976b8e73b67ee2d29e2b5b247a7eb2
Amanda Tange <amandatange@hotmail.com>
2022-03-22 09:46:13 +0100

    try commenting out the useEffect to make it complie without issues to see if it updates correctly on the deployed site

--- code/src/components/Thoughts.js
+++ code/src/components/Thoughts.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { /*formatRelative,*/ formatDistance } from 'date-fns';
 import parseISO from 'date-fns/parseISO';
 
@@ -43,10 +43,12 @@ const Thoughts = ({heartIcon, thoughtsAPI}) => {
 			.then(json => setThoughts(json))
     }
 
-    useEffect(() => {
-        fetchThoughts();
-        setInterval(fetchThoughts, 5000);
-    }, [])
+    fetchThoughts()
+    setInterval(fetchThoughts, 5000);
+    // useEffect(() => {
+    //     fetchThoughts();
+    //     setInterval(fetchThoughts, 5000);
+    // }, [fetchThoughts])
     
 
     return (
