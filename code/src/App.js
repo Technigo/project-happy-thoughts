04a205983f9e7573805ca70230c389cd3f3bd270
anki palviainen <anki.palviainen@gmail.com>
2022-03-23 10:28:04 +0200

    start project

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,93 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
+import ThoughtForm from 'components/ThoughtForm';
+import ThoughtItem from 'components/ThoughtItem';
+import LoadingItem from 'components/Loading';
+import Header from 'components/Header';
+
+import { API_URL, LIKES_URL } from './utils/urls';
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState ([])
+  const [newThought, setNewThought] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  useEffect (() => {
+    fetchThoughts()
+  }, [])
+
+  const fetchThoughts = () => {
+    setLoading(true)
+    fetch(API_URL)
+    .then(res => res.json())
+    .then(data => setThoughts(data))
+    .finally(() => setLoading(false))
+  }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault()
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ message: newThought })
+    }
+
+    fetch(API_URL, options)
+    .then((res) => res.json())
+    .then((data) => {
+      //v1
+      //setThoughts([data, ...thoughts])
+
+      //v2
+      fetchThoughts()
+    })
+  }
+
+  const handleLikesIncrease = (thoughtId) => {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    fetch(LIKES_URL(thoughtId), options)
+    .then((res) => res.json())
+    .then((data) => {
+      //v1
+
+      //const updatedThoughts = thoughts.map(item => {
+       // if (item._id === data._id) {
+         // item.hearts += 1
+         // return item
+        //} else {
+        //  return item
+        //}
+     // })
+
+     // setThoughts(updatedThoughts)
+
+      //v2
+      fetchThoughts()
+    })
+  }
+
   return (
     <div>
-      Find me in src/app.js!
+      {loading && <LoadingItem />}
+
+      <Header />
+
+      <ThoughtForm onFormSubmit={handleFormSubmit} newThought={newThought} setNewThought={setNewThought} />
+
+      {thoughts.map((thought) => (
+        <ThoughtItem key={thought._id} thought={thought} onLikesIncrease={handleLikesIncrease}/>
+      ))}
     </div>
   )
+  
+
 }
