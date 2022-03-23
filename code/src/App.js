42ca50c032f3880e7820f965ddece6e7c1a9caab
Josefin Eliasson <91781497+Josse79@users.noreply.github.com>
2022-03-23 16:47:23 +0100

    added ThoughtForm component

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,56 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import ThoughtList from 'components/ThoughtList.js'
+import ThoughtForm from 'components/ThoughtForm.js'
 
 
 
 export const App = () => {
+
+    const [thoughtList, setThoughtList] = useState([])
+    const [loading, setLoading] =useState(false)
+    const [newThought, setNewThought] = useState ('')
+
+    useEffect(() => {
+      fetchThoughts();
+      }, []);
+      
+    const fetchThoughts = () => {
+      setLoading(true)
+      fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts')
+      .then((res) => res.json())
+      .then((data) => setThoughtList(data))
+      .finally (() => setLoading(false))
+    }
+           console.log(thoughtList)
+
+    const handleNewThought = (event) => {
+      setNewThought(event.target.value)
+    }
+
+    const handleFormSubmit = (event) => {
+      event.preventDefault()
+    
+        const options = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+              message: newThought
+          })
+        }
+
+        fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts', options)
+          .then(res =>res.json())
+          .then(() => fetchThoughts())
+          .finally(() => setNewThought(''))
+    }
+    
+
   return (
     <div>
-      <ThoughtList />
+      <ThoughtForm newThought={newThought} handleNewThought={handleNewThought} handleFormSubmit={handleFormSubmit} />
+      <ThoughtList loading={loading} thoughtList={thoughtList}/>
     </div>
   )
 }
--- code/src/components/ThoughtList.js
+++ code/src/components/ThoughtList.js
@@ -1,29 +1,37 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
+import moment from 'moment'
 
-const ThoughtList = () => {
-    const [thoughtList, setThoughtList] = useState([]);
+const ThoughtList = ({loading, thoughtList}) => {
+    // const [thoughtList, setThoughtList] = useState([])
+    // const [loading, setLoading] =useState(false)
     
-    useEffect(() => {
-    fetchThoughts();
-    }, []);
+    // useEffect(() => {
+    // fetchThoughts();
+    // }, []);
          
-         const fetchThoughts = () => {
-             fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts')
-             .then((res) => res.json())
-             .then((data) => setThoughtList(data))
+    //      const fetchThoughts = () => {
+    //          setLoading(true)
+    //          fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts')
+    //          .then((res) => res.json())
+    //          .then((data) => setThoughtList(data))
+    //          .finally (() => setLoading(false))
+    //      }
+    //      console.log(thoughtList)
+
+         if (loading){
+             return <h1>Loading in progress...</h1>
          }
-         console.log(thoughtList)
 
          return (
              <section>
                  {thoughtList.map((singleThought) => (
-                     <ul key={singleThought._id}>
-                         {singleThought.message}
-                     </ul>
+                     <div key={singleThought._id}>
+                         <h3>{singleThought.message}</h3>
+                         <p>{moment.utc(singleThought.createdAt).fromNow()}</p>
+                     </div>
                  ))}
              </section>
          )
- 
      }
  
  export default ThoughtList
