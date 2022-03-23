04b11754e5b65a61553eeac785ffbb95dc84461f
SofiePellegrini <sofie.pellegrini@outlook.com>
2022-03-23 20:55:02 +0100

    styling

--- code/src/Components/ThoughtsInput.js
+++ code/src/Components/ThoughtsInput.js
@@ -3,13 +3,9 @@ import React from 'react';
 const ThoughtsInput = ({ onFormSubmit, newThought, setNewThought }) => {
   const redCharacters = () => {
     return newThought.length > 140 ? (
-      <p style={{ color: 'red' }}>
-        <p style={{ color: 'red' }}>{140 - newThought.length}/140</p>
-      </p>
+      <p style={{ color: 'red' }}>{140 - newThought.length}/140</p>
     ) : (
-      <p style={{ color: 'black' }}>
-        <p style={{ color: 'black' }}>{140 - newThought.length}/140</p>
-      </p>
+      <p style={{ color: 'black' }}>{140 - newThought.length}/140</p>
     );
   };
   return (
@@ -24,7 +20,6 @@ const ThoughtsInput = ({ onFormSubmit, newThought, setNewThought }) => {
           value={newThought}
           onChange={(event) => setNewThought(event.target.value)}
           rows='4'
-          cols='30'
         />
         <div className='buttonContainer'>
           <button
