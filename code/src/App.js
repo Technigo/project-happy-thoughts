e452b0d7b5c4de0fc3787a12405e2e76081537e2
Jenny Fiskaare <jennyfiskaare@Jennys-MacBook-Air.local>
2022-03-23 23:11:26 +0100

    basic css and footer

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,17 @@
+import Footer from 'components/Footer'
 import React from 'react'
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+  <section>
+      <div className="form-container">Happy Thoughts!!!
+        <div className="typing-area">Hej
+       </div> 
+       <button className="send-button">Send Happy Thought<span>❤️</span></button>
+        </div>
+      <Footer />
+      </section>
+
+    
   )
 }
