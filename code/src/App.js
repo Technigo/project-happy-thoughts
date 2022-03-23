745efd5ac7277b921630b2331f5aa9a042691378
Lisa Bergström <lisa.bergstrom@live.se>
2022-03-23 14:36:01 +0100

    installed moment

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import React from "react";
+import { ThoughtsList } from "./components/ThoughtsList";
+import ThoughtsForm from "components/ThoughtsForm";
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <ThoughtsForm />
+      <ThoughtsList />
     </div>
-  )
-}
+  );
+};
---
+++ code/src/components/ThoughtsList.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+export const ThoughtsList = () => {
+  const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    fetchList();
+  }, []);
+  const fetchList = () => {
+    setLoading(true);
+    fetch("https://happy-thoughts-technigo.herokuapp.com/thoughts")
+      .then((res) => res.json())
+      .then((data) => setList(data))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  };
+  if (loading) {
+    return <h1>Loading in progress....</h1>;
+  }
+  return (
+    <section>
+      {list.map((thoughts) => (
+        <div key={thoughts._id}>
+          <h4>{thoughts.message}</h4>
+          <p>{moment(thoughts.createAT).fromNow()}</p>
+        </div>
+      ))}
+    </section>
+  );
+};
