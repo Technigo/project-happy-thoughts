d011e47df2c0c4f1ac65b47e07996958c40aea8a
Isabell Wästfelt <isabell@iMac-som-tillhor-Isabell.local>
2022-03-24 20:22:41 +0100

    fixed counter and text position

--- code/src/Components/ThoughtForm.js
+++ code/src/Components/ThoughtForm.js
@@ -1,17 +1,6 @@
 import React from "react";
 
 const ThoughtForm = ({ onFormSubmit, newThought, setNewThought }) => {
-  // const [postable, setPostable] = useState(false);
-
-  // "TENERARY COndition"  GOOGLE IT!!
-  // postable = newThought.length > 5 && newThought.length < 140;
-
-  // if (postable) {
-  //   return true
-  // } else {
-  //   return false
-  // }
-
   return (
     <form className="form card" onSubmit={onFormSubmit}>
       <label htmlFor="newThought">What is making you happy right now?</label>
@@ -24,21 +13,22 @@ const ThoughtForm = ({ onFormSubmit, newThought, setNewThought }) => {
         onChange={(e) => setNewThought(e.target.value)}
         placeholder="Write your happy thought here..."
       />
-
-      <button
-        className="submit-button"
-        type="submit"
-        disabled={newThought.length < 5 || newThought.length > 140}
-      >
-        <span role="img" aria-label="heart">
-          &#10084;&#65039;
-        </span>{" "}
-        Send Happy Thought{" "}
-        <span role="img" aria-label="heart">
-          &#10084;&#65039;
-        </span>
-      </button>
-      <p>{0 + newThought.length}/140</p>
+      <div className="form-container">
+        <button
+          className="submit-button"
+          type="submit"
+          disabled={newThought.length < 5 || newThought.length > 140}
+        >
+          <span role="img" aria-label="heart">
+            &#10084;&#65039;
+          </span>{" "}
+          Send Happy Thought{" "}
+          <span role="img" aria-label="heart">
+            &#10084;&#65039;
+          </span>
+        </button>
+        <p>{0 + newThought.length}/140</p>
+      </div>
     </form>
   );
 };
