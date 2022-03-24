680e6dd2f8c4e6d83e40145c04b7c2bfe0b85746
Isabell Wästfelt <isabell@iMac-som-tillhor-Isabell.local>
2022-03-24 15:22:50 +0100

    added counter and disable when the requirements is not fulfilled.

--- code/src/Components/ThoughtForm.js
+++ code/src/Components/ThoughtForm.js
@@ -1,18 +1,35 @@
 import React from "react";
 
 const ThoughtForm = ({ onFormSubmit, newThought, setNewThought }) => {
+  // const [postable, setPostable] = useState(false);
+
+  // "TENERARY COndition"  GOOGLE IT!!
+  // postable = newThought.length > 5 && newThought.length < 140;
+
+  // if (postable) {
+  //   return true
+  // } else {
+  //   return false
+  // }
+
   return (
     <form className="form card" onSubmit={onFormSubmit}>
       <label htmlFor="newThought">What is making you happy right now?</label>
       <textarea
         id="newThought"
         type="text"
+        rows="5"
+        columns="150"
         value={newThought}
         onChange={(e) => setNewThought(e.target.value)}
         placeholder="Write your happy thought here..."
       />
 
-      <button className="submit-button" type="submit">
+      <button
+        className="submit-button"
+        type="submit"
+        disabled={newThought.length < 5 || newThought.length > 140}
+      >
         <span role="img" aria-label="heart">
           &#10084;&#65039;
         </span>{" "}
@@ -21,6 +38,7 @@ const ThoughtForm = ({ onFormSubmit, newThought, setNewThought }) => {
           &#10084;&#65039;
         </span>
       </button>
+      <p>{0 + newThought.length}/140</p>
     </form>
   );
 };
