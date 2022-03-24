b03235aee013b16dfdf98b55e7f662a6e45ab2a5
StepbyStepp <91485842+SteppbySteph@users.noreply.github.com>
2022-03-24 20:39:07 +0100

    created new component,  added to App.js

---
+++ code/src/components/Overview.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+import Form from 'components/Form'
+import Thought from 'components/Thought'
+
+
+export const Overview = () => {
+  const [thoughts, setThoughts] = useState([])
+  const [newThought, setNewThought] = useState('')
+
+  const handleOnNewThought = (event) => {
+    setNewThought(event.target.value)
+  }
+
+  useEffect(()=> {
+    fetchThoughts();
+  }, [])
+
+  const fetchThoughts = () => {
+    fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts')
+    .then(res => res.json())
+    .then(json => setThoughts(json))
+  }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault()
+
+    const options = { 
+      method: 'POST', 
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ message: newThought })
+    }
+  
+    fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts', options)
+      .then((res) => res.json())
+      .then(() => fetchThoughts())
+      .finally(()=> setNewThought(''))
+  }
+
+  const handleLikes = (_id) => {
+
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' }
+    }
+
+    fetch(`https://happy-thoughts-technigo.herokuapp.com/thoughts/${_id}/like`, options)
+      .then((res) => res.json())
+      . then(() => fetchThoughts())
+  }
+
+  return (
+    <section className='container'>
+      <Form 
+        onFormSubmit={handleFormSubmit}
+        newThought={newThought}
+        onSetThoughtChange={handleOnNewThought}/>
+
+      {thoughts.map((thought)=> (
+      <Thought 
+        key={thought._id}
+        thought={thought}
+        handleLikes={handleLikes}
+        />
+      ))}
+    </section>
+  )
+}
