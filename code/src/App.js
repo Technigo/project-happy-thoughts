91609f9237afb20bbdf2f382527d6a3709f3f5f3
StepbyStepp <91485842+SteppbySteph@users.noreply.github.com>
2022-03-24 15:09:42 +0100

    styling CSS

--- code/src/App.js
+++ code/src/App.js
@@ -8,43 +8,70 @@ import Thought from 'components/Thought'
 export const App = () => {
   const [thoughts, setThoughts] = useState([])
   const [newThought, setNewThought] = useState('')
+  // const [loading, setLoading] = useState(false)
 
+  const handleOnNewThought = (event) => {
+    setNewThought(event.target.value)
+  }
 
-  //This useEffect fetches data to get recent posts
   useEffect(()=> {
-    fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts')
-        .then(res => res.json())
-        .then(json => setThoughts(json))
+    fetchThoughts();
   }, [])
+
+  const fetchThoughts = () => {
+    fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts')
+    .then(res => res.json())
+    .then(json => setThoughts(json))
+    // .finally(()=> setLoading (false))
+  }
+
+  // if (loading) {
+  //   return <h1>Loading in progress</h1>
+  // }
+ 
 
   const handleFormSubmit = (event) => {
     event.preventDefault()
-  
-    fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts', { 
+
+    const options = { 
       method: 'POST', 
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ message: newThought })
-    })
+    }
+  
+    fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts', options)
       .then((res) => res.json())
-      .then((newThought) => {
-        setThoughts((previousThoughts) => [newThought, ...previousThoughts])
-    })
+      .then(() => fetchThoughts())
+      .finally(()=> setNewThought(''))
+  }
+
+  const handleLikes = (_id) => {
+
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' }
+    }
+
+    fetch(`https://happy-thoughts-technigo.herokuapp.com/thoughts/${_id}/like`, options)
+      .then((res) => res.json())
+      . then(() => fetchThoughts())
   }
 
   return (
-    <div>
+    <section className='container'>
       <Form 
         onFormSubmit={handleFormSubmit}
         newThought={newThought}
-        setNewThought={setNewThought}/>
+        onSetThoughtChange={handleOnNewThought}/>
 
       {thoughts.map((thought)=> (
       <Thought 
         key={thought._id}
-        thoughts={thought}
+        thought={thought}
+        handleLikes={handleLikes}
         />
       ))}
-    </div>
+    </section>
   )
 }
 
--- code/src/components/Form.js
+++ code/src/components/Form.js
@@ -1,24 +1,34 @@
 import React from 'react'
 
-const Form = ( { onFormSubmit, newThought, setNewThought } ) => {
+const Form = ( { onFormSubmit, newThought, onSetThoughtChange } ) => {
 
     return (
-       <section>
-           <form onSubmit={onFormSubmit}>
-               <label htmlFor='new-thought'>
-                   <input 
-                    type='text'
-                    onChange={event => setNewThought(event.target.value)}
-                    value={newThought}
-                    id='new-thought'
-                    name='new-thought'
-                />
-               </label>
-               <button 
-                    type='submit'
-                    // disabled={newThought.length < 6 || newThought.length > 140}
-                >Send Happy Thought</button>
-           </form>
+       <section className='form-container'>
+            <form 
+                onSubmit={onFormSubmit}
+                className='form-content'
+            >
+                <p>What's making you happy right now?</p>
+                <label htmlFor='new-thought'>
+                    <textarea 
+                        className='input-text'
+                        rows="3"
+                        cols="37"
+                        onChange={onSetThoughtChange}
+                        value={newThought}
+                        id='new-thought'
+                        name='new-thought'
+                        placeholder='A penny for your thought!'
+                    />
+                </label>
+                <button 
+                        type='submit'
+                        className='submit-btn'
+                        disabled={newThought.length < 6 || newThought.length > 140}
+                    ><span role='img' aria-label='heart'>
+                    ❤️ Send Happy Thought ❤️</span>
+                </button>
+            </form>
        </section>
     )
 
