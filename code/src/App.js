8dfc56811e1fbb2a4456007e9e32042d44b8cf58
Dorothea-B <barre.dorothea@gmail.com>
2022-03-24 01:28:38 +0100

    initial setup

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const App = () => {
+
+  const [thoughts, setThoughts] = useState([])
+  useEffect(() => {
+
+    fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts')
+    .then((res) => res.json())
+    .then(data => setThoughts(data))
+
+
+
+    console.log("mounted or uppd.");
+  }, [] )
+
+  console.log(`my ${thoughts}`);
+
   return (
     <div>
-      Find me in src/app.js!
+  {thoughts.map(thought => (
+    <div key={thought._id}>
+      <p>{thought.message}</p>
+      <button> &hearts; {thought.hearts} </button>
+      <p>Created at: {thought.createdAt} </p>
+    </div>
+  ))  
+  }
     </div>
   )
 }
