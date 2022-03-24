b41a6da3c0f5050fdb134c190ec7f8d5272fee7d
Dorothea-B <barre.dorothea@gmail.com>
2022-03-24 08:38:16 +0100

    POST function

--- code/src/App.js
+++ code/src/App.js
@@ -1,28 +1,51 @@
 import React, { useState, useEffect } from 'react'
+import moment from 'moment'
+import { API_URL } from './utils/urls'
 
 export const App = () => {
 
+
+  const [newThought, setNewThought] = useState([])
   const [thoughts, setThoughts] = useState([])
+
   useEffect(() => {
 
-    fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts')
+    fetch(API_URL)
     .then((res) => res.json())
     .then(data => setThoughts(data))
 
-
-
-    console.log("mounted or uppd.");
   }, [] )
 
-  console.log(`my ${thoughts}`);
+
+  const onFormSubmit = (e) => {
+    e.preventDefault()
+    // console.log("submitted: ", newThought)
+    fetch( API_URL, 
+        { method: 'POST', headers: { 'Content-Type': 'application/json',}, body: JSON.stringify({ message: newThought }) }
+      ).then((res) => res.json()).then(data => setThoughts([data, ...thoughts]))
+  }
+
+  console.log(thoughts);
 
   return (
+
     <div>
+
+    <form onSubmit={onFormSubmit}> 
+      <label htmlFor="newThought" >Type input here</label>
+      <input value={newThought} 
+      onChange={(e) => setNewThought(e.target.value)}
+      id={newThought}
+      />
+
+      <button type="submit">Submit</button>
+    </form>
+
   {thoughts.map(thought => (
     <div key={thought._id}>
       <p>{thought.message}</p>
       <button> &hearts; {thought.hearts} </button>
-      <p>Created at: {thought.createdAt} </p>
+      <p className="date">Created at: {moment(thought.createdAt).fromNow()} </p>
     </div>
   ))  
   }
---
+++ code/src/utils/urls.js
@@ -0,0 +1,2 @@
+
+export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
