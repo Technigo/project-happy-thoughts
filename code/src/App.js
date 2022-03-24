939316d39f67b8b7159ed688d70de392e597385e
Isabell Wästfelt <isabell@iMac-som-tillhor-Isabell.local>
2022-03-24 14:36:08 +0100

    finished fild blue level

--- code/src/App.js
+++ code/src/App.js
@@ -3,10 +3,11 @@ import React, { useEffect, useState } from "react";
 import ThoughtForm from "Components/ThoughtForm";
 import ThoughtItem from "Components/ThoughtItem";
 
-import { API_URL, URL_LIKES } from "utils/urls";
+const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
 
 export const App = () => {
   const [thoughts, setThoughts] = useState([]);
+  const [newThought, setNewThought] = useState("");
 
   useEffect(() => {
     fetchThoughts();
@@ -17,4 +18,48 @@ export const App = () => {
       .then((res) => res.json())
       .then((data) => setThoughts(data));
   };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: newThought }),
+    })
+      .then((res) => res.json())
+      .then(() => {
+        fetchThoughts();
+      });
+  };
+
+  const handleLikesIncrease = (thoughtId) => {
+    fetch(`${API_URL}/${thoughtId}/like`, {
+      method: "POST",
+    })
+      .then((res) => res.json())
+      .then(() => {
+        fetchThoughts();
+      });
+  };
+
+  return (
+    <main className="main-container">
+      <ThoughtForm
+        onFormSubmit={handleFormSubmit}
+        newThought={newThought}
+        setNewThought={setNewThought}
+      />
+
+      {thoughts.map((thought) => (
+        <ThoughtItem
+          key={thought._id}
+          thought={thought}
+          onLikesIncrease={handleLikesIncrease}
+        />
+      ))}
+    </main>
+  );
 };
--- code/src/Components/ThoughtForm.js
+++ code/src/Components/ThoughtForm.js
@@ -1,1 +1,28 @@
 import React from "react";
+
+const ThoughtForm = ({ onFormSubmit, newThought, setNewThought }) => {
+  return (
+    <form className="form card" onSubmit={onFormSubmit}>
+      <label htmlFor="newThought">What is making you happy right now?</label>
+      <textarea
+        id="newThought"
+        type="text"
+        value={newThought}
+        onChange={(e) => setNewThought(e.target.value)}
+        placeholder="Write your happy thought here..."
+      />
+
+      <button className="submit-button" type="submit">
+        <span role="img" aria-label="heart">
+          &#10084;&#65039;
+        </span>{" "}
+        Send Happy Thought{" "}
+        <span role="img" aria-label="heart">
+          &#10084;&#65039;
+        </span>
+      </button>
+    </form>
+  );
+};
+
+export default ThoughtForm;
