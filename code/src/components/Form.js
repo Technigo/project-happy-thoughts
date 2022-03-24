60e6c1a2dec64c6a4294e3cc468f5a5d4dd3390e
joannaringqvist <joanna.ringqvist@gmail.com>
2022-03-24 19:48:48 +0100

    Added aria label and role for the emojis

--- code/src/components/Form.js
+++ code/src/components/Form.js
@@ -8,9 +8,9 @@ const Form = ({ onNewThoughtChange, newThought, onFormSubmit }) => {
           <label htmlFor="happythoughts">What's making you happy right now?</label>
           <textarea value={newThought} id="happythoughts" onChange={onNewThoughtChange} />
           <button type="submit">
-            <span className="heart-emoji">❤️</span> 
+            <span className="heart-emoji" role="img" aria-label="Heart">❤️</span> 
             Send Happy Thought 
-            <span className="heart-emoji">❤️</span>
+            <span className="heart-emoji" role="img" aria-label="Heart">❤️</span>
           </button>
       </form>
     </div>
--- code/src/components/Thoughts.js
+++ code/src/components/Thoughts.js
@@ -14,12 +14,12 @@ const Thoughts = ({ loading, thoughts, onHeartClick }) => {
               <p className="thought-message">{thought.message}</p>
               <div className="thought-footer">
                 <div className="thought-heart">
-                  <button className={thought.hearts > 0 ? 'moreThanZeroClicks' : 'zeroClicks' } onClick={() => onHeartClick(thought._id)}><span className="heart-emoji">❤️</span></button>
+                  <button className={thought.hearts > 0 ? 'moreThanZeroClicks' : 'zeroClicks' } onClick={() => onHeartClick(thought._id)}>
+                    <span className="heart-emoji" role="img" aria-label="Heart">❤️</span>
+                  </button>
                   <div className="thought-heart-times">x{thought.hearts}</div>
                 </div>
-                {/* <p>{formatRelative(thought.createdAt, new Date())}</p> */}
                 <p>{formatDistanceToNow(new Date(thought.createdAt))} ago</p>
-
               </div>
           </div>
         ))}
