0dd6e18636c4390bf41b80c2059e1db0bd6c207c
Tiina <tiina_liukkonen@hotmail.com>
2022-03-25 16:09:00 +0200

    fixed footer

--- code/src/components/Footer.js
+++ code/src/components/Footer.js
@@ -2,10 +2,10 @@ import React from "react"
 
 const Footer = () => {
     return (
-        <div>
+        <footer>
             <p className="footer-text">&copy; Tiina Liukkonen</p>
             <p className="footer-text">Technigo Bootcamp Spring '22</p>
-        </div>
+        </footer>
     )
 }
 
