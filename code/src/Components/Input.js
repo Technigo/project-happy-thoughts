5f5cb0e655aa1f69b4f7f72cf417bfdd7c5aa376
sofiaannajonsson <84452023+sofiaannajonsson@users.noreply.github.com>
2022-03-25 14:28:42 +0100

    corrected form label

--- code/src/Components/Input.js
+++ code/src/Components/Input.js
@@ -1,22 +1,22 @@
 import React from "react";
 
 const Input = ({ message, onInputChange, onFormSubmit} ) => {
-
   return (
     <header>
-      <label htmlFor="thoughtInput" aria-label="thoughtInput"/>
-      <form onSubmit={onFormSubmit}>
-        <h3>What's making you happy right now?</h3>
-        <textarea 
-          value={message} 
-          onChange={onInputChange}
-          placeholder="Week 11 is making me happy!"
-          />
-        <button className="form-button" type="submit">
-          <span role="img" aria-label="like-emoji">💖</span>
-            Send happy thought
-          <span role="img" aria-label="like-emoji">💖</span></button>
-      </form>
+        <form onSubmit={onFormSubmit}>
+          <label htmlFor="What's making you happy right now?" aria-label="What's making you happy right now?"/>
+            <h3>What's making you happy right now?</h3>
+            <textarea 
+            value={message} 
+            onChange={onInputChange}
+            placeholder="Week 11 is making me happy!"
+            />
+            <button className="form-button" type="submit">
+              <span role="img" aria-label="like-emoji">💖</span>
+                Send happy thought
+              <span role="img" aria-label="like-emoji">💖</span>
+            </button>
+        </form>
     </header>
   )
 }
