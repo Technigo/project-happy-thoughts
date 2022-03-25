dcd854373e06d64eebfeb73b6f70b60a0d5244e2
Tiina <tiina_liukkonen@hotmail.com>
2022-03-25 16:05:18 +0200

    added padding to footer and fixed a bug

--- code/src/components/Footer.js
+++ code/src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from "react"
 
 const Footer = () => {
-    <div>
-        <p className="footer-text">&copy; Tiina Liukkonen</p>
-    </div>
+    return (
+        <div>
+            <p className="footer-text">&copy; Tiina Liukkonen</p>
+        </div>
+    )
 }
 
 export default Footer
