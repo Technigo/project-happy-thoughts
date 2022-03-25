b4ec1b902dd456b9d91ee5da84734c1a17b118f6
joannaringqvist <joanna.ringqvist@gmail.com>
2022-03-25 12:20:39 +0100

    Fixed so that you'll have to write at least 6 and max 140 characters

--- code/src/components/Form.js
+++ code/src/components/Form.js
@@ -7,11 +7,16 @@ const Form = ({ onNewThoughtChange, newThought, onFormSubmit }) => {
       <form onSubmit={onFormSubmit}>
           <label htmlFor="happythoughts">What's making you happy right now?</label>
           <textarea value={newThought} id="happythoughts" onChange={onNewThoughtChange} />
-          <button type="submit">
-            <span className="heart-emoji" role="img" aria-label="Heart">❤️</span> 
-            Send Happy Thought 
-            <span className="heart-emoji" role="img" aria-label="Heart">❤️</span>
-          </button>
+          <div class="thought-footer">
+            <button type="submit" disabled={newThought.length < 6 || newThought.length > 140} >
+                <span className="heart-emoji" role="img" aria-label="Heart">❤️</span> 
+                <span className="button-text">Send Happy Thought </span>
+                <span className="heart-emoji" role="img" aria-label="Heart">❤️</span>
+            </button>
+            <p className={newThought.length < 6 || newThought.length > 140 ? "incorrect-number-of-characters" : "correct-number-of-characters"}>
+              {newThought.length}/140
+            </p>
+          </div>
       </form>
     </div>
   )
