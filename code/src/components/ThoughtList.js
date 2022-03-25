b316ffbc2a801c86001cd9477009a60f52de8933
Sherin Susan Thomas <sherinsusanthomas95@gmail.com>
2022-03-25 10:46:20 +0100

    updated css

--- code/src/components/ThoughtList.js
+++ code/src/components/ThoughtList.js
@@ -5,8 +5,9 @@ const ThoughtList = ({ thoughts, onLikeIncrease }) => {
   return (
     <div className="Thoughtlist-container">
       {thoughts.map((thought) => (
-        <div className="main-container" key={thought._id}>
+        <div className="main-listcontainer" key={thought._id}>
           <p>{thought.message}</p>
+
           <button
             className="like-button"
             onClick={() => onLikeIncrease(thought._id)}
@@ -17,7 +18,7 @@ const ThoughtList = ({ thoughts, onLikeIncrease }) => {
           </button>
           <span className="likes"> x {thought.hearts}</span>
           <p className="time-created">
-            {formatDistance(new Date(thought.createdAt), new Date())}
+            {formatDistance(new Date(thought.createdAt), new Date())} ago
           </p>
         </div>
       ))}
