1eb83551c3a919b0d601f6715325fc450493bf1c
Louisa <lundislon@gmail.com>
2022-03-26 22:38:05 +0100

    heart-button change on click added

--- code/src/Components/HappyMessages.js
+++ code/src/Components/HappyMessages.js
@@ -8,7 +8,8 @@ export const HappyMessages = ({ twentyThoughts, handleHearts }) => {
             <p>{twentyThoughts.message}</p>
                 <div className="message-card-bottom-items">
                     <div>
-                        <button className="heart-button" onClick={() => handleHearts(twentyThoughts._id)}>
+                        <button className={twentyThoughts.hearts > 0 ? "heart-button clicked": "heart-button"}
+                        onClick={() => handleHearts(twentyThoughts._id)}>
                         <span role="img" aria-label="heart">❤️</span>
                         </button> 
                         <span> x {twentyThoughts.hearts}</span>
