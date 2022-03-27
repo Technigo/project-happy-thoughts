cab35a7a3611ee2931f418f89bfd8072e421f192
vanhaj <70697392+vanhaj@users.noreply.github.com>
2022-03-27 11:15:49 +0200

    more styling

--- code/src/components/NewThoughts.js
+++ code/src/components/NewThoughts.js
@@ -10,7 +10,7 @@ const NewThoughts = ({
 }) => {
   return (
     <form onSubmit={handleFormSubmit}>
-      <div className='thoughts-message'>
+      <div className='thoughts-message new-thoughts'>
         <label htmlFor='newThoughts'><p className='happy-header'>What`s making you happy right now?</p></label>
         <textarea className={counter < 6 || counter > 140 ? 'disabled-textarea' : 'textarea'} _id='newThoughts' type='text' placeholder='' value={newThoughts} onChange={onNewThoughtsChange} />
         <p>{140 - counter} / 140</p>
