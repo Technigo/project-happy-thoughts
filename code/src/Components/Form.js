4617327e982da22d5fcfb3b1d9333c7d94e031ab
Louisa <lundislon@gmail.com>
2022-03-27 14:24:04 +0200

    new test label

--- code/src/Components/Form.js
+++ code/src/Components/Form.js
@@ -4,7 +4,7 @@ export const Form = ({ newMessage, onNewMessage, onFormSubmit }) => {
 
 return (
         <form className="posting-form" onSubmit={onFormSubmit}>
-            <label htmlfor="newMessage"><h4>What's making you happy right now?</h4></label>
+            <label htmlfor="newMessage"><h4>What's making you happy right now?</h4>
              <textarea className="form-textarea"
                 value={newMessage} 
                 onChange={onNewMessage} 
@@ -13,6 +13,7 @@ return (
             <button className="form-button" type="submit" disabled={newMessage.length < 6 || newMessage.length > 140}>
             <span role="img" aria-label="heart">🖤 Send happy thought! 🖤</span>
             </button>
+            </label>
         </form>
     )
 }
