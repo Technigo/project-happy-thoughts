ac9e31f2850d5800c2dff1cc3c83d3ae59627f2e
Josefin Eliasson <91781497+Josse79@users.noreply.github.com>
2022-03-28 08:52:09 +0200

    htmlFor in textarea

--- code/src/components/ThoughtForm.js
+++ code/src/components/ThoughtForm.js
@@ -6,13 +6,11 @@ const ThoughtForm = ({newThought, handleNewThought, handleFormSubmit}) => {
         <section>
             <form className='card grey' onSubmit={handleFormSubmit}>
                 <h2>What's making you happy right now?</h2>
-                <label htmlFor='add-thought'>
                     <textarea 
-                    className='text-field'
+                    htmlFor='add-thought'
                     value={newThought} 
                     onChange={handleNewThought} 
                     placeholder ='Share your happy thought...'/>
-                </label>
                 <button
                     className='form-button'
                     type='submit'
