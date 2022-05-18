9d064a7e3e6c6df3aac60a011a642b94d6e0415f
EmmaaBerg <emmaberg0819@gmail.com>
2022-05-18 20:08:00 +0200

    updated the API.js with my own API

--- code/src/Fetch/API.js
+++ code/src/Fetch/API.js
@@ -1,3 +1,4 @@
-export const HAPPY_THOUGHTS_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
+export const HAPPY_THOUGHTS_URL = 'https://emmashappythoughts.herokuapp.com/thoughts'
 
-export const LIKED_THOUGHTS_URL = (thoughtId) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`
+export const LIKED_THOUGHTS_URL = (thoughtId) => `https://emmashappythoughts.herokuapp.com/thoughts/${thoughtId}/like`
+
