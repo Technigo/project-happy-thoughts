c3c8d2129ad60b7ee7daacc2837656b628e2d357
Emma Högberg <emmahogberg88@gmail.com>
2022-05-18 23:01:46 +0200

    Update README

--- code/src/components/utils/urls.js
+++ code/src/components/utils/urls.js
@@ -1,3 +1,3 @@
-export const API_URL = 'https://make-happy-thoughts.herokuapp.com/thought'
+export const API_URL = 'https://make-happy-thoughts.herokuapp.com/thoughts'
 
 export const API_LIKES = (thoughtId) => `https://make-happy-thoughts.herokuapp.com/thoughts/${thoughtId}/like`
