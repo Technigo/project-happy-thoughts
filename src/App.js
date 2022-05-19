82c5e39af0974b4e699db297d0aa3bd1a8ffd018
StepbyStepp <91485842+SteppbySteph@users.noreply.github.com>
2022-05-19 21:32:43 +0200

    changed API-url

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Overview } from './components/Overview'
+
+
+export const App = () => {
+
+  return (
+    <section>
+      <Overview />
+    </section>
+  )
+}
+
---
+++ src/components/Overview.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+import Form from './Form'
+import Thought from './Thought'
+
+
+export const Overview = () => {
+  const [thoughts, setThoughts] = useState([])
+  const [newThought, setNewThought] = useState('')
+
+  const handleOnNewThought = (event) => {
+    setNewThought(event.target.value)
+  }
+
+  useEffect(()=> {
+    fetchThoughts();
+  }, [])
+
+  const fetchThoughts = () => {
+    fetch('https://blooming-brushlands-33610.herokuapp.com/thoughts')
+    .then(res => res.json())
+    .then(data => setThoughts(data.response))
+  }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault()
+
+    const options = { 
+      method: 'POST', 
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ message: newThought })
+    }
+  
+    fetch('https://blooming-brushlands-33610.herokuapp.com/thoughts', options)
+      .then((res) => res.json())
+      .then(() => fetchThoughts())
+      .finally(()=> setNewThought(''))
+  }
+
+  const handleLikes = (_id) => {
+
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' }
+    }
+
+    fetch(`https://blooming-brushlands-33610.herokuapp.com/thoughts/${_id}/like`, options)
+      .then((res) => res.json())
+      .then(() => fetchThoughts())
+  }
+
+  return (
+    <section className='container'>
+      <Form 
+        onFormSubmit={handleFormSubmit}
+        newThought={newThought}
+        onSetThoughtChange={handleOnNewThought}
+      />
+
+      {thoughts.map((thought)=> (
+        <Thought 
+          key={thought._id}
+          thought={thought}
+          handleLikes={handleLikes}
+        />
+      ))}
+    </section>
+  )
+}
