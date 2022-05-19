445810cc94148eb2ea1472863f9c59fe1983fca6
Isabell Wästfelt <isabell@iMac-som-tillhor-Isabell.local>
2022-05-19 10:21:02 +0200

    changed api

--- code/src/App.js
+++ code/src/App.js
@@ -1,53 +1,53 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
 
-import ThoughtForm from "Components/ThoughtForm";
-import ThoughtItem from "Components/ThoughtItem";
+import ThoughtForm from 'Components/ThoughtForm'
+import ThoughtItem from 'Components/ThoughtItem'
 
-const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
+const API_URL = 'https://happy-thoughts-level-up.herokuapp.com/'
 
 export const App = () => {
-  const [thoughts, setThoughts] = useState([]);
-  const [newThought, setNewThought] = useState("");
+  const [thoughts, setThoughts] = useState([])
+  const [newThought, setNewThought] = useState('')
 
   useEffect(() => {
-    fetchThoughts();
-  }, []);
+    fetchThoughts()
+  }, [])
 
   const fetchThoughts = () => {
     fetch(API_URL)
       .then((res) => res.json())
-      .then((data) => setThoughts(data));
-  };
+      .then((data) => setThoughts(data))
+  }
 
   const handleFormSubmit = (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
     fetch(API_URL, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ message: newThought }),
     })
       .then((res) => res.json())
       .then(() => {
-        fetchThoughts();
-        setNewThought("");
-      });
-  };
+        fetchThoughts()
+        setNewThought('')
+      })
+  }
 
   const handleLikesIncrease = (thoughtId) => {
     fetch(`${API_URL}/${thoughtId}/like`, {
-      method: "POST",
+      method: 'POST',
     })
       .then((res) => res.json())
       .then(() => {
-        fetchThoughts();
-      });
-  };
+        fetchThoughts()
+      })
+  }
 
   return (
-    <main className="main-container">
+    <main className='main-container'>
       <ThoughtForm
         onFormSubmit={handleFormSubmit}
         newThought={newThought}
@@ -62,5 +62,5 @@ export const App = () => {
         />
       ))}
     </main>
-  );
-};
+  )
+}
