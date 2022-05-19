2463418b4d97f7e49f34764711bbd9fbf7dd370f
Marianne Ardin <55622908+MarianneArdin@users.noreply.github.com>
2022-05-19 10:51:22 +0200

    added new URL

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,2 +1,3 @@
-export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts';
-export const LIKES_URL = (thoughtId) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`;
+export const API_URL = 'https://git.heroku.com/marianne-happythoughts.git';
+export const LIKES_URL = (thoughtId) =>
+	`https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`;
