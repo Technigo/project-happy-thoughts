2d11161afd49265d5ef22175b10e242da84f9652
vanhaj <vanessa.hajek@hotmail.com>
2022-05-19 11:41:19 +0200

    fixed like

--- code/src/components/RecentThoughts.js
+++ code/src/components/RecentThoughts.js
@@ -7,22 +7,27 @@ import { formatDistance } from 'date-fns';
 
 const RecentThoughts = ({ thoughts, handleLikesInc }) => {
   return (
-    <section className='thoughts-container'>
+    <section className="thoughts-container">
       {thoughts.map((thought) => (
-        <div className='thoughts-message' key={thought._id}>
-          <p className='thoughts-txt'>{thought.message}</p>
-          <div className='group-container'>
-            <div className='heart-times-gr'>
-            <button className='heart-btn' onClick={() => handleLikesInc(thought._id)}>
-              <span>{'❤️'}</span>
-            </button>
-            <span className='times-liked'> x {thought.hearts}</span>
-          </div>
-          <p className='time-txt'>
-            {formatDistance(new Date(thought.createdAt), Date.now(), {
-              addSuffix: true,
-            })}
-          </p>
+        <div className="thoughts-message" key={thought._id}>
+          <p className="thoughts-txt">{thought.message}</p>
+          <div className="group-container">
+            <div className="heart-times-gr">
+              <button
+                // className="heart-btn"
+                className={`heart-btn ${
+                  thought.hearts === 0 ? 'not-liked' : 'liked'
+                }`}
+                onClick={() => handleLikesInc(thought._id)}>
+                <span>{'❤️'}</span>
+              </button>
+              <span className="times-liked"> x {thought.like}</span>
+            </div>
+            <p className="time-txt">
+              {formatDistance(new Date(thought.createdAt), Date.now(), {
+                addSuffix: true,
+              })}
+            </p>
           </div>
         </div>
       ))}
