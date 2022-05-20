cdfcf399a10df85d4e03a58eaddb8290792333ae
Mimmisen <mimmi.fordaluddin@hotmail.com>
2022-05-20 12:04:49 +0200

    added my own api

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,7 +1,3 @@
-// export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
-// export const LIKES_URL = (thoughtId) =>
-//   `https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`;
-
 export const API_URL =
   "https://project-happy-thoughts-week-19.herokuapp.com/thoughts";
 export const LIKES_URL = (thoughtId) =>
