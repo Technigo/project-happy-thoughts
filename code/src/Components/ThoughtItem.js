c25d11d6af5a26cf5f2eb5e33546cc274070e7ad
Isabell Wästfelt <isabell@iMac-som-tillhor-Isabell.local>
2022-05-20 16:56:48 +0200

    update the api

--- code/src/Components/ThoughtItem.js
+++ code/src/Components/ThoughtItem.js
@@ -1,36 +1,36 @@
-import React from "react";
-import { formatDistance } from "date-fns";
+import React from 'react'
+import { formatDistance } from 'date-fns'
 
 const ThoughtItem = ({ thought, onLikesIncrease }) => {
   return (
-    <div className="card">
-      <p className="thought-text">{thought.message}</p>
+    <div className='card'>
+      <p className='thought-text'>{thought.message}</p>
 
-      <div className="likes">
-        <div className="button-card">
+      <div className='likes'>
+        <div className='button-card'>
           <button
-            className="like-button"
+            className='like-button'
             onClick={() => onLikesIncrease(thought._id)}
             style={{
-              backgroundColor: thought.hearts > 0 ? "#ffadad" : "#eaeaea",
+              backgroundColor: thought.hearts > 0 ? '#ffadad' : '#eaeaea',
             }}
           >
-            {" "}
-            <span role="img" aria-label="heart emoji">
+            {' '}
+            <span role='img' aria-label='heart emoji'>
               &#10084;&#65039;
             </span>
           </button>
-          <p className="likes-amount">x {thought.hearts}</p>
+          <p className='likes-amount'>x {thought.hearts}</p>
         </div>
 
-        <p className="date">
+        <p className='date'>
           {formatDistance(new Date(thought.createdAt), Date.now(), {
             addSuffix: true,
           })}
         </p>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ThoughtItem;
+export default ThoughtItem
