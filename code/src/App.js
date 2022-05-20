069f56a8574258b82db3d6e6466ee5e1d4480e79
Mimmisen <mimmi.fordaluddin@hotmail.com>
2022-05-20 12:03:05 +0200

    did some changes

--- code/src/App.js
+++ code/src/App.js
@@ -20,6 +20,7 @@ export const App = () => {
     fetch(API_URL)
       .then((res) => res.json())
       .then((data) => setThoughts(data))
+      .catch((err) => console.error(err))
       .finally(() => setLoading(false));
   };
 
--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,3 +1,8 @@
-export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
+// export const API_URL = "https://happy-thoughts-technigo.herokuapp.com/thoughts";
+// export const LIKES_URL = (thoughtId) =>
+//   `https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`;
+
+export const API_URL =
+  "https://project-happy-thoughts-week-19.herokuapp.com/thoughts";
 export const LIKES_URL = (thoughtId) =>
-  `https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`;
+  `https://project-happy-thoughts-week-19.herokuapp.com/thoughts/${thoughtId}/like`;
