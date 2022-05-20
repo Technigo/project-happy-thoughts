78a4761c02e211396ec64a828a3394a3a99e3c45
anki palviainen <anki.palviainen@gmail.com>
2022-05-20 11:21:15 +0300

    connect to backend

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,5 +1,7 @@
 export const API_URL =
-'https://happy-thoughts-technigo.herokuapp.com/thoughts'
+//'https://happy-thoughts-technigo.herokuapp.com/thoughts'
+'https://mongo-happy-thoughts.herokuapp.com/thoughts'
 
 export const LIKES_URL = (thoughtId) =>
-`https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`
+//`https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`
+`https://mongo-happy-thoughts.herokuapp.com/thoughts/${thoughtId}/like`
