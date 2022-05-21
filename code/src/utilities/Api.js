4c1c5bdf42c0a7b8cde030e96bb16ae8af26f2e4
Joanna Lodell <joannalodell@c77-218-53-198.bredband.tele2.se>
2022-05-21 12:44:03 +0200

    updated to my own API

--- code/src/utilities/Api.js
+++ code/src/utilities/Api.js
@@ -1,3 +1,3 @@
-export const API_URL = `https://happy-thoughts-technigo.herokuapp.com/thoughts`
+export const API_URL = `https://project-happy-thoughts-api-joa.herokuapp.com/thoughts`
 export const API_LIKES_URL = (singleThoughtId) =>
-  `https://happy-thoughts-technigo.herokuapp.com/thoughts/${singleThoughtId}/like`
+  `https://project-happy-thoughts-api-joa.herokuapp.com/thoughts/${singleThoughtId}/like`
