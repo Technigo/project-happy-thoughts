6c1a8ee350b79513497c2db5eb7df835950752ae
Therese Nyman <theresenyman@Thereses-MacBook-Air.local>
2022-05-23 16:14:56 +0200

    updated API to my own project-api

--- code/src/components/Form.js
+++ code/src/components/Form.js
@@ -1,78 +1,79 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-import Recent from './Recent'
-import Create from './Create'
+import Recent from "./Recent";
+import Create from "./Create";
 
 const Form = () => {
-
-  const [newThought, setnewThought] = useState('');
+  const [newThought, setnewThought] = useState("");
   const [thoughts, setThoughts] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     fetchThoughts();
-  }, [])
+  }, []);
 
   const fetchThoughts = () => {
-    setLoading(true)
-    fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts')
-      .then(res => res.json())
-      .then(json => setThoughts(json))
-      .catch(error => console.error(error))
-      .finally(() => setLoading(false))
-  }
+    setLoading(true);
+    fetch("https://thereese-happy-thoughts.herokuapp.com/thoughts")
+      .then((res) => res.json())
+      .then((json) => setThoughts(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  };
 
   const handleNewThoughtChange = (event) => {
-    setnewThought(event.target.value)
-  }
-   
+    setnewThought(event.target.value);
+  };
+
   const handleFormSubmit = (event) => {
-    event.preventDefault()
-    
+    event.preventDefault();
+
     const options = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-       message: newThought
-      })
-    }
-    
-    fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts', options)
-      .then(res => res.json())
-      .then(data => fetchThoughts())
-      .finally(() => setnewThought(''))
-  }
+        message: newThought,
+      }),
+    };
+
+    fetch("https://thereese-happy-thoughts.herokuapp.com/thoughts", options)
+      .then((res) => res.json())
+      .then((data) => fetchThoughts())
+      .finally(() => setnewThought(""));
+  };
 
   const handleLikeIncrease = (thoughts_id) => {
-      
     const options = {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-    }
-    
-    fetch(`https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughts_id}/like`, options)
-      .then(res => res.json())
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    };
+
+    fetch(
+      `https://thereese-happy-thoughts.herokuapp.com/thoughts/${thoughts_id}/like`,
+      options
+    )
+      .then((res) => res.json())
       .then(() => {
-        fetchThoughts()
-    }) 
-  }
-    
+        fetchThoughts();
+      });
+  };
+
   return (
-    <div className='form-box'>
-      <Create 
-        newThought={newThought} 
-        onNewThoughtChange={handleNewThoughtChange} 
-        onFormSubmit={handleFormSubmit} 
+    <div className="form-box">
+      <Create
+        newThought={newThought}
+        onNewThoughtChange={handleNewThoughtChange}
+        onFormSubmit={handleFormSubmit}
       />
-      <Recent 
-        loading={loading} 
+      <Recent
+        loading={loading}
         thoughts={thoughts}
         onLikeIncrease={handleLikeIncrease}
       />
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
