42c0519ea12af517387e339c9d613e205a38fd46
Sarah Mottram <smelbows@gmail.com>
2022-05-29 11:21:59 +0200

    moved urls to single file

--- code/src/components/ExistingThoughts.js
+++ code/src/components/ExistingThoughts.js
@@ -1,15 +1,16 @@
 import React from "react";
 import moment from "moment";
 
+import { LIKES_URL } from "urls/urls";
+
 const ExistingThoughts = ({ allThoughts, setAllThoughts, fetchThoughts }) => {
   const onLikesIncrease = (thoughtId) => {
-    const LIKES_URL = `https://sarah-mottram-happy-thoughts.herokuapp.com/thoughts/${thoughtId}/like`;
 
     const options = {
       method: "POST",
     };
 
-    fetch(LIKES_URL, options).then(
+    fetch(LIKES_URL(thoughtId), options).then(
       fetchThoughts()
     );
   };
--- code/src/urls/urls.js
+++ code/src/urls/urls.js
@@ -1,1 +1,3 @@
 export const API_URL = 'https://sarah-mottram-happy-thoughts.herokuapp.com/thoughts'
+
+export const LIKES_URL = (thoughtId) => `https://sarah-mottram-happy-thoughts.herokuapp.com/thoughts/${thoughtId}/like`
