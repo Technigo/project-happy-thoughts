2b29c4d61d9d814bb21e7765adb134f28df34193
Joanna Lodell <joannalodell@c77-218-53-244.bredband.tele2.se>
2022-06-18 16:42:35 +0200

    update to styling and fixes

--- code/src/utilities/Api.js
+++ code/src/utilities/Api.js
@@ -1,3 +1,9 @@
 export const API_URL = `https://project-happy-thoughts-api-joa.herokuapp.com/thoughts`
 export const API_LIKES_URL = (singleThoughtId) =>
   `https://project-happy-thoughts-api-joa.herokuapp.com/thoughts/${singleThoughtId}/like`
+
+// export const API_URL = `http://localhost:8080/thoughts`
+// export const API_LIKES_URL = (singleThoughtId) =>
+//   `http://localhost:8080/${singleThoughtId}/like`
+
+  
