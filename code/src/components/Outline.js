605cfcbc0060cbfd45bd85f32b92dff947a0f454
Sailornina <sylcardozo.sc@gmail.com>
2022-10-03 19:15:45 +0200

    extra spaces was fixed in Outline.js

--- code/src/components/Outline.js
+++ code/src/components/Outline.js
@@ -2,20 +2,20 @@ import React, { useState, useEffect } from 'react';
 import Form from './Form';
 
 const Outline = () => {
-    const [thoughts, setThoughts] = useState([])
+  const [thoughts, setThoughts] = useState([])
 
-    /*Global Api*/
-    useEffect(() => {
-        fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts')
-            .then(res => res.json())
-            .then(json => setThoughts(json))
-    }, [])
+  /* Global Api */
+  useEffect(() => {
+    fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts')
+      .then((res) => res.json())
+      .then((json) => setThoughts(json))
+  }, [])
 
-    return (
-        <section className='container'>
-            <Form />
-        </section>
-    )
+  return (
+    <section className="container">
+      <Form />
+    </section>
+  )
 };
 
 export default Outline;
