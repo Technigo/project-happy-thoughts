809e17e03743e0185c0a92a4926a6908c95e4a21
Sofia Andersson <sofia.e.i.andersson@gmail.com>
2022-10-05 21:59:57 +0200

    Test version compeleted

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import TaskList from 'components/TaskList';
+import TaskForm from 'components/TaskForm';
 
 export const App = () => {
+  const [taskList, setTaskList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [newTodo, setNewTodo] = useState('');
+
+  const fetchTasks = () => {
+    setLoading(true);
+    fetch('https://week7-backend.herokuapp.com/tasks')
+      .then((res) => res.json())
+      .then((data) => setTaskList(data))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const handleNewTodoChange = (event) => {
+    setNewTodo(event.target.value)
+  }
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        description: newTodo
+      })
+    }
+
+    fetch('https://week7-backend.herokuapp.com/tasks', options)
+      .then((res) => res.json())
+      .then(() => fetchTasks())
+      .finally(() => setNewTodo(''));
+  }
+
   return (
     <div>
-      Find me in src/app.js!
+      <TaskForm
+        newTodo={newTodo}
+        onNewTodoChange={handleNewTodoChange}
+        onFormSubmit={onFormSubmit} />
+      <TaskList
+        loading={loading}
+        taskList={taskList}
+        setTaskList={setTaskList} />
     </div>
   );
 }
