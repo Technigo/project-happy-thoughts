2a1b8f9f0b2557a2c82cb3ec305b77e2e7b03f3b
Petra Söderström <petrasoderstrom1612@gmail.com>
2022-10-05 15:33:23 +0200

    making progress

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,90 @@
-import React from 'react';
+/* eslint-disable no-use-before-define */
+import React, { useState, useEffect } from 'react';
+import TaskList from 'components/TaskList';
+import TaskForm from 'components/TaskForm';
+import './reset.css';
+import './index';
 
 export const App = () => {
+  /* const [counter, setCounter] = useState(0);  */
+  const [taskList, setTaskList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [newTodo, setNewTodo] = useState('');
+
+  /* useEffect(() => { */
+  /* console.log('test'); */
+  /*   fetch('https://week7-backend.herokuapp.com/tasks')
+      .then((data) => data.json())
+      .then((transformedData) => setTaskList(transformedData))
+      .catch((error) => console.error(error))
+      .finally(() => console.log('everything is fine'));
+  }, []);
+  const onCounterIncrease = () => {
+    setCounter(counter + 1);
+  }  */
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = () => {
+    setLoading(true);
+    fetch('https://week7-backend.herokuapp.com/tasks')
+      .then((res) => res.json())
+      .then((data) => setTaskList(data))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }
+
+  /* useEffect(() => {
+     window.alert(`the current counter is ${counter}`)
+  }, [counter]) */
+
+  const handleNewTodoChange = (event) => {
+    setNewTodo(event.target.value)
+  }
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        description: newTodo
+      })
+    }
+    fetch('https://week7-backend.herokuapp.com/tasks', options)
+      .then((res) => res.json())
+      .then(() => fetchTasks())
+      .finally(() => setNewTodo(''));
+  }
   return (
+    <div className="outer-container">
+      <TaskForm
+        newTodo={newTodo}
+        onNewTodoChange={handleNewTodoChange}
+        onFormSubmit={onFormSubmit} />
+      <TaskList
+        loading={loading}
+        taskList={taskList}
+        setTaskList={setTaskList} />
+    </div>
+  )
+
+  /* return (
     <div>
       Find me src/app.js!
+      {<><p>{counter}</p><button onClick={onCounterIncrease} type="button">
+      counter increase
+      </button></>
+      {counter === 1 && (<TaskList list={taskList} />)}}
+      <TaskList list={taskList} />
     </div>
-  );
+  ); */
 }
+
+/* dummy toDo API => https://week7-backend.herokuapp.com/tasks */
+
---
+++ code/src/components/TaskForm.js
@@ -0,0 +1,18 @@
+/* eslint-disable linebreak-style */
+import React from 'react';
+
+const TaskForm = ({ newTodo, onNewTodoChange, onFormSubmit }) => {
+  return (
+    <form onSubmit={onFormSubmit} className="form-container">
+      <h2 className="form-headline">What&apos;s making you happy right now?</h2>
+      <textarea className="form-textarea" type="text" id="label-id" value={newTodo} onChange={onNewTodoChange} />
+      <button type="submit" className="form-submit-button" disabled={newTodo.length < 6 || newTodo.length > 140}>
+        <img src="https://img.icons8.com/emoji/48/000000/red-heart.png" alt="like" className="form-submit-button-image" />
+        <span>Send Happy Thought</span>
+        <img src="https://img.icons8.com/emoji/48/000000/red-heart.png" alt="like" className="form-submit-button-image" />
+      </button>
+    </form>
+  )
+};
+
+export default TaskForm;
