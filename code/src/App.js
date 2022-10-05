6801de848a80c7b5b5a70ba8011c8f1441e48fb3
Archkrull <102250838+Archkrull@users.noreply.github.com>
2022-10-05 16:40:11 +0200

    creating layout

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,60 @@
-import React from 'react';
+/* eslint-disable no-use-before-define */
+import React, { useState, useEffect } from 'react';
+
+import TaskList from 'components/TaskList';
+import SubmitForm from 'components/SubmitForm';
 
 export const App = () => {
+  const [taskList, setTaskList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [newThought, setNewThought] = useState('');
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = () => {
+    setLoading(true);
+    fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts')
+      .then((res) => res.json())
+      .then((data) => setTaskList(data))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }
+
+  const handleNewThoughtChange = (event) => {
+    setNewThought(event.target.value)
+  }
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        message: newThought
+      })
+    }
+
+    fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts', options)
+      .then((res) => res.json())
+      .then(() => fetchTasks())
+      .finally(() => setNewThought(''));
+  }
+
   return (
     <div>
-      Find me in src/app.js!
+      <SubmitForm
+        newThought={newThought}
+        onNewThoughtChange={handleNewThoughtChange}
+        onFormSubmit={onFormSubmit} />
+      <TaskList
+        loading={loading}
+        taskList={taskList}
+        setTaskList={setTaskList} />
     </div>
   );
 }
---
+++ code/src/components/SubmitForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SubmitForm = ({ newThought, onNewThoughtChange, onFormSubmit }) => {
+  return (
+    <div className="box submitBox">
+      <form className="submitForm" onSubmit={onFormSubmit}>
+        <h1 className="submitTitle">What is making you happy right now?</h1>
+        <textarea className="inputArea" value={newThought} onChange={onNewThoughtChange} />
+        <button className="submitButton" type="submit"><span className="heart" role="img" aria-label="heart">❤</span>Send Happy Thought!<span role="img" aria-label="heart">❤</span></button>
+      </form>
+    </div>
+  )
+}
+
+export default SubmitForm;
