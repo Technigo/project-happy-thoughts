43a404db00c46379f599abf12331974188d5b507
Annika Gadman <annika.gadman@gmail.com>
2022-10-05 22:54:00 -0500

    changed styling on charactersLeft

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-      <h1><span>💗</span> Happy <span>&#10084;&#65039;</span> Thoughts&nbsp;
+      <h1><span>💗</span>&nbsp;Happy<span>&#10084;&#65039;</span>Thoughts&nbsp;
         <span>💗</span>
       </h1>
     </header>
