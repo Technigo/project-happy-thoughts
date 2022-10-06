cac6cd86a22172ec655d52fdfdb94f897ccac7c2
Amanda Elvkull <108291356+AmandaElvkull@users.noreply.github.com>
2022-10-06 11:57:55 +0200

    added how many charachters has been writing and some styling

--- code/src/components/ThougthInput.js
+++ code/src/components/ThougthInput.js
@@ -3,12 +3,16 @@ import React from 'react'
 const ThougthInput = ({ newThought, onSend, onNewThought }) => {
   return (
     <form onSubmit={onSend} className="container-child-input">
+
       <p className="header">What´s making you happy right now?</p>
+
       <textarea
         value={newThought}
         onChange={onNewThought}
         className="inputBox"
         placeholder="What's on your mind? " />
+
+      <div className="max-length">{140 - newThought.length}  chracters left</div>
       <button type="submit" className="btn-send" disabled={newThought.length < 6 || newThought > 140}>  Send love ❤️</button>
     </form>
   )
