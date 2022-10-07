fa9ce09275167346b307045ee80b53360549b33a
Jessika Lind <jessika@MacBook-Air-som-tillhor-Jessika.local>
2022-10-07 13:55:39 +0200

    overflow added to ShowMessage

--- code/src/App.js
+++ code/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import React, { useState, useEffect } from 'react'
 import SendMessage from './Components/SendMessage'
 import ShowMessage from './Components/ShowMessage'
