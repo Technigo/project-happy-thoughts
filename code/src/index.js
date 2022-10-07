9ab8aea60fe0bac6bb6010e883622bfe69509b86
Petra Söderström <petrasoderstrom1612@gmail.com>
2022-10-07 11:08:36 +0200

    crlf

--- code/src/index.js
+++ code/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 // eslint-disable-next-line import/no-cycle
-import { App } from './App';
+import { App } from './App';n
 
 const container = document.getElementById('root');
 const root = createRoot(container);
