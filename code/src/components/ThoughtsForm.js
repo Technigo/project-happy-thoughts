d6e05fa95d2c8f90e10039bd1dbc6b2edf942921
NaghmehOkhovat <99764109+NaghmehOkhovat@users.noreply.github.com>
2022-10-07 10:17:06 +0200

    Added counter under input

--- code/src/components/ThoughtsForm.js
+++ code/src/components/ThoughtsForm.js
@@ -6,6 +6,7 @@ const ThoughtsForm = ({ newMessage, onNewMessageChange, onFormSubmit }) => {
     <form className="form-style" onSubmit={onFormSubmit}>
       <h1>What is making you happy right now?</h1>
       <textarea className="textBox" placeholder="Happy Thoughts here!" defaultValue={newMessage} onChange={onNewMessageChange} />
+      <p className="lenght-newMessage">{newMessage.length} / 140</p>
       <div className="main">
         <button className="button" disabled={newMessage.length < 4 || newMessage.length > 140} type="submit">❤️Send Happy Thoughts❤️</button>
       </div>
