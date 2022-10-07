9dcbb1345aef3fa05968bbb6b6cfaa4bd0278b15
Fredrik Holm <karlfredrikholm@gmail.com>
2022-10-07 10:32:30 +0200

    added some comments

--- code/src/components/LikeButton/LikeButton.jsx
+++ code/src/components/LikeButton/LikeButton.jsx
@@ -3,16 +3,20 @@ import React from 'react';
 import styles from './LikeButton.module.css';
 
 const LikeButton = ({ postedThought, postedThoughtId, thoughtsFeed, setThoughtsFeed }) => {
-  const handleMessageLiked = (likedThoughtId) => {
+  // this function maps through the thoughts and adds
+  // 1 like to the thought with the matching ID of
+  // the liked thought, and then updates the feed:
+  const handleMessageLiked = (likedThought) => {
     const updatedThoughtsFeed = thoughtsFeed.map((item) => {
-      if (item._id === likedThoughtId) {
+      if (item._id === likedThought) {
         item.hearts += 1
       }
       return item
     });
     setThoughtsFeed(updatedThoughtsFeed);
   };
-
+  // when clicking the like button a post request to that particular thought
+  // is sent and then we invoke the handleMessageLiked function
   const handleLikeButtonClick = () => {
     fetch(`https://happy-thoughts-technigo.herokuapp.com/thoughts/${postedThoughtId}/like`, {
       method: 'POST',
@@ -24,6 +28,7 @@ const LikeButton = ({ postedThought, postedThoughtId, thoughtsFeed, setThoughtsF
 
   return (
     <button
+    // if the posted thought has no likes background is gray, else pink
       className={postedThought.hearts === 0 ? styles.likeButton : styles.clickedLikeButton}
       onClick={handleLikeButtonClick}
       type="button">
