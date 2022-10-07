74d394bb095711584c1d98c70b68a1a341576390
Jessica Hansson <jessica.hansson@hotmail.se>
2022-10-07 15:53:04 +0200

    Trying to get button to work

--- code/src/App.js
+++ code/src/App.js
@@ -10,7 +10,9 @@ export const App = () => {
   const [newMessage, setNewMessage] = useState('');
   const [messageList, setMessageList] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [clickCounts, setClickCounts] = useState(0);
 
+  //Fetch API
   const fetchMessages = () => {
     setLoading(false);
     fetch(API)
@@ -20,14 +22,17 @@ export const App = () => {
       .finally(() => setLoading(false));
   }
 
+  //UseEffect hook gets all 20 recent thoughts
   useEffect(() => {
     fetchMessages();
   }, [newMessage]);
 
+ 
   const onNewMessageChange = (event) => {
     setNewMessage(event.target.value)
   }
 
+  //Post for new thought
   const onNewMessageSubmit = (event) => {
     event.preventDefault();
 
@@ -47,6 +52,36 @@ export const App = () => {
       .finally(() => setNewMessage(''))
   }
 
+   /* */
+
+   const addLike = (id, clicks) => {
+    //addLike gets an id as an argument which is the id we got from the function
+    //handleLikes, which is the function posting thoughts to the API. This was we make
+    //sure we are adding the like to the right thought. addLike will add +1 to the
+    //amount of hearts that thought had previously and show that in the browser
+    //right away without triggering a new fetch. That way the user gets inmediate
+    //confirmation that the like went thru. We check if the thought.id is equal to
+    //the id argument we got, and if that's the case we add +1
+    const updatedLikes = messageList.map((thought) => {
+      if (thought._id === id) {
+        thought.hearts += 1;
+      }
+      return thought;
+    });
+    //we setThoughts again so the thoughts now show with the updated amount of hearts
+    setMessageList(updatedLikes);
+    //Created a State for clickCounts which will show how many time the Heart button
+    //has been clicked, we get the number of clicks from the callback function in
+    //ThoughtCard component and add it to the existing clickCounts
+    setClickCounts(clicks + clickCounts);
+  };
+
+  
+
+/* */ 
+
+
+
   return (
     <div className="outer-wrapper">
       <div className="inner-wrapper">
@@ -59,7 +94,9 @@ export const App = () => {
         <MessageList
           loading={loading}
           messageList={messageList}
-          setMessageList={setMessageList} />
+          setMessageList={setMessageList}
+          addLike={addLike}
+           />
 
       </div>
     </div>
--- code/src/components/MessageList.js
+++ code/src/components/MessageList.js
@@ -1,20 +1,37 @@
 import React from "react";
 
-const MessageList = ({ loading, messageList, setMessageList, hearts, onLiked, id }) => {
-  if (loading === true) {
+
+const MessageList = ({ loading, messageList, hearts, addLike, id, setMessageList }) => {
+  if (loading) {
     return (
-      <h1>Loading in progress...</h1>
-      )
+    <h1>Loading in progress...</h1>
+    ) 
   }
-    
+    /*
   const handleClick = () => {
-    fetch(API, {
+    
+    fetch(`https://happy-thoughts-technigo.herokuapp.com/thoughts/${id}/like`, {
       method: 'POST',
       body: '',
       headers: { 'Content-Type': 'application/json' }
     }).then(() => onLiked(id))
-  }   
+  }   */
 
+  const handleLikes = () => {
+    //Stretch goal: created a click counter so we know how many times the Heart button
+    //has been clicked and send that data in the callback function to App component
+    let clicks = 0;
+    clicks += 1;
+    fetch(`https://vane-happy-thoughts.herokuapp.com/thoughts/${id}/like`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      //addLike is a function we got from App sent as prop, and is responsible for visually
+      //adding +1 to the amount of likes a thought has (it is explained in the App component)
+    }).then(() => {
+      addLike(id, clicks);
+    });
+  };
+  
   return (
     <section>
           {messageList.map((event) => (
@@ -22,13 +39,13 @@ const MessageList = ({ loading, messageList, setMessageList, hearts, onLiked, id
               
               <p className="message">{event.message}</p>
               
-              <div className="heart-and-counter-container">
+              <div className="heart-counter-container">
                 <span className="heart-counter">
                   <button 
-                  className="heart-btn" 
-                  onClick={handleClick}>❤️</button> x {hearts}
+                  className="heart-button" 
+                  onClick={handleLikes}>❤️</button> x {hearts}
+                  
                 </span>
-                
                 
               </div>
 
