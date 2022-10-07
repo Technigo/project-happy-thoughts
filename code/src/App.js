1237c7911fd49647527770f15859e11cac180eeb
Scarylie <saralie.bognandi@gmail.com>
2022-10-07 14:31:01 +0200

    footer added

--- code/src/App.js
+++ code/src/App.js
@@ -3,8 +3,14 @@ import { ThoughtsPage } from 'components/ThoughtsPage';
 
 export const App = () => {
   return (
-    <div>
+    <div className="app">
       <ThoughtsPage />
+      <footer>
+        <p>Created as a project at Technigo</p>
+        <p>
+          © <a href="mailto: mia_dahlgren@outlook.com">Saralie Bognandi</a> 2022
+        </p>
+      </footer>
     </div>
   );
 };
