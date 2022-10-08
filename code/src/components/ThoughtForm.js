c12d4cf94161db95206c04ab90b9ca109199facc
Sofia Andersson <sofia.e.i.andersson@gmail.com>
2022-10-08 13:18:56 +0200

    Message length, counter, likes grey/pink, box size

--- code/src/components/ThoughtForm.js
+++ code/src/components/ThoughtForm.js
@@ -6,8 +6,13 @@ const ThoughtForm = ({ newThought, onNewThoughtChange, onFormSubmit }) => {
     <form onSubmit={onFormSubmit}>
       <div className="thought-input-wrapper">
         <p className="thought-message">What&apos;s making you happy right now?</p>
-        <textarea className="text-input-area" value={newThought} onChange={onNewThoughtChange} />
-        <button type="submit" className="send-thought-button">❤️ Send Happy Thought ❤️</button>
+        <textarea className="text-input-area" value={newThought} placeholder="I'm happy because..." onChange={onNewThoughtChange} />
+        <div className="thought-form-details">
+          <button type="submit" className="send-thought-button">❤️ Send Happy Thought ❤️</button>
+          <p className="character-counter">
+            {140 - newThought.length} /140
+          </p>
+        </div>
       </div>
     </form>
   )
--- code/src/components/ThoughtList.js
+++ code/src/components/ThoughtList.js
@@ -11,10 +11,14 @@ const ThoughtList = ({ loading, thoughtList, handleNewLikeChange }) => {
       {thoughtList.map((thought) => (
         // eslint-disable-next-line no-underscore-dangle
         <div className="message-wrapper" key={thought._id}>
-          <h4>{thought.message}</h4>
+          <h4 className="thought-message">{thought.message}</h4>
           <div className="heart-counter-wrapper">
             <div className="heart-details">
-              <button className="heart-button" type="button" onClick={() => handleNewLikeChange(thought._id)}>❤️</button>
+              <button
+                type="submit"
+                className={thought.hearts === 0 ? 'no-likes-button' : 'has-likes-button'}
+                onClick={() => handleNewLikeChange(thought._id)}>❤️
+              </button>
               <p className="heart-count"> {thought.hearts}</p>
             </div>
             <p className="timestamp">{formatDistance(new Date(thought.createdAt), Date.now(), {
