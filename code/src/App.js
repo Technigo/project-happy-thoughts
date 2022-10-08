405e03f6d725368a4c49bab3f8e490b1a881bb0a
YoTesfam <102465267+YoTesfam@users.noreply.github.com>
2022-10-08 16:00:39 +0200

    Worked on formsubmission, response from the API, etc

--- code/src/App.js
+++ code/src/App.js
@@ -1,14 +1,67 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { NewThought } from 'components/NewThought';
 import { Thoughts } from 'components/Thoughts';
 
 export const App = () => {
-  const
+  const [newMessage, setNewMessage] = useState('');
+  const [thoughts, setThoughts] = useState([]);
+  const [loading, setLoading] = useState(false)
+
+  const handleNewMessageChange = (event) => {
+    setThoughts(event.target.value)
+  }
+
+  const fetchThoughts = () => {
+    setLoading(true)
+    fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts')
+      .then((res) => res.json())
+      .then((json) => setThoughts(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false))
+  }
+
+  useEffect(() => {
+    fetchThoughts();
+  }, []);
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const options = {
+      method: 'POST',
+      headers: { 'Content/Type': 'application/json' },
+      body: JSON.stringify({
+        message: newMessage
+      })
+    }
+
+    fetch('https://happy-thoughts-technigo.herokuapp.com/thoughts', options)
+      .then((res) => res.json())
+      .then(() => fetchThoughts())
+      .finally(() => setNewMessage(''));
+  }
+  const onNewThoughtSubmit = (_id) => {
+    const options = { method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      } }
+
+    fetch(`https://happy-thoughts-technigo.herokuapp.com/thoughts/${_id}/like`, options)
+      .then((res) => res.json())
+      .catch((error) => console.error(error))
+      .finally(() => fetchThoughts())
+  }
 
   return (
     <div>
-      <NewThought />
-      <Thoughts />
+      <NewThought
+        newMessage={newMessage}
+        handleNewMessageChange={handleNewMessageChange}
+        onFormSubmit={onFormSubmit} />
+      <Thoughts
+        loading={loading}
+        thoughts={thoughts}
+        onNewThoughtSubmit={onNewThoughtSubmit} />
     </div>
-  );
-}
+  )
+};
--- code/src/components/NewThought.js
+++ code/src/components/NewThought.js
@@ -3,13 +3,15 @@ import React from 'react';
 export const NewThought = ({ newMessage, handleNewMessageChange, onFormSubmit }) => {
   return (
 
-    <form className="message-container" onSubmit={onFormSubmit}>
-      <h1>what is making you happy right now?</h1>
-      <textarea
-        value={newMessage}
-        onChange={handleNewMessageChange}
-        placeholder="Let us know your happy thoughts here..." />
-      <button className="submit-btn" type="submit" disabled={newMessage.length < 6 || newMessage.length > 140}>❤️Send a Happy Thought❤️</button>
-    </form>
+    <div className="message-container">
+      <form onSubmit={onFormSubmit}>
+        <h1>what is making you happy right now?</h1>
+        <textarea
+          value={newMessage}
+          onChange={handleNewMessageChange}
+          placeholder="Let us know your happy thoughts here..." />
+        <button className="submit-btn" type="submit" disabled={newMessage.length < 6 || newMessage.length > 140}>❤️Send a Happy Thought❤️</button>
+      </form>
+    </div>
   )
 }
