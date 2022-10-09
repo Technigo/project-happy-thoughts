1d6247a57d28484af76b742e4ff3838acf633bf7
Linda Malm <Linda@Lindas-MacBook-Pro.local>
2022-10-09 14:31:06 +0200

    Got the width working again and fixed the button.

--- code/src/components/MessageList.js
+++ code/src/components/MessageList.js
@@ -5,23 +5,21 @@ import LikeBtn from './LikeBtn';
 
 const MessageList = ({ loading, setLoading, messageList, setNewLike }) => {
   return (
-    <div className="inner-wrapper">
-      <section className="message-boxes">
-        {messageList.map((event) => (
-          <div key={event._id} className="message-box">
-            <p className="message">{event.message}</p>
-            <div className="likes-and-time">
-              <LikeBtn
-                setNewLike={setNewLike}
-                event={event}
-                loading={loading}
-                setLoading={setLoading} />
-              <TimeStamp event={event} />
-            </div>
+    <section className="message-boxes">
+      {messageList.map((event) => (
+        <div key={event._id} className="message-box">
+          <p className="message">{event.message}</p>
+          <div className="likes-and-time">
+            <LikeBtn
+              setNewLike={setNewLike}
+              event={event}
+              loading={loading}
+              setLoading={setLoading} />
+            <TimeStamp event={event} />
           </div>
-        ))}
-      </section>
-    </div>
+        </div>
+      ))}
+    </section>
   )
 }
 
