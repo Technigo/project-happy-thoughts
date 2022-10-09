5f290b401d91f43226233077e6976820e64e403f
joelohman <joel.ohman@entryevent.se>
2022-10-09 14:40:45 +0200

    Implements localStorage counter for liked thoughts

--- code/src/components/ThoughtDetails.js
+++ code/src/components/ThoughtDetails.js
@@ -1,6 +1,5 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-closing-bracket-location */
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { formatDistanceToNow } from 'date-fns';
 import { HandleLikeButtonClick } from './util';
 
@@ -8,31 +7,50 @@ const ThoughtDetails = ({ thought }) => {
   // destructuring of variables from the thought object
   const { createdAt, hearts, _id } = thought;
 
-  // Local state variable to update only the thought that was liked
+  // Local state variable linked to the API to update only the thought that was liked
   const [likes, setLikes] = useState(hearts);
 
+  // local state variable to set/get localStorage
+  const [localLikes, setLocalLikes] = useState(
+    Number(localStorage.getItem(`localLikes ${_id}`))
+  );
+
+  const handleHeartButtonClick = () => {
+    // 1. sends a new heart to the api for the current thought
+    HandleLikeButtonClick(_id, setLikes);
+    // 2. updates local state variable which reloads the component
+    setLocalLikes(localLikes + 1);
+    // 3. updates localStorage which keeps track of how many times user has liked something locally
+    localStorage.setItem(`localLikes ${_id}`, JSON.stringify(localLikes));
+  };
+
   return (
-    <div className="thought-detail-container">
-      <div className="button-container">
-        <button
-          className="heart-button"
-          type="submit"
-          onClick={() => {
-            HandleLikeButtonClick(_id, setLikes);
-          }}
-        >
-          <span className="heart-emoji" role="img" aria-label="heart emoji">
-            💕
-          </span>
-        </button>
-        <p>x {likes}</p>
+    <div className="bajs">
+      <div className="thought-detail-container">
+        <div className="button-container">
+          <button
+            className="heart-button"
+            type="submit"
+            onClick={handleHeartButtonClick}
+          >
+            <span className="heart-emoji" role="img" aria-label="heart emoji">
+              💕
+            </span>
+          </button>
+          <p>x {likes}</p>
+        </div>
+        <p className="date-label">
+          {formatDistanceToNow(new Date(createdAt), Date.now(), {
+            addSuffix: true
+          })}
+          {' ago'}
+        </p>
       </div>
-      <p className="date-label">
-        {formatDistanceToNow(new Date(createdAt), Date.now(), {
-          addSuffix: true
-        })}
-        {' ago'}
-      </p>
+      {localLikes > 0 && (
+        <p className="like-text">
+          You have liked this thought {localLikes} times
+        </p>
+      )}
     </div>
   );
 };
