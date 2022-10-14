1cd656cae481b7a35c9165976774df025a7e91c3
KajaWilbik <wilbik.kaja@gmail.com>
2022-10-14 09:40:34 +0200

    last changes before hand-in

--- code/src/components/ThoughtForm.js
+++ code/src/components/ThoughtForm.js
@@ -4,7 +4,11 @@ const ThoughtForm = ({ handleFormSubmit, newThought, onNewThoughtChange }) => {
   return (
     <form onSubmit={handleFormSubmit}>
       <h2> Hello there, please enter your positive thought </h2>
-      <textarea value={newThought} onChange={onNewThoughtChange} />
+      <textarea
+        value={newThought}
+        onChange={onNewThoughtChange}
+        placeholder="Please, share your happy though here..."
+        maxLength="140" />
       <button type="submit">❤️ Submit your thought !❤️</button>
     </form>
   )
