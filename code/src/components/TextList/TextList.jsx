bd02341a1eed42cdc7e229dc892fccc6b510b041
Tara78 <70331037+Tara78@users.noreply.github.com>
2022-10-15 19:02:29 +0200

    responsiveness

--- code/src/components/TextList/TextList.jsx
+++ code/src/components/TextList/TextList.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import SingleText from "../SingleText/SingleText";
 import styles from "./TextList.module.css";
 import { formatRelative } from "date-fns";
-import LikeButton from "../LikeButton/LikeButton";
 
 /* 
 - This component renders an array of thoughts from the HappyThoughts API making a list and rendered the list by using the map method.
@@ -10,35 +9,47 @@ import LikeButton from "../LikeButton/LikeButton";
 - 
 */
 
-const TextList = ({ list, loading }) => {
-  if(loading) {
+const TextList = ({ list, loading, handleLikeSubmit, data }) => {
+  if (loading) {
     return <h4>Happy thoughts incoming...</h4>;
   }
   console.log(list);
 
   return (
     <div className={styles.wrapper}>
-     
-        {list.map((singleText) => {
-          return (
-            <section>
-              <div key={singleText._id} className={styles.sectionDescription}>
-                <SingleText
-                  description={singleText.description}
-                  checked={singleText.isChecked}
-                />
+      {list.map((singleText) => {
+        return (
+          <section>
+            <div key={singleText._id} className={styles.sectionDescription}>
+              <SingleText
+                description={singleText.description}
+                checked={singleText.isChecked}
+              />
 
-                <div className={styles.likeDatediv}>
-                  <LikeButton />
-                  <p>{formatRelative(singleText.date, new Date())}</p>
+              <div className={styles.likeDatediv}>
+                <div className={styles.likePlusLikeCounter}>
+                  <button
+                    type="submit"
+                    onClick={() => handleLikeSubmit(data._id)}
+                    className={styles.likeButton}
+                  >
+                    <span
+                      className="heart-span"
+                      role="img"
+                      aria-label="heart emoji"
+                    >
+                      ❤️
+                    </span>
+                  </button>
+
+                  <p className={styles.likeCounter}> × 0</p>
                 </div>
-
+                <p>{formatRelative(singleText.date, new Date())}</p>
               </div>
-            </section>
-          );
-        })}
-     
-      
+            </div>
+          </section>
+        );
+      })}
     </div>
   );
 };
