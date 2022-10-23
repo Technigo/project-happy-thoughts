e9723e3b8cffb8b91a7a35f26575d01105b1020c
carimore <110664183+carimore@users.noreply.github.com>
2022-10-23 20:44:20 +0200

    After code review

--- code/src/components/Thoughts.js
+++ code/src/components/Thoughts.js
@@ -57,7 +57,7 @@ const Thoughts = () => {
   };
 
   return (
-    <div>
+    <div className="thought-container">
       <NewThought
         newThought={newThought}
         newThoughtChange={newThoughtChange}
