0d45bf9604bc09164ede4e07e8725659a549517d
Scarylie <saralie.bognandi@gmail.com>
2022-12-01 15:26:00 +0100

    added email

--- code/src/App.js
+++ code/src/App.js
@@ -7,6 +7,10 @@ export const App = () => {
       <ThoughtsPage />
       <footer>
         <p>Created as a project at Technigo</p>
+        <p>
+          © <a href="mailto: saralie.bognandi@gmail.com">Saralie Bognandi</a>{' '}
+          2022
+        </p>
       </footer>
     </div>
   );
