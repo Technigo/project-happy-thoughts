c92d7eb117f40f25dd2de31d0977d1e98d15797e
Elin <elin.segelov@gmail.com>
2022-12-03 16:13:43 +0100

    Changes to loading state

--- code/src/components/LoadingComponent.js
+++ code/src/components/LoadingComponent.js
@@ -2,15 +2,13 @@ import React from 'react';
 
 const LoadingComponent = () => {
   return (
-    <article className="message-container animation-container" aria-label="Loading">
-      <lottie-player
-        src="https://assets4.lottiefiles.com/packages/lf20_khnalzic.json"
-        background="transparent"
-        speed="0.6"
-        loop
-        autoplay />
 
-    </article>
+    <lottie-player
+      src="https://assets4.lottiefiles.com/packages/lf20_khnalzic.json"
+      background="transparent"
+      speed="0.6"
+      loop
+      autoplay />
   )
 }
 
