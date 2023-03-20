ead8b9867db6b37db0ad7ec31d45c60fe935b834
Andreas Axelsson <pererikandreas@gmail.com>
2023-03-20 22:42:26 +0100

    tried the example

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import TaskList from 'Components/TaskList';
+import TaskForm from 'Components/TaskForm';
 
 export const App = () => {
+  const [taskList, setTaskList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [newTodo, setNewTodo] = useState('');
+
+  useEffect(() => {
+    // eslint-disable-next-line no-use-before-define
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = () => {
+    setLoading(true);
+    fetch('https://week-7-backend.onrender.com/tasks')
+      .then((res) => res.json())
+      .then((data) => setTaskList(data))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }
+
+  const handleNewTodoChange = (event) => {
+    setNewTodo(event.target.value)
+  }
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        description: newTodo
+      })
+    }
+
+    fetch('https://week-7-backend.onrender.com/tasks', options)
+      .then((res) => res.json())
+      .then(() => fetchTasks())
+      .finally(() => setNewTodo(''));
+  }
+
   return (
     <div>
-      Find me in src/app.js!
+      <TaskForm
+        newTodo={newTodo}
+        onNewTodoChange={handleNewTodoChange}
+        onFormSubmit={onFormSubmit} />
+      <TaskList
+        loading={loading}
+        taskList={taskList}
+        setTaskList={setTaskList} />
     </div>
-  );
+  )
 }
