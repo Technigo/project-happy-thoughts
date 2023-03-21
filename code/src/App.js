c35127df061d4fb29796b3218ae2ec1d5dd85ce1
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-03-21 14:16:38 +0100

    Displaying thoughts + Added send form component

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+/* eslint-disable no-underscore-dangle */
+import React, { useState, useEffect } from 'react';
+import { SendThoughtForm } from 'SendThoughtForm';
 
 export const App = () => {
+  const [thoughtList, setThoughtList] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts')
+      .then((response) => response.json())
+      .then((data) => setThoughtList(data))
+      .catch((error) => console.log(error))
+      .finally(() => { setLoading(false) })
+  }, []);
+
   return (
     <div>
-      Find me in src/app.js!
+      <SendThoughtForm />
+      <h1>Somethingsomething title here</h1>
+      {!loading && thoughtList.map((thought) => {
+        return (<p key={thought._id}>{thought.message}</p>)
+      })}
+      {loading && (<h2>LOADING</h2>)}
     </div>
   );
 }
---
+++ code/src/SendThoughtForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'Button'
+
+export const SendThoughtForm = () => {
+  return (
+    <div>
+      <form>
+        <h1>What is making you happy today?</h1>
+        <textarea />
+        <Button />
+      </form>
+    </div>
+  )
+}
