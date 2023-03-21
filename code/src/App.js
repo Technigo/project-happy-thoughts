39b96d9e72f26e7c1d0e9b5568f97a99f4819e2b
GG_Cugs <giorgio.cugusi@gmail.com>
2023-03-21 15:21:52 +0100

    first changes

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const App = () => {
+  const [messageList, setMessageList] = useState([])
+  const [loading, setLoading] = useState(false)
+  
+  const fetchMessages = () => {
+    setLoading(true);
+    fetch('https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts')
+      .then((res) => res.json())
+      .then((data) => setMessageList(data))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false))
+  }
+
+  useEffect(() => {
+    fetchMessages();
+  }, [])
   return (
     <div>
-      Find me in src/app.js!
     </div>
   );
 }
+
+/* npm install --save date-fns */
