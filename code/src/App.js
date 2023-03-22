2b79733bc98b3d5e95061bc7b702396c69dc3d6b
Annika Lindberg <annika.edit.lindberg@gmail.com>
2023-03-22 09:46:19 +0100

    added needed components and planned structure

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import Header from 'Components/Header';
+import { NewThoughtList } from 'Components/NewThought';
+import { ThoughtList } from 'Components/ThoughtList';
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <Header />
     </div>
   );
 }
---
+++ code/src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>
+        <span> ❤️️ </span>
+        Happy Thoughts
+        <span> ❤️ ️</span>
+      </h1>
+    </header>
+  );
+};
+export default Header 
