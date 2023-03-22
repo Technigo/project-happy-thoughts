5cf03c8194a7336628a76e16148f356ccef3bc56
Jonas Jakobson <jonas.jakobson@gmail.com>
2023-03-22 12:10:07 +0100

    added heart icons(need css)

--- code/src/components/Submit.js
+++ code/src/components/Submit.js
@@ -6,6 +6,15 @@ export const Submit = () => {
   const submitThought = (event) => {
     event.preventDefault();
     console.log(inputText);
+    // Replace console.log with api-post
+    fetch('https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts', {
+      method: 'POST',
+      body: JSON.stringify(inputText)
+    })
+      .then((response) => response.json())
+      .then((newThought) => {
+        console.log(newThought)
+      })
     setInputText('');
   };
 
@@ -13,7 +22,9 @@ export const Submit = () => {
     <form onSubmit={submitThought}>
       <label htmlFor="text-area">Whats making you happy right now?
         <textarea name="text-area" id="text-area" value={inputText} onChange={((event) => setInputText(event.target.value))} />
-        <button type="submit">Send Happy Thought</button>
+        <button type="submit">
+            &#x2764;Send Happy Thought&#x2764;
+        </button>
       </label>
     </form>
   )
--- code/src/components/Thoughts.js
+++ code/src/components/Thoughts.js
@@ -4,6 +4,9 @@ export const Thoughts = ({ message }) => {
   return (
     <div>
       <h2>{message}</h2>
+      <button type="button" className="heart-button">
+        &#x2764;
+      </button>
     </div>
   )
 }
