3436ea9799f4e990bc216bb0cb54a3f218638e41
Annika Lindberg <annika.edit.lindberg@gmail.com>
2023-03-22 10:52:40 +0100

    changed structure for components

--- code/src/App.js
+++ code/src/App.js
@@ -1,13 +1,31 @@
 import React, { useEffect, useState } from 'react';
 
 import Header from 'Components/Header';
-import { NewThoughtList } from 'Components/NewThought';
-import { ThoughtList } from 'Components/ThoughtList';
+import { NewThought } from 'Components/NewThought';
+import { ThoughtList } from 'Components/ThoughtList'
+
 
 export const App = () => {
+const [thoughts, setThoughts] = useState([]); //innan: thoughts, setThoughtList 
+const [loading, setLoading] = useState(false);
+  const [newMessage, setNewMessage] = useState(''); //
+
+  const handleNewThoughtsChange = (event) => {
+    setNewMessage(event.target.value)
+  }
   return (
-    <div>
-      <Header />
-    </div>
+    <div className='main-container'>
+    <Header />
+    <div className='thoughts'>
+    <NewThought
+      newMessage={newMessage}
+      handleNewThoughtsChange={handleNewThoughtsChange}
+      onFormSubmit={onFormSubmit} />
+    <ThoughtList
+      loading={loading}
+      thoughts={thoughts}
+      onLikesIncrease={onLikesIncrease} />
+   </div>
+  </div> 
   );
-}
+};
---
+++ code/src/Components/ThoughtList.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const ThoughtList = ({ loading, thoughts, onLikesIncrease }) => {
+    
+}
