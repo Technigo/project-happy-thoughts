9fd9694c0938d0b01452278e214eed92199fea86
Vera Sjunnesson <Vera@admins-mbp.home>
2023-03-22 12:40:11 +0100

    fetched data - initial start

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const App = () => {
+  const [thoughtList, setThoughtList] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts')
+      .then((response) => response.json())
+      .then((data) => setThoughtList(data))
+      .catch((error) => console.log(error))
+      .finally(() => { setLoading(false) })
+  }, [])
+
   return (
     <div>
-      Find me in src/app.js!
+      {!loading && thoughtList.map((thought) => {
+        // eslint-disable-next-line no-underscore-dangle
+        return (<p key={thought._id}>{thought.message}</p>)
+      })}
+      {loading && (<h2>Loading...</h2>)}
     </div>
   );
 }
