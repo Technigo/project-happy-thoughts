88bcf934c7cc9ae1f99fe3e9ea16c4b5ef7041ef
Fiona Klacar <fiona.klacar@gmail.com>
2023-03-22 14:15:41 +0100

    fetched posts to the app and posted thoughts to the app

--- code/src/App.js
+++ code/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React, { useState, useEffect } from 'react';
 import { ThoughtsList } from 'components/ThoughtsList';
 
@@ -8,16 +9,13 @@ export const App = () => {
   const [thoughtsList, setThoughtsList] = useState([]);
   // because the Thoughts List changes state when a new thought is added
 
-  // const [onNewThoughtChange, setOnNewThoughtChange] = useState(false); -
-  // is this the same as the thoughtsList changing state?
-
   const fetchThoughts = () => {
     fetch('https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts')
-    // this first one to fetch, and the second one, to post?
-      .then((res) => res.json())
-      .then((data) => setThoughtsList(data))
-      .catch((error) => console.error(error))
-      // .finally(() => setLoading(false));
+      .then((res) => res.json()) // converts the 'response' object to a JSON object
+      .then((data) => setThoughtsList(data)) // updates the state with the data from the
+    // response using the 'setThoughtsList' function
+      .catch((error) => console.error(error)) // catches errors
+      .finally(() => { console.log('fetch was successful') }) // this is where setLoading(false)); would go
   }
 
   useEffect(() => {
@@ -25,35 +23,64 @@ export const App = () => {
   // we want to fetch the most recent thoughts as a side effect
   // every time the page is mounted/reloaded - so we use an empty array as a dependency
   }, []);
+  // const handleNewThoughtChange = (event) => {
+  //   setNewThought(event.target.value)
+  // }
 
-  const handleNewThoughtChange = (event) => {
+  const onFormSubmit = (event) => { // difference between onFormSubmit and handleFormSubmit - the same thing
+    event.preventDefault(); // prevents from reloading entirely when user submits a thought
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        message: newThought
+      })
+    }
+
+    fetch('https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts', options)
+      .then((res) => res.json())
+      .then((data) => console.log(data)) // Don't put setThoughtsList or setNewThoughts, as the whole thing will be replaced
+      .catch((error) => console.error(error)) // catches errors
+      .finally(() => fetchThoughts()); // This collects all the updated thoughts including the one you just posted, and displays
+  }
+
+  // THIS IS ANOTHER WAY OF DOING IT
+  // .then((res) => res.json())
+  // .then((newThought) => {
+  //   setThoughts((previousThoughts) => [newThought,...previousThoughts]) // Here is object destructuring - it takes the
+  // new thought to the array as an object and adds the previous thoughts to it after
+  // })
+
+  // To stop the default behaviour of a form submission leading to page being refreshed or
+  // navigating to a new URL when a new thought is typed
+
+  // ON NOTION:
+  // fetch('https://technigo-thoughts.herokuapp.com/', {
+  //   method: 'POST',
+  //   body: JSON.stringify({ message: 'Hello world' })
+  // })
+  //   .then((res) => res.json())
+  //   .then((newThought) => {
+  //     // Now you have `newThought` which is the response from the
+  //     // API as documented at the top of this readme. You can use
+  //     // it to update the `thoughts` array:
+  //     setThoughts((previousThoughts) => [newThought, ...previousThoughts])
+  //   })
+  const onNewThoughtChange = (event) => {
     setNewThought(event.target.value)
   }
 
-  // const onFormSubmit = (event) = {
-  //   event.preventDefault();
-  // }
-  // To stop the default behaviour of a form submission leading to page being refreshed or 
-  // navigating to a new URL
-
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      description: newThought
-    })
-  }
-
-  fetch('https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts', options)
-    .then((res) => res.json())
-    .then(() => fetchThoughts())
-    .finally(() => setNewThoughts(''));
-
   return (
     <div>
-      <ThoughtsList />
+      <ThoughtsList newThought={newThought} onNewThoughtChange={onNewThoughtChange} onFormSubmit={onFormSubmit} />
+      {thoughtsList.map((thought) => {
+        // eslint-disable-next-line no-underscore-dangle
+        return (<p key={thought._id}>{thought.message}</p>)
+      })}
     </div>
   )
 }
+// event handler where setNewThought state updates?
