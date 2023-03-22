2bfcac69711632aa82cae689f0f9da327e6386fe
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-03-22 21:55:36 +0100

    Added like and date

--- code/src/LikeButton.js
+++ code/src/LikeButton.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const LikeButton = () => {
   return (
     <div>
-      <button type="submit">Like</button>
+      <button type="submit">128516 Like</button>
     </div>
 
   )
--- code/src/Thought.js
+++ code/src/Thought.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { formatDistance } from 'date-fns';
 
 export const Thought = (props) => {
   return (
@@ -6,11 +7,14 @@ export const Thought = (props) => {
       <p>{props.thoughtMessage}</p>
       <div className="like-counter-date-container">
         <div className="like-counter-container">
-          <button type="submit" handleLikeSubmit={props.handleLikeSubmit}>Like</button>
+          <button type="submit" handleLikeSubmit={props.handleLikeSubmit}> ❤️ Like</button>
           <p>like counter</p>
         </div>
         <div>
-          <p>{props.timeStamp}</p>
+          <p>{formatDistance(new Date(props.timeStamp), Date.now(), {
+            addSuffix: true
+          })}
+          </p>
         </div>
       </div>
     </article>
