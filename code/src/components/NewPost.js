c3fa1c7bfe5414734ab9fb4af6f3c0e941dee652
fannystenberg <ffannystenberg@gmail.com>
2023-03-22 17:19:17 +0100

    added error msg, submit animation and text counter

--- code/src/components/NewPost.js
+++ code/src/components/NewPost.js
@@ -1,27 +1,35 @@
 import React, { useState } from 'react';
+import confetti from 'https://esm.run/canvas-confetti@1';
 
 const NewPost = () => {
   const [newPost, setNewPost] = useState('');
 
-  const handleFormSubmit = (event) => {
+  const HandleFormSubmit = (event) => {
     event.preventDefault();
-    const Submit = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ message: newPost })
+    if (newPost.length < 5) {
+      return alert('Oh no! Too short message, needs to be minimum 5 characters')
+    } else if (newPost.length > 140) {
+      return alert('Oh no! Too long message, keep it within 140 characters')
+    } else {
+      const Submit = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ message: newPost })
+      };
+      confetti({ particleCount: 700, spread: 90 })
+      fetch('https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts', Submit)
+        .then((response) => response.json())
+        .then(() => {
+          window.location.reload();
+          setNewPost('');
+        })
     }
-    fetch('https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts', Submit)
-      .then((response) => response.json())
-      .then(() => {
-        window.location.reload();
-        setNewPost('');
-      })
   };
 
   return (
     <div className="newPostWrapper">
       <h3 className="newPostTitle">What is making you happy right now?</h3>
-      <form onSubmit={handleFormSubmit}>
+      <form onSubmit={HandleFormSubmit}>
         <textarea
           id="newPost"
           type="text"
@@ -29,10 +37,8 @@ const NewPost = () => {
           rows="4"
           value={newPost}
           onChange={(event) => setNewPost(event.target.value)} />
-        <button
-          className="postBtn"
-          type="submit"
-          disabled={newPost.length < 5 || newPost.length > 140}>
+        <p className="counter">{140 - newPost.length} / 140</p>
+        <button className="postBtn" type="submit">
           <span>
             <span role="img" aria-label="heart">❤️ </span>
           Send happy thought
