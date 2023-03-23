c0a3bc67441b47b23fc41b8cfe2ebd5673b5f5ea
cpz-manifold <carol.pinzon.ardila@gmail.com>
2023-03-23 19:01:09 +0100

    blue

--- code/src/App.js
+++ code/src/App.js
@@ -1,57 +1,87 @@
+/* eslint-disable no-underscore-dangle */
 import React, { useState, useEffect } from 'react';
 
 export const App = () => {
   const [stateVariable, setStateVariable] = useState('');
   const [thoughtsList, setThoughtsList] = useState([]);
-  const [refreshData, setRefreshData] = useState(true);
-  const happyEndpoint = 'https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts'
+  const happyEndpoint = 'https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts';
 
   const fetchHappy = () => {
-    fetch(happyEndpoint) // fetching the api data
+    fetch(happyEndpoint)
       .then((response) => response.json())
       .then((data) => setThoughtsList(data))
       .catch((error) => console.log(error))
-      .finally(() => { console.log('fetch was successfull') })
+      .finally(() => { console.log('fetch was successful') });
   }
-  // this function will send the message to the api.
+
   const sendHappy = () => {
     fetch(happyEndpoint, {
       method: 'POST',
-      body: JSON.stringify({
-        message: stateVariable
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8'
-      }
+      body: JSON.stringify({ message: stateVariable }),
+      headers: { 'Content-type': 'application/json; charset=UTF-8' }
     })
       .then(() => {
-        // to clear the input after the message has been sent to the API.
-        setStateVariable('')
-
-        // to refresh the list with the new data.
-        setRefreshData(false) // erase the old version of happy thoughts.
-        fetchHappy();
-        setRefreshData(true) // display new version of happy thoughts.
+        setStateVariable(''); // Clear the input after the message has been sent to the API.
+        fetchHappy(); // Update/Bring me the thought list with the new ones
       })
-      .finally(
-        console.log('POST request successful')
-      )
+      .finally(() => {
+        console.log('POST request was successful');
+      })
   }
+  const increaseLike = (thought) => {
+    const options = {
+      method: 'POST'
+    }
 
-  useEffect(fetchHappy, [])
+    console.log('options', options)
+    // eslint-disable-next-line no-underscore-dangle
+    fetch(`https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${thought._id}/like
+    `, options)
+      .then((response) => response.json())
+      .then((data) => { console.log(data) })
+      .catch((error) => console.log(error))
+      .finally(() => {
+        fetchHappy();
+        console.log('heart count increased');
+      })
+  }
+  // excecute the fetch happy on the first render since the array is empty
+  useEffect(fetchHappy, []);
 
   return (
     <>
-      <div className="Card"> <h1>What is making you happy now?</h1>
-        <input type="text" value={stateVariable} onChange={(event) => setStateVariable(event.target.value)} />
-        <button className="send" type="button" onClick={(event) => { sendHappy(event) }}>❤️ Send happiness ❤️ </button>
+      <div className="Card">
+        <h1>What is making you happy now?</h1>
+        <input
+          type="text"
+          value={stateVariable}
+          onChange={(event) => setStateVariable(event.target.value)} />
+        <button
+          className="send"
+          type="button"
+          onClick={() => { sendHappy() }}>❤️ Send happy thought ❤️
+        </button>
       </div>
-      <div className="messages">
-        {refreshData && thoughtsList.map((thought) => {
-        // eslint-disable-next-line no-underscore-dangle
-          return (<p key={thought._id}>{thought.message} ❤️: {thought.hearts}</p>)
-        })}
+      <div>
+        {thoughtsList.map((thought) => (
+          <div className="messages" key={thought._id}>
+
+            <div>
+
+              <p key={thought._id}>{thought.message}</p>
+              <p>
+                <button className="like-button" onClick={() => { increaseLike(thought) }} type="button">
+                ❤️
+                </button>
+                <span> x {thought.hearts}</span>
+
+              </p>
+            </div>
+          </div>
+        ))}
       </div>
+
     </>
   );
-}
+};
+
