2316eadd99e93ec01dacb054b574529679513738
Annaselvine <120709649+AnnaElvine@users.noreply.github.com>
2023-03-23 14:11:37 +0100

    some css stuff left

---
+++ code/src/Components/NewThought.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+// The newMessage state is passed in as a prop to the component
+// The handleNewThoughtsChange function is called when
+// the user interacts with the input field and and onFormSubmit when user submits the form.
+export const NewThought = ({ newMessage, handleNewThoughtsChange, onFormSubmit }) => {
+  const [remainingChars, setRemainingChars] = useState(160 - newMessage.length);
+
+  const handleInputChange = (event) => {
+    const input = event.target.value;
+    const remaining = 160 - input.length;
+    setRemainingChars(remaining);
+    handleNewThoughtsChange(event);
+  };
+
+  return (
+    <form className="message-container" onSubmit={onFormSubmit}>
+      <p className="thought-text">Share a happy thought!</p>
+      <textarea
+        placeholder="What's on your mind?"
+        value={newMessage}
+        onChange={handleInputChange} />
+      <div className="main">
+        <div className="char-count">{remainingChars} characters remaining</div>
+        <button className="submit-btn" type="submit" disabled={newMessage.length < 1 || newMessage.length > 160}>
+          ❤️ Send a Happy Thought ❤️
+        </button>
+      </div>
+    </form>
+  );
+};
