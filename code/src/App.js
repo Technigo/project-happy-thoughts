6749885078bc72c9d73f1eacfd8f75a80dc4e2f5
GG_Cugs <giorgio.cugusi@gmail.com>
2023-03-23 14:29:14 +0100

    nothing works, getting sad

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,16 @@
+/* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable max-len */
 import React, { useState, useEffect } from 'react';
+import { DisplayMessage } from './components/displayMessage/displayMessage.js'
+import { PostMessage } from './components/postMessage/postMessage.js'
 
 export const App = () => {
   const [messageList, setMessageList] = useState([])
   const [loading, setLoading] = useState(false)
-  
+  const [newMessage, setNewMessage] = useState('');
+
   const fetchMessages = () => {
     setLoading(true);
     fetch('https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts')
@@ -16,10 +23,59 @@ export const App = () => {
   useEffect(() => {
     fetchMessages();
   }, [])
-  return (
-    <div>
-    </div>
-  );
-}
 
-/* npm install --save date-fns */
+  const handleNewMessage = (event) => {
+    setNewMessage(event.target.value)
+  }
+
+  const onFormSubmit = (event) => {
+    event.preventDefault()
+
+    // options that says how the information we send should be added into the API
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        message: newMessage
+      })
+    }
+    // 2nd fetch request within OnFormSubmit
+    fetch('https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts', options)
+      .then((result) => result.json())
+      .then(() => fetchMessages())
+      .finally(() => setNewMessage(''))
+
+    // function to increase the likes
+    const LikeCounter = (LikeID) => {
+      // options-object decides how changes in the API should look
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+      fetch(`https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${LikeID}/like`, options)
+        .then((result) => result.json())
+        .then(() => fetchMessages())
+        .catch((error) => console.log(error))
+        .finally(() => { setLoading(false) })
+      return (
+        <div className="happy-thoughts-box">
+          <PostMessage
+            newMessage={newMessage}
+            onFormSubmit={onFormSubmit}
+            handleNewMessage={handleNewMessage} />
+          <div>
+            <DisplayMessage
+              loading={loading}
+              messageList={messageList}
+              setMessageList={setMessageList}
+              LikeCounter={LikeCounter} />
+          </div>
+        </div>
+      );
+    }
+  }
+}
---
+++ code/src/components/displayMessage/displayMessage.js
@@ -0,0 +1,31 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable no-underscore-dangle */
+import React from 'react';
+import { formatDistance } from 'date-fns';
+import './displaymessage.css'
+
+export const DisplayMessage = ({ loading, messageList, LikeCounter }) => {
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading in progress...</h1>
+      </div>
+    )
+  }
+  return (
+    <div className="message-list-container">
+      {messageList.map((list) => (
+        <div className="message-box" key={list._id}>
+          <p className="message-text">{list.message}</p>
+          <div className="like-and-date">
+            <div className="button-and-counter">
+              <button className={(list.hearts === 0 ? 'no-like-button' : 'like-button')} onClick={() => LikeCounter(list._id)}>❤️</button>
+              <p className="counter">x {list.hearts}</p>
+            </div>
+            <p className="date-text">{formatDistance(new Date(list.createdAt), Date.now(), { addSuffix: true })}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
