93c0e083563b97c3d5eabe17eff113aa31f803d2
Frid Matilda <matilda.x.frid@scania.com>
2023-03-23 09:59:40 +0100

    styling on main parts

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-      <h1>Project Happy Thoughts Feed</h1>
+      <h1>Project Happy Thoughts</h1>
     </header>
   );
 };
