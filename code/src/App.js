e2f5d732747cdccfc2e010702ca9312783898134
Emma Engvall <emma.engvall@ica.se>
2023-03-23 10:20:58 +0100

    started creating components

--- code/src/App.js
+++ code/src/App.js
@@ -1,10 +1,37 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ThoughtList } from 'components/ThoughtsList'
 
 export const App = () => {
+
+  const [thoughtList, setThoughtsList] = useState ([]);
+  const [loading, setLoading] = useState(false);
+
+useEffect(() => {
+  fetchThoughts();
+}, []);
+
+const fetchThoughts = () => {
+  setLoading(true);
+  fetch('https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts')
+    .then(res => res.json())
+    .catch(error => console.error(error))
+    .finally(() => setLoading(false));
+}
+// add .then(data => setThoughtsList(data))
+
+
+const handleFormSubmit = (event) => {
+  event.preventDefault()
+}
+
+
   return (
     <div>
       Here goes the heart ❤️
     </div>
   );
 }
+
+//button type="submit"
+// Other components: ThoughtsList look at TaskList.js createdAt instead of date
