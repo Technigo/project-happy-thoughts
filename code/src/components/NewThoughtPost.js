c34bebda23dc8d9bda9899c4d3fab0860f2ecf1e
theresBL <tbrannberglendt@gmail.com>
2023-03-24 21:48:56 +0100

    added characters-counter

--- code/src/components/NewThoughtPost.js
+++ code/src/components/NewThoughtPost.js
@@ -3,8 +3,9 @@ import React from 'react'
 export const NewThoughtPost = ({ newThought, onNewThoughtChange, onFormSubmit }) => {
   return (
     <form className="thought-form" onSubmit={onFormSubmit}>
-      <h1>Spread the love and share your happy thought..</h1>
+      <h1>Spread the love and share your happy thought...</h1>
       <textarea value={newThought} onChange={onNewThoughtChange} />
+      <p className="post-length">{newThought.length} / 140</p>
       <button
         className="submit-btn"
         type="submit"
