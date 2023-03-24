41a0b75a45b053cb35d25ab9363010120c343a66
Ylva Karlsson <121539816+YlvaKarlsson@users.noreply.github.com>
2023-03-24 22:28:36 +0100

    First commit

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
+import HappyThoughtsFeed from './HappyThoughtsFeed';
 
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  );
-}
+  return <HappyThoughtsFeed />;
+};
---
+++ code/src/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div aria-hidden="true" className="contact">
+        <p>
+          CREATED BY YLVA KARLSSON
+          <br />
+          STUDENT @ TECHNIGO 2023
+        </p>
+        <button
+          type="button"
+          className="contactMe"
+          onClick={() => window.open('https://www.linkedin.com/in/ylvakarlsson87/')}>
+          Contact
+        </button>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
