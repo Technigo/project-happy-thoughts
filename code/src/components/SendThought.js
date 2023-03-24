09723f92545c20b3175bb47066474d22e36e4c1d
AntoniaGranit <antonia.granit@gmail.com>
2023-03-24 08:13:39 +0200

    add max length to textarea input form

--- code/src/components/SendThought.js
+++ code/src/components/SendThought.js
@@ -33,12 +33,15 @@ export const SendThought = ({ sendThought, setSendThought }) => {
         </p>
         <textarea
           placeholder="My happy thought..."
+          maxLength="140"
           value={sendThought}
           onChange={handleSendThought} />
         <div className="character-div"><p className="character-count">{sendThought.length} / 140</p></div>
         <button
           type="submit"
-          className="send-thought-btn">&#128140; Send Happy Thought &#128140;
+          className="send-thought-btn"
+          disabled={sendThought.length < 5 || sendThought.length > 140}>
+            &#128140; Send Happy Thought &#128140;
         </button>
       </form>
     </div>
