046197313358fc71270b43f1a59a9c955d4337a1
Ylva Karlsson <121539816+YlvaKarlsson@users.noreply.github.com>
2023-03-24 22:47:09 +0100

    And again

--- code/src/HappyThought.js
+++ code/src/HappyThought.js
@@ -1,27 +1,15 @@
 /* eslint-disable linebreak-style */
-import React, { useState } from 'react';
+import React from 'react';
 import moment from 'moment';
+// {props.text}
 
 const HappyThought = (props) => {
-  const [hearts, setHearts] = useState(props.hearts); // initialize state with the number of likes
-
-  const handleLike = () => {
-    fetch(`https://<API_URL>/thoughts/${props.id}/like`, { // send a POST request to your API endpoint to increase the number of likes for this thought
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ increment: 1 })
-    })
-      .then((response) => response.json())
-      .then((data) => setHearts(data.hearts)); // update the state with the new number of likes
-  };
-
   return (
     <div className="happy-thought-wrapper">
       <p>{props.text}</p>
       <div className="data-wrapper">
         <div className="likes">
-          <button type="submit" onClick={handleLike}>❤️</button>x{hearts}
-          {/* update the number of likes with the state value */}
+          <p>❤️x{props.hearts}</p>
         </div>
         <div className="timestamp">
           <p>{moment(props.timestamp).startOf('hour').fromNow()}</p>
