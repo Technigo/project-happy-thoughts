1e251d6741d466b21f5478eba58cec9a9f7bfd61
EmiliaGA <emilia-lundmark@hotmail.com>
2023-03-24 13:22:36 +0100

    bugs

--- code/src/components/ThoughtList.js
+++ code/src/components/ThoughtList.js
@@ -1,10 +1,15 @@
 /* eslint-disable no-underscore-dangle */
+
 import React from 'react';
 import { formatDistance } from 'date-fns';
 
 export const ThoughtList = ({ loading, thoughts, onLikesIncrease }) => {
   if (loading) {
     return <div className="Loading">Loading</div>
+  }
+
+  const handleOnLikesIncrease = (id) => {
+    onLikesIncrease(id);
   }
 
   return (
@@ -14,7 +19,7 @@ export const ThoughtList = ({ loading, thoughts, onLikesIncrease }) => {
         <div className="Thoughts" key={list._id}>
           <p className="thought-text">{list.message}</p>
           <div className="likes">
-            <button type="button" className={(list.hearts === 0 ? 'like-btn' : 'no-like-btn')} onClick={() => onLikesIncrease(list._id)}>❤️</button>
+            <button type="button" className={(list.hearts === 0 ? 'like-btn' : 'no-like-btn')} onClick={() => handleOnLikesIncrease(list._id)}>❤️</button>
             <p className="counter">x {list.hearts}</p>
             <p className="date">
               {formatDistance(new Date(list.createdAt), Date.now(), { addSuffix: true })}
