70838856554d777bd7b68c7b02745c97932aafaa
Ed <edvard.ekstrom@gmail.com>
2023-03-25 09:59:13 +0100

    init, fetching thoughts

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Thoughts } from './components/Thoughts';
 
 export const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [thoughtsArray, setThoughtsArray] = useState([]);
+
+  const API_URL = 'https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts'
+
+  const fetchHappyThoughts = () => {
+    setIsLoading(true);
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => setThoughtsArray(data))
+      .catch((error) => console.log(error))
+      .finally(() => setIsLoading(false));
+  }
+
+  useEffect(() => {
+    fetchHappyThoughts();
+  }, [])
+
   return (
-    <div>
-      Find me in src/app.js!
+    <div className="app-container">
+      <div className="thought-container">
+        <Thoughts loading={isLoading} thoughts={thoughtsArray} />
+      </div>
     </div>
   );
 }
