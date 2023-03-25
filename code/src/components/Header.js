2c7ae6c4d9ac1f8b0e9a9a290ac45714c73c8f8d
Emma Holtegaard <emmaholtegaard@gmail.com>
2023-03-25 14:23:25 +0100

    spend way to much time making a logo for the header

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,8 +1,14 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
+import logo from './assets/logo2.png'
 
 const Header = () => {
-  return (<header><h1 className="header-text">Happy Thoughts</h1></header>)
+  return (
+    <header>
+      <div className="image-wrapper">
+        <img className="logo" src={logo} alt="Happy Thoughts" />
+      </div>
+    </header>)
 }
 
 export default Header;
