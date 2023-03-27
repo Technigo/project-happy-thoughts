02efccf69359865a4cf4dbb2440602b84f53ce22
camcron <camillacronqvist@hotmail.com>
2023-03-27 09:34:16 +0200

    Changed fetch of api-posts from app to postslist

--- code/src/App.js
+++ code/src/App.js
@@ -1,38 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Header } from 'components/Header/Header';
 import { PostList } from './components/PostsList/PostsList';
 import { NewPost } from './components/NewPost/NewPost';
 import './App.css';
 
 export const App = () => {
-  const [postList, setPostList] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  const fetchThoughts = () => {
-    setLoading(true);
-    fetch('https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts')
-      .then((response) => response.json())
-      .then((data) => setPostList(data))
-      .catch((error) => console.log(error))
-      .finally(() => { setLoading(false) })
-  }
-
-  useEffect(() => {
-    fetchThoughts();
-  }, []);
-
-  const addNewPost = (newPost) => {
-    setPostList([newPost, ...postList]);
-  };
-
   return (
     <>
       <header>
         <Header />
       </header>
       <main className="main-wrapper">
-        <NewPost newPost={addNewPost} fetchThoughts={fetchThoughts} />
-        <PostList postList={postList} loading={loading} />
+        <NewPost />
+        <PostList />
       </main>
     </>
   );
--- code/src/components/PostsList/PostsList.js
+++ code/src/components/PostsList/PostsList.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { SinglePost } from './SinglePost/SinglePost.js';
 import './PostList.css';
 
-export const PostList = ({ postList, loading }) => {
+export const PostList = () => {
+  const [postList, setPostList] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchThoughts = () => {
+    setLoading(true);
+    fetch('https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts')
+      .then((response) => response.json())
+      .then((data) => setPostList(data))
+      .catch((error) => console.log(error))
+      .finally(() => { setLoading(false) })
+  }
+
+  useEffect(() => {
+    fetchThoughts();
+  }, []);
+
   return (
     <section className="postlist" aria-label="List of thoughts">
       <ul>
