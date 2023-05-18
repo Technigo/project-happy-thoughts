41037b050ac911c7be09d979ff3c233c1f4821cf
camcron <camillacronqvist@hotmail.com>
2023-05-18 15:40:18 +0200

    Added text to loading-state

--- code/src/components/Loader/Loader.js
+++ code/src/components/Loader/Loader.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import './Loader.css';
 import { Player } from '@lottiefiles/react-lottie-player';
 
 export const Loader = () => {
   return (
-    <Player
-      src="https://assets2.lottiefiles.com/packages/lf20_karvytca.json"
-      className="player"
-      loop
-      autoplay
-      speed={1} />
+    <>
+      <p className="loader-text">Loading happy thoughts...</p>
+      <Player
+        src="https://assets2.lottiefiles.com/packages/lf20_karvytca.json"
+        className="player"
+        loop
+        autoplay
+        speed={1} />
+    </>
   )
 }
