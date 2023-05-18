580829d271613569024591a8664757adfebb7d1d
Annika Lindberg <annika.edit.lindberg@gmail.com>
2023-05-18 20:58:35 +0200

    fixed some things

--- code/src/Components/Footer.js
+++ code/src/Components/Footer.js
@@ -3,17 +3,17 @@ import styled from 'styled-components';
 
 const FooterWrapper = styled.footer`
     display: flex;
-    flex-direction: row;
-    justify-content: space-around;
+    flex-direction: column;
+    justify-content: center;
     align-items: center;
     background-color: #f5f5f5;
-    padding: 20px;
-    margin-top: 20px;
+    padding: 10px;
+    margin-top: 10px;
     margin-bottom: 0px;
     position: fixed;
     bottom: 0;
     width: 100%;
-    height: 50px;
+    height: auto;
     border-top: 1px solid #e6e6e6;
     box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.2);
 `;
@@ -21,8 +21,11 @@ const FooterWrapper = styled.footer`
 const FooterText = styled.p`
     font-size: 12px;
     color: #4d4d4d;
-    margin: 0px;
-    text-align: center; /* Center align the text */
+ 
+    text-align: center;
+    display: flex;
+    align-items: flex-start
+
 `;
 
 const FooterLink = styled.a`
@@ -31,15 +34,44 @@ const FooterLink = styled.a`
     font-weight: bold;
     &:hover {
         color: #4d4d4d;
+
+
     }
+`;
+const LinksWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+  
+    justify-content: center;
+    align-items: flex-start;
+    margin-left: 10px;
+
+`;
+
+const FooterImage = styled.img`
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    margin-right: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+
+
 `;
 
 export const Footer = () => {
   return (
     <FooterWrapper>
-      <img src="/annika.jpg" alt="annika portrait" width={50} />
-      <FooterText>Created by Annika Lindberg</FooterText>
-      <FooterText><FooterLink href="https://annikalindberg-portfolio.netlify.app/" /></FooterText><FooterText><FooterLink href="https://www.linkedin.com/in/annika-lindberg-a3398b263/">Annikas LinkedIn Profile</FooterLink></FooterText><FooterText> <FooterLink href="https://github.com/annikalindberg">Annikas Github profile</FooterLink></FooterText>
+      <FooterText>
+        <FooterImage src="/annika.jpg" alt="annika portrait" />
+        <LinksWrapper>
+          <FooterText>Created by Annika Lindberg</FooterText>
+          <FooterLink href="https://www.annikalindberg-portfolio.netlify.app/">Portfolio</FooterLink>
+          <FooterLink href="https://www.linkedin.com/in/annika-lindberg-a3398b263/">LinkedIn Profile</FooterLink>
+          <FooterLink href="https://www.github.com/annikalindberg">Github profile</FooterLink>
+        </LinksWrapper>
+      </FooterText>
     </FooterWrapper>
   );
 };
