a5baa5825b1afac765e9a662dc21425140bbfafa
Fiona Klacar <fiona.klacar@gmail.com>
2023-05-19 15:04:57 +0200

    tidied up code

--- code/src/components/Hearts.js
+++ code/src/components/Hearts.js
@@ -2,6 +2,7 @@
 import React from 'react';
 
 export const Hearts = ({ thought, fetchThoughts }) => {
+  // thoughtInput, setThoughtInput also added when commented out code used
   const onHeartCountIncreaseButtonClick = () => {
     const options = {
       method: 'PATCH'
@@ -9,6 +10,7 @@ export const Hearts = ({ thought, fetchThoughts }) => {
     console.log('options', options)
 
     // fetch(`https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${thought._id}/like`, options)
+
     fetch(`https://fiona-klacar-project-happy-thoughts-api.onrender.com/thoughts/${thought._id}/like`, options)
       .then((res) => res.json())
       .then((data) => console.log(data))
@@ -16,6 +18,24 @@ export const Hearts = ({ thought, fetchThoughts }) => {
       .finally(() => fetchThoughts())
     console.log('heart count increase')
   }
+
+  // ANOTHER WAY OF DOING IT:
+  //   fetch(`https://fiona-klacar-project-happy-thoughts-api.onrender.com/thoughts/${thought._id}/like`, options)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       const updatedThoughts = thoughtInput.map((like) => {
+  //         if (like._id === data.response._id) {
+  //           like.hearts += 1;
+  //         }
+  //         return like;
+  //       });
+  //       setThoughtInput(updatedThoughts); // Update the state with the updated array
+  //       fetchThoughts();
+  //     })
+  //     .catch((error) => console.log(error))
+  //     .finally(() => console.log('heart count increase'));
+  // };
 
   return (
     // eslint-disable-next-line react/jsx-no-comment-textnodes
