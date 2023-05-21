81125466dbd9897b422c9a1733991fd6b9e7d5e6
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-05-21 15:19:29 +0200

    Update Feed.js

--- code/src/Feed.js
+++ code/src/Feed.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-underscore-dangle */
 import React, { useState, useEffect } from 'react';
 import { SendThoughtForm } from 'SendThoughtForm';
