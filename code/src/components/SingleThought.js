df3e9f0529a86ec193fde857868423681aeb0ec6
JosefinRobertsson <josefinrobertsson@hotmail.com>
2023-05-22 16:40:51 +0200

    testing library 'moment' to solve time delay issue

--- code/src/components/SingleThought.js
+++ code/src/components/SingleThought.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import moment from 'moment';
 
 const SingleThought = ({ thought, onHeartClick }) => {
-  let timeStamp;
+  /* let timeStamp;
   const date = new Date(thought.createdAt);
   const timeDiff = Math.round((new Date() - date) / (1000 * 60));
   if (timeDiff < 1) {
@@ -12,7 +13,7 @@ const SingleThought = ({ thought, onHeartClick }) => {
     const hoursDiff = Math.round(timeDiff / 60);
     timeStamp = `${hoursDiff} hour${hoursDiff > 1 ? 's' : ''} ago`;
   }
-  console.log('singlelistitem')
+  console.log('singlelistitem') */
   return (
     <div className="singleListItem">
       <h3 id="stretched">{thought.message}</h3>
@@ -20,7 +21,7 @@ const SingleThought = ({ thought, onHeartClick }) => {
         <div className="heartCounter"><button onClick={() => onHeartClick(thought)} type="button"><span id="heartButton">🧡</span></button>
           <span> x {thought.hearts}</span>
         </div>
-        <p className="timeStamp">{timeStamp}</p>
+        {moment(thought.createdAt).fromNow()}
       </div>
     </div>
   );
