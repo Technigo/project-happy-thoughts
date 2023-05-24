1f71e67f8a1dfd76cc414fb8032837c9c9d07043
JosefinRobertsson <josefinrobertsson@hotmail.com>
2023-05-24 10:32:02 +0200

    changed timestamp handling, dlt redundant comp

--- code/src/components/DataList.js
+++ code/src/components/DataList.js
@@ -1,8 +1,26 @@
 /* eslint-disable no-underscore-dangle */
 import React from 'react';
-import moment from 'moment';
 
 const DataList = ({ happyThoughtsList, setHappyThoughtsList }) => {
+  const getTimestamp = (createdAt) => {
+    const date = new Date(createdAt);
+    const timeDiff = Math.round((new Date() - date) / (1000 * 60));
+    if (timeDiff < 1) {
+      return 'just now';
+    } else if (timeDiff < 90) {
+      return `${timeDiff} min ago`;
+    } else if (timeDiff < 4320) {
+      const hoursDiff = Math.round(timeDiff / 60);
+      return `${hoursDiff} hour${hoursDiff > 1 ? 's' : ''} ago`;
+    } else if (timeDiff < 10000) {
+      const daysDiff = Math.round(timeDiff / 1440);
+      return `${daysDiff} day${daysDiff > 1 ? 's' : ''} ago`;
+    } else {
+      const weeksDiff = Math.round(timeDiff / 10080);
+      return `${weeksDiff} week${weeksDiff > 1 ? 's' : ''} ago`;
+    }
+  };
+
   const heartCountClick = (thought) => {
     const options = {
       method: 'POST',
@@ -47,7 +65,7 @@ const DataList = ({ happyThoughtsList, setHappyThoughtsList }) => {
               </button>
               <span> x {thought.hearts}</span>
             </div>
-            {moment(thought.createdAt).fromNow()}
+            <p className="timeStamp">{getTimestamp(thought.createdAt)}</p>
           </div>
         </div>
       ))}
