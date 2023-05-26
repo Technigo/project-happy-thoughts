323e0bdb8e26f12024c053a3de525923f8bcc3c1
Annika Lindberg <annika.edit.lindberg@gmail.com>
2023-05-26 11:57:53 +0200

    updated links

--- code/src/Components/Footer.js
+++ code/src/Components/Footer.js
@@ -1,6 +1,8 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react';
-import styled from 'styled-components';
+import styled from '@emotion/styled';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const FooterWrapper = styled.footer`
     display: flex;
@@ -22,7 +24,6 @@ const FooterWrapper = styled.footer`
 const FooterText = styled.p`
     font-size: 12px;
     color: #4d4d4d;
- 
     text-align: center;
     display: flex;
     align-items: flex-start
@@ -33,12 +34,13 @@ const FooterLink = styled.a`
     text-decoration: none;
     color: #4d4d4d;
     font-weight: bold;
+    font-size: 22px;
     &:hover {
-        color: #4d4d4d;
-
-
+    color: #888888;
     }
+
 `;
+
 const LinksWrapper = styled.div`
     display: flex;
     flex-direction: column;
@@ -55,9 +57,18 @@ export const Footer = () => {
       <FooterText>
         <LinksWrapper>
           <FooterText>Created by Annika Lindberg</FooterText>
-          <FooterLink href="https://www.annikalindberg-portfolio.netlify.app/">Portfolio</FooterLink>
-          <FooterLink href="https://www.linkedin.com/in/annika-lindberg-a3398b263/">LinkedIn Profile</FooterLink>
-          <FooterLink href="https://www.github.com/annikalindberg">Github profile</FooterLink>
+          <FooterLink href="https://www.annikalindberg-portfolio.netlify.app/">Portfolio
+          </FooterLink>
+          <FooterLink
+            href="https://www.linkedin.com/in/annika-lindberg-a3398b263/"
+            data-new-tab="true">
+            <FontAwesomeIcon icon={faLinkedin} /> LinkedIn Profile
+          </FooterLink>
+          <FooterLink
+            href="https://www.github.com/annikalindberg"
+            data-new-tab="true">
+            <FontAwesomeIcon icon={faGithub} /> GitHub Profile
+          </FooterLink>
         </LinksWrapper>
       </FooterText>
     </FooterWrapper>
